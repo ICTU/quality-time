2c12e9bd0393f1ee83bd4292da7e9c9adf7356ee
Frank Niessink <frank@niessink.com>
2018-12-07 23:17:32 +0100

    Fix code quality issues.

--- tests/client.py
+++ tests/client.py
@@ -1,8 +1,11 @@
-import aiohttp
+"""Test client for the Quality-time API."""
+
 import asyncio
 import json
-import requests
 import sys
+
+import aiohttp
+
 
 NON_EXISTING = "http://non-existing-hostname.io"
 SONARQUBE = "http://sonarcloud.io"
@@ -24,7 +27,7 @@ APIS_OLD = [
     f"sonarqube/{SONARQUBE}/m/critical_violations/nl.ictu:hq",
     f"sonarqube/{SONARQUBE}/m/issues/nl.ictu:hq",
     f"sonarqube/{SONARQUBE}/m/version"]
-    
+
 APIS = [
     f"tests/junit?url={JUNIT_TEST}/junit-complex_suites.xml&url={JUNIT_TEST}/junit-simple_suite.xml",
     f"failed_tests/junit?url={JUNIT_TEST}/junit-complex_suites.xml&url={JUNIT_TEST}/junit-simple_suite.xml",
@@ -38,9 +41,10 @@ APIS = [
     f"failed_tests/sonarqube?url={SONARQUBE}&component=fniessink:next-action",
     f"ncloc/sonarqube?url={SONARQUBE}&component=fniessink:next-action",
     f"version/sonarqube?url={SONARQUBE}"]
-    
+
 
 async def fetch(api):
+    """Fetch one APIs asynchronously."""
     async with aiohttp.ClientSession() as session:
         async with session.get(f"http://localhost:8080/{api}") as response:
             json_content = await response.json()
@@ -48,14 +52,20 @@ async def fetch(api):
 
 
 async def fetch_all(apis):
+    """Fetch all APIs asynchronously."""
     await asyncio.gather(*(fetch(api) for api in apis))
 
 
-if __name__ == "__main__":
-    apis = APIS 
+def main():
+    """Read, filter and fetch the APIs."""
+    apis = APIS
     try:
         apis.extend([url.strip() for url in open(".urls.txt").readlines()])
     except FileNotFoundError:
         pass
     apis = [api for api in APIS if sys.argv[1] in api] if len(sys.argv) > 1 else APIS
     asyncio.run(fetch_all(apis))
+
+
+if __name__ == "__main__":
+    main()
