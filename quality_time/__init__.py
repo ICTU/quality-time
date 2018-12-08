210b4273bd34a4380d610358b78c3e6c35f9d015
Frank Niessink <frank@niessink.com>
2018-12-08 23:25:28 +0100

    Add Jenkins version metric.

--- quality_time/__init__.py
+++ quality_time/__init__.py
@@ -1,6 +1,7 @@
 """Entry point and routes for Quality-time."""
 
 import logging
+from typing import cast, Type
 
 from bottle import request, route, run
 
@@ -24,29 +25,25 @@ METRIC_SOURCE_ID = {
     (LOC, SonarQube): "lines",
     (Tests, JUnit): "tests",
     (Tests, SonarQube): "tests",
+    (Version, Jenkins): "version",
     (Version, SonarQube): "version",
     (Violations, SonarQube): "violations"
 }
-
-
-def process(metric, source, urls, components):
-    """Process."""
-    metric_id = METRIC_SOURCE_ID[(metric, source)]
-    response = source.get(metric_id, urls, components)
-    measurements = [source_response["measurement"] for source_response in response["source_responses"]]
-    response.update(metric.get(measurements))
-    return response
 
 
 @route("/<metric_name>/<source_name>")
 def get(metric_name: str, source_name: str) -> MeasurementResponse:
     """Handler for the get-metric-from-source API."""
     logging.info(request)
-    metric = Metric.subclass_for_api(metric_name)
-    source = Source.subclass_for_api(source_name)
     urls = request.query.getall("url")  # pylint: disable=no-member
     components = request.query.getall("component")  # pylint: disable=no-member
-    return process(metric, source, urls, components)
+    metric = cast(Type[Metric], Metric.subclass_for_api(metric_name))
+    source = cast(Type[Source], Source.subclass_for_api(source_name))
+    metric_id = METRIC_SOURCE_ID[(metric, source)]
+    response = source.get(metric_id, urls, components)
+    measurements = [source_response["measurement"] for source_response in response["source_responses"]]
+    response.update(metric.get(measurements))
+    return response
 
 
 def quality_time():
--- quality_time/sources/jenkins.py
+++ quality_time/sources/jenkins.py
@@ -1,13 +1,31 @@
 """Jenkins metric source."""
 
+from typing import Sequence
+
 import requests
 
 from quality_time.source import Source
-from quality_time.type import Measurement, URL
+from quality_time.type import Measurement, MeasurementResponse, URL
 
 
-class Jenkins(Source):
-    """Source class to get measurements from Jenkins."""
+class _Jenkins(Source):
+    """Base class for metric-specific Jenkins sources."""
+
+    @classmethod
+    def name(cls):
+        return "Jenkins"
+
+
+class JenkinsVersion(_Jenkins):
+    """Return the Jenkins version."""
+
+    @classmethod
+    def parse_source_response(cls, metric: str, response: requests.Response) -> Measurement:
+        return Measurement(response.headers["X-Jenkins"])
+
+
+class JenkinsJobs(_Jenkins):
+    """Source class to get job counts from Jenkins."""
 
     @classmethod
     def api_url(cls, metric: str, url: URL, component: str) -> URL:
@@ -19,3 +37,12 @@ class Jenkins(Source):
         if metric == "failed_jobs":
             jobs = [job for job in jobs if not job.get("color", "").startswith("blue")]
         return Measurement(len(jobs))
+
+
+class Jenkins(Source):
+    """Jenkins source."""
+
+    @classmethod
+    def get(cls, metric: str, urls: Sequence[URL], components: Sequence[str]) -> MeasurementResponse:
+        delegate = dict(version=JenkinsVersion).get(metric, JenkinsJobs)
+        return delegate.get(metric, urls, components)
