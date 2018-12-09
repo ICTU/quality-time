86b57346bca96139ff568980807cdb1a5545081c
Frank Niessink <frank@niessink.com>
2018-12-09 21:20:17 +0100

    Add metric status. (#7)

--- quality_time/metrics/size.py
+++ quality_time/metrics/size.py
@@ -6,6 +6,10 @@ from quality_time.metric import Metric
 class NCLOC(Metric):
     """Size of source code in Non-Commented Lines of Code."""
 
+    default_target = 50000
+
 
 class LOC(Metric):
     """Size of source code in Lines of Code."""
+
+    default_target = 75000
