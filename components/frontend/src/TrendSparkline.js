7dbbe7ad49c826feaae5c745c39cf32e13949afd
Frank Niessink <frank@niessink.com>
2019-01-09 09:49:44 +0100

    Convert numbers to Number class. (#29)

--- components/frontend/src/TrendSparkline.js
+++ components/frontend/src/TrendSparkline.js
@@ -7,8 +7,8 @@ function TrendSparkline(props) {
   let targets = [];
   for (var i = 0; i < props.measurements.length; i++) {
     const measurement = props.measurements[i].measurement;
-    const m = measurement.measurement;
-    const t = measurement.target;
+    const m = measurement.measurement !== null ? Number(measurement.measurement) : null;
+    const t = Number(measurement.target);
     const x1 = new Date(measurement.start);
     const x2 = new Date(measurement.end);
     measurements.push({y: m, x: x1});
@@ -17,10 +17,10 @@ function TrendSparkline(props) {
     targets.push({y: t, x: x2});
   }
   return (
-    <VictoryGroup theme={VictoryTheme.material} scale={{ x: "time", y: "linear" }} height={50} padding={0}>
-      <VictoryLine data={measurements} style={{
+    <VictoryGroup theme={VictoryTheme.material} scale={{ x: "time", y: "linear" }} height={60} padding={0}>
+      <VictoryLine data={measurements} interpolation="stepBefore" style={{
         data: {
-          stroke: "black", strokeWidth: 1
+          stroke: "black", strokeWidth: 3
         }
       }}/>
       <VictoryLine data={targets} style={{
