99f3d15af093967b94abaf2959615c1122f157a3
Frank Niessink <frank@niessink.com>
2019-02-04 22:51:48 +0100

    Add linebreaks and make popup hoverable.

--- components/frontend/src/SourceStatus.js
+++ components/frontend/src/SourceStatus.js
@@ -6,12 +6,20 @@ function SourceStatus(props) {
   const source_type = props.metric["sources"][props.source_uuid]["type"];
   const source_name = props.datamodel["sources"][source_type]["name"];
   if (props.source.connection_error || props.source.parse_error) {
+    let content = props.source.connection_error ? props.source.connection_error : props.source.parse_error;
     return (
       <Popup
-        wide='very'
-        content={props.source.connection_error ? props.source.connection_error : props.source.parse_error}
-        header={props.source.connection_error ? 'Connection error' : 'Parse error'}
-        trigger={<Label color='red' tabIndex="0"><a href={props.source.landing_url}>{source_name}</a></Label>} />)
+        wide='very' size='tiny' hoverable
+        trigger={<Label color='red' tabIndex="0"><a href={props.source.landing_url}>{source_name}</a></Label>}>
+        <Popup.Header>
+          {props.source.connection_error ? 'Connection error' : 'Parse error'}
+        </Popup.Header>
+        <Popup.Content>
+          {content.split(/[\r\n]/g).map((line) =>
+            <div key={line}>{line.replace(/ {4}/, '>>> ').replace(/ {2}/g, '- ')}</div>)}
+        </Popup.Content>
+      </Popup>
+    )
   } else {
     return (
       <Label tabIndex="0">
