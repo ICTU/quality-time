363f5afc3ac24bc1fbe025a01aef7cfa717ff0cd
Frank Niessink <frank@niessink.com>
2019-02-18 17:16:51 +0100

    Add severity column.

--- components/collector/collector/collectors/ojaudit.py
+++ components/collector/collector/collectors/ojaudit.py
@@ -23,15 +23,20 @@ class OJAuditViolations(Collector):
         # ElementTree has no API to get the namespace so we extract it from the root tag:
         namespace = tree.tag.split('}')[0][1:]
         violation_count = int(tree.findtext(f"./{{{namespace}}}violation-count"))
-        url = self.landing_url(**parameters)
+        models = {}
+        for model in tree.findall(f".//{{{namespace}}}model"):
+            model_file_path = model.findtext(f"./{{{namespace}}}file/{{{namespace}}}path")
+            models[model.get("id")] = model_file_path
         violations = []
-        for construct in tree.findall(f".//{{{namespace}}}construct"):
-            construct_name = construct.findtext(f"{{{namespace}}}name")
-            for violation in construct.findall(f"./{{{namespace}}}children/{{{namespace}}}violation"):
-                message = violation.findtext(f"{{{namespace}}}message")
-                line_number = violation.findtext(f".//{{{namespace}}}line-number")
-                component = f"{construct_name}:{line_number}"
-                violations.append(dict(
-                    key=hashlib.md5(f"{message}:{component}".encode("utf-8")).hexdigest(),
-                    message=message, component=component, url=url))
+        for violation in tree.findall(f".//{{{namespace}}}violation"):
+            location = violation.find(f"./{{{namespace}}}location")
+            model_id = location.get("model")
+            message = violation.findtext(f"{{{namespace}}}message")
+            line_number = violation.findtext(f".//{{{namespace}}}line-number")
+            column_offset = violation.findtext(f".//{{{namespace}}}column-offset")
+            severity = violation.findtext(f"./{{{namespace}}}values/{{{namespace}}}value")
+            component = f"{models[model_id]}:{line_number}:{column_offset}"
+            violations.append(dict(
+                key=hashlib.md5(f"{message}:{component}".encode("utf-8")).hexdigest(),
+                severity=severity, message=message, component=component))
         return str(violation_count), violations
