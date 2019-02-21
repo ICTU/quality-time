f54a83961a2f4f3c0633942a19750896da5f6eb5
Wessel van Meijbeek <w.vanmeijbeek@gmail.com>
2019-02-21 16:01:55 +0100

    Fixed trashcan alignment. (#79)

--- components/frontend/src/SubjectTitle.js
+++ components/frontend/src/SubjectTitle.js
@@ -95,7 +95,7 @@ class SubjectTitleDisplay extends Component {
                     </Header>
                 </Grid.Column>
                 <Grid.Column key={1} width={1}>
-                    <Button icon primary negative basic
+                    <Button floated='right' icon primary negative basic
                         onClick={(e) => this.delete_subject(e)}>
                         <Icon name='trash alternate' />
                     </Button>
