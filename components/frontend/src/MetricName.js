af91f7b7fe12278891258c51be7c462a153b82ed
Frank Niessink <frank@niessink.com>
2019-02-23 17:15:19 +0100

    Treat metric details pane as a form.

--- components/frontend/src/MetricName.js
+++ components/frontend/src/MetricName.js
@@ -5,36 +5,24 @@ import { Form } from 'semantic-ui-react';
 class MetricName extends Component {
   constructor(props) {
     super(props);
-    this.state = { edited_name: props.metric_name, edit: false, hover: false }
+    this.state = { edited_name: props.metric_name }
   }
   componentDidUpdate(prevProps) {
     if (prevProps.metric_name !== this.props.metric_name) {
       this.setState({ edited_name: this.props.metric_name })
     }
   }
-  onMouseEnter(event) {
-    event.preventDefault();
-    this.setState({ hover: true })
-  }
-  onMouseLeave(event) {
-    event.preventDefault();
-    this.setState({ hover: false })
-  }
-  onEdit() {
-    this.setState({ edit: true });
-  }
   onChange(event) {
     this.setState({ edited_name: event.target.value });
   }
   onKeyDown(event) {
     if (event.key === "Escape") {
-      this.setState({ edit: false, edited_name: this.props.metric_name })
+      this.setState({ edited_name: this.props.metric_name })
     }
   }
   onSubmit(event) {
     event.preventDefault();
     const self = this;
-    this.setState({ edit: false });
     fetch(`http://localhost:8080/report/${this.props.report_uuid}/metric/${this.props.metric_uuid}/name`, {
       method: 'post',
       mode: 'cors',
@@ -47,20 +35,11 @@ class MetricName extends Component {
     )
   }
   render() {
-    if (this.state.edit) {
-      return (
-        <Form onSubmit={(e) => this.onSubmit(e)}>
-          <Form.Input autoFocus focus fluid defaultValue={this.state.edited_name}
-            onChange={(e) => this.onChange(e)} onKeyDown={(e) => this.onKeyDown(e)} />
-        </Form>
-      )
-    }
-    const style = this.state.hover ? { overflow: "hidden", borderBottom: "1px dotted #000000" } : { overflow: "hidden" };
     return (
-      <div onClick={(e) => this.onEdit(e)} onKeyPress={(e) => this.onEdit(e)}
-        onMouseEnter={(e) => this.onMouseEnter(e)} onMouseLeave={(e) => this.onMouseLeave(e)} style={style} tabIndex="0">
-        {this.state.edited_name}
-      </div>
+      <Form onSubmit={(e) => this.onSubmit(e)}>
+        <Form.Input focus fluid defaultValue={this.state.edited_name}
+          onChange={(e) => this.onChange(e)} onKeyDown={(e) => this.onKeyDown(e)} />
+      </Form>
     )
   }
 }
--- components/frontend/src/SourceName.js
+++ components/frontend/src/SourceName.js
@@ -5,34 +5,24 @@ import { Form } from 'semantic-ui-react';
 class SourceName extends Component {
   constructor(props) {
     super(props);
-    this.state = { edited_name: props.name, edit: false, hover: false }
+    this.state = { edited_name: props.name }
   }
   componentDidUpdate(prevProps) {
     if (prevProps.name !== this.props.name) {
       this.setState({ edited_name: this.props.name })
     }
   }
-  onMouseEnter(event) {
-    this.setState({ hover: true })
-  }
-  onMouseLeave(event) {
-    this.setState({ hover: false })
-  }
-  onEdit() {
-    this.setState({ edit: true });
-  }
   onChange(event) {
     this.setState({ edited_name: event.target.value });
   }
   onKeyDown(event) {
     if (event.key === "Escape") {
-      this.setState({ edit: false, edited_name: this.props.name })
+      this.setState({ edited_name: this.props.name })
     }
   }
   onSubmit(event) {
     event.preventDefault();
     const self = this;
-    this.setState({ edit: false });
     fetch(`http://localhost:8080/report/${this.props.report_uuid}/source/${this.props.source_uuid}/name`, {
       method: 'post',
       mode: 'cors',
@@ -45,20 +35,11 @@ class SourceName extends Component {
     )
   }
   render() {
-    if (this.state.edit) {
-      return (
-        <Form onSubmit={(e) => this.onSubmit(e)}>
-          <Form.Input autoFocus focus fluid defaultValue={this.state.edited_name}
-            onChange={(e) => this.onChange(e)} onKeyDown={(e) => this.onKeyDown(e)} />
-        </Form>
-      )
-    }
-    const style = this.state.hover ? { overflow: "hidden", borderBottom: "1px dotted #000000" } : { overflow: "hidden" };
     return (
-      <div onClick={(e) => this.onEdit(e)} onKeyPress={(e) => this.onEdit(e)}
-        onMouseEnter={(e) => this.onMouseEnter(e)} onMouseLeave={(e) => this.onMouseLeave(e)} style={style} tabIndex="0">
-        {this.state.edited_name}
-      </div>
+      <Form onSubmit={(e) => this.onSubmit(e)}>
+        <Form.Input focus fluid defaultValue={this.state.edited_name}
+          onChange={(e) => this.onChange(e)} onKeyDown={(e) => this.onKeyDown(e)} />
+      </Form>
     )
   }
 }
