c8791195506541f19e941f8813ccc1dc7bd0a1aa
Frank Niessink <frank@niessink.com>
2019-02-23 16:30:14 +0100

    Move source type state to the source type component.

--- components/frontend/src/Source.js
+++ components/frontend/src/Source.js
@@ -6,27 +6,6 @@ import { SourceParameters } from './SourceParameters';
 
 
 class Source extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { edited_source_type: props.source.type };
-    }
-    post_source_type(source_type) {
-        this.setState({ edited_source_type: source_type });
-        const self = this;
-        fetch(`http://localhost:8080/report/${this.props.report_uuid}/source/${this.props.source_uuid}/type`, {
-            method: 'post',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ type: source_type })
-        }).then(
-            () => self.props.reload()
-        );
-    }
-    reset_source_type() {
-        this.setState({ edited_source_type: this.props.source_type });
-    }
     delete_source(event) {
         event.preventDefault();
         const self = this;
@@ -43,16 +22,16 @@ class Source extends Component {
     }
     render() {
         const props = this.props;
-        const source_type_name = props.datamodel["sources"][this.state.edited_source_type]["name"];
+        const source_type_name = props.datamodel["sources"][props.source.type]["name"];
         const source_name = props.source.name || source_type_name;
         return (
             <Table.Row verticalAlign='top'>
                 <Table.Cell>
                     <SourceType
-                        source_type={this.state.edited_source_type}
-                        metric_type={props.metric_type} datamodel={props.datamodel}
-                        post_source_type={(s) => this.post_source_type(s)}
-                        reset_source_type={() => this.reset_source_type()} />
+                        report_uuid={props.report_uuid} source_uuid={props.source_uuid}
+                        reload={props.reload}
+                        source_type={props.source.type}
+                        metric_type={props.metric_type} datamodel={props.datamodel} />
                 </Table.Cell>
                 <Table.Cell>
                     <SourceName name={source_name} report_uuid={props.report_uuid}
@@ -62,7 +41,7 @@ class Source extends Component {
                     <SourceParameters
                         report_uuid={props.report_uuid} reload={props.reload}
                         source_uuid={props.source_uuid} metric_type={props.metric_type}
-                        source_type={this.state.edited_source_type}
+                        source_type={props.source.type}
                         source={props.source} datamodel={props.datamodel} />
                 </Table.Cell>
                 <Table.Cell>
--- components/frontend/src/SourceType.js
+++ components/frontend/src/SourceType.js
@@ -4,48 +4,36 @@ import { Dropdown } from 'semantic-ui-react';
 class SourceType extends Component {
   constructor(props) {
     super(props);
-    this.state = { edit: false, hover: false }
-  }
-  onMouseEnter(event) {
-    this.setState({ hover: true })
-  }
-  onMouseLeave(event) {
-    this.setState({ hover: false })
-  }
-  onEdit() {
-    this.setState({ edit: true });
-  }
-  onClose() {
-    this.setState({ edit: false });
+    this.state = { edited_source_type: props.source_type };
   }
   onKeyDown(event) {
     if (event.key === "Escape") {
-      this.setState({ edit: false });
-      this.props.reset_source_type();
+      this.setState({ edited_source_type: this.props.source_type });
     }
   }
   onSubmit(event, { name, value }) {
     event.preventDefault();
-    this.setState({ edit: false })
-    this.props.post_source_type(value);
+    this.setState({ edited_source_type: value });
+    const self = this;
+    fetch(`http://localhost:8080/report/${this.props.report_uuid}/source/${this.props.source_uuid}/type`, {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ type: value })
+    }).then(
+      () => self.props.reload()
+    );
   }
   render() {
     let options = [];
     let self = this;
     this.props.datamodel["metrics"][this.props.metric_type]["sources"].forEach(
       (key) => { options.push({ text: self.props.datamodel["sources"][key]["name"], value: key }) });
-    if (this.state.edit) {
-      return (
-        <Dropdown fluid selectOnNavigation={false} defaultOpen value={this.props.source_type}
-          options={options} onChange={(e, { name, value }) => this.onSubmit(e, { name, value })} tabIndex="0" />
-      )
-    }
-    const style = this.state.hover ? { borderBottom: "1px dotted #000000" } : { height: "1em" };
     return (
-      <div onClick={(e) => this.onEdit(e)} onKeyPress={(e) => this.onEdit(e)}
-        onMouseEnter={(e) => this.onMouseEnter(e)} onMouseLeave={(e) => this.onMouseLeave(e)} style={style} tabIndex="0">
-        {this.props.datamodel["sources"][this.props.source_type]["name"]}
-      </div>
+      <Dropdown selectOnNavigation={false} value={this.props.source_type}
+        options={options} onChange={(e, { name, value }) => this.onSubmit(e, { name, value })} tabIndex="0" />
     )
   }
 }
