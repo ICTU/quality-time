cf40076adc9556e55ef3123980cb3105292e646d
Frank Niessink <frank@niessink.com>
2019-02-24 23:50:49 +0100

    Show traceback in details pane. (#88)

--- components/frontend/src/Sources.js
+++ components/frontend/src/Sources.js
@@ -18,6 +18,16 @@ class Sources extends Component {
             () => self.props.reload()
         );
     }
+    source_error(measurement_sources, source_uuid, error_type) {
+        let message = '';
+        measurement_sources.forEach((source) => {
+            if (source.source_uuid === source_uuid) {
+                message = source[error_type] || '';
+                return
+            }
+        });
+        return message;
+    }
     render() {
         const source_uuids = Object.keys(this.props.sources).filter((source_uuid) =>
             this.props.datamodel.metrics[this.props.metric_type].sources.includes(this.props.sources[source_uuid].type)
@@ -26,6 +36,8 @@ class Sources extends Component {
             (
                 <Segment vertical key={source_uuid}>
                     <Source report_uuid={this.props.report_uuid} source_uuid={source_uuid}
+                        connection_error={this.source_error(this.props.measurement.sources, source_uuid, "connection_error")}
+                        parse_error={this.source_error(this.props.measurement.sources, source_uuid, "parse_error")}
                         source={this.props.sources[source_uuid]} reload={this.props.reload}
                         metric_type={this.props.metric_type} datamodel={this.props.datamodel} />
                 </Segment>
