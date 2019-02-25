4b993bc0834119a9a1d86b79e451bcc69047b499
Frank Niessink <frank@niessink.com>
2019-02-25 18:03:52 +0100

    Make target field fluid.

--- components/frontend/src/MetricTarget.js
+++ components/frontend/src/MetricTarget.js
@@ -23,7 +23,7 @@ class MetricTarget extends Component {
     return (
       <Form onSubmit={(e) => this.onSubmit(e)}>
         <Form.Group style={{marginBottom: '0px'}}>
-          <Form.Input label='Metric target' focus type="number" defaultValue={this.state.edited_target}
+          <Form.Input width={16} label='Metric target' focus type="number" defaultValue={this.state.edited_target}
             onChange={(e) => this.onChange(e)} onKeyDown={(e) => this.onKeyDown(e)}
             labelPosition='right'>
             <Label basic>{this.props.direction}</Label>
