25dc2ca07017d9d2fa9583ab5f80d2ecf99a99f6
Frank Niessink <frank@niessink.com>
2019-03-02 23:14:18 +0100

    Include summary of current measurements in report.

--- components/frontend/src/Report.js
+++ components/frontend/src/Report.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Subjects } from './Subjects.js';
 import { Card } from 'semantic-ui-react';
 import { VictoryPie } from 'victory';
 
 function DashboardSubject(props) {
+    const nr_metrics = props.red + props.green + props.yellow;
     return (
         <Card>
             <VictoryPie
                 colorScale={["rgb(211,59,55)", "rgb(30,148,78)", "rgb(253,197,54)"]}
+                padding={20}
                 style={{
-                    data: { stroke: "grey", strokeWidth: 0 }
+                    data: { strokeWidth: 0 }
                 }}
+                innerRadius={75}
                 labels={() => null}
                 animate={{ duration: 2000 }}
                 data={[
@@ -21,57 +24,28 @@ function DashboardSubject(props) {
             />
             <Card.Content>
                 <Card.Header>{props.title}</Card.Header>
+                <Card.Meta>Metrics: {nr_metrics}</Card.Meta>
             </Card.Content>
         </Card>
     )
 }
 
-class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { summary: {} }
-    }
-    componentDidMount() {
-        this.fetch_summary();
-    }
-    componentDidUpdate(prevProps) {
-        if (prevProps.report_date !== this.props.report_date ||
-          (prevProps.nr_new_measurements !== 0 && this.props.nr_new_measurements === 0)) {
-          this.fetch_summary();
-        }
-      }
-    fetch_summary() {
-        let self = this;
-        const report_date = this.props.report_date ? this.props.report_date : new Date();
-        fetch(`http://localhost:8080/report/${this.props.report.report_uuid}/summary?report_date=${report_date.toISOString()}`)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                self.setState({ summary: json });
-            })
-    }
-    render() {
-        const summary = this.state.summary;
-        const cards = Object.entries(this.props.report.subjects).map(([subject_uuid, subject]) =>
-            <DashboardSubject key={subject_uuid} title={subject.title}
-                red={summary[subject_uuid] ? summary[subject_uuid].red : 0}
-                green={summary[subject_uuid] ? summary[subject_uuid].green : 0}
-                yellow={summary[subject_uuid] ? summary[subject_uuid].yellow : 1} />
-        )
-        return (
-            <Card.Group itemsPerRow={6}>
-                {cards}
-            </Card.Group>
-        )
-    }
+function Dashboard(props) {
+    const cards = Object.entries(props.report.subjects).map(([subject_uuid, subject]) =>
+        <DashboardSubject key={subject_uuid} title={subject.title}
+            red={subject.summary.red} green={subject.summary.green}
+            yellow={subject.summary.yellow} />);
+    return (
+        <Card.Group itemsPerRow={6}>
+            {cards}
+        </Card.Group>
+    )
 }
 
 function Report(props) {
     return (
         <>
-            <Dashboard report={props.report} report_date={props.report_date}
-                nr_new_measurements={props.nr_new_measurements} />
+            <Dashboard report={props.report} />
             <Subjects datamodel={props.datamodel} subjects={props.report.subjects}
                 report_uuid={props.report.report_uuid}
                 nr_new_measurements={props.nr_new_measurements} reload={props.reload}
