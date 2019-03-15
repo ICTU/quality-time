3290965c1152fca62b0b0f6290bfa0e85a1dae25
Frank Niessink <frank@niessink.com>
2019-03-15 19:13:15 +0100

    Add more HQ collectors.

--- components/collector/collector/collectors/hq.py
+++ components/collector/collector/collectors/hq.py
@@ -1,14 +1,39 @@
 """HQ metrics collector."""
 
+from urllib.parse import urljoin
+
 import requests
 
 from collector.collector import Collector
-from collector.type import Value
+from collector.type import URL, Value
 
 
-class HQViolations(Collector):
-    """HQ violations collector."""
+class HQ(Collector):
+    """HQ collector."""
+
+    def api_url(self, **parameters) -> URL:
+        return URL(urljoin(parameters.get("url", ""), "json/metrics.json"))
 
     def parse_source_response_value(self, response: requests.Response, **parameters) -> Value:
         metric = [m for m in response.json()["metrics"] if m["stable_metric_id"] == parameters.get("metric_id")][0]
         return metric["value"]
+
+
+class HQViolations(HQ):
+    """HQ violations collector."""
+
+
+class HQFailedJobs(HQ):
+    """HQ failed jobs collector."""
+
+
+class HQFailedTests(HQ):
+    """HQ failed tests collector."""
+
+
+class HQIssues(HQ):
+    """HQ issues collector."""
+
+
+class HQNCLOC(HQ):
+    """HQ non-commented lines of code collector."""
