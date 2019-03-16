94d0e916364f223a844e3f9cb95b3f0140bec5ed
Frank Niessink <frank@niessink.com>
2019-03-16 19:58:34 +0100

    Logos (#127)
    Add source logos.

--- components/frontend/src/Source.js
+++ components/frontend/src/Source.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import { Button, Grid, Icon, Message } from 'semantic-ui-react';
+import { Button, Grid, Header, Icon, Image, Message } from 'semantic-ui-react';
 import { SourceType } from './SourceType';
 import { SourceParameters } from './SourceParameters';
 import { StringParameter } from './StringParameter';
+
+import Gitlab from './logos/gitlab.png';
+import HQ from './logos/hq.png';
+import Jenkins from './logos/jenkins.png';
+import Jira from './logos/jira.png';
+import Sonarqube from './logos/sonarqube.png';
 
 class Source extends Component {
     delete_source(event) {
@@ -35,61 +41,73 @@ class Source extends Component {
     render() {
         const props = this.props;
         const source_type = props.datamodel.sources[props.source.type];
+        const logo = {
+            gitlab: Gitlab,
+            hq: HQ,
+            jenkins: Jenkins,
+            jira: Jira,
+            sonarqube: Sonarqube }[props.source.type];
         return (
-            <Grid stackable>
-                <Grid.Row columns={1}>
-                    <Grid.Column>
-                        <Message header={source_type.name} content={source_type.description} />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row columns={2}>
-                    <Grid.Column>
-                        <SourceType
-                            source_type={props.source.type} readOnly={props.readOnly}
-                            metric_type={props.metric_type} datamodel={props.datamodel}
-                            set_source_attribute={(a, v) => this.set_source_attribute(a, v)} />
-                    </Grid.Column>
-                    <Grid.Column>
-                        <StringParameter
-                            parameter_key="name" parameter_name={"Source name"} parameter_value={props.source.name}
-                            set_parameter={(a, v) => this.set_source_attribute(a, v)}
-                            placeholder={source_type.name} readOnly={props.readOnly} />
-                    </Grid.Column>
-                    <SourceParameters
-                        datamodel={props.datamodel}
-                        metric_type={props.metric_type}
-                        readOnly={props.readOnly}
-                        reload={props.reload}
-                        report={props.report}
-                        source={props.source}
-                        source_uuid={props.source_uuid}
-                    />
-                </Grid.Row>
-                {props.connection_error && <Grid.Row columns={1}>
-                    <Grid.Column>
-                        <Message negative>
-                            <Message.Header>Connection error</Message.Header>
-                            <pre style={{ whiteSpace: 'pre-wrap' }}>{props.connection_error}</pre>
-                        </Message>
-                    </Grid.Column>
-                </Grid.Row>}
-                {props.parse_error && <Grid.Row columns={1}>
-                    <Grid.Column>
-                        <Message negative>
-                            <Message.Header>Parse error</Message.Header>
-                            <pre style={{ whiteSpace: 'pre-wrap' }}>{props.parse_error}</pre>
-                        </Message>
-                    </Grid.Column>
-                </Grid.Row>}
-                {!props.readOnly &&
-                    <Grid.Row columns={1}>
+            <>
+                <Header>
+                    {logo &&
+                        <Image src={logo} alt={`${source_type.name} logo`} size="mini" spaced="right" />
+                    }
+                    {source_type.name}
+                    <Header.Subheader>
+                        {source_type.description} <a href={source_type.url}><Icon name="external" link/></a>
+                    </Header.Subheader>
+                </Header>
+                <Grid stackable>
+                    <Grid.Row columns={2}>
                         <Grid.Column>
-                            <Button floated='right' icon primary negative basic onClick={(e) => this.delete_source(e)}>
-                                <Icon name='trash' /> Delete source
-                            </Button>
+                            <SourceType
+                                source_type={props.source.type} readOnly={props.readOnly}
+                                metric_type={props.metric_type} datamodel={props.datamodel}
+                                set_source_attribute={(a, v) => this.set_source_attribute(a, v)} />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <StringParameter
+                                parameter_key="name" parameter_name={"Source name"} parameter_value={props.source.name}
+                                set_parameter={(a, v) => this.set_source_attribute(a, v)}
+                                placeholder={source_type.name} readOnly={props.readOnly} />
+                        </Grid.Column>
+                        <SourceParameters
+                            datamodel={props.datamodel}
+                            metric_type={props.metric_type}
+                            readOnly={props.readOnly}
+                            reload={props.reload}
+                            report={props.report}
+                            source={props.source}
+                            source_uuid={props.source_uuid}
+                        />
+                    </Grid.Row>
+                    {props.connection_error && <Grid.Row columns={1}>
+                        <Grid.Column>
+                            <Message negative>
+                                <Message.Header>Connection error</Message.Header>
+                                <pre style={{ whiteSpace: 'pre-wrap' }}>{props.connection_error}</pre>
+                            </Message>
                         </Grid.Column>
                     </Grid.Row>}
-            </Grid>
+                    {props.parse_error && <Grid.Row columns={1}>
+                        <Grid.Column>
+                            <Message negative>
+                                <Message.Header>Parse error</Message.Header>
+                                <pre style={{ whiteSpace: 'pre-wrap' }}>{props.parse_error}</pre>
+                            </Message>
+                        </Grid.Column>
+                    </Grid.Row>}
+                    {!props.readOnly &&
+                        <Grid.Row columns={1}>
+                            <Grid.Column>
+                                <Button floated='right' icon primary negative basic onClick={(e) => this.delete_source(e)}>
+                                    <Icon name='trash' /> Delete source
+                            </Button>
+                            </Grid.Column>
+                        </Grid.Row>}
+                </Grid>
+            </>
         )
     }
 }
