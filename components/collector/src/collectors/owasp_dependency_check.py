281f6b999f2c9e5b92e0e33eed71e422d28c0658
Frank Niessink <frank@niessink.com>
2019-03-29 21:54:40 +0100

    Refactoring.

--- components/collector/src/collectors/owasp_dependency_check.py
+++ components/collector/src/collectors/owasp_dependency_check.py
@@ -1,9 +1,12 @@
 """OWASP Dependency Check metric collector."""
 
+from typing import List
+from xml.etree.cElementTree import Element
+
 import requests
 
 from ..collector import Collector
-from ..type import Units, URL, Value
+from ..type import Namespaces, Units, URL, Value
 from ..util import parse_source_response_xml
 
 
@@ -15,27 +18,26 @@ class OWASPDependencyCheckSecurityWarnings(Collector):
 
     def parse_source_response_value(self, response: requests.Response, **parameters) -> Value:
         tree, namespaces = parse_source_response_xml(response)
-        severities = parameters.get("severities") or ["low", "medium", "high"]
-        vulnerabilities = []
-        for severity in severities:
-            vulnerabilities.extend(
-                tree.findall(
-                    f".//ns:dependency/ns:vulnerabilities/ns:vulnerability[ns:severity='{severity.capitalize()}']",
-                    namespaces))
-        return str(len(vulnerabilities))
+        return str(len(self.vulnerabilities(tree, namespaces, **parameters)))
 
     def parse_source_response_units(self, response: requests.Response, **parameters) -> Units:
         tree, namespaces = parse_source_response_xml(response)
-        severities = parameters.get("severities") or ["low", "medium", "high"]
         units = []
         for dependency in tree.findall(".//ns:dependency", namespaces):
             file_path = dependency.findtext("ns:filePath", namespaces=namespaces)
-            for severity in severities:
-                for vulnerability in dependency.findall(
-                        f"./ns:vulnerabilities/ns:vulnerability[ns:severity='{severity.capitalize()}']", namespaces):
-                    key = ":".join([file_path, vulnerability.findtext("ns:name", namespaces=namespaces)])
-                    units.append(
-                        dict(key=key, location=file_path, name=vulnerability.findtext("ns:name", namespaces=namespaces),
-                             description=vulnerability.findtext("ns:description", namespaces=namespaces),
-                             severity=vulnerability.findtext("ns:severity", namespaces=namespaces)))
+            vulnerabilities = self.vulnerabilities(dependency, namespaces, **parameters)
+            for vulnerability in vulnerabilities:
+                key = ":".join([file_path, vulnerability.findtext("ns:name", namespaces=namespaces)])
+                units.append(
+                    dict(key=key, location=file_path, name=vulnerability.findtext("ns:name", namespaces=namespaces),
+                         description=vulnerability.findtext("ns:description", namespaces=namespaces),
+                         severity=vulnerability.findtext("ns:severity", namespaces=namespaces)))
         return units
+
+    @staticmethod
+    def vulnerabilities(element: Element, namespaces: Namespaces, **parameters) -> List[Element]:
+        """Return the vulnerabilities that have one of the severities specified in the parameters."""
+        severities = parameters.get("severities") or ["low", "medium", "high"]
+        vulnerabilities = element.findall(".//ns:vulnerabilities/ns:vulnerability", namespaces)
+        return [vulnerability for vulnerability in vulnerabilities if
+                vulnerability.findtext("ns:severity", namespaces=namespaces).lower() in severities]
