8978133463553d4c3325740285516a2273ef44aa
Wessel van Meijbeek <w.vanmeijbeek@gmail.com>
2019-04-11 17:23:27 +0200

    Summary tag home screen (#221)
    Show a summary of metrics per tag on the home page.

--- components/frontend/src/Reports.js
+++ components/frontend/src/Reports.js
@@ -7,13 +7,45 @@ function ReportCard(props) {
   const summary = props.report.summary;
   const nr_metrics = summary.red + summary.green + summary.yellow + summary.grey;
   return (
-    <Card onClick={(e) => props.open_report(e, props.report)}>
+    <Card onClick={props.onClick}>
       <StatusPieChart red={summary.red} green={summary.green} yellow={summary.yellow} grey={summary.grey} />
       <Card.Content>
         <Card.Header>{props.report.title}</Card.Header>
         <Card.Meta>Metrics: {nr_metrics}</Card.Meta>
       </Card.Content>
     </Card>
+  )
+}
+
+function Dashboard(props) {
+  var tag_counts = {};
+  props.reports.forEach((report) => {
+    Object.entries(report.summary_by_tag).forEach(([tag, counts]) => {
+      if (Object.keys(tag_counts).indexOf(tag) === -1) {
+        tag_counts[tag] = {"red": 0, "green": 0, "yellow": 0, "grey": 0}
+      }
+      Object.entries(counts).forEach(([color, color_count]) => {tag_counts[tag][color] += color_count})
+    })
+  });
+  let tags = [];
+  Object.entries(tag_counts).forEach(([tag, counts]) => {
+    tags.push({"summary": counts, "title": tag});
+  });
+  const report_cards = props.reports.map((report) =>
+    <ReportCard key={report.report_uuid} report={report} onClick={(e) => props.open_report(e, report)} />);
+  const tag_cards = tags.map((tag) =>
+    <ReportCard key={tag.title} report={tag} />);
+  const report_cards_per_row = Math.min(Math.max(report_cards.length, 5), 7);
+  const tag_cards_per_row = Math.min(Math.max(tag_cards.length, 8), 10);
+  return (
+    <>
+      <Card.Group doubling stackable itemsPerRow={report_cards_per_row}>
+            {report_cards}
+      </Card.Group>
+      <Card.Group doubling stackable itemsPerRow={tag_cards_per_row}>
+            {tag_cards}
+      </Card.Group>
+    </>
   )
 }
 
@@ -36,11 +68,7 @@ class Reports extends Component {
   render() {
     return (
       <>
-        <Card.Group doubling stackable itemsPerRow={5}>
-          {this.props.reports.map((report) =>
-            <ReportCard key={report.report_uuid} report={report} open_report={this.props.open_report} />)
-          }
-        </Card.Group>
+        <Dashboard reports={this.props.reports} open_report={this.props.open_report} />
         {!this.props.readOnly &&
         <Segment basic>
           <Button icon primary basic onClick={(e) => this.add_report(e)}>
