59b5068cf732c26647451e81e814542dc596cff7
Wessel <w.vanmeijbeek@gmail.com>
2019-04-11 13:49:14 +0200

    Refactored some code and changed the size of the tag charts

--- components/frontend/src/Reports.js
+++ components/frontend/src/Reports.js
@@ -19,35 +19,37 @@ function ReportCard(props) {
 
 function Dashboard(props) {
   const tags = [{
-      "summary": {
-        "red": 15,
-        "green": 5,
-        "yellow": 0,
-        "grey": 1
-      },
-      "title": "security"
+    "summary": {
+      "red": 15,
+      "green": 5,
+      "yellow": 0,
+      "grey": 1
     },
-    {
-      "summary": {
-        "red": 5,
-        "green": 13,
-        "yellow": 4,
-        "grey": 2
-      },
-      "title": "testability"
-    }
-  ];
+    "title": "security"
+  },
+  {
+    "summary": {
+      "red": 5,
+      "green": 13,
+      "yellow": 4,
+      "grey": 2
+    },
+    "title": "testability"
+  }
+];
+  const report_cards = props.reports.map((report) =>
+                      <ReportCard key={report.report_uuid} report={report} open_report={props.open_report} />);
+  const tag_cards = tags.map((tag) =>
+                      <ReportCard key={tag.title} report={tag} />);
+  const report_cards_per_row = Math.min(Math.max(report_cards.length, 5), 7);
+  const tag_cards_per_row = Math.min(Math.max(tag_cards.length, 8), 10);
   return (
     <>
-      <Card.Group doubling stackable itemsPerRow={5}>
-            {props.reports.map((report) =>
-              <ReportCard key={report.report_uuid} report={report} open_report={props.open_report} />)
-            }
+      <Card.Group doubling stackable itemsPerRow={report_cards_per_row}>
+            {report_cards}
       </Card.Group>
-      <Card.Group doubling stackable itemsPerRow={5}>
-            {tags.map((tag) =>
-              <ReportCard key={tag.title} report={tag} />)
-            }
+      <Card.Group doubling stackable itemsPerRow={tag_cards_per_row}>
+            {tag_cards}
       </Card.Group>
     </>
 
