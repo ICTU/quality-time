78b145efa4fbc54cece7d4ae7c4cf4b651201b23
Frank Niessink <frank@niessink.com>
2019-04-13 18:28:50 +0200

    Reorder tabs. (#233)

--- components/frontend/src/MeasurementDetails.js
+++ components/frontend/src/MeasurementDetails.js
@@ -24,39 +24,9 @@ class MeasurementDetails extends Component {
   render() {
     const props = this.props;
     const metric = props.report.subjects[props.subject_uuid].metrics[props.metric_uuid];
-    const panes = [
-      {
-        menuItem: <Menu.Item key='metric'><FocusableTab>{'Metric'}</FocusableTab></Menu.Item>,
-        render: () => <Tab.Pane>
-          <MetricParameters datamodel={props.datamodel} metric={metric}
-            readOnly={props.readOnly} set_metric_attribute={props.set_metric_attribute} />
-        </Tab.Pane>
-      },
-      {
-        menuItem: <Menu.Item key='sources'><FocusableTab>{'Sources'}</FocusableTab></Menu.Item>,
-        render: () => <Tab.Pane>
-          <Sources
-            datamodel={props.datamodel}
-            measurement={props.measurement}
-            metric_type={metric.type}
-            metric_uuid={props.metric_uuid}
-            readOnly={props.readOnly}
-            reload={props.reload}
-            report={props.report}
-            sources={metric.sources}
-          />
-        </Tab.Pane>
-      }
-    ];
+    const panes = [];
     if (props.measurement !== null) {
       const unit_name = props.unit.charAt(0).toUpperCase() + props.unit.slice(1);
-      panes.push(
-        {
-          menuItem: <Menu.Item key='trend'><FocusableTab>{'Trend'}</FocusableTab></Menu.Item>,
-          render: () => <Tab.Pane>
-            <TrendGraph measurements={props.measurements} unit={unit_name} />
-          </Tab.Pane>
-        });
       const nr_units = props.measurement.sources.reduce((nr_units, source) => nr_units + (source.units && source.units.length) || 0, 0);
       if (nr_units > 0) {
         panes.push({
@@ -76,7 +46,41 @@ class MeasurementDetails extends Component {
           </Tab.Pane>
         })
       }
+      panes.push(
+        {
+          menuItem: <Menu.Item key='trend'><FocusableTab>{'Trend'}</FocusableTab></Menu.Item>,
+          render: () => <Tab.Pane>
+            <TrendGraph measurements={props.measurements} unit={unit_name} />
+          </Tab.Pane>
+        }
+      );
     }
+    panes.push(
+      {
+        menuItem: <Menu.Item key='metric'><FocusableTab>{'Metric'}</FocusableTab></Menu.Item>,
+        render: () => <Tab.Pane>
+          <MetricParameters datamodel={props.datamodel} metric={metric}
+            readOnly={props.readOnly} set_metric_attribute={props.set_metric_attribute} />
+        </Tab.Pane>
+      }
+    );
+    panes.push(
+      {
+        menuItem: <Menu.Item key='sources'><FocusableTab>{'Sources'}</FocusableTab></Menu.Item>,
+        render: () => <Tab.Pane>
+          <Sources
+            datamodel={props.datamodel}
+            measurement={props.measurement}
+            metric_type={metric.type}
+            metric_uuid={props.metric_uuid}
+            readOnly={props.readOnly}
+            reload={props.reload}
+            report={props.report}
+            sources={metric.sources}
+          />
+        </Tab.Pane>
+      }
+    );
     return (
       <Table.Row>
         <Table.Cell colSpan="9">
