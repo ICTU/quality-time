3f027435dfbffdd6e60197968c4eeebff0de30b2
Wessel <w.vanmeijbeek@gmail.com>
2019-05-02 16:35:53 +0200

    Fixed issue 151 by adding a automatic expand when a new metric has no source configured yet.

--- components/frontend/src/TableRowWithDetails.js
+++ components/frontend/src/TableRowWithDetails.js
@@ -4,14 +4,14 @@ import { Icon, Table } from 'semantic-ui-react';
 export class TableRowWithDetails extends Component {
   constructor(props) {
     super(props);
-    this.state = { show_details: false };
+    this.state = { show_details: props.show_details};
   }
   expand_or_collapse = (event) => {
     event.preventDefault();
     this.setState((state) => ({ show_details: !state.show_details }));
   };
   render() {
-    var { children, details, ...otherProps } = this.props;
+    var { children, show_details, details, ...otherProps } = this.props;
     return (<>
       <Table.Row {...otherProps}>
         <Table.Cell collapsing onClick={this.expand_or_collapse} onKeyPress={this.expand_or_collapse} tabIndex="0">
