15e9c38e5b65f564150de074988df950d5fc3a8a
joerivrij <joerivrijaldenhoven@gmail.com>
2019-05-08 22:38:12 +0200

    Feature/circleciintegration (#286)
    Created workflow for circle and added uitests in the art component.

---
+++ components/art/cypress/integration/common/RedoMetrics.js
@@ -0,0 +1,32 @@
+import {Given} from "cypress-cucumber-preprocessor/steps";
+
+Given(/^I make sure the Metric debt target is (.+), Metric target is (.+) and Accept technical debt (.+)$/, (debt, metric, technicalDebt) => {
+    cy.get("#root > div.ui.fluid.container > div.ui.doubling.stackable.five.cards > a:nth-child(1)").click();
+
+    cy.get("#root > div.ui.fluid.container > div:nth-child(3) > table > tbody > tr:nth-child(3)").within(() => {
+        cy.get('td.collapsing').click();
+    });
+
+    cy.get("#root > div.ui.fluid.container > div:nth-child(3) > table > tbody > tr:nth-child(4)").within(() => {
+        cy.get('.tabbutton').contains('Metric').click();
+        cy.get('td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(2)').within(() => {
+            cy.get('div:nth-child(1) > form > div > div').within( () => {
+                cy.get('div > input[type=number]')
+                    .clear()
+                    .type(metric);
+            })
+        })
+
+        cy.get('td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(3)').within(() => {
+            cy.get('div:nth-child(2) > form > div > div').within( () => {
+                cy.get('div > input[type=number]')
+                    .clear()
+                    .type(debt);
+            });
+            cy.get('div:nth-child(1) > form > div > div').within(() => {
+                cy.get('> i').click();
+                cy.get(`div.visible.menu.transition > div:nth-child(${technicalDebt})`).click();
+            })
+        });
+    });
+});
