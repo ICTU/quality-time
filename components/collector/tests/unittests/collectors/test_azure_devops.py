93cbbe6ab143ebf45b953e1d29b49660317370f0
Frank Niessink <frank@niessink.com>
2019-05-11 23:09:04 +0200

    Add Azure DevOps as source for ready user story points. (#296)

--- components/collector/tests/unittests/collectors/test_azure_devops.py
+++ components/collector/tests/unittests/collectors/test_azure_devops.py
@@ -6,8 +6,8 @@ from unittest.mock import Mock, patch
 from src.collector import collect_measurement
 
 
-class AzureDevopsTest(unittest.TestCase):
-    """Unit tests for the Azure Devops Server metrics."""
+class AzureDevopsIssuesTest(unittest.TestCase):
+    """Unit tests for the Azure Devops Server issues metric."""
 
     def setUp(self):
         """Test fixture."""
@@ -48,3 +48,37 @@ class AzureDevopsTest(unittest.TestCase):
         self.assertEqual(
             [dict(key="id", project="Project", title="Title", work_item_type="Task", state="New", url="http://url")],
             response["sources"][0]["units"])
+
+
+class AzureDevopsReadyStoryPointsTest(unittest.TestCase):
+    """Unit tests for the Azure Devops Server ready story points metric."""
+
+    def setUp(self):
+        """Test fixture."""
+        self.mock_response = Mock()
+        self.mock_unit_response = Mock()
+        self.work_item = dict(
+            id="id", url="http://url",
+            fields={"System.TeamProject": "Project", "System.Title": "Title", "System.WorkItemType": "Task",
+                    "System.State": "New", "Microsoft.VSTS.Scheduling.StoryPoints": 2.0})
+        self.sources = dict(
+            source_id=dict(type="azure_devops", parameters=dict(url="http://azure_devops", private_token="xxx")))
+        self.metric = dict(type="ready_user_story_points", sources=self.sources, addition="sum")
+
+    def test_story_points(self):
+        """Test that the number of story points are returned."""
+        self.mock_response.json.return_value = dict(workItems=[dict(id="id1"), dict(id="id2")])
+        self.mock_unit_response.json.return_value = dict(value=[self.work_item, self.work_item])
+        with patch("requests.post", return_value=self.mock_response):
+            with patch("requests.get", return_value=self.mock_unit_response):
+                response = collect_measurement(self.metric)
+        self.assertEqual("4", response["sources"][0]["value"])
+
+    def test_story_points_without_stories(self):
+        """Test that the number of story points is zero when there are no work items."""
+        self.mock_response.json.return_value = dict(workItems=[])
+        self.mock_unit_response.json.return_value = dict(value=[])
+        with patch("requests.post", return_value=self.mock_response):
+            with patch("requests.get", return_value=self.mock_unit_response):
+                response = collect_measurement(self.metric)
+        self.assertEqual("0", response["sources"][0]["value"])
