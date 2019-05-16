e7287c7078277db8206e56cd08ade78dd4a5a227
Wessel <w.vanmeijbeek@gmail.com>
2019-05-16 16:01:00 +0200

    Fixed the issue again, now actually working.

--- components/collector/src/__init__.py
+++ components/collector/src/__init__.py
@@ -63,14 +63,13 @@ class MetricsCollector:
     def skip(self, metric_uuid: str, metric) -> bool:
         """Return whether the metric needs to be measured."""
         sources = metric.get("sources")
-        empty = ""
         if not sources:
             return True  # Always skip if the metric has no sources
         if len(sources) == 1:
-            source = sources.values()[0]
+            source = list(sources.values())[0]
             parameters = source.get("parameters", {})
             url = parameters.get("url")
-            if url == empty or not url:
+            if not url:
                 return True  # Skip this source if it has no url
         if self.last_parameters.get(metric_uuid) != metric:
             return False  # Don't skip if metric parameters changed
