3e6f7713d744eeff1ef6723aaf4d7d194e623382
Frank Niessink <frank@niessink.com>
2019-05-23 17:12:50 +0200

    Sort by remaining columns.

--- components/frontend/src/Subject.js
+++ components/frontend/src/Subject.js
@@ -63,29 +63,53 @@ class Subject extends Component {
     );
     if (sort_column !== null) {
       let self = this;
-      const status_order = {"": "0", target_not_met: "1", debt_target_met: "2", near_target_met: "3", target_met: "4"};
-      metric_components.sort(function (m1, m2) {
-        let attribute1 = '';
-        let attribute2 = '';
-        if (sort_column === 'name') {
-          attribute1 = m1.props.metric.name || self.props.datamodel.metrics[m1.props.metric.type].name;
-          attribute2 = m2.props.metric.name || self.props.datamodel.metrics[m2.props.metric.type].name;
-        } else if (sort_column === 'comment') {
-          attribute1 = m1.props.metric.comment || '';
-          attribute2 = m2.props.metric.comment || '';
-        } else if (sort_column === 'status') {
-          attribute1 = status_order[self.state.last_measurements[m1.props.metric_uuid] && self.state.last_measurements[m1.props.metric_uuid].status || ''];
-          attribute2 = status_order[self.state.last_measurements[m2.props.metric_uuid] && self.state.last_measurements[m2.props.metric_uuid].status || ''];
-        } else if (sort_column === 'source') {
+      const status_order = { "": "0", target_not_met: "1", debt_target_met: "2", near_target_met: "3", target_met: "4" };
+      const sorters = {
+        name: function(m1, m2) {
+          const attribute1 = m1.props.metric.name || self.props.datamodel.metrics[m1.props.metric.type].name;
+          const attribute2 = m2.props.metric.name || self.props.datamodel.metrics[m2.props.metric.type].name;
+          return attribute1.localeCompare(attribute2)
+        },
+        measurement: function(m1, m2) {
+          const attribute1 = self.state.last_measurements[m1.props.metric_uuid] && self.state.last_measurements[m1.props.metric_uuid].value || '';
+          const attribute2 = self.state.last_measurements[m2.props.metric_uuid] && self.state.last_measurements[m2.props.metric_uuid].value || '';
+          return attribute1.localeCompare(attribute2)
+        },
+        target: function(m1, m2) {
+          const attribute1 = m1.props.metric.accept_debt ? m1.props.metric.debt_target : m1.props.metric.target;
+          const attribute2 = m2.props.metric.accept_debt ? m2.props.metric.debt_target : m2.props.metric.target;
+          return attribute1.localeCompare(attribute2)
+        },
+        comment: function(m1, m2) {
+          const attribute1 = m1.props.metric.comment || '';
+          const attribute2 = m2.props.metric.comment || '';
+          return attribute1.localeCompare(attribute2)
+        },
+        status: function(m1, m2) {
+          const attribute1 = status_order[self.state.last_measurements[m1.props.metric_uuid] && self.state.last_measurements[m1.props.metric_uuid].status || ''];
+          const attribute2 = status_order[self.state.last_measurements[m2.props.metric_uuid] && self.state.last_measurements[m2.props.metric_uuid].status || ''];
+          return attribute1.localeCompare(attribute2)
+        },
+        source: function(m1, m2) {
           let m1_sources = Object.values(m1.props.metric.sources).map((source) => source.name || self.props.datamodel.sources[source.type].name);
           m1_sources.sort();
           let m2_sources = Object.values(m2.props.metric.sources).map((source) => source.name || self.props.datamodel.sources[source.type].name);
           m2_sources.sort();
-          attribute1 = m1_sources.length > 0 ? m1_sources[0] : '';
-          attribute2 = m2_sources.length > 0 ? m2_sources[0] : '';
+          const attribute1 = m1_sources.length > 0 ? m1_sources[0] : '';
+          const attribute2 = m2_sources.length > 0 ? m2_sources[0] : '';
+          return attribute1.localeCompare(attribute2)
+        },
+        tags: function(m1, m2) {
+          let m1_tags = m1.props.metric.tags;
+          m1_tags.sort();
+          let m2_tags = m2.props.metric.tags;
+          m2_tags.sort();
+          const attribute1 = m1_tags.length > 0 ? m1_tags[0] : '';
+          const attribute2 = m2_tags.length > 0 ? m2_tags[0] : '';
+          return attribute1.localeCompare(attribute2)
         }
-        return attribute1.localeCompare(attribute2);
-      });
+      }
+      metric_components.sort(sorters[sort_column]);
       if (sort_direction === 'descending') {
         metric_components.reverse()
       }
@@ -117,8 +141,18 @@ class Subject extends Component {
               >
                 Status
               </Table.HeaderCell>
-              <Table.HeaderCell>Measurement</Table.HeaderCell>
-              <Table.HeaderCell>Target</Table.HeaderCell>
+              <Table.HeaderCell
+                onClick={(event) => this.handle_sort(event, 'measurement')}
+                sorted={sort_column === 'measurement' ? sort_direction : null}
+              >
+                Measurement
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                onClick={(event) => this.handle_sort(event, 'target')}
+                sorted={sort_column === 'target' ? sort_direction : null}
+              >
+                Target
+              </Table.HeaderCell>
               <Table.HeaderCell
                 onClick={(event) => this.handle_sort(event, 'source')}
                 sorted={sort_column === 'source' ? sort_direction : null}
@@ -130,7 +164,12 @@ class Subject extends Component {
                 sorted={sort_column === 'comment' ? sort_direction : null}
               >
                 Comment</Table.HeaderCell>
-              <Table.HeaderCell>Tags</Table.HeaderCell>
+              <Table.HeaderCell
+                onClick={(event) => this.handle_sort(event, 'tags')}
+                sorted={sort_column === 'tags' ? sort_direction : null}
+              >
+                Tags
+              </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>{metric_components}</Table.Body>
