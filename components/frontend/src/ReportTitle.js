af581df4336bca7675c1c345f14adcac9e39b820
Frank Niessink <frank@niessink.com>
2019-05-26 15:42:52 +0200

    Refactoring (#335)
    * Move up API calls.
    
    * Remove duplication.
    
    * Make class a function.

--- components/frontend/src/ReportTitle.js
+++ components/frontend/src/ReportTitle.js
@@ -1,90 +1,49 @@
-import React, { Component } from 'react';
-import { Button, Grid, Header, Icon, Segment } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Grid, Icon, Segment } from 'semantic-ui-react';
 import { StringInput } from './fields/StringInput';
+import { HeaderWithDetails } from './HeaderWithDetails';
 
-class ReportTitle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { show_details: false }
-    }
-    onExpand(event) {
-        event.preventDefault();
-        this.setState((state) => ({ show_details: !state.show_details }));
-    }
-    set_report_attribute(key, value) {
-        const self = this;
-        fetch(`${window.server_url}/report/${this.props.report.report_uuid}/${key}`, {
-            method: 'post',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ [key]: value })
-        }).then(
-            () => self.props.reload()
-        )
-    }
-    render() {
-        return (
-            <>
-                <Header as='h1'
-                    onClick={(e) => this.onExpand(e)}
-                    onKeyPress={(e) => this.onExpand(e)}
-                    tabIndex="0">
-                    <Icon
-                        name={this.state.show_details ? "caret down" : "caret right"}
-                        size='large'
-                    />
-                    <Header.Content>
-                        {this.props.report.title}
-                        <Header.Subheader>{this.props.report.subtitle}</Header.Subheader>
-                    </Header.Content>
-                </Header>
-                {
-                    this.state.show_details &&
-                    <Segment>
-                        <Grid stackable>
-                            <Grid.Row columns={3}>
-                                <Grid.Column>
-                                    <StringInput
-                                        label="Report title"
-                                        readOnly={this.props.readOnly}
-                                        set_value={(value) => this.set_report_attribute("title", value)}
-                                        value={this.props.report.title}
-                                    />
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <StringInput
-                                        label="Report subtitle"
-                                        readOnly={this.props.readOnly}
-                                        set_value={(value) => this.set_report_attribute("subtitle", value)}
-                                        value={this.props.report.subtitle}
-                                    />
-                                </Grid.Column>
-                            </Grid.Row>
-                            {!this.props.readOnly &&
-                                <Grid.Row>
-                                    <Grid.Column>
-                                        <Button
-                                            basic
-                                            floated='right'
-                                            negative
-                                            icon
-                                            onClick={(e) => this.props.delete_report(e)}
-                                            primary
-                                        >
-                                            <Icon name='trash' /> Delete report
-                                        </Button>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            }
-                        </Grid>
-                    </Segment>
-                }
-            </>
-        )
-    }
+export function ReportTitle(props) {
+    return (
+        <HeaderWithDetails level="h2" header={props.report.title} subheader={props.report.subtitle}>
+            <Segment>
+                <Grid stackable>
+                    <Grid.Row columns={3}>
+                        <Grid.Column>
+                            <StringInput
+                                label="Report title"
+                                readOnly={props.readOnly}
+                                set_value={(value) => props.set_report_attribute("title", value)}
+                                value={props.report.title}
+                            />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <StringInput
+                                label="Report subtitle"
+                                readOnly={props.readOnly}
+                                set_value={(value) => props.set_report_attribute("subtitle", value)}
+                                value={props.report.subtitle}
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    {!props.readOnly &&
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Button
+                                    basic
+                                    floated='right'
+                                    negative
+                                    icon
+                                    onClick={(e) => props.delete_report(e)}
+                                    primary
+                                >
+                                    <Icon name='trash' /> Delete report
+                                </Button>
+                            </Grid.Column>
+                        </Grid.Row>
+                    }
+                </Grid>
+            </Segment>
+        </HeaderWithDetails>
+    )
 }
-
-export { ReportTitle };
--- components/frontend/src/SubjectTitle.js
+++ components/frontend/src/SubjectTitle.js
@@ -1,113 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Grid, Header, Icon, Segment } from 'semantic-ui-react';
 import { StringInput } from './fields/StringInput';
 import { SubjectType } from './SubjectType';
+import { HeaderWithDetails } from './HeaderWithDetails';
 
-class SubjectTitle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { show_details: false }
-    }
-    onExpand(event) {
-        event.preventDefault();
-        this.setState((state) => ({ show_details: !state.show_details }));
-    }
-    set_subject_attribute(key, value) {
-        const self = this;
-        fetch(`${window.server_url}/report/${this.props.report_uuid}/subject/${this.props.subject_uuid}/${key}`, {
-            method: 'post',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ [key]: value })
-        }).then(
-            () => self.props.reload()
-        )
-    }
-    delete_subject(event) {
-        event.preventDefault();
-        const self = this;
-        fetch(`${window.server_url}/report/${this.props.report_uuid}/subject/${this.props.subject_uuid}`, {
-            method: 'delete',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({})
-        }).then(
-            () => self.props.reload()
-        );
-    }
-    render() {
-        const current_subject_type = this.props.datamodel.subjects[this.props.subject.type] || { name: "Unknown subject type", description: "No description" };
-        return (
-            <>
-                <Header as='h2' style={{marginTop: 50}}
-                    onClick={(e) => this.onExpand(e)}
-                    onKeyPress={(e) => this.onExpand(e)}
-                    tabIndex="0">
-                    <Icon
-                        name={this.state.show_details ? "caret down" : "caret right"}
-                        size='large'
-                    />
-                    {this.props.subject.name}
+export function SubjectTitle(props) {
+    const current_subject_type = props.datamodel.subjects[props.subject.type] || { name: "Unknown subject type", description: "No description" };
+    return (
+        <HeaderWithDetails level="h2" header={props.subject.name}>
+            <Segment>
+                <Header>
+                    <Header.Content>
+                        {current_subject_type.name}
+                        <Header.Subheader>
+                            {current_subject_type.description}
+                        </Header.Subheader>
+                    </Header.Content>
                 </Header>
-                {
-                    this.state.show_details &&
-                    <Segment>
-                        <Header>
-                            <Header.Content>
-                                {current_subject_type.name}
-                                <Header.Subheader>
-                                    {current_subject_type.description}
-                                </Header.Subheader>
-                            </Header.Content>
-                        </Header>
-                        <Grid stackable>
-                            <Grid.Row columns={3}>
-                                <Grid.Column>
-                                    <SubjectType
-                                        datamodel={this.props.datamodel}
-                                        readOnly={this.props.readOnly}
-                                        set_value={(value) => this.set_subject_attribute("type", value)}
-                                        subject_type={this.props.subject.type}
-                                    />
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <StringInput
-                                        label="Subject name"
-                                        placeholder={current_subject_type.name}
-                                        readOnly={this.props.readOnly}
-                                        set_value={(value) => this.set_subject_attribute("name", value)}
-                                        value={this.props.subject.name}
-                                    />
-                                </Grid.Column>
-                            </Grid.Row>
-                            {!this.props.readOnly &&
-                                <Grid.Row>
-                                    <Grid.Column>
-                                        <Button
-                                            basic
-                                            floated='right'
-                                            negative
-                                            icon
-                                            onClick={(e) => this.delete_subject(e)}
-                                            primary
-                                        >
-                                            <Icon name='trash' /> Delete subject
-                                        </Button>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            }
-                        </Grid>
-                    </Segment>
-                }
-            </>
-        )
-    }
+                <Grid stackable>
+                    <Grid.Row columns={3}>
+                        <Grid.Column>
+                            <SubjectType
+                                datamodel={props.datamodel}
+                                readOnly={props.readOnly}
+                                set_value={(value) => props.set_subject_attribute("type", value)}
+                                subject_type={props.subject.type}
+                            />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <StringInput
+                                label="Subject name"
+                                placeholder={current_subject_type.name}
+                                readOnly={props.readOnly}
+                                set_value={(value) => props.set_subject_attribute("name", value)}
+                                value={props.subject.name}
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    {!props.readOnly &&
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Button
+                                    basic
+                                    floated='right'
+                                    negative
+                                    icon
+                                    onClick={(e) => props.delete_subject(e)}
+                                    primary
+                                >
+                                    <Icon name='trash' /> Delete subject
+                                </Button>
+                            </Grid.Column>
+                        </Grid.Row>
+                    }
+                </Grid>
+            </Segment>
+        </HeaderWithDetails>
+    )
 }
-
-export { SubjectTitle };
