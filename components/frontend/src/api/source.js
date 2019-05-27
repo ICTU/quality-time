e9e2b8600b38d912582c9800879fafc21220a308
Frank Niessink <frank@niessink.com>
2019-05-27 17:06:37 +0200

    Move add_source to api module.

--- components/frontend/src/api/source.js
+++ components/frontend/src/api/source.js
@@ -1,7 +1,11 @@
 import { fetch_server_api } from "./fetch_server_api";
 
+function add_source(report_uuid, metric_uuid, reload) {
+    fetch_server_api('post', `report/${report_uuid}/metric/${metric_uuid}/source/new`, {}, reload)
+}
+
 function delete_source(report_uuid, source_uuid, reload) {
     fetch_server_api('delete', `report/${report_uuid}/source/${source_uuid}`, {}, reload)
 }
 
-export { delete_source }
+export { add_source, delete_source }
--- components/frontend/src/source/Sources.js
+++ components/frontend/src/source/Sources.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
 import { Button, Icon, Segment } from 'semantic-ui-react';
 import { Source } from './Source';
-
+import { add_source } from '../api/source';
 
 class Sources extends Component {
-    onAddSource(event) {
-        event.preventDefault();
-        const self = this;
-        fetch(`${window.server_url}/report/${this.props.report.report_uuid}/metric/${this.props.metric_uuid}/source/new`, {
-            method: 'post',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({})
-        }).then(
-            () => self.props.reload()
-        );
-    }
     source_error(measurement_sources, source_uuid, error_type) {
         let message = '';
         measurement_sources.forEach((source) => {
@@ -55,7 +40,12 @@ class Sources extends Component {
             <>
                 {sources}
                 {!this.props.readOnly && <Segment vertical>
-                    <Button icon primary basic onClick={(e) => this.onAddSource(e)}>
+                    <Button
+                        basic
+                        icon
+                        onClick={() => add_source(this.props.report.report_uuid, this.props.metric_uuid, this.props.reload)}
+                        primary
+                    >
                         <Icon name='plus' /> Add source
                     </Button>
                 </Segment>}
