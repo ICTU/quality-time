a6954f9f3db20d2e829043eeaf9b7929d64a5375
Frank Niessink <frank@niessink.com>
2019-06-01 17:32:13 +0200

    Use React hooks for date input.

--- components/frontend/src/fields/DateInput.js
+++ components/frontend/src/fields/DateInput.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form } from 'semantic-ui-react';
 import { DateInput as CalendarDateInput } from 'semantic-ui-calendar-react';
 import { Input } from './Input';
 
-class EditableDateInput extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { edited_date: props.value }
-    }
-    onChange = (event, { name, value }) => {
-        this.setState({ edited_date: value })
-        this.props.set_value(value)
-    }
-    render() {
-        return (
-            <Form>
-                <CalendarDateInput
-                    dateFormat="YYYY-MM-DD"
-                    disabled={this.props.readOnly}
-                    label={this.props.label}
-                    onChange={this.onChange}
-                    error={this.props.required && this.state.edited_date === ""}
-                    value={this.state.edited_date}
-                />
-            </Form>
-        )
-    }
+function EditableDateInput(props) {
+    const [date, setDate] = useState(props.value);
+    useEffect(() => { props.set_value(date) }, [date]);
+    return (
+        <Form>
+            <CalendarDateInput
+                dateFormat="YYYY-MM-DD"
+                disabled={props.readOnly}
+                label={props.label}
+                onChange={(event, { name, value }) => setDate(value)}
+                error={props.required && date === ""}
+                value={date}
+            />
+        </Form>
+    )
 }
 
-function DateInput(props) {
+export function DateInput(props) {
     return props.readOnly ?
-      <Input {...props} icon='calendar' />
-      :
-      <EditableDateInput {...props} />
-  }
-
-export { DateInput };
+        <Input {...props} icon='calendar' />
+        :
+        <EditableDateInput {...props} />
+}
