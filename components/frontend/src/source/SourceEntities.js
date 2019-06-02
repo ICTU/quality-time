91a43b18ad399012fe4159f44bd81541dc530a43
Frank Niessink <frank@niessink.com>
2019-06-02 16:29:37 +0200

    Use React hooks for source entities.

--- components/frontend/src/source/SourceEntities.js
+++ components/frontend/src/source/SourceEntities.js
@@ -1,97 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Popup, Table } from 'semantic-ui-react';
 import { SourceEntity } from './SourceEntity';
 
-class SourceEntities extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hide_ignored_entities: false, sort_column: null, sort_direction: null };
-  }
+export function SourceEntities(props) {
+  const [hideIgnoredEntities, setHideIgnoredEntities] = useState(false);
+  const [sortColumn, setSortColumn] = useState(null);
+  const [sortDirection, setSortDirection] = useState('ascending');
 
-  hide_ignored_entities(event) {
-    event.preventDefault();
-    this.setState({ hide_ignored_entities: !this.state.hide_ignored_entities })
+  if (!Array.isArray(props.source.entities) || props.source.entities.length === 0) {
+    return null;
   }
-
-  handle_sort(event, column) {
-    event.preventDefault();
-    const sort_direction = this.state.sort_direction;
-    this.setState(
-      {
-        sort_column: column,
-        sort_direction: sort_direction === 'ascending' ? 'descending' : 'ascending'
-      })
-  }
-
-  render() {
-    if (!Array.isArray(this.props.source.entities) || this.props.source.entities.length === 0) {
-      return null;
-    }
-    const report_source = this.props.metric.sources[this.props.source.source_uuid];
-    const source_type = report_source.type;
-    const entity_attributes = this.props.datamodel.sources[source_type].entities[this.props.metric.type].attributes;
-    const entity_name = this.props.datamodel.sources[source_type].entities[this.props.metric.type].name;
-    const entity_name_plural = this.props.datamodel.sources[source_type].entities[this.props.metric.type].name_plural;
-    const { hide_ignored_entities, sort_column, sort_direction } = this.state;
-    const headers =
-      <Table.Row>
-        <Table.HeaderCell collapsing textAlign="center">
-          <Popup trigger={
-            <Button
-              basic
-              compact
-              icon={hide_ignored_entities ? 'unhide' : 'hide'}
-              onClick={(e) => this.hide_ignored_entities(e)}
-              primary
-            />
-          } content={hide_ignored_entities ? `Show resolved ${entity_name_plural}` : `Hide resolved ${entity_name_plural}`} />
-        </Table.HeaderCell>
-        {entity_attributes.map((entity_attribute) =>
-          <Table.HeaderCell
-            key={entity_attribute.key}
-            sorted={sort_column === entity_attribute.key ? sort_direction : null}
-            onClick={(event) => this.handle_sort(event, entity_attribute.key)}
-          >
-            {entity_attribute.name}
-          </Table.HeaderCell>)
-        }
-      </Table.Row>
-    let entities = Array.from(this.props.source.entities);
-    if (sort_column !== null) {
-      entities.sort((a, b) => a[sort_column] < b[sort_column] ? -1 : 1)
-      if (sort_direction === 'descending') {
-        entities.reverse()
+  const report_source = props.metric.sources[props.source.source_uuid];
+  const source_type = report_source.type;
+  const entity_attributes = props.datamodel.sources[source_type].entities[props.metric.type].attributes;
+  const entity_name = props.datamodel.sources[source_type].entities[props.metric.type].name;
+  const entity_name_plural = props.datamodel.sources[source_type].entities[props.metric.type].name_plural;
+  const headers =
+    <Table.Row>
+      <Table.HeaderCell collapsing textAlign="center">
+        <Popup trigger={
+          <Button
+            basic
+            compact
+            icon={hideIgnoredEntities ? 'unhide' : 'hide'}
+            onClick={() => setHideIgnoredEntities(!hideIgnoredEntities)}
+            primary
+          />
+        } content={hideIgnoredEntities ? `Show resolved ${entity_name_plural}` : `Hide resolved ${entity_name_plural}`} />
+      </Table.HeaderCell>
+      {entity_attributes.map((entity_attribute) =>
+        <Table.HeaderCell
+          key={entity_attribute.key}
+          sorted={sortColumn === entity_attribute.key ? sortDirection : null}
+          onClick={() => {
+            if (entity_attribute.key === sortColumn) {
+              setSortDirection(sortDirection === 'ascending' ? 'descending' : 'ascending')
+            } else {
+              setSortColumn(entity_attribute.key)
+            }
+          }}
+        >
+          {entity_attribute.name}
+        </Table.HeaderCell>)
       }
+    </Table.Row>
+  let entities = Array.from(props.source.entities);
+  if (sortColumn !== null) {
+    entities.sort((a, b) => a[sortColumn] < b[sortColumn] ? -1 : 1)
+    if (sortDirection === 'descending') {
+      entities.reverse()
     }
-    const rows = entities.map((entity) =>
-      <SourceEntity
-        entity={entity}
-        entity_attributes={entity_attributes}
-        entity_name={entity_name}
-        fetch_measurement_and_reload={this.props.fetch_measurement_and_reload}
-        hide_ignored_entities={hide_ignored_entities}
-        key={entity.key}
-        metric_uuid={this.props.metric_uuid}
-        status={
-          this.props.source.entity_user_data && this.props.source.entity_user_data[entity.key] &&
-            this.props.source.entity_user_data[entity.key].status ? this.props.source.entity_user_data[entity.key].status : "unconfirmed"}
-        rationale={
-          this.props.source.entity_user_data && this.props.source.entity_user_data[entity.key] &&
-            this.props.source.entity_user_data[entity.key].rationale ? this.props.source.entity_user_data[entity.key].rationale : ""}
-        readOnly={this.props.readOnly}
-        source_uuid={this.props.source.source_uuid}
-      />);
-    return (
-      <Table sortable size='small'>
-        <Table.Header>
-          {headers}
-        </Table.Header>
-        <Table.Body>
-          {rows}
-        </Table.Body>
-      </Table>
-    )
   }
+  const rows = entities.map((entity) =>
+    <SourceEntity
+      entity={entity}
+      entity_attributes={entity_attributes}
+      entity_name={entity_name}
+      fetch_measurement_and_reload={props.fetch_measurement_and_reload}
+      hide_ignored_entities={hideIgnoredEntities}
+      key={entity.key}
+      metric_uuid={props.metric_uuid}
+      status={
+        props.source.entity_user_data && props.source.entity_user_data[entity.key] &&
+          props.source.entity_user_data[entity.key].status ? props.source.entity_user_data[entity.key].status : "unconfirmed"}
+      rationale={
+        props.source.entity_user_data && props.source.entity_user_data[entity.key] &&
+          props.source.entity_user_data[entity.key].rationale ? props.source.entity_user_data[entity.key].rationale : ""}
+      readOnly={props.readOnly}
+      source_uuid={props.source.source_uuid}
+    />);
+  return (
+    <Table sortable size='small'>
+      <Table.Header>
+        {headers}
+      </Table.Header>
+      <Table.Body>
+        {rows}
+      </Table.Body>
+    </Table>
+  )
 }
-
-export { SourceEntities };
