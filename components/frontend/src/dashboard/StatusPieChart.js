246e084c06b2e1d0c2cc71297a13a23c5264f43d
Frank Niessink <frank@niessink.com>
2019-06-04 10:19:26 +0200

    Make inner radius of donut charts depend on the number of metrics. (#358)

--- components/frontend/src/dashboard/StatusPieChart.js
+++ components/frontend/src/dashboard/StatusPieChart.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { VictoryPie } from 'victory';
 
 export function StatusPieChart(props) {
+    const nr_metrics = props.red + props.green + props.yellow + props.grey + props.white;
+    const radius = 175;
+    const innerRadius = Math.max(0, (radius - 50) - Math.pow(nr_metrics, 1.35));
     return (
         <VictoryPie
             colorScale={["rgb(211,59,55)", "rgb(30,148,78)", "rgb(253,197,54)", "rgb(150,150,150)", "rgb(245,245,245)"]}
             padding={20}
             style={{
-                data: { strokeWidth: 0 }
+                data: { stroke: 'grey', strokeWidth: 1, strokeOpacity: 0.5 }
             }}
-            innerRadius={75}
+            radius={radius}
+            innerRadius={innerRadius}
             labels={() => null}
             animate={{ duration: 2000 }}
             data={[
