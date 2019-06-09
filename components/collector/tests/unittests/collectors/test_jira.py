fcb6f42041600c23f7ed81a926793b07481713db
Frank Niessink <frank@niessink.com>
2019-06-10 00:04:45 +0200

    Add Jira collector for the ready user story points collector. (#367)

--- components/collector/tests/unittests/collectors/test_jira.py
+++ components/collector/tests/unittests/collectors/test_jira.py
@@ -6,8 +6,8 @@ from unittest.mock import Mock, patch
 from src.collector import MetricCollector
 
 
-class JiraTest(unittest.TestCase):
-    """Unit tests for the Jira metrics."""
+class JiraIssuesTest(unittest.TestCase):
+    """Unit tests for the Jira issue collector."""
 
     def setUp(self):
         self.metric = dict(type="issues", addition="sum",
@@ -30,3 +30,24 @@ class JiraTest(unittest.TestCase):
         self.assertEqual(
             [dict(key="id", summary="Summary", url="http://jira/browse/key")],
             response["sources"][0]["entities"])
+
+
+class JiraReadyUserStoryPointsTest(unittest.TestCase):
+    """Unit tests for the Jira ready story points collector."""
+
+    def setUp(self):
+        self.metric = dict(
+            type="ready_user_story_points", addition="sum",
+            sources=dict(
+                a=dict(type="jira", parameters=dict(url="http://jira", jql="query", story_points_field="field"))))
+        self.mock_response = Mock()
+
+    def test_nr_story_points(self):
+        """Test that the number of story points is returned."""
+        self.mock_response.json.return_value = dict(
+            issues=[
+                dict(key="1", id="1", fields=dict(summary="summary 1", field=10)),
+                dict(key="2", id="2", fields=dict(summary="summary 2", field=32))])
+        with patch("requests.get", return_value=self.mock_response):
+            response = MetricCollector(self.metric).get()
+        self.assertEqual("42", response["sources"][0]["value"])
