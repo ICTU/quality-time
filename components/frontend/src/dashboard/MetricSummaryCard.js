ea6c395ecf4cf79c27f942f967f8fe1db9212ec9
Frank Niessink <frank@niessink.com>
2019-06-16 16:18:11 +0200

    Small improvements to the dashboards.

--- components/frontend/src/dashboard/MetricSummaryCard.js
+++ components/frontend/src/dashboard/MetricSummaryCard.js
@@ -3,13 +3,11 @@ import { Card } from 'semantic-ui-react';
 import { StatusPieChart } from './StatusPieChart';
 
 export function MetricSummaryCard(props) {
-    const nr_metrics = props.red + props.green + props.yellow + props.grey + props.white;
     return (
         <Card fluid onClick={props.onClick} onKeyPress={props.onClick} tabIndex="0">
             <StatusPieChart {...props} />
             <Card.Content>
-                <Card.Header>{props.header}</Card.Header>
-                <Card.Meta>Metrics: {nr_metrics}</Card.Meta>
+                <Card.Header textAlign='center'>{props.header}</Card.Header>
             </Card.Content>
         </Card>
     );
