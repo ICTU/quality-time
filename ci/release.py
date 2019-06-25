f448e5414d4b45d4a118d44af6f9355f800ee355
Frank Niessink <frank@niessink.com>
2019-06-25 17:04:05 +0200

    Use bump2version for changing version number.

--- ci/release.py
+++ ci/release.py
@@ -2,28 +2,19 @@
 
 """Release the application."""
 
-import os
-import re
+import subprocess
 import sys
 
 
-def check_version_number(version: str) -> None:
-    """Check that the release version number conforms to http://semver.org/spec/v2.0.0.html."""
-    if version.count(".") != 2:
-        sys.exit("Version number should have two dots.")
-    for index, part in enumerate(version.split(".")):
-        if not re.match(r"^\d+$", part):
-            sys.exit(f"Part {index+1} of {version} is not a positive number.")
-    for index, part in enumerate(version.split(".")):
-        if re.match(r"^0\d+$", part):
-            sys.exit(f"Part {index+1} of {version} has leading zeroes.")
-
 def main():
     """Create the release."""
-    release = sys.argv[1]
-    check_version_number(release)
-    os.system(f"cd components/frontend && npm version --no-git-tag-version --allow-same-version {release}")
-    os.system(f"git tag v{release} && git push --tags")
+    bump = sys.argv[1]
+    assert bump in ('patch', 'minor', 'major')
+    output = subprocess.run(f"bumpversion --dry-run --list {bump}", capture_output=True, shell=True, text=True)
+    new_version = [line for line in output.stdout.split("\n") if line.startswith("new_version")][0].split("=")[1]
+    subprocess.run("bumpversion", bump)
+    subprocess.run(f"git tag v{new_version} && git push --tags")
+
 
 if __name__ == "__main__":
     main()
