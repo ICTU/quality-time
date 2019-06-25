77819ae048bdc2cec88a22dcd2e86aa6f0a3d315
Frank Niessink <frank@niessink.com>
2019-06-25 17:17:01 +0200

    Run in shell.

--- ci/release.py
+++ ci/release.py
@@ -14,7 +14,7 @@ def main():
         f"bumpversion --dry-run --list {bump}", capture_output=True, check=True, shell=True, text=True)
     new_version = [line for line in output.stdout.split("\n") if line.startswith("new_version")][0].split("=")[1]
     subprocess.run(("bumpversion", bump), check=True)
-    subprocess.run(f"git tag v{new_version} && git push --tags")
+    subprocess.run(f"git tag v{new_version} && git push --tags", shell=True, check=True)
 
 
 if __name__ == "__main__":
