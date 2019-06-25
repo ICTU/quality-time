9890250ce846ae33278607403301f837fe5fe5ff
Frank Niessink <frank@niessink.com>
2019-06-25 17:20:32 +0200

    Let bump2version commit and tag.

--- ci/release.py
+++ ci/release.py
@@ -10,11 +10,8 @@ def main():
     """Create the release."""
     bump = sys.argv[1]
     assert bump in ('patch', 'minor', 'major')
-    output = subprocess.run(
-        f"bumpversion --dry-run --list {bump}", capture_output=True, check=True, shell=True, text=True)
-    new_version = [line for line in output.stdout.split("\n") if line.startswith("new_version")][0].split("=")[1]
     subprocess.run(("bumpversion", bump), check=True)
-    subprocess.run(f"git tag v{new_version} && git push --tags", shell=True, check=True)
+    subprocess.run(("git", "push", "--tags"), check=True)
 
 
 if __name__ == "__main__":
