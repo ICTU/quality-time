84a524c96b84e91585ca4a210e8b06b46ef2f5f1
Frank Niessink <frank@niessink.com>
2019-06-25 17:15:14 +0200

    Add checks for exit code.

--- ci/release.py
+++ ci/release.py
@@ -10,9 +10,10 @@ def main():
     """Create the release."""
     bump = sys.argv[1]
     assert bump in ('patch', 'minor', 'major')
-    output = subprocess.run(f"bumpversion --dry-run --list {bump}", capture_output=True, shell=True, text=True)
+    output = subprocess.run(
+        f"bumpversion --dry-run --list {bump}", capture_output=True, check=True, shell=True, text=True)
     new_version = [line for line in output.stdout.split("\n") if line.startswith("new_version")][0].split("=")[1]
-    subprocess.run("bumpversion", bump)
+    subprocess.run(("bumpversion", bump), check=True)
     subprocess.run(f"git tag v{new_version} && git push --tags")
 
 
