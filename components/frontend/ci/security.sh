a6435fa6e299953b7c6009cf6fe64a1dde1d33e9
Wessel <w.vanmeijbeek@gmail.com>
2019-06-27 14:35:56 +0200

    Batch file for owasp dependency check added to the CI folder in the frontend.

---
+++ components/frontend/ci/security.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+OWASPDC_DIRECTORY=$(pwd)/owasp_dependency_check
+DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
+REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+
+if [ ! -d "$DATA_DIRECTORY" ]; then
+    echo "Initially creating persistent directories"
+    mkdir -p "$DATA_DIRECTORY"
+    chmod -R 777 "$DATA_DIRECTORY"
+
+    mkdir -p "$REPORT_DIRECTORY"
+    chmod -R 777 "$REPORT_DIRECTORY"
+fi
+
+# Make sure we are using the latest version
+docker pull owasp/dependency-check
+
+docker run --rm \
+    --volume $(pwd):/src \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$REPORT_DIRECTORY":/report \
+    owasp/dependency-check \
+    --scan /src \
+    --format "ALL" \
+    --project "Quality-time" \
+    --out /report
+    # Use suppression like this: (/src == $pwd)
+    # --suppression "/src/security/dependency-check-suppression.xml"
