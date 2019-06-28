e3561d92d67abaad84cf6ebe0b87288379f2f6c0
Frank Niessink <frank@niessink.com>
2019-06-28 14:06:20 +0200

    Rollback version number.

--- components/collector/setup.py
+++ components/collector/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Quality-time collector",
-    version="0.2.2",
+    version="0.2.1",
     description="Quality report metrics collector.",
     author="Frank Niessink",
     author_email="frank.niessink@ictu.nl",
