8ebfba9e43ee25ab5164089c18901a98625e4a9a
Frank Niessink <frank@niessink.com>
2019-06-28 15:30:39 +0200

    Fix ui test (#421)
    * Rollback version number.
    
    * Fix tests.
    
    * Increase wait times.

--- components/art/cypress/integration/common/init_metric_target_and_debt.js
+++ components/art/cypress/integration/common/init_metric_target_and_debt.js
@@ -1,29 +1,20 @@
 import {Given} from "cypress-cucumber-preprocessor/steps";
 
 Given(/^I make sure the Metric debt target is (.+), Metric target is (.+) and Accept technical debt (.+)$/, (debt, metric, technicalDebt) => {
-    cy.get("table.ui.sortable.table > tbody > tr:nth-child(4)").within(() => {
-        cy.get('td.collapsing > i.caret.right').click();
-    });
+    cy.wait(4000);
+    cy.get("table.ui.sortable.table > tbody > tr:nth-child(4) > td.collapsing > i.caret.right").click();
 
     cy.get("table.ui.sortable.table > tbody > tr:nth-child(5)").within(() => {
         cy.get('.tabbutton').contains('Metric').click();
-        cy.get('td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(2)').within(() => {
-            cy.get('div:nth-child(1) > form > div > div').within( () => {
-                cy.get('div > input[type=number]')
-                    .clear()
-                    .type(metric);
-            })
+        cy.get('> td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(2)').within(() => {
+            cy.get('> div:nth-child(1) > form > div > div > div > input[type=number]').clear().type(metric);
         })
 
         cy.get('td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(3)').within(() => {
-            cy.get('div:nth-child(2) > form > div > div').within( () => {
-                cy.get('div > input[type=number]')
-                    .clear()
-                    .type(debt);
-            });
+            cy.get('div:nth-child(2) > form > div > div > div > input[type=number]').clear().type(debt);
             cy.get('div:nth-child(1) > form > div > div').within(() => {
-                cy.get('i').click();
-                cy.get(`div.visible.menu.transition > div:nth-child(${technicalDebt})`).click();
+                cy.get('> i').click();
+                cy.get(`> div.visible.menu.transition > div:nth-child(${technicalDebt})`).click();
             })
         });
     });
--- components/art/cypress/integration/technical_debt/technical_debt.js
+++ components/art/cypress/integration/technical_debt/technical_debt.js
@@ -6,7 +6,7 @@ Given(/^a metric whose measurement value is (.+) as the target value$/, (startVa
         .as('testLineCoverage');
 
     cy.get("@testLineCoverage").within(() => {
-        cy.get('td:nth-child(4) > i')
+        cy.get('> td:nth-child(4) > i')
         .should('have.class', startValue);
     });
     cy.get("table.ui.sortable.table > tbody > tr:nth-child(5)")
@@ -22,33 +22,24 @@ When(/^the quality manager changes debt target to (.+), Metric target to (.+) an
     cy.get("@metricDetails").within(() => {
         cy.get('> td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(3)')
             .as('metricDebt');
-        cy.get('td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(2)').within(() => {
-            cy.get('div:nth-child(1) > form > div > div').within( () => {
-                cy.get('div > input[type=number]')
-                    .clear()
-                    .type(metricChanged);
-            })
+        cy.get('> td > div > div.ui.bottom.attached.segment.active.tab > div.ui.stackable.grid > div:nth-child(2)').within(() => {
+            cy.get('> div:nth-child(1) > form > div > div > div > input[type=number]').clear().type(metricChanged);
         })
     });
 
     cy.get('@metricDebt').within( () => {
-        cy.get('> div:nth-child(2) > form > div > div').within( () => {
-            cy.get('div > input[type=number]')
-                .clear()
-                .type(debtChanged);
-        });
-        cy.wait(2000);
+        cy.get('> div:nth-child(2) > form > div > div > div > input[type=number]').clear().type(debtChanged);
+        cy.wait(4000);
         cy.get('> div:nth-child(1) > form > div > div').within(() => {
             cy.get('> i').click();
-            cy.get(`div.visible.menu.transition > div:nth-child(${acceptTdChanged})`).click();
+            cy.get(`> div.visible.menu.transition > div:nth-child(${acceptTdChanged})`).click();
         });
     });
 });
 
 Then(/^the metric is marked as (.+)$/, (endTarget) => {
-    cy.wait(2000);
+    cy.wait(4000);
     cy.get("@testLineCoverage").within(() => {
-        cy.get('> td:nth-child(4) > i')
-            .should('have.class', endTarget);
+        cy.get('> td:nth-child(4) > i').should('have.class', endTarget);
     });
 });
