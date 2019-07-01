06ceff49f5e69f1655e169251ef1498f55495549
Frank Niessink <frank@niessink.com>
2019-07-01 10:23:59 +0200

    Update changelog when releasing (#427)
    * Also change CHANGELOG.md when releasing.

--- ci/release.py
+++ ci/release.py
@@ -2,13 +2,15 @@
 
 """Release the application."""
 
+import datetime
 import argparse
+import os
 import subprocess
 
 
 def parse_arguments():
     """Return the command line arguments."""
-    bump = subprocess.check_output(("bumpversion", "--list", "--dry-run", "--allow-dirty", "part"), text=True)
+    bump = subprocess.check_output(("bump2version", "--list", "--dry-run", "--allow-dirty", "part"), text=True)
     current_version = [line for line in bump.split("\n") if line.startswith("current_version")][0].split("=")[1]
     parser = argparse.ArgumentParser(description=f'Release Quality-time. Current version is {current_version}.')
     parser.add_argument('version', choices=('patch', 'minor', 'major'))
@@ -17,7 +19,8 @@ def parse_arguments():
 
 def main():
     """Create the release."""
-    subprocess.run(("bumpversion", parse_arguments().version), check=True)
+    os.environ["RELEASE_DATE"] = datetime.date.today().isoformat()  # Used by bump2version to update CHANGELOG.md
+    subprocess.run(("bump2version", parse_arguments().version), check=True)
     subprocess.run(("git", "push", "--follow-tags"), check=True)
 
 
