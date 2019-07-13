13efb8a24e5dcbad3c83ce7e5067ccb72f480bac
Frank Niessink <frank@niessink.com>
2019-07-13 14:38:14 +0200

    Fix bug.

--- components/server/src/serve.py
+++ components/server/src/serve.py
@@ -20,7 +20,7 @@ def serve() -> None:  # pragma: nocover
     """Connect to the database and start the application server."""
     logging.getLogger().setLevel(logging.INFO)
     init_ldap()
-    init_database()
-    init_bottle()
+    database = init_database()
+    init_bottle(database)
     server = urllib.parse.urlparse(os.environ.get("SERVER_URL", "http://localhost:5001"))
     bottle.run(server="gevent", host="0.0.0.0", port=server.port, reloader=True, log=logging.getLogger())  # nosec
