40694bf745ebe22b3dbb7311ea1a3b7f1bb8e2d6
Hedde van der Heide <me@heddevanderheide.nl>
2019-07-30 09:28:46 +0200

    added basic implementation (no tests)

--- components/server/src/routes/auth.py
+++ components/server/src/routes/auth.py
@@ -13,6 +13,7 @@ from pymongo.database import Database
 
 from utilities.functions import uuid
 from database import sessions
+from utilities.ldap import LDAPUserObject
 
 
 def generate_session_id() -> str:
@@ -37,9 +38,26 @@ def login(database: Database) -> Dict[str, bool]:
     credentials = dict(bottle.request.json)
     unsafe_characters = re.compile(r"[^\w ]+", re.UNICODE)
     username = re.sub(unsafe_characters, "", credentials.get("username", "no username given"))
+
     ldap_root_dn = os.environ.get("LDAP_ROOT_DN", "dc=example,dc=org")
+    
+    ldap_lookup_user = os.environ.get("LDAP_LOOKUP_USER", "admin")
+    ldap_lookup_user_password = os.environ.get("LDAP_LOOKUP_USER_PASSWORD", "admin")
+    
     ldap_url = os.environ.get("LDAP_URL", "ldap://localhost:389")
     ldap_server = ldap.initialize(ldap_url)
+
+    try:
+        ldap_server.simple_bind_s(f"cn={ldap_lookup_user},{ldap_root_dn}", ldap_lookup_user_password)
+        result = ldap_server.search_s(
+            ldap_root_dn, ldap.SCOPE_SUBTREE, f"(|(uid={username})(cn={username}))", ['dn', 'uid', 'cn']
+        )
+        if result:
+            username = LDAPUserObject(result[0]).cn
+    except (ldap.INVALID_CREDENTIALS, ldap.UNWILLING_TO_PERFORM, ldap.INVALID_DN_SYNTAX, ldap.SERVER_DOWN) as reason:  # pylint: disable=no-member
+        logging.warning("Couldn't bind cn=%s,%s: %s", username, ldap_root_dn, reason)
+        return dict(ok=False)
+
     try:
         ldap_server.simple_bind_s(f"cn={username},{ldap_root_dn}", credentials.get("password"))
     except (ldap.INVALID_CREDENTIALS, ldap.UNWILLING_TO_PERFORM, ldap.INVALID_DN_SYNTAX, ldap.SERVER_DOWN) as reason:  # pylint: disable=no-member
@@ -47,6 +65,7 @@ def login(database: Database) -> Dict[str, bool]:
         return dict(ok=False)
     finally:
         ldap_server.unbind_s()
+
     session_id = generate_session_id()
     sessions.upsert(database, username, session_id)
     set_session_cookie(session_id)
