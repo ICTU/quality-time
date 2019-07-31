19c9cd4c7208c40683ebb19266611d4827f11cd4
Frank Niessink <frank@niessink.com>
2019-07-31 17:02:10 +0200

    Fix shared setup.py.

--- components/shared/setup.py
+++ components/shared/setup.py
@@ -1,7 +1,7 @@
 """Shared Python code for Quality-time backend components."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='shared', version='0.5.1', description='Shared Python code for Quality-time backend components',
-    author='Frank Niessink', author_email='frank.niessink@ictu.nl', packages=['shared'], zip_safe=True)
+    author='Frank Niessink', author_email='frank.niessink@ictu.nl', packages=find_packages(), zip_safe=True)
