ffa4d37348d4b505fcd9a743390f6c1995a95550
Frank Niessink <frank@niessink.com>
2019-09-04 11:59:02 +0200

    Add sleep to ART.

--- components/art/cypress/integration/create_report/create_report.js
+++ components/art/cypress/integration/create_report/create_report.js
@@ -2,11 +2,12 @@ import { When, Then } from "cypress-cucumber-preprocessor/steps";
 
 When(/^the quality manager creates a new report$/, () => {
     cy.contains('button', 'Add report').click();
+    cy.wait(4000);  // Only thing that works from things suggested @ https://github.com/cypress-io/cypress/issues/695
     cy.contains('a.ui.card', 'New report').click();
 });
 
 When(/^the quality manager adds a new subject$/, () => {
-    cy.wait(4000);  // Only thing that works from things suggested @ https://github.com/cypress-io/cypress/issues/695
+    cy.wait(4000);
     cy.contains('button', 'Add subject').click();
 });
 
