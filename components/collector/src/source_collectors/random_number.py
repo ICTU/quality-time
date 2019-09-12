4b96430595cdfe714070569470ba43c4c967c89e
Frank Niessink <frank@niessink.com>
2019-09-12 15:48:23 +0200

    Random source supports percentage scale.

--- components/collector/src/source_collectors/random_number.py
+++ components/collector/src/source_collectors/random_number.py
@@ -1,12 +1,12 @@
 """Metric collector that returns random values."""
 
-import io
 import random
+from http import HTTPStatus
 from typing import List
 
 import requests
 
-from utilities.type import URL
+from utilities.type import URL, Value
 from .source_collector import SourceCollector
 
 
@@ -14,11 +14,15 @@ class Random(SourceCollector):
     """Random number metric collector."""
     min = 1
     max = 99
+    total = 100
 
     def _get_source_responses(self, api_url: URL) -> List[requests.Response]:
-        """Return a random number as the response."""
-        response = requests.Response()
-        # Turn off security checks on the next line. The random number is not used for security/cryptography.
-        response.raw = io.BytesIO(bytes(str(random.randint(self.min, self.max)), "utf-8"))  # nosec
-        response.status_code = requests.status_codes.codes["OK"]
-        return [response]
+        fake_response = requests.Response()  # Return a fake response so that the parse methods will be called
+        fake_response.status_code = HTTPStatus.OK
+        return [fake_response]
+
+    def _parse_source_responses_value(self, responses: List[requests.Response]) -> Value:
+        return str(random.randint(self.min, self.max))  # nosec, random generator is not used for security purpose
+
+    def _parse_source_responses_total(self, responses: List[requests.Response]) -> Value:
+        return str(self.total)
