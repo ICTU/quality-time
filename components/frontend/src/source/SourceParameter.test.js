21ec56dfd1c8befe42dcf6912c569b70ce083af9
ivan <ivan.milenkovic@ictu.nl>
2019-10-08 18:05:28 +0200

    frontend unit tests added

--- components/frontend/src/source/SourceParameter.test.js
+++ components/frontend/src/source/SourceParameter.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import { SourceParameter } from './SourceParameter';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SourceParameter parameter_type="single_choice" parameter_values={["a"]} parameter_value="a" />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+var example_report={"subjects": [{"metrics": [{"sources": [{"type": "x", "parameters": {"key": "b"}}]}]}]}
+
+it('renders url type', () => {
+  const wrapper = shallow(<SourceParameter placeholder="place-holder" readOnly={true} parameter_name="name"
+                            required={false} source_uuid="source-uuid" parameter_key="key"
+                            parameter_type="url" reload="reload-" parameter_value="value-" warning={true}
+                            source={{"type": "x"}} report={example_report}/>);
+
+  expect(wrapper.find('StringInput').prop('label')).toBe("name");
+  expect(Array.from(wrapper.find('StringInput').prop('options'))).toStrictEqual(["b"]);
+  expect(wrapper.find('StringInput').prop('placeholder')).toBe("place-holder");
+  expect(wrapper.find('StringInput').prop('readOnly')).toBe(true);
+  expect(wrapper.find('StringInput').prop('required')).toBe(false);
+  expect(wrapper.find('StringInput').prop('value')).toBe('value-');
+  expect(wrapper.find('StringInput').prop('warning')).toBe(true);
+});
+
+it('renders string type', () => {
+    const wrapper = shallow(<SourceParameter parameter_type="string" source={{"type": "x"}} report={example_report}/>);
+    expect(wrapper.find('StringInput').exists()).toBe(true);
 });
