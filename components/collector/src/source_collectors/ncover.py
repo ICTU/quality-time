4fff3b736f0ed2a146358ea1025185dc029d66e3
Frank Niessink <frank@niessink.com>
2019-10-09 22:55:10 +0200

    NCover reports weren't parsed correctly. Fixes #675.

--- components/collector/src/source_collectors/ncover.py
+++ components/collector/src/source_collectors/ncover.py
@@ -1,11 +1,12 @@
 """NCover collectors."""
 
 import re
+import json
 from abc import ABC
+from datetime import datetime
 from typing import Tuple
 
-from bs4 import BeautifulSoup, Tag
-from dateutil.parser import parse
+from bs4 import BeautifulSoup
 
 from utilities.functions import days_ago
 from utilities.type import Responses, Value
@@ -16,15 +17,18 @@ class NCoverBase(SourceCollector, ABC):  # pylint: disable=abstract-method
     """Base class for NCover collectors."""
 
     @staticmethod
-    def _execution_summary(responses: Responses) -> Tag:
-        """Return the execution summary part of the NCover report."""
-        return BeautifulSoup(responses[0].text, "html.parser").find("div", id="execution_summary")
+    def _find_script(responses: Responses, text: str) -> str:
+        """Return the script containing the text."""
+        for script in BeautifulSoup(responses[0].text, "html.parser").find_all("script", type="text/javascript"):
+            if text in script.string:
+                return str(script.string)
+        return ""  # pragma: nocover
 
 
 class NCoverCoverageBase(NCoverBase, ABC):  # pylint: disable=abstract-method
     """Base class for NCover coverage collectors."""
 
-    coverage_label = "Subclass responsibility"
+    coverage_type = "Subclass responsibility"
 
     def _parse_source_responses_value(self, responses: Responses) -> Value:
         covered_items, total_items = self.__coverage(responses)
@@ -35,31 +39,30 @@ class NCoverCoverageBase(NCoverBase, ABC):  # pylint: disable=abstract-method
 
     def __coverage(self, responses) -> Tuple[str, str]:
         """Return the coverage (covered items, total number) from the execution summary with the specified label."""
-        execution_summary = self._execution_summary(responses)
-        label_div = execution_summary.find("div", class_="label", string=f"{self.coverage_label}:")
-        value_div = label_div.find_next_sibling("div")
-        coverage_div = value_div.find("div", string=re.compile(r"\(\d+ of \d+\)"))
-        coverage_parts = str(coverage_div.string)[1:-1].split(" of ")
-        return coverage_parts[0], coverage_parts[1]
+        script = self._find_script(responses, "ncover.execution.stats = ")
+        json_string = script.strip()[len('ncover.execution.stats = '):].strip(";")
+        coverage = json.loads(json_string)[f"{self.coverage_type}Coverage"]
+        return str(coverage["coveredPoints"]), str(coverage["coveragePoints"])
 
 
 class NCoverUncoveredLines(NCoverCoverageBase):
     """Collector to get the uncovered lines. Since NCover doesn't report lines, but sequence points, we use those.
     See http://www.ncover.com/blog/code-coverage-metrics-sequence-point-coverage/."""
 
-    coverage_label = "Sequence Points"
+    coverage_type = "sequencePoint"
 
 
 class NCoverUncoveredBranches(NCoverCoverageBase):
     """Collector to get the uncovered branches."""
 
-    coverage_label = "Branches"
+    coverage_type = "branch"
 
 
 class NCoverSourceUpToDateness(NCoverBase):
     """Collector to collect the NCover report age."""
 
     def _parse_source_responses_value(self, responses: Responses) -> Value:
-        execution_summary = self._execution_summary(responses)
-        collection_date = execution_summary.find("div", class_="value")
-        return str(days_ago(parse(collection_date.string)))
+        script = self._find_script(responses, "ncover.createDateTime")
+        match = re.search(r"ncover\.createDateTime = '(\d+)'", script)
+        timestamp = match.group(1) if match else ""
+        return str(days_ago(datetime.fromtimestamp(float(timestamp) / 1000)))
--- components/collector/tests/unittests/source_collectors_tests/test_ncover.py
+++ components/collector/tests/unittests/source_collectors_tests/test_ncover.py
@@ -16,39 +16,45 @@ class NCoverTest(SourceCollectorTestCase):
         """Test that the number of uncovered sequence points is returned."""
         metric = dict(type="uncovered_lines", sources=self.sources, addition="sum")
         response = self.collect(metric, get_request_text="""
-        <div id="execution_summary">
-            <div class="label">Sequence Points:</div>
-            <div class="value">
-                <div class="partial larger">82.0%</div>
-                <div class="partial smaller">(14068 of 17151)</div>
-            <div>
-        </div>
-        """)
-        self.assert_value(f"{17151-14068}", response)
-        self.assert_total("17151", response)
+        <script type="text/javascript">
+            Other javascript
+        </script>
+        <script type="text/javascript">
+            ncover.execution.stats = {
+                "sequencePointCoverage": {
+                    "coveragePoints": 17153,
+                    "coveredPoints": 14070
+                }
+            };
+        </script>""")
+        self.assert_value(f"{17153-14070}", response)
+        self.assert_total("17153", response)
 
     def test_uncovered_branches(self):
         """Test that the number of uncovered branches is returned."""
         metric = dict(type="uncovered_branches", sources=self.sources, addition="sum")
         response = self.collect(metric, get_request_text="""
-        <div id="execution_summary">
-            <div class="label">Branches:</div>
-            <div class="value">
-                <div class="partial larger">81.2%</div>
-                <div class="partial smaller">(9769 of 12034)</div>
-            </div>
-        </div>
-        """)
-        self.assert_value(f"{12034-9769}", response)
+            <script type="text/javascript">
+                Other javascript
+            </script>
+            <script type="text/javascript">
+                ncover.execution.stats = {
+                    "branchCoverage": {
+                        "coveragePoints": 12034,
+                        "coveredPoints": 9767
+                    }
+                };
+            </script>""")
+        self.assert_value(f"{12034-9767}", response)
         self.assert_total("12034", response)
 
     def test_source_up_to_dateness(self):
         """Test that the source age in days is returned."""
         metric = dict(type="source_up_to_dateness", sources=self.sources, addition="sum")
         response = self.collect(metric, get_request_text="""
-        <div id="execution_summary">
-            <div class="label">Collection Date:</div>
-            <div class="value">Tue Oct 01 2019 03:04:10</div>
-        </div>""")
-        expected_age = (datetime.utcnow() - datetime(2019, 10, 1, 3, 4, 10)).days
+         <script type="text/javascript">
+            ncover.serverRoot = 'http://127.0.0.1:11235';
+            ncover.createDateTime = '1440425155042';
+        </script>""")
+        expected_age = (datetime.utcnow() - datetime.fromtimestamp(1440425155042 / 1000.)).days
         self.assert_value(str(expected_age), response)
