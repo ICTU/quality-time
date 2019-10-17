ea2ebc17b3fe99849787c74797c2ed86dc0863cf
Frank Niessink <frank@niessink.com>
2019-10-17 23:22:43 +0200

    Increase test coverage.

---
+++ components/frontend/src/source/SourceParameters.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SourceParameters } from './SourceParameters';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<SourceParameters
+    datamodel={{sources: {source_type: {parameters: {parameter_key: {metrics: []}}}}}}
+    source={{type: "source_type"}} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
