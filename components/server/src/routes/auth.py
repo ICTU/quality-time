5318d4b87327f1d4efd1b135a189ed7a4434af04
ivan <ivan.milenkovic@ictu.nl>
2019-10-18 17:30:34 +0200

    unit tests fixed

--- components/server/src/routes/auth.py
+++ components/server/src/routes/auth.py
@@ -32,10 +32,10 @@ def set_session_cookie(session_id: str, expires_datetime: datetime) -> None:
         options["domain"] = domain
     bottle.response.set_cookie("session_id", session_id, **options)
 
-def check_password(tagged_digest_salt, password):
+def check_password(ssha_ldap_salted_password, password):
     """Checks the OpenLDAP tagged digest against the given password"""
 
-    digest_salt_b64 = tagged_digest_salt[6:]  # strip {SSHA}
+    digest_salt_b64 = ssha_ldap_salted_password[6:]  # strip {SSHA}
 
     digest_salt = base64.b64decode(digest_salt_b64)
     digest = digest_salt[:20]
@@ -49,8 +49,6 @@ def check_password(tagged_digest_salt, password):
 @bottle.post("/login")
 def login(database: Database) -> Dict[str, bool]:
     """Log the user in."""
-    # Pylint can't find the ldap.* constants for some reason, turn off the error message:
-    # pylint: disable=no-member
     credentials = dict(bottle.request.json)
     unsafe_characters = re.compile(r"[^\w ]+", re.UNICODE)
     username = re.sub(unsafe_characters, "", credentials.get("username", "no username given"))
@@ -58,31 +56,25 @@ def login(database: Database) -> Dict[str, bool]:
     ldap_url = os.environ.get("LDAP_URL", "ldap://localhost:389")
     ldap_lookup_user = os.environ.get("LDAP_LOOKUP_USER", "admin")
     ldap_lookup_user_password = os.environ.get("LDAP_LOOKUP_USER_PASSWORD", "admin")
-    un_field = 'cn'
 
-    ldap_server = Server(ldap_url, get_info=ALL)
     try:
-        with Connection(ldap_server, user=f"{un_field}={ldap_lookup_user},{ldap_root_dn}", password=ldap_lookup_user_password) as conn:
+        ldap_server = Server(ldap_url, get_info=ALL)
+        with Connection(ldap_server, user=f"cn={ldap_lookup_user},{ldap_root_dn}", password=ldap_lookup_user_password) as conn:
             if not conn.bind():
-                logging.error("LDAP: bind error occurred: {}".format(conn.result))
+                username = ldap_lookup_user
                 raise exceptions.LDAPBindError
 
             conn.search(ldap_root_dn, f"(|(uid={username})(cn={username}))", attributes=['userPassword']) # ALL_ATTRIBUTES
+            result = conn.entries[0]
+            # if not result.userPassword:
+            #     raise exceptions.LDAPInvalidAttributeSyntaxResult
 
-            result = conn.entries[0] # form b'{SSHA}64basedhashed'
+            password = credentials.get("password", "no password given")
+            if not check_password(result.userPassword.value, password):
+                return dict(ok=False)
 
-            if result.userPassword:
-                password = credentials.get("password", "no password given")
-                if not check_password(result.userPassword.value, password):
-                    return dict(ok=False)
-
-    except (exceptions.LDAPInvalidCredentialsResult, exceptions.LDAPUnwillingToPerformResult, exceptions.LDAPInvalidDNSyntaxResult,
-            exceptions.LDAPInvalidServerError, exceptions.LDAPServerPoolError, exceptions.LDAPServerPoolExhaustedError) as reason:
-
-        logging.warning("Couldn't bind cn=%s,%s: %s", username, ldap_root_dn, reason)
-        return dict(ok=False)
-    except Exception as xreason:
-        logging.warning("Couldn't bind cn=%s,%s: %s", username, ldap_root_dn, xreason)
+    except Exception as reason:
+        logging.warning("LDAP error for cn=%s,%s: %s", username, ldap_root_dn, reason)
         return dict(ok=False)
 
     session_id, session_expiration_datetime = generate_session()
--- components/server/tests/unittests/routes/test_auth.py
+++ components/server/tests/unittests/routes/test_auth.py
@@ -2,10 +2,12 @@
 
 import logging
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, Mock, patch
 
 import bottle
-import ldap  # pylint: disable=import-error
+import ldap3
+from ldap3.core import exceptions
+from database import sessions
 
 from src.routes import auth
 
@@ -13,78 +15,184 @@ from src.routes import auth
 class LoginTests(unittest.TestCase):
     """Unit tests for the login route."""
     def setUp(self):
-        logging.disable()
+        # logging.disable()
         self.database = Mock()
         self.ldap_server = Mock()
         self.ldap_server.search_s.return_value = [
             ('cn=John Doe,ou=users,dc=example,dc=org', {'cn': [b'John Doe'], 'uid': [b'jodoe', b'jodoe1']})]
         self.lookup_json = Mock(json=dict(username="admin", password="admin"))
         self.invalid_creds_json = Mock(json=dict(username="wrong", password="wrong"))
-        self.valid_creds_uid_json = Mock(json=dict(username="jodoe", password="secret"))
+        self.valid_creds_uid_json = MagicMock()
+        self.valid_creds_uid_json.json = dict(username="jodoe", password="secret")
         self.valid_creds_cn_json = Mock(json=dict(username="John Doe", password="secret"))
+        self.environ_get = MagicMock(side_effect=["dc=example,dc=org", "ldap://localhost:389", "admin", "admin", "ldap://localhost:389"])
 
     def tearDown(self):
         bottle.response._cookies = None  # pylint: disable=protected-access
         logging.disable(logging.NOTSET)
 
-    def test_successful_login(self):
+    @patch.object(ldap3.Connection, '__exit__')
+    @patch.object(ldap3.Connection, '__enter__')
+    @patch.object(ldap3.Connection, '__init__')
+    @patch.object(ldap3.Server, '__init__')
+    @patch('bottle.request')
+    def test_successful_login(self, request_json_mock, server_mock, connection_mock, connection_enter, connection_exit):
         """Test successful login."""
-        with patch("os.environ.get", Mock(return_value="http://www.quality-time.my-org.org:5001")):
-            with patch("bottle.request", return_value=self.valid_creds_uid_json):
-                with patch("ldap.initialize", return_value=self.ldap_server):
-                    self.assertEqual(dict(ok=True), auth.login(self.database))
+        environ_get = MagicMock(side_effect=["dc=example,dc=org", 'http://www.quality-time.my-org.org:5001',
+                                             "admin", "admin", 'http://www.quality-time.my-org.org:5001'])
+        request_json_mock.json = dict(username="jodoe", password="secret")
+        server_mock.return_value = None
+        connection_mock.return_value = None
+        connection_exit.return_value = None
+        fake_con = MagicMock()
+        fake_con.bind = MagicMock(return_value=True)
+        fake_con.search = MagicMock()
+        ldap_entry = Mock()
+        ldap_entry.userPassword = Mock()
+        ldap_entry.userPassword.value = b'{SSHA}W841/YybjO4TmqcNTqnBxFKd3SJggaPr'
+        fake_con.entries = [ldap_entry]
+        connection_enter.return_value = fake_con
+        with patch("os.environ.get", environ_get):
+            self.assertEqual(dict(ok=True), auth.login(self.database))
+
         cookie = str(bottle.response._cookies)  # pylint: disable=protected-access
         self.assertTrue(cookie.startswith("Set-Cookie: session_id="))
         self.assertTrue("domain=" in cookie.lower())
+        fake_con.search.assert_called_with("dc=example,dc=org", '(|(uid=jodoe)(cn=jodoe))', attributes=['userPassword'])
 
-    def test_successful_login_localhost_uid(self):
-        """Test successful login on localhost."""
-        with patch("bottle.request", return_value=self.valid_creds_uid_json):
-            with patch("ldap.initialize", return_value=self.ldap_server):
-                self.assertEqual(dict(ok=True), auth.login(self.database))
+    @patch.object(ldap3.Connection, '__exit__')
+    @patch.object(ldap3.Connection, '__enter__')
+    @patch.object(ldap3.Connection, '__init__')
+    @patch.object(ldap3.Server, '__init__')
+    @patch('bottle.request')
+    def test_successful_login_local(self, request_json_mock, server_mock, connection_mock, connection_enter, connection_exit):
+        """Test successful login."""
+        request_json_mock.json = dict(username="jodoe", password="secret")
+        server_mock.return_value = None
+        connection_mock.return_value = None
+        connection_exit.return_value = None
+        fake_con = MagicMock()
+        fake_con.bind = MagicMock(return_value=True)
+        fake_con.search = MagicMock()
+        ldap_entry = Mock()
+        ldap_entry.userPassword = Mock()
+        ldap_entry.userPassword.value = b'{SSHA}W841/YybjO4TmqcNTqnBxFKd3SJggaPr'
+        fake_con.entries = [ldap_entry]
+        connection_enter.return_value = fake_con
+        with patch("os.environ.get", self.environ_get):
+            self.assertEqual(dict(ok=True), auth.login(self.database))
+
         cookie = str(bottle.response._cookies)  # pylint: disable=protected-access
         self.assertTrue(cookie.startswith("Set-Cookie: session_id="))
         self.assertFalse("domain=" in cookie.lower())
+        fake_con.search.assert_called_with("dc=example,dc=org", '(|(uid=jodoe)(cn=jodoe))', attributes=['userPassword'])
 
-    def test_successful_login_localhost_cn(self):
-        """Test successful login on localhost."""
-        with patch("bottle.request", return_value=self.valid_creds_cn_json):
-            with patch("ldap.initialize", return_value=self.ldap_server):
-                self.assertEqual(dict(ok=True), auth.login(self.database))
-        cookie = str(bottle.response._cookies)  # pylint: disable=protected-access
-        self.assertTrue(cookie.startswith("Set-Cookie: session_id="))
-        self.assertFalse("domain=" in cookie.lower())
+    @patch.object(logging, 'warning')
+    @patch.object(ldap3.Connection, '__init__')
+    @patch.object(ldap3.Server, '__init__')
+    @patch('bottle.request')
+    def test_login_server_error(self, request_json_mock, server_mock, connection_mock, logging_mock):
+        """Test login when a server creation error occurs."""
+        request_json_mock.json = dict(username="jodoe", password="secret")
+        server_mock.side_effect = exceptions.LDAPServerPoolError
+        connection_mock.return_value = None
+        with patch("os.environ.get", self.environ_get):
+            self.assertEqual(dict(ok=False), auth.login(self.database))
+        connection_mock.assert_not_called()
+        self.assertEqual('LDAP error for cn=%s,%s: %s', logging_mock.call_args[0][0])
+        self.assertEqual('jodoe', logging_mock.call_args[0][1])
+        self.assertEqual("dc=example,dc=org", logging_mock.call_args[0][2])
+        self.assertIsInstance(logging_mock.call_args[0][3], exceptions.LDAPServerPoolError)
 
-    def test_lookup_user_simple_bind_s_failure(self):
-        self.ldap_server.simple_bind_s.side_effect = ldap.INVALID_CREDENTIALS
-        with patch("bottle.request", return_value=self.valid_creds_uid_json):
-            with patch("ldap.initialize", return_value=self.ldap_server):
-                self.assertEqual(dict(ok=False), auth.login(self.database))
+    @patch.object(logging, 'warning')
+    @patch.object(ldap3.Connection, '__exit__')
+    @patch.object(ldap3.Connection, '__enter__')
+    @patch.object(ldap3.Connection, '__init__')
+    @patch.object(ldap3.Server, '__init__')
+    @patch('bottle.request')
+    def test_login_bind_error(self, request_json_mock, server_mock, connection_mock, connection_enter, connection_exit, logging_mock):
+        """Test login when an error of binding dn reader occurs."""
+        request_json_mock.json = dict(username="jodoe", password="secret")
+        server_mock.return_value = None
+        connection_mock.return_value = None
+        connection_exit.return_value = None
+        fake_con = MagicMock()
+        fake_con.bind = MagicMock(return_value=False)
+        connection_enter.return_value = fake_con
+        with patch("os.environ.get", self.environ_get):
+            self.assertEqual(dict(ok=False), auth.login(self.database))
 
-    def test_simple_search_s_failure(self):
-        self.ldap_server.search_s.side_effect = ldap.INVALID_CREDENTIALS
-        with patch("bottle.request", return_value=self.invalid_creds_json):
-            with patch("ldap.initialize", return_value=self.ldap_server):
-                self.assertEqual(dict(ok=False), auth.login(self.database))
+        connection_mock.assert_called_once()
+        fake_con.bind.assert_called_once()
+        self.assertEqual('LDAP error for cn=%s,%s: %s', logging_mock.call_args[0][0])
+        self.assertEqual('admin', logging_mock.call_args[0][1])
+        self.assertEqual("dc=example,dc=org", logging_mock.call_args[0][2])
+        self.assertIsInstance(logging_mock.call_args[0][3], exceptions.LDAPBindError)
 
-    def test_unsuccessful_login_localhost_cn(self):
-        """Test unsuccessful login on localhost."""
-        self.ldap_server.simple_bind_s.side_effect = [None, ldap.INVALID_CREDENTIALS]
-        with patch("bottle.request", return_value=self.invalid_creds_json):
-            with patch("ldap.initialize", return_value=self.ldap_server):
-                self.assertEqual(dict(ok=False), auth.login(self.database))
+    @patch.object(logging, 'warning')
+    @patch.object(ldap3.Connection, '__exit__')
+    @patch.object(ldap3.Connection, '__enter__')
+    @patch.object(ldap3.Connection, '__init__')
+    @patch.object(ldap3.Server, '__init__')
+    @patch('bottle.request')
+    def test_login_search_error(self, request_json_mock, server_mock, connection_mock, connection_enter, connection_exit, logging_mock):
+        """Test login when search error of the login user occurs."""
+        request_json_mock.json = dict(username="jodoe", password="secret")
+        server_mock.return_value = None
+        connection_mock.return_value = None
+        connection_exit.return_value = None
+        fake_con = MagicMock()
+        fake_con.bind = MagicMock(return_value=True)
+        fake_con.search = MagicMock(side_effect=exceptions.LDAPResponseTimeoutError)
+        connection_enter.return_value = fake_con
+        with patch("os.environ.get", self.environ_get):
+            self.assertEqual(dict(ok=False), auth.login(self.database))
 
-    def test_failed_login(self):
-        """Test failed login."""
-        self.ldap_server.search_s.return_value = []
-        with patch("bottle.request", return_value=self.lookup_json):
-            with patch("ldap.initialize", return_value=self.ldap_server):
-                self.assertEqual(dict(ok=False), auth.login(self.database))
+        connection_mock.assert_called_once()
+        fake_con.bind.assert_called_once()
+        self.assertEqual('LDAP error for cn=%s,%s: %s', logging_mock.call_args[0][0])
+        self.assertEqual('jodoe', logging_mock.call_args[0][1])
+        self.assertEqual("dc=example,dc=org", logging_mock.call_args[0][2])
+        self.assertIsInstance(logging_mock.call_args[0][3], exceptions.LDAPResponseTimeoutError)
 
+    @patch.object(logging, 'warning')
+    @patch.object(ldap3.Connection, '__exit__')
+    @patch.object(ldap3.Connection, '__enter__')
+    @patch.object(ldap3.Connection, '__init__')
+    @patch.object(ldap3.Server, '__init__')
+    @patch('bottle.request')
+    def test_login_wrong_password(self, request_json_mock, server_mock, connection_mock, connection_enter,
+                                connection_exit, logging_mock):
+        """Test login when search error of the login user occurs."""
+        request_json_mock.json = dict(username="jodoe", password="wrong password!")
+        server_mock.return_value = None
+        connection_mock.return_value = None
+        connection_exit.return_value = None
+        fake_con = MagicMock()
+        fake_con.bind = MagicMock(return_value=True)
+        fake_con.search = MagicMock()
+        ldap_entry = Mock()
+        ldap_entry.userPassword = Mock()
+        ldap_entry.userPassword.value = b'{SSHA}W841/YybjO4TmqcNTqnBxFKd3SJggaPr'
+        fake_con.entries = [ldap_entry]
+        connection_enter.return_value = fake_con
+        with patch("os.environ.get", self.environ_get):
+            self.assertEqual(dict(ok=False), auth.login(self.database))
+        fake_con.search.assert_called_with("dc=example,dc=org", '(|(uid=jodoe)(cn=jodoe))', attributes=['userPassword'])
+        logging_mock.assert_not_called()
 
 class LogoutTests(unittest.TestCase):
     """Unit tests for the logout route."""
-    def test_logout(self):
+
+    @patch.object(sessions, 'delete')
+    @patch('bottle.request')
+    def test_logout(self, request_mock, delete_mock):
         """Test successful logout."""
-        database = Mock()
+        request_mock.get_cookie = MagicMock(return_value='the session id')
+        database = MagicMock()
         self.assertEqual(dict(ok=True), auth.logout(database))
+        cookie = str(bottle.response._cookies)  # pylint: disable=protected-access
+        self.assertTrue(cookie.startswith("Set-Cookie: session_id="))
+        self.assertTrue(cookie.find("the session id")>0)
+        self.assertTrue(cookie.find("expires=Mon, 01 Jan 0001")>0)
+
