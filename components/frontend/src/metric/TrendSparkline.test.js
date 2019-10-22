89ab9dd5def4f031067b60244dfb42ac4b1522b2
Frank Niessink <frank@niessink.com>
2019-10-22 19:56:07 +0200

    Increase test coverage.

--- components/frontend/src/metric/TrendSparkline.test.js
+++ components/frontend/src/metric/TrendSparkline.test.js
@@ -10,6 +10,6 @@ it('renders without crashing', () => {
 
 it('renders measurements without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<TrendSparkline measurements={[{count: {value: "1"}, start: "2019-09-29", end: "2019-09-30"}]} scale="count" q/>, div);
+  ReactDOM.render(<TrendSparkline measurements={[{count: {value: "1"}, start: "2019-09-29", end: "2019-09-30"}]} scale="count" />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- components/frontend/src/widgets/MoveButton.test.js
+++ components/frontend/src/widgets/MoveButton.test.js
@@ -1,9 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import { MoveButtonGroup } from './MoveButton';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MoveButtonGroup />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("<MoveButtonGroup />", () => {
+    it('calls the callback on click first', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup onClick={mockCallBack}/>);
+        wrapper.find("Button").at(0).simulate("click");
+        expect(mockCallBack).toHaveBeenCalledWith("first");
+    });
+
+    it('does not call the callback on click first when the button group is first', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup first={true} onClick={mockCallBack}/>);
+        wrapper.find("Button").at(0).simulate("click");
+        expect(mockCallBack).not.toHaveBeenCalled();
+    });
+
+    it('calls the callback on click previous', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup onClick={mockCallBack}/>);
+        wrapper.find("Button").at(1).simulate("click");
+        expect(mockCallBack).toHaveBeenCalledWith("previous");
+    });
+
+    it('does not call the callback on click previous when the button group is first', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup first={true} onClick={mockCallBack}/>);
+        wrapper.find("Button").at(1).simulate("click");
+        expect(mockCallBack).not.toHaveBeenCalled();
+    });
+
+    it('calls the callback on click next', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup onClick={mockCallBack}/>);
+        wrapper.find("Button").at(2).simulate("click");
+        expect(mockCallBack).toHaveBeenCalledWith("next");
+    });
+
+    it('does not call the callback on click next when the button group is last', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup last={true} onClick={mockCallBack}/>);
+        wrapper.find("Button").at(2).simulate("click");
+        expect(mockCallBack).not.toHaveBeenCalled();
+    });
+
+    it('calls the callback on click last', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup onClick={mockCallBack}/>);
+        wrapper.find("Button").at(3).simulate("click");
+        expect(mockCallBack).toHaveBeenCalledWith("last");
+    });
+
+    it('does not call the callback on click last when the button group is last', () => {
+        const mockCallBack = jest.fn();
+        const wrapper = mount(<MoveButtonGroup last={true} onClick={mockCallBack}/>);
+        wrapper.find("Button").at(3).simulate("click");
+        expect(mockCallBack).not.toHaveBeenCalled();
+    });
+})
