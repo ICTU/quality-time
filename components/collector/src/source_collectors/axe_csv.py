0a95b9fa5e5147177295a732d5d951c3f831b5d0
Frank Niessink <frank@niessink.com>
2019-10-23 18:45:26 +0200

    Allow for filtering accessibility violations from Axe CSV files by violation type.

--- components/collector/src/source_collectors/axe_csv.py
+++ components/collector/src/source_collectors/axe_csv.py
@@ -21,16 +21,19 @@ class AxeCSVAccessibility(SourceCollector):
         """Convert csv rows of the Axe report into entities, in this case accessibility violations."""
         return [
             dict(
-                key=hashlib.md5(row.encode('utf-8')).hexdigest(),  # nosec
-                violation_type=items["Violation Type"], impact=items["Impact"], url=str(items["URL"]),
-                element=items["DOM Element"], page=re.sub(r'http[s]?://[^/]+', '', items['URL']),
-                description=items["Messages"], help=items["Help"])
-            for row, items in self.__parse_csv(responses)]
+                key=hashlib.md5(row.strip().encode('utf-8')).hexdigest(),  # nosec
+                violation_type=parsed_row["Violation Type"], impact=parsed_row["Impact"], url=str(parsed_row["URL"]),
+                element=parsed_row["DOM Element"], page=re.sub(r'http[s]?://[^/]+', '', parsed_row['URL']),
+                description=parsed_row["Messages"], help=parsed_row["Help"])
+            for row, parsed_row in self.__parse_csv(responses)]
 
     def __parse_csv(self, responses: Responses) -> List[Tuple[str, Dict[str, str]]]:
         """Parse the CSV and return the rows and parsed items ."""
         impact_levels = self._parameter("impact")
-        rows = responses[0].text.split("\n")[1:]
-        parsed_rows = csv.DictReader(StringIO(responses[0].text, newline=None))
+        violation_types = self._parameter("violation_type")
+        csv_text = responses[0].text.strip()
+        rows = csv_text.split("\n")[1:]
+        parsed_rows = csv.DictReader(StringIO(csv_text))
         return [
-            (row, parsed_row) for row, parsed_row in zip(rows, parsed_rows) if parsed_row["Impact"] in impact_levels]
+            (row, parsed_row) for (row, parsed_row) in zip(rows, parsed_rows)
+            if parsed_row["Impact"] in impact_levels and parsed_row["Violation Type"] in violation_types]
--- components/collector/tests/unittests/source_collectors_tests/test_axe_csv.py
+++ components/collector/tests/unittests/source_collectors_tests/test_axe_csv.py
@@ -11,38 +11,40 @@ class AxeCSVAccessibility(SourceCollectorTestCase):
     def setUp(self):
         super().setUp()
         self.header_row = "URL,Violation Type,Impact,Help,HTML Element,Messages,DOM Element\n"
+        self.serious_violation = "url1,aria-input-field-name,serious,help1,html1\n"
+        self.moderate_violation = "url2,aria-hidden-focus,moderate,help2,html2,messages2,dom2\n"
+        self.csv = self.header_row + self.serious_violation + self.moderate_violation
         self.metric = dict(
-            type="accessibility",
-            sources=dict(
-                source_id=dict(
-                    type="axecsv", parameters=dict(url="https://axecsv", private_token="xxx"))), addition="sum")
+            type="accessibility", addition="sum",
+            sources=dict(source_id=dict(type="axecsv", parameters=dict(url="https://axecsv"))))
 
     def test_nr_of_issues(self):
         """Test that the number of issues is returned."""
-        response = self.collect(
-            self.metric, get_request_text=self.header_row + "1,2,serious,6,7\n11,12,moderate,14,15,16,17")
-        self.assert_measurement(response, value="2")
+        response = self.collect(self.metric, get_request_text=self.csv)
+        expected_key1 = hashlib.md5(self.serious_violation.strip().encode("utf-8")).hexdigest()
+        expected_key2 = hashlib.md5(self.moderate_violation.strip().encode("utf-8")).hexdigest()
+        expected_entities = [
+            {'description': None, 'element': None, 'help': 'help1', 'impact': 'serious',
+             'key': expected_key1, 'page': 'url1', 'url': 'url1', 'violation_type': 'aria-input-field-name'},
+            {'description': 'messages2', 'element': 'dom2', 'help': 'help2', 'impact': 'moderate',
+             'key': expected_key2, 'page': 'url2', 'url': 'url2', 'violation_type': 'aria-hidden-focus'},
+        ]
+        self.assert_measurement(response, value="2", entities=expected_entities)
 
     def test_no_issues(self):
         """Test zero issues."""
         response = self.collect(self.metric, get_request_text="")
-        self.assert_measurement(response, value="0")
-
-    def test_issues(self):
-        """Test that the issues are returned."""
-        csv_row = "https://axe/1,2,critical,4,5,6,7"
-        response = self.collect(self.metric, get_request_text=self.header_row + csv_row)
-        expected_key = hashlib.md5(csv_row.encode("utf-8")).hexdigest()
-        self.assert_measurement(
-            response,
-            entities=[
-                {'description': '6', 'element': '7', 'help': '4', 'impact': 'critical',
-                 'key': expected_key, 'page': '/1', 'url': 'https://axe/1',
-                 'violation_type': '2'}])
+        self.assert_measurement(response, value="0", entities=[])
 
     def test_filter_by_impact(self):
         """Test that violations can be filtered by impact level."""
         self.metric["sources"]["source_id"]["parameters"]["impact"] = ["serious", "critical"]
-        response = self.collect(
-            self.metric, get_request_text=self.header_row + "1,2,serious,6,7\n11,12,moderate,14,15,16,17")
+        response = self.collect(self.metric, get_request_text=self.csv)
+        self.assert_measurement(response, value="1")
+
+    def test_filter_by_violation_type(self):
+        """Test that violations can be filtered by violation type."""
+        self.metric["sources"]["source_id"]["parameters"]["violation_type"] = \
+            ["aria-input-field-name", "area-hidden-focus"]
+        response = self.collect(self.metric, get_request_text=self.csv)
         self.assert_measurement(response, value="1")
