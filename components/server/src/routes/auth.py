25a3c6cb497cf139b4df0291885ed4706b53409f
Frank Niessink <frank@niessink.com>
2019-10-25 16:09:45 +0200

    Use correct URL for cookies. (#741)

--- components/server/src/routes/auth.py
+++ components/server/src/routes/auth.py
@@ -27,7 +27,7 @@ def generate_session() -> Tuple[SessionId, datetime]:
 def set_session_cookie(session_id: str, expires_datetime: datetime) -> None:
     """Set the session cookie on the response. To clear the cookie, pass an expiration datetime of datetime.min."""
     options = dict(expires=expires_datetime, path="/", httponly=True)
-    server_url = os.environ.get("SERVER_URL", "http://localhost:5001")
+    server_url = os.environ.get("SERVER_URL_EXTERNAL", "http://localhost:5001")
     domain = urllib.parse.urlparse(server_url).netloc.split(":")[0]
     if domain != "localhost":
         options["domain"] = domain
