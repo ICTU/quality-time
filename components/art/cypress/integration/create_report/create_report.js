68bb0bd393a4830c8bc418e450c0030ff6696333
Frank Niessink <frank@niessink.com>
2019-10-25 19:07:20 +0200

    Fix ART (hopefully).

--- components/art/cypress/integration/create_report/create_report.js
+++ components/art/cypress/integration/create_report/create_report.js
@@ -1,28 +1,34 @@
 import { When, Then } from "cypress-cucumber-preprocessor/steps";
 
+function waitForBrowser() {
+    cy.window().then(win => {
+        return new Cypress.Promise(resolve => win['requestIdleCallback'](resolve));
+    });
+}
+
 When(/^the quality manager creates a new report$/, () => {
     cy.contains('button', 'Add report').click();
-    cy.wait(4000);  // Only thing that works from things suggested @ https://github.com/cypress-io/cypress/issues/695
-    cy.contains('a.ui.card', 'New report').click();
+    waitForBrowser();
+    cy.get('a.ui.card').contains("New report").click();
 });
 
 When(/^the quality manager adds a new subject$/, () => {
-    cy.wait(4000);
-    cy.contains('button', 'Add subject').click();
+    waitForBrowser();
+    cy.contains('.button', 'Add subject', {}).click();
 });
 
 When(/^the quality manager adds a new metric$/, () => {
-    cy.wait(4000);
-    cy.contains('button', 'Add metric').click();
+    waitForBrowser();
+    cy.contains('.button', 'Add metric').click();
 });
 
 When(/^the quality manager sets the metric target value to (.+)$/, (metric_target_value) => {
-    cy.wait(4000);
+    waitForBrowser();
     cy.get('input[type=number]').first().type("{selectall}").type(metric_target_value).blur();
 });
 
 Then(/^the metric target is (.+)$/, (expected_metric_target_value) => {
-    cy.wait(4000);
+    waitForBrowser();
     cy.get("table.ui.sortable.table > tbody > tr:first-child > td:nth-child(6)").should('contain',
         expected_metric_target_value);
 });
