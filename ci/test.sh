718ffd08fa1da255b4c28f8e717df17ceb6f7c09
Frank Niessink <frank@niessink.com>
2019-10-27 19:12:06 +0100

    Install chromedriver by hand on CircleCI.

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-docker build -t ictu/quality-time_selenium tests
-docker run -v `pwd`:`pwd` -w `pwd` -it ictu/quality-time_selenium python -m unittest discover --start-directory tests --quiet
+python -m unittest discover --start-directory tests --quiet
