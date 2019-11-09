d3b23b832fa2d81037de5aff72153b7056074b0f
Frank Niessink <frank@niessink.com>
2019-11-09 19:47:04 +0100

    Open Axe CSV files in universal newline mode. Fixes #777. (#780)

--- components/collector/src/source_collectors/axe_csv.py
+++ components/collector/src/source_collectors/axe_csv.py
@@ -4,9 +4,9 @@ import csv
 import hashlib
 import re
 from io import StringIO
-from typing import Dict, List, Tuple
+from typing import Dict, List
 
-from collector_utilities.type import Responses, Value, Entities
+from collector_utilities.type import Responses, Value, Entities, Entity
 from .source_collector import FileSourceCollector
 
 
@@ -21,23 +21,27 @@ class AxeCSVAccessibility(FileSourceCollector):
 
     def _parse_source_responses_entities(self, responses: Responses) -> Entities:
         """Convert csv rows of the Axe report into entities, in this case accessibility violations."""
-        return [
+        entities: Entities = [
             dict(
-                key=hashlib.md5(row.strip().encode('utf-8')).hexdigest(),  # nosec
-                violation_type=parsed_row["Violation Type"], impact=parsed_row["Impact"], url=str(parsed_row["URL"]),
-                element=parsed_row["DOM Element"], page=re.sub(r'http[s]?://[^/]+', '', parsed_row['URL']),
-                description=parsed_row["Messages"], help=parsed_row["Help"])
-            for row, parsed_row in self.__parse_csv(responses)]
+                url=str(row["URL"]), violation_type=row["Violation Type"], impact=row["Impact"],
+                element=row["DOM Element"], page=re.sub(r'http[s]?://[^/]+', '', row['URL']),
+                description=row["Messages"], help=row["Help"])
+            for row in self.__parse_csv(responses)]
+        for entity in entities:
+            entity["key"] = self.hash_entity(entity)
+        return entities
 
-    def __parse_csv(self, responses: Responses) -> List[Tuple[str, Dict[str, str]]]:
+    def __parse_csv(self, responses: Responses) -> List[Dict[str, str]]:
         """Parse the CSV and return the rows and parsed items ."""
         impact_levels = self._parameter("impact")
         violation_types = self._parameter("violation_type")
-        rows, parsed_rows = [], []
+        rows = []
         for response in responses:
             csv_text = response.text.strip()
-            rows.extend(csv_text.split("\n")[1:])
-            parsed_rows.extend(list(csv.DictReader(StringIO(csv_text))))
-        return [
-            (row, parsed_row) for (row, parsed_row) in zip(rows, parsed_rows)
-            if parsed_row["Impact"] in impact_levels and parsed_row["Violation Type"] in violation_types]
+            rows.extend(list(csv.DictReader(StringIO(csv_text, newline=""))))
+        return [row for row in rows if row["Impact"] in impact_levels and row["Violation Type"] in violation_types]
+
+    @staticmethod
+    def hash_entity(entity: Entity) -> str:
+        """Return a hash of the entity."""
+        return hashlib.md5(",".join([str(value) for value in entity.values()]).encode('utf-8')).hexdigest()  # nosec
