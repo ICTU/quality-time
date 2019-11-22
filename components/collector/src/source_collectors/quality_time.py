c524a94f806a900ff4137917d44c3ee9e71b2249
Frank Niessink <frank@niessink.com>
2019-11-22 17:03:49 +0100

    Fix quality issues.

--- components/collector/src/source_collectors/quality_time.py
+++ components/collector/src/source_collectors/quality_time.py
@@ -1,10 +1,10 @@
 """Collector for Quality-time."""
 
 from functools import lru_cache
-from typing import Dict, Optional
+from typing import Any, Dict, List, Optional, Tuple
 from urllib import parse
 
-from collector_utilities.type import Entities, Response, Responses, URL, Value
+from collector_utilities.type import Entity, Entities, Measurement, Response, Responses, URL, Value
 from .source_collector import SourceCollector
 
 
@@ -13,7 +13,7 @@ class QualityTimeMetrics(SourceCollector):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.__counted_metrics: Optional[Dict[str, Dict]] = None
+        self.__entities: Optional[Entities] = None
 
     def _api_url(self) -> URL:
         parts = parse.urlsplit(super()._api_url())
@@ -24,83 +24,90 @@ class QualityTimeMetrics(SourceCollector):
         # First, get the report(s):
         responses = super()._get_source_responses(URL(f"{api_url}/reports"))
         # Then, add the measurements for each of the applicable metrics:
-        for metric_uuid in self.__get_metrics(responses[0]):
-            responses.extend(super()._get_source_responses(URL(f"{api_url}/measurements/{metric_uuid}")))
+        for _, entity in self.__get_metrics_and_entities(responses[0]):
+            responses.extend(super()._get_source_responses(URL(f"{api_url}/measurements/{entity['key']}")))
         return responses
 
     def _parse_source_responses_value(self, responses: Responses) -> Value:
         return str(len(self._parse_source_responses_entities(responses)))
 
     def _parse_source_responses_total(self, responses: Responses) -> Value:
-        return str(len(self.__get_metrics(responses[0])))
+        return str(len(self.__get_metrics_and_entities(responses[0])))
 
     def _parse_source_responses_entities(self, responses: Responses) -> Entities:
-        if self.__counted_metrics is None:  # Can't use lru_cache because responses is a list. Cache by hand instead.
-            self.__counted_metrics = self.__count_metrics(responses)
+        if self.__entities is None:  # Can't use lru_cache because responses is a list. Cache by hand instead.
+            self.__entities = self.__get_entities(responses)
+        return self.__entities
+
+    def __get_entities(self, responses: Responses) -> Entities:
+        """Get the metric entities from the responses."""
+        last_measurements = self.__get_last_measurements(responses)
+        status_to_count = self._parameter("status")
         landing_url = self._landing_url(responses)
         entities: Entities = []
-        for metric_uuid, metric in self.__counted_metrics.items():
-            metric_name = metric.get("name") or self._datamodel["metrics"][metric["type"]]["name"]
-            report_url = f"{landing_url}/{metric['report_uuid']}"
-            subject_url = f"{report_url}#{metric['subject_uuid']}"
-            metric_url = f"{report_url}#{metric_uuid}"
-            entities.append(
-                dict(
-                    key=metric_uuid, report=metric["report_title"], subject=metric["subject_name"], metric=metric_name,
-                    report_url=report_url, subject_url=subject_url, metric_url=metric_url, status=metric["status"],
-                    target=metric["target"], measurement=metric["measurement"]))
-        return entities
-
-    def __count_metrics(self, responses: Responses) -> Dict[str, Dict]:
-        """Count the metrics from the responses."""
-        measurements_by_metric_uuid = dict()
-        for response in responses[1:]:
-            if measurements := response.json()["measurements"]:
-                last = measurements[-1]
-                measurements_by_metric_uuid[last["metric_uuid"]] = last
-        status_to_count = self._parameter("status")
-        metrics = dict()
-        for metric_uuid, metric in self.__get_metrics(responses[0]).items():
-            scale = metric.get("scale", "count")
-            scale_data = measurements_by_metric_uuid.get(metric_uuid, {}).get(scale, {})
-            status = scale_data.get("status")
-            if status in status_to_count or (status is None and "unknown" in status_to_count):
-                metric["status"] = status
+        for metric, entity in self.__get_metrics_and_entities(responses[0]):
+            status, value = self.__get_status_and_value(metric, last_measurements.get(entity["key"], {}))
+            if status in status_to_count:
+                entity["report_url"] = report_url = f"{landing_url}/{metric['report_uuid']}"
+                entity["subject_url"] = f"{report_url}#{metric['subject_uuid']}"
+                entity["metric_url"] = f"{report_url}#{entity['key']}"
+                entity["metric"] = str(metric.get("name") or self._datamodel["metrics"][metric["type"]]["name"])
+                entity["status"] = status
                 unit = metric.get("unit") or self._datamodel["metrics"][metric["type"]]["unit"]
-                metric["measurement"] = f"{scale_data.get('value') or '?'} {unit}"
-                direction = metric.get("direction") or self._datamodel["metrics"][metric["type"]]["direction"]
+                entity["measurement"] = f"{value or '?'} {unit}"
+                direction = str(metric.get("direction") or self._datamodel["metrics"][metric["type"]]["direction"])
                 direction = {"<": "≦", ">": "≧"}.get(direction, direction)
                 target = metric.get("target") or self._datamodel["metrics"][metric["type"]]["target"]
-                metric["target"] = f"{direction} {target} {unit}"
-                metrics[metric_uuid] = metric
-        return metrics
+                entity["target"] = f"{direction} {target} {unit}"
+                entities.append(entity)
+        return entities
+
+    @staticmethod
+    def __get_last_measurements(responses: Responses) -> Dict[str, Measurement]:
+        """Return the last measurements by metric UUID for easy lookup."""
+        last_measurements = dict()
+        for response in responses[1:]:
+            if measurements := response.json()["measurements"]:
+                last_measurement = measurements[-1]
+                last_measurements[last_measurement["metric_uuid"]] = last_measurement
+        return last_measurements
+
+    @staticmethod
+    def __get_status_and_value(metric, measurement: Measurement) -> Tuple[str, Value]:
+        """Return the measurement value and status."""
+        scale = metric.get("scale", "count")
+        scale_data = measurement.get(scale, {})
+        return scale_data.get("status") or "unknown", scale_data.get("value")
 
     @lru_cache(maxsize=4)
-    def __get_metrics(self, response: Response) -> Dict[str, Dict]:
+    def __get_metrics_and_entities(self, response: Response) -> List[Tuple[Dict[str, Dict], Entity]]:
         """Get the relevant metrics from the reports response."""
-
-        def metric_is_to_be_measured(metric, metric_types, source_types, tags) -> bool:
-            """Return whether the metric has been selected by the user by means of metric type, source type or tag."""
-            if tags and (tags & set(metric.get("tags", {}))) == set():
-                return False
-            if metric_types and metric["type"] not in metric_types:
-                return False
-            metric_source_types = {source["type"] for source in metric.get("sources", {}).values()}
-            return not (source_types and (source_types & metric_source_types) == set())
-
-        report_titles_or_ids = set(self._parameter("reports"))
         tags = set(self._parameter("tags"))
         metric_types = self._parameter("metric_type")
         source_types = set(self._parameter("source_type"))
-        metrics = dict()
-        for report in response.json()["reports"]:
-            if report_titles_or_ids and (report_titles_or_ids & {report["title"], report["report_uuid"]} == set()):
-                continue
+        metrics_and_entities = []
+        for report in self.__get_reports(response):
             for subject_uuid, subject in report.get("subjects", {}).items():
                 for metric_uuid, metric in subject.get("metrics", {}).items():
-                    if metric_is_to_be_measured(metric, metric_types, source_types, tags):
-                        metric["report_title"] = report["title"]
-                        metric["subject_name"] = subject["name"]
+                    if self.__metric_is_to_be_measured(metric, metric_types, source_types, tags):
                         metric["subject_uuid"] = subject_uuid
-                        metrics[metric_uuid] = metric
-        return metrics
+                        entity = dict(key=metric_uuid, report=report["title"], subject=subject["name"])
+                        metrics_and_entities.append((metric, entity))
+        return metrics_and_entities
+
+    def __get_reports(self, response) -> List[Dict[str, Any]]:
+        """Get the relevant reports from the reports response."""
+        report_titles_or_ids = set(self._parameter("reports"))
+        reports = list(response.json()["reports"])
+        return [report for report in reports if (report_titles_or_ids & {report["title"], report["report_uuid"]})] \
+            if report_titles_or_ids else reports
+
+    @staticmethod
+    def __metric_is_to_be_measured(metric, metric_types, source_types, tags) -> bool:
+        """Return whether the metric has been selected by the user by means of metric type, source type or tag."""
+        if tags and (tags & set(metric.get("tags", {}))) == set():
+            return False
+        if metric_types and metric["type"] not in metric_types:
+            return False
+        metric_source_types = {source["type"] for source in metric.get("sources", {}).values()}
+        return not (source_types and (source_types & metric_source_types) == set())
