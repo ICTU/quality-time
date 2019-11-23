c812c51955d0fc675c070731a545a14461da305a
Frank Niessink <frank@niessink.com>
2019-11-23 22:50:27 +0100

    Unittest folders (#809)
    * Move unit tests immediately below the tests folder.
    
    * Consistent names for test folders.

--- components/collector/.vulture_white_list.py
+++ components/collector/.vulture_white_list.py
@@ -1,4 +1,4 @@
 _.raw  # unused attribute (src/source_collectors/source_collector.py:169)
 _.status_code  # unused attribute (src/source_collectors/source_collector.py:170)
 _.status_code  # unused attribute (src/source_collectors/source_collector.py:181)
-SourceMetric  # unused class (tests/unittests/metric_collectors_tests/test_metrics_collector.py:20)
+SourceMetric  # unused class (tests/metric_collectors/test_metrics_collector.py:20)
