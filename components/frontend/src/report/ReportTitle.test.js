84e4ae26197f7202f5dab18ff3da98a1ad5bbbed
Frank Niessink <frank@niessink.com>
2019-11-30 14:32:51 +0100

    Increase test coverage.

---
+++ components/frontend/src/report/ReportTitle.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReportTitle } from './ReportTitle';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ReportTitle
+    report={{title: "Report"}}
+  />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
