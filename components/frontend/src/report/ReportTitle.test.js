d08d20f0369135ab91e51c88b2cfd31df1793d84
Frank Niessink <frank@niessink.com>
2019-11-30 16:32:39 +0100

    Increase test coverage.

--- components/frontend/src/report/ReportTitle.test.js
+++ components/frontend/src/report/ReportTitle.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Header } from 'semantic-ui-react';
+import { mount } from 'enzyme';
 import { ReportTitle } from './ReportTitle';
 
 it('renders without crashing', () => {
@@ -8,4 +10,17 @@ it('renders without crashing', () => {
     report={{title: "Report"}}
   />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+const report = {
+    report_uuid: "report_uuid"
+};
+
+describe("<ReportTitle />", () => {
+    it('shows a loading indicator on download button click', () => {
+        const wrapper = mount(<ReportTitle report={report} />);
+        wrapper.find(Header).simulate("click");  // Expand title
+        wrapper.find("button.left").simulate("click");
+        expect(wrapper.find("button.left").hasClass("loading")).to.equal(true);
+    });
 });
