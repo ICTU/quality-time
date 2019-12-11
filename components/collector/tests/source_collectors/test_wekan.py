95dc10ba8ef993ab405d250005df8a7e4649df6f
Frank Niessink <frank@niessink.com>
2019-12-11 22:23:11 +0100

    Remove duplication.

--- components/collector/tests/source_collectors/test_wekan.py
+++ components/collector/tests/source_collectors/test_wekan.py
@@ -1,6 +1,7 @@
 """Unit tests for the Wekan metric source."""
 
 from datetime import datetime
+from typing import Dict
 
 from .source_collector_test_case import SourceCollectorTestCase
 
@@ -20,18 +21,35 @@ class WekanTestCase(SourceCollectorTestCase):
             dict(_id="user_id"),
             [dict(_id="board1", title="Board 1")],
             dict(slug="board-slug"),
-            [dict(_id="list1", title="List 1", archived=False), dict(_id="list2", title="List 2", archived=False),
-             dict(_id="list3", archived=True)],
-            [dict(_id="card1", title="Card 1"), dict(_id="card2", title="Card 2")],
-            dict(_id="card1", title="Card 1", archived=False, boardId="board1", dateLastActivity="2019-01-01"),
-            dict(_id="card2", title="Card 2", archived=False, boardId="board1", dateLastActivity="2019-01-01"),
-            [dict(_id="card3", title="Card 3")],
-            dict(_id="card3", title="Card 3", archived=False, boardId="board1", dateLastActivity="2019-01-01")]
-        self.entities = [
-            dict(key="card1", url="https://wekan/b/board1/board-slug/card1", title="Card 1", list="List 1",
-                 due_date="", date_last_activity="2019-01-01"),
-            dict(key="card3", url="https://wekan/b/board1/board-slug/card3", title="Card 3", list="List 2",
-                 due_date="", date_last_activity="2019-01-01")]
+            [self.list(1), self.list(2), self.list(3, archived=True)],
+            [self.card(1), self.card(2)],
+            self.full_card(1),
+            self.full_card(2),
+            [self.card(3)],
+            self.full_card(3)]
+        self.entities = [self.entity(1, 1), self.entity(3, 2)]
+
+    @staticmethod
+    def card(index: int) -> Dict:
+        """Create a card."""
+        return dict(_id=f"card{index}", title=f"Card {index}")
+
+    @classmethod
+    def full_card(cls, index: int) -> Dict:
+        """Create a full card."""
+        return dict(archived=False, boardId="board1", dateLastActivity="2019-01-01", **cls.card(index))
+
+    @staticmethod
+    def list(index: int, archived: bool = False) -> Dict:
+        """Create a list."""
+        return dict(_id=f"list{index}", title=f"List {index}", archived=archived)
+
+    @staticmethod
+    def entity(card_index: int, list_index: int) -> Dict:
+        """Return an entity"""
+        return dict(
+            key=f"card{card_index}", url=f"https://wekan/b/board1/board-slug/card{card_index}",
+            title=f"Card {card_index}", list=f"List {list_index}", due_date="", date_last_activity="2019-01-01")
 
 
 class WekanIssuesTest(WekanTestCase):
