8e1a00379e29175d99c599e9733c5ee7ab1e5e20
Frank Niessink <frank@niessink.com>
2019-12-11 19:39:25 +0100

    Remove duplication. (#862)

--- components/collector/tests/source_collectors/test_trello.py
+++ components/collector/tests/source_collectors/test_trello.py
@@ -20,6 +20,21 @@ class TrelloTestCase(SourceCollectorTestCase):
                     token="4533dea",
                     inactive_days="30",
                     lists_to_ignore=[])))
+        self.cards = dict(
+            id="board1", url="https://trello/board1", dateLastActivity="2019-02-10",
+            cards=[
+                dict(
+                    id="card1", name="Card 1", idList="list1", due=None, dateLastActivity="2019-03-03",
+                    url="https://trello/card1"),
+                dict(
+                    id="card2", name="Card 2", idList="list2", due="2019-01-01", dateLastActivity="2019-01-01",
+                    url="https://trello/card2")],
+            lists=[dict(id="list1", name="List 1"), dict(id="list2", name="List 2")])
+        self.entities = [
+            dict(key="card1", url="https://trello/card1", title="Card 1", list="List 1", due_date=None,
+                 date_last_activity="2019-03-03"),
+            dict(key="card2", url="https://trello/card2", title="Card 2", list="List 2",
+                 due_date="2019-01-01", date_last_activity="2019-01-01")]
 
 
 class TrelloIssuesTest(TrelloTestCase):
@@ -28,82 +43,31 @@ class TrelloIssuesTest(TrelloTestCase):
     def setUp(self):
         super().setUp()
         self.metric = dict(type="issues", addition="sum", sources=self.sources)
+        self.json = [[dict(id="board1", name="Board1")], self.cards, self.cards]
 
     def test_issues(self):
         """Test that the number of issues and the individual issues are returned."""
-        cards = dict(
-            id="board1", url="https://trello/board1",
-            cards=[
-                dict(
-                    id="card1", name="Card 1", idList="list1", due=None, dateLastActivity="2019-01-01",
-                    url="https://trello/card1")],
-            lists=[dict(id="list1", name="List 1")])
-        json = [[dict(id="board1", name="Board1")], cards, cards]
-        response = self.collect(self.metric, get_request_json_side_effect=json)
-        expected_entities = [
-            dict(key="card1", url="https://trello/card1", title="Card 1", list="List 1", due_date=None,
-                 date_last_activity="2019-01-01")]
-        self.assert_measurement(response, value="1", entities=expected_entities)
+        response = self.collect(self.metric, get_request_json_side_effect=self.json)
+        self.assert_measurement(response, value="2", entities=self.entities)
 
     def test_issues_with_ignored_list(self):
         """Test that lists can be ignored when counting issues."""
         self.metric["sources"]["source_id"]["parameters"]["lists_to_ignore"] = ["list1"]
-        cards = dict(
-            id="board1", url="https://trello/board1",
-            cards=[
-                dict(
-                    id="card1", name="Card 1", idList="list1", due=None, dateLastActivity="2019-01-01",
-                    url="https://trello/card1"),
-                dict(
-                    id="card2", name="Card 2", idList="list2", due=None, dateLastActivity="2019-01-01",
-                    url="https://trello/card2")],
-            lists=[dict(id="list1", name="List 1"), dict(id="list2", name="List 2")])
-        json = [[dict(id="board1", name="Board1")], cards, cards]
-        response = self.collect(self.metric, get_request_json_side_effect=json)
-        expected_entities = [
-            dict(key="card2", url="https://trello/card2", title="Card 2", list="List 2",
-                 due_date=None, date_last_activity="2019-01-01")]
-        self.assert_measurement(response, value="1", entities=expected_entities)
+        response = self.collect(self.metric, get_request_json_side_effect=self.json)
+        self.assert_measurement(response, value="1", entities=[self.entities[1]])
 
     def test_overdue_issues(self):
         """Test overdue issues."""
         self.metric["sources"]["source_id"]["parameters"]["cards_to_count"] = ["overdue"]
-        cards = dict(
-            id="board1", url="https://trello/board1",
-            cards=[
-                dict(
-                    id="card1", name="Card 1", idList="list1", due=None, dateLastActivity="2019-01-01",
-                    url="https://trello/card1"),
-                dict(
-                    id="card2", name="Card 2", idList="list1", due="2019-01-01", dateLastActivity="2019-01-01",
-                    url="https://trello/card2")],
-            lists=[dict(id="list1", name="List 1")])
-        json = [[dict(id="board1", name="Board1")], cards, cards]
-        response = self.collect(self.metric, get_request_json_side_effect=json)
-        expected_entities = [
-            dict(key="card2", url="https://trello/card2", title="Card 2", list="List 1",
-                 due_date="2019-01-01", date_last_activity="2019-01-01")]
-        self.assert_measurement(response, value="1", entities=expected_entities)
+        response = self.collect(self.metric, get_request_json_side_effect=self.json)
+        self.assert_measurement(response, value="1", entities=[self.entities[1]])
 
     def test_inactive_issues(self):
         """Test inactive issues."""
         self.metric["sources"]["source_id"]["parameters"]["cards_to_count"] = ["inactive"]
-        cards = dict(
-            id="board1", url="https://trello/board1",
-            cards=[
-                dict(
-                    id="card1", name="Card 1", idList="list1", due=None,
-                    dateLastActivity=datetime.now().isoformat(), url="https://trello/card1"),
-                dict(
-                    id="card2", name="Card 2", idList="list1", due=None, dateLastActivity="2019-01-01",
-                    url="https://trello/card2")],
-            lists=[dict(id="list1", name="List 1")])
-        json = [[dict(id="board1", name="Board1")], cards, cards]
-        response = self.collect(self.metric, get_request_json_side_effect=json)
-        expected_entities = [
-            dict(key="card2", url="https://trello/card2", title="Card 2", list="List 1", due_date=None,
-                 date_last_activity="2019-01-01")]
-        self.assert_measurement(response, value="1", entities=expected_entities)
+        self.cards["cards"][0]["dateLastActivity"] = datetime.now().isoformat()
+        response = self.collect(self.metric, get_request_json_side_effect=self.json)
+        self.assert_measurement(response, value="1", entities=[self.entities[1]])
 
 
 class TrelloSourceUpToDatenessTest(TrelloTestCase):
@@ -112,13 +76,6 @@ class TrelloSourceUpToDatenessTest(TrelloTestCase):
     def setUp(self):
         super().setUp()
         self.metric = dict(type="source_up_to_dateness", addition="max", sources=self.sources)
-        self.cards = dict(
-            id="board1", url="https://trello/board1",
-            dateLastActivity="2019-02-10",
-            cards=[
-                dict(id="card1", name="Card 1", idList="list1", dateLastActivity="2019-03-03"),
-                dict(id="card2", name="Card 2", idList="list2", dateLastActivity="2019-01-01")],
-            lists=[dict(id="list1", name="List 1"), dict(id="list2", name="List 2")])
         self.side_effect = [[dict(id="board1", name="Board1")], self.cards, self.cards]
 
     def test_age(self):
