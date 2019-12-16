775a2af1e31ed0a298c4fa81a6527a3bda271a7f
Frank Niessink <frank@niessink.com>
2019-12-16 14:25:39 +0100

    Remove duplication.

--- components/frontend/src/dashboard/CardDashboard.test.js
+++ components/frontend/src/dashboard/CardDashboard.test.js
@@ -9,29 +9,27 @@ describe("<CardDashboard />", () => {
         const wrapper = mount(<CardDashboard cards={[]} />);
         expect(wrapper.instance()).toBe(null);
     });
-    it('calls the callback on drag', () => {
-        const mockCallBack = jest.fn();
-        const wrapper = mount(
+});
+
+describe("<CardDashboard />", () => {
+    var mockCallBack, wrapper;
+    beforeEach(() => {
+        mockCallBack = jest.fn();
+        wrapper = mount(
             <ReadOnlyContext.Provider value={false}>
                 <CardDashboard
                     cards={[<MetricSummaryCard red={1} green={2} yellow={1} white={0} grey={0} />]}
                     initial_layout={[]}
                     save_layout={mockCallBack}
                 />
-            </ReadOnlyContext.Provider>);
+            </ReadOnlyContext.Provider>
+        );
+    });
+    it('calls the callback on drag', () => {
         wrapper.find("div.react-draggable").at(0).simulate("drag");
         expect(mockCallBack).toHaveBeenCalled();
     });
-    it('calls the callback on drag', () => {
-        const mockCallBack = jest.fn();
-        const wrapper = mount(
-            <ReadOnlyContext.Provider value={false}>
-                <CardDashboard
-                    cards={[<MetricSummaryCard red={1} green={2} yellow={1} white={0} grey={0} />]}
-                    initial_layout={[]}
-                    save_layout={mockCallBack}
-                />
-            </ReadOnlyContext.Provider>);
+    it('calls the callback on click', () => {
         wrapper.find("div.react-draggable").at(0).simulate("click");
         expect(mockCallBack).toHaveBeenCalled();
     });
