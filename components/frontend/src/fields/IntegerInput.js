c32be0444a25f3672d99ab72bec8051456b60f23
Frank Niessink <frank@niessink.com>
2019-12-19 17:44:37 +0100

    Prevent users from entering invalid percentages. Fixes #888.

--- components/frontend/src/fields/IntegerInput.js
+++ components/frontend/src/fields/IntegerInput.js
@@ -6,19 +6,40 @@ export function IntegerInput(props) {
     let { prefix, set_value, unit, ...otherProps } = props;
     const [value, setValue] = useState(props.value || 0)
     useEffect(() => setValue(props.value || 0), [props.value]);
+
+    function is_valid(a_value) {
+        if (props.min !== null && props.max !== null) {
+            return (Number(props.min) <= Number(a_value)) && (Number(a_value) <= Number(props.max))
+        }
+        if (props.min !== null) {
+            return Number(props.min) <= Number(a_value)
+        }
+        if (props.max !== null) {
+            return Number(a_value) <= Number(props.max)
+        }
+        return true
+    }
+
+    function submit_if_changed_and_valid() {
+        if (value !== (props.value || 0) && is_valid(value)) {
+            props.set_value(value)
+        }
+    }
+
     return (
-        <Form onSubmit={() => { if (value !== (props.value || 0)) { props.set_value(value) } }}>
+        <Form onSubmit={() => { submit_if_changed_and_valid() } }>
             <Form.Group style={{ marginBottom: '0px' }}>
                 <ReadOnlyContext.Consumer>{(readOnly) => (
                     <Form.Input
                         {...otherProps}
+                        error={!is_valid(value)}
                         fluid
                         focus
                         labelPosition="right"
-                        onBlur={() => { if (value !== (props.value || 0)) { props.set_value(value) } }}
-                        onChange={(event) => setValue(event.target.value || 0)}
+                        onBlur={() => { submit_if_changed_and_valid() }}
+                        onChange={(event) => { if (is_valid(event.target.value)) { setValue(event.target.value) } }}
                         onKeyDown={(event) => { if (event.key === "Escape") { setValue(props.value || 0) } }}
-                        onSubmit={() => { if (value !== (props.value || 0)) { props.set_value(value) } }}
+                        onSubmit={() => { submit_if_changed_and_valid() }}
                         readOnly={readOnly}
                         type="number"
                         value={value}
