2f49ebad48ef2b700b526565a50a1c4c2965bb79
Frank Niessink <frank@niessink.com>
2019-12-21 17:10:30 +0100

    Increase test coverage.

--- components/frontend/src/fields/IntegerInput.test.js
+++ components/frontend/src/fields/IntegerInput.test.js
@@ -16,5 +16,61 @@ describe("<IntegerInput />", () => {
         </ReadOnlyContext.Provider>);
       expect(wrapper.find("FormInput").prop("value")).toStrictEqual("42");
       expect(wrapper.find("FormInput").prop("readOnly")).toBe(false);
-  })
+  });
+  it('renders values less than the minimum as error', () => {
+      const wrapper = mount(
+        <ReadOnlyContext.Provider value={false}>
+          <IntegerInput value="-42" min="0" />
+        </ReadOnlyContext.Provider>);
+      expect(wrapper.find("FormInput").prop("value")).toStrictEqual("-42");
+      expect(wrapper.find("FormInput").prop("error")).toBe(true);
+  });
+  it('renders values less than the minimum as error', () => {
+      const wrapper = mount(
+        <ReadOnlyContext.Provider value={false}>
+          <IntegerInput value="42" max="10" />
+        </ReadOnlyContext.Provider>);
+      expect(wrapper.find("FormInput").prop("value")).toStrictEqual("42");
+      expect(wrapper.find("FormInput").prop("error")).toBe(true);
+  });
+  it('submits the changed value', () => {
+      let mock_set_value = jest.fn();
+      const wrapper = mount(
+        <ReadOnlyContext.Provider value={false}>
+          <IntegerInput value="42" set_value={mock_set_value} />
+        </ReadOnlyContext.Provider>);
+      wrapper.find("input").simulate("change", {target: {value: "10"}});
+      wrapper.find("FormInput").simulate("submit");
+      expect(mock_set_value).toHaveBeenCalled();
+  });
+  it('submits the changed value on blur', () => {
+    let mock_set_value = jest.fn();
+    const wrapper = mount(
+      <ReadOnlyContext.Provider value={false}>
+        <IntegerInput value="0" set_value={mock_set_value} />
+      </ReadOnlyContext.Provider>);
+    wrapper.find("input").simulate("change", {target: {value: "42"}});
+    wrapper.find("input").simulate("blur");
+    expect(mock_set_value).toHaveBeenCalled();
+});
+  it('does not submit an unchanged value', () => {
+      let mock_set_value = jest.fn();
+      const wrapper = mount(
+        <ReadOnlyContext.Provider value={false}>
+          <IntegerInput value="42" set_value={mock_set_value} />
+        </ReadOnlyContext.Provider>);
+      wrapper.find("input").simulate("change", {target: {value: "42"}});
+      wrapper.find("FormInput").simulate("submit");
+      expect(mock_set_value).toHaveBeenCalledTimes(0);
+  });
+  it('does not submit an invalid value', () => {
+    let mock_set_value = jest.fn();
+    const wrapper = mount(
+      <ReadOnlyContext.Provider value={false}>
+        <IntegerInput value="0" min="0" set_value={mock_set_value} />
+      </ReadOnlyContext.Provider>);
+    wrapper.find("input").simulate("change", {target: {value: "-42"}});
+    wrapper.find("FormInput").simulate("submit");
+    expect(mock_set_value).toHaveBeenCalledTimes(0);
+  });
 });
