1296d8ff34312d13fd328b11387e90b010459276
Frank Niessink <frank@niessink.com>
2019-12-23 16:44:10 +0100

    Fix bug and refactor.

--- components/frontend/src/fields/Input.js
+++ components/frontend/src/fields/Input.js
@@ -6,18 +6,23 @@ export function Input(props) {
   let { prefix, required, set_value, ...otherProps } = props;
   const initialValue = props.value || "";
   const [value, setValue] = useState(initialValue);
-  useEffect(() => setValue(initialValue), [props.value]);
+  useEffect(() => setValue(initialValue), [initialValue]);
+
+  function submit_if_changed() {
+    if (value !== initialValue) { set_value(value) }
+  }
+
   const fixedProps = {...otherProps, error: required && value === "", fluid: true, focus: true, labelPosition: "left", value: value}
   return (
     <Form>
       <ReadOnlyContext.Consumer>{(readOnly) => (
         <Form.Input
           {...fixedProps}
-          onBlur={() => { if (value !== initialValue) { set_value(value) } }}
+          onBlur={() => { submit_if_changed() }}
           onChange={(event) => setValue(event.target.value)}
           onKeyDown={(event) => {
             if (event.key === "Escape") { setValue(initialValue) }
-            if (event.key === "Enter" && value !== initialValue) { set_value(value) }
+            if (event.key === "Enter") { submit_if_changed() }
           }}
           readOnly={readOnly}
         >
