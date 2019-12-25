6ef9fcc08e26f28af57fc515ed4584708b6adb7f
Frank Niessink <frank@niessink.com>
2019-12-25 21:03:50 +0100

    Fix unit test.

--- components/server/tests/initialization/test_report.py
+++ components/server/tests/initialization/test_report.py
@@ -13,14 +13,6 @@ class ReportInitTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.database = Mock()
-
-    def import_report(self, report_json: str) -> None:
-        """Import the report."""
-        with patch.object(pathlib.Path, "open", mock_open(read_data=report_json)):
-            import_report(self.database, pathlib.Path("filename"))
-
-    def test_import(self):
-        """Test that a report can be imported."""
         self.database.reports.find_one.return_value = None
         self.database.datamodels.find_one.return_value = dict(
             _id="id",
@@ -32,27 +24,31 @@ class ReportInitTest(unittest.TestCase):
                     parameters=dict(
                         p1=dict(default_value="p1", metrics=["metric_type"]),
                         p2=dict(default_value="p2", metrics=["metric_type"])))))
-        report_json = json.dumps(
-            dict(
-                report_uuid="id",
-                subjects=[dict(name="name", type="subject_type", metrics=[
+        self.report_json = json.dumps(
+            dict(report_uuid="id", subjects=[
+                dict(name="name", type="subject_type", metrics=[
                     dict(type="metric_type", sources=[
-                        dict(type="source_type", parameters=dict(p1=dict()))
-                    ])
-                ])]))
-        self.import_report(report_json)
+                        dict(type="source_type", parameters=dict(p1=dict()))])])]))
+
+    def import_report(self, report_json: str) -> None:
+        """Import the report."""
+        with patch.object(pathlib.Path, "open", mock_open(read_data=report_json)):
+            import_report(self.database, pathlib.Path("filename"))
+
+    def test_import(self):
+        """Test that a report can be imported."""
+        self.import_report(self.report_json)
         self.database.reports.insert.assert_called_once()
 
     def test_import_is_skipped(self):
         """Test that a report isn't imported when it's already in the database."""
         self.database.reports.find_one.return_value = True
-        self.import_report('{"report_uuid": "id", "subjects": []}')
+        self.import_report(self.report_json)
         self.database.reports.insert.assert_not_called()
 
     def test_import_example_report(self):
         """Test that the example reports are imported."""
-        mock_glob = Mock(return_value=["filename"])
-        with patch("glob.glob", mock_glob):
-            with patch("builtins.open", mock_open(read_data='{"report_uuid": "id", "subjects": []}')):
+        with patch.object(pathlib.Path, "glob", Mock(return_value=[pathlib.Path("example-report.json")])):
+            with patch.object(pathlib.Path, "open", mock_open(read_data=self.report_json)):
                 import_example_reports(self.database)
-        self.database.reports.insert.assert_not_called()
+        self.database.reports.insert.assert_called_once()
