981bb22c2f073052c89717c783c085554dd9b293
Frank Niessink <frank@niessink.com>
2020-01-14 12:17:03 +0100

    Metrics with status unknown in the details view of the 'Metrics' metric did not show a question mark. Fixes #935.

--- components/frontend/src/metric/StatusIcon.test.js
+++ components/frontend/src/metric/StatusIcon.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import { StatusIcon } from './StatusIcon';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<StatusIcon status="target_met" />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<StatusIcon/>' , () => {
+  it('renders a checkmark if the status is target_met', () => {
+    const wrapper = mount(<StatusIcon status="target_met" />);
+    expect(wrapper.find("Icon").prop("name")).toBe("check");
+  })
+  it('renders a question mark if the status is missing', () => {
+    const wrapper = mount(<StatusIcon/>);
+    expect(wrapper.find("Icon").prop("name")).toBe("question");
+  })
+})
