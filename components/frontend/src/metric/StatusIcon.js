3e184032473a2c481a466ec227689e08e1fe783e
Frank Niessink <frank@niessink.com>
2020-01-15 16:50:33 +0100

    Subject cards in the report dashboard would not have a default subject title when the subject had no title. Fixes #942.

--- components/frontend/src/metric/StatusIcon.js
+++ components/frontend/src/metric/StatusIcon.js
@@ -3,10 +3,7 @@ import { Icon } from 'semantic-ui-react';
 
 export function StatusIcon({ status }) {
   const status_icon = {
-    target_met: 'check', near_target_met: 'warning', debt_target_met: 'money',
-    target_not_met: 'x', missing: 'question'
-  }[status || 'missing'];
-  return (
-    <Icon size='large' name={status_icon} />
-  )
+    target_met: 'check', near_target_met: 'warning', debt_target_met: 'money', target_not_met: 'x',
+    unknown: 'question'};
+  return <Icon size='large' name={status_icon[status || 'unknown']} />
 }
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 <!-- The line "## <square-bracket>Unreleased</square-bracket>" is replaced by the ci/release.py script with the new release version and release date. -->
 
+## [Unreleased]
+
+### Fixed
+
+- Metrics with status unknown in the details view of the 'Metrics' metric did not show a question mark. Fixes [#935](https://github.com/ICTU/quality-time/issues/935).
+
 ## [1.3.3] - [2020-01-15]
 
 ### Fixed
