a10ec252c246de6b3ac363cc6d032139950d4109
Frank Niessink <frank@niessink.com>
2020-01-24 15:11:05 +0100

    Increase test coverage.

--- components/frontend/src/api/fetch_server_api.test.js
+++ components/frontend/src/api/fetch_server_api.test.js
@@ -1,23 +1,21 @@
-import * as fetch_server_api from './fetch_server_api';
-
-jest.mock("./fetch_server_api.js");
+import { fetch_server_api } from './fetch_server_api';
 
 it('fetches the api', async () => {
-    fetch_server_api.fetch_server_api.mockImplementation(() => Promise.resolve({ }));
-    fetch_server_api.fetch_server_api('get', 'api').then((response) => expect(response).toBe({})).catch(()=>{});
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ }));
+    fetch_server_api('get', 'api').then((response) => expect(response).toBe({})).catch(()=>{});
 });
 
 it('posts to the api', async () => {
-    fetch_server_api.fetch_server_api.mockImplementation(() => Promise.resolve({ }));
-    fetch_server_api.fetch_server_api('post', 'api', {body: "body"}).then((response) => expect(response).toBe({})).catch(()=>{});
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ }));
+    fetch_server_api('post', 'api', {body: "body"}).then((response) => expect(response).toBe({})).catch(()=>{});
 });
 
 it('gets the json from the response', async () => {
-    fetch_server_api.fetch_server_api.mockImplementation(() => Promise.resolve({ ok: true, json: () => ({json: true})}));
-    fetch_server_api.fetch_server_api('get', 'api').then((response) => expect(response).toBe({json: true})).catch(()=>{});
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ ok: true, json: () => ({json: true}) }));
+    fetch_server_api('get', 'api').then((response) => expect(response).toBe({json: true})).catch(()=>{});
 });
 
 it('gets the blob from the response', async () => {
-    fetch_server_api.fetch_server_api.mockImplementation(() => Promise.resolve({ ok: true, blob: () => ({blob: true})}));
-    fetch_server_api.fetch_server_api('get', 'api', {}, 'application/blob').then((response) => expect(response).toBe({blob: true})).catch(()=>{});
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ ok: true, blob: () => ({blob: true}) }));
+    fetch_server_api('get', 'api', {}, 'application/blob').then((response) => expect(response).toBe({blob: true})).catch(()=>{});
 });
