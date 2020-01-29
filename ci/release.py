49de75b2109103e158d5f4641ca0b217c8cfacd9
Frank Niessink <frank@niessink.com>
2020-01-29 22:25:02 +0100

    Go to rc in one step.

--- ci/release.py
+++ ci/release.py
@@ -11,10 +11,21 @@ import sys
 import git
 
 
+def get_current_version() -> str:
+    """Return the current version."""
+    output = subprocess.check_output(("bump2version", "--list", "--dry-run", "--allow-dirty", "part"), text=True)
+    return [line for line in output.split("\n") if line.startswith("current_version")][0].split("=")[1]
+
+
+def get_next_version(current_version: str, bump: str) -> str:
+    """Return the next version."""
+    output = subprocess.check_output(("bump2version", "--list", "--dry-run", "--allow-dirty", "--current-version", current_version, bump), text=True)
+    return [line for line in output.split("\n") if line.startswith("new_version")][0].split("=")[1]
+
+
 def parse_arguments():
     """Return the command line arguments."""
-    output = subprocess.check_output(("bump2version", "--list", "--dry-run", "--allow-dirty", "part"), text=True)
-    current_version = [line for line in output.split("\n") if line.startswith("current_version")][0].split("=")[1]
+    current_version = get_current_version()
     parser = argparse.ArgumentParser(description=f"Release Quality-time. Current version is {current_version}.")
     rc_choices = ("rc",) if "rc" in current_version else ("rc-patch", "rc-minor", "rc-major")
     parser.add_argument("bump", choices=rc_choices + ("patch", "minor", "major"))
@@ -23,16 +34,19 @@ def parse_arguments():
 
 def main():
     """Create the release."""
-    if git.Repo(".").active_branch.name != "master":
-        sys.exit("Please release Quality-time from the master branch.")
+    #if git.Repo(".").active_branch.name != "master":
+    #    sys.exit("Please release Quality-time from the master branch.")
     os.environ["RELEASE_DATE"] = datetime.date.today().isoformat()  # Used by bump2version to update CHANGELOG.md
     current_version, bump = parse_arguments()
     if bump.startswith("rc-"):
         bump, pre_bump = bump.split("-")
-        subprocess.run(("bump2version", "--no-commit", "--no-tag", pre_bump), check=True)
-    delta = f"Bump version: {current_version} → {{new_version}}"
-    subprocess.run(("bump2version", "--message", delta, "--tag-message", delta, bump), check=True)
-    subprocess.run(("git", "push", "--follow-tags"), check=True)
+        intermediate_version = get_next_version(current_version, pre_bump)
+        new_version = get_next_version(intermediate_version, bump)
+        print(current_version, intermediate_version, new_version)
+        subprocess.run(("bump2version", "--current-version", current_version, "--new-version", new_version, "--allow-dirty", "--no-commit", "--no-tag", bump), check=True)
+    else:
+        subprocess.run(("bump2version", "--current-version", current_version, "--allow-dirty", "--no-commit", "--no-tag", bump), check=True)
+    #subprocess.run(("git", "push", "--follow-tags"), check=True)
 
 
 if __name__ == "__main__":
