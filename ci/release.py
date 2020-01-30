b10b96ea5d20ad66082531c896724e87ba1f6127
Frank Niessink <frank@niessink.com>
2020-01-30 17:30:06 +0100

    Release script fix (#974)
    * Use two config files to prevent the changelog to be changed two times.

--- ci/release.py
+++ ci/release.py
@@ -2,9 +2,10 @@
 
 """Release the application."""
 
-import datetime
 import argparse
+import datetime
 import os
+import pathlib
 import subprocess
 import sys
 from typing import Tuple
@@ -12,13 +13,11 @@ from typing import Tuple
 import git
 
 
-def get_version(current_version: str = None, bump: str = "whatever") -> str:
-    """Return the current version or the next version relative to the specified current version."""
-    current_version_args = f"--current-version {current_version} " if current_version else ""
-    command = f"bump2version --list --dry-run --allow-dirty {current_version_args}{bump}"
+def get_version() -> str:
+    """Return the current version."""
+    command = f"bump2version --list --dry-run --allow-dirty part-is-a-mandatory-argument"
     output = subprocess.check_output(command.split(" "), text=True)
-    version_type = f"{'new' if current_version else 'current'}_version"
-    return [line for line in output.split("\n") if line.startswith(version_type)][0].split("=")[1]
+    return [line for line in output.split("\n") if line.startswith("current_version")][0].split("=")[1]
 
 
 def parse_arguments() -> Tuple[str, str]:
@@ -30,23 +29,39 @@ def parse_arguments() -> Tuple[str, str]:
     return current_version, parser.parse_args().bump
 
 
+def check_preconditions():
+    """Check preconditions for version bump."""
+    repo = git.Repo(".")
+    if repo.active_branch.name != "master":
+        sys.exit("Please release Quality-time from the master branch.")
+    if repo.is_dirty():
+        sys.exit("Please release Quality-time from a clean workspace.")
+    with pathlib.Path("docs/CHANGELOG.md").open() as changelog:
+        if "[Unreleased]" not in changelog.read():
+            sys.exit("Please add an [Unreleased] header to docs/CHANGELOG.md.")
+
+
 def main():
     """Create the release."""
-    if git.Repo(".").active_branch.name != "master":
-        sys.exit("Please release Quality-time from the master branch.")
+    check_preconditions()
     os.environ["RELEASE_DATE"] = datetime.date.today().isoformat()  # Used by bump2version to update CHANGELOG.md
     current_version, bump = parse_arguments()
+    commands = []
     if bump.startswith("rc-"):
         rc_bump, non_rc_bump = bump.split("-")
-        # Determine the new version by first bumping the patch/minor/major part and then bumping/adding the rc part
-        new_version = get_version(get_version(current_version, non_rc_bump), rc_bump)
-        command = f"bump2version --new-version {new_version} whatever"
+        commands.append(["bump2version", "--no-tag", "--no-commit", non_rc_bump])
+        message = f'"Bump version: {current_version} → {{new_version}}"'
+        # Previous command makes the work space dirty, so allow dirty in the next version bump
+        commands.append(
+            ["bump2version", "--config-file", ".bumpversion-rc.cfg", "--message", message, "--tag-message", message,
+             "--allow-dirty", rc_bump])
     elif bump == "drop-rc":
         new_version = current_version.split("-")[0]
-        command = f"bump2version --new-version {new_version} whatever"
+        commands.append(["bump2version", "--new-version", new_version, "whatever"])
     else:
-        command = f"bump2version {bump}"
-    subprocess.run(command.split(" "), check=True)
+        commands.append(["bump2version", bump])
+    for command in commands:
+        subprocess.run(tuple(command), check=True)
     subprocess.run(("git", "push", "--follow-tags"), check=True)
 
 
