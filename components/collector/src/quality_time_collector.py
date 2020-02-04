7f90f3624e88c471cca49b7d62fed52e23e16159
Frank Niessink <frank@niessink.com>
2020-02-04 14:51:28 +0100

    Suppress LGTM warnings.

--- components/collector/src/quality_time_collector.py
+++ components/collector/src/quality_time_collector.py
@@ -5,7 +5,7 @@ from typing import NoReturn
 
 from metric_collectors import MetricsCollector
 # Make sure subclasses are registered
-from source_collectors import *  # pylint: disable=unused-wildcard-import,wildcard-import
+from source_collectors import *  # lgtm [py/polluting-import] pylint: disable=unused-wildcard-import,wildcard-import
 
 
 def collect(log_level: int = None) -> NoReturn:
