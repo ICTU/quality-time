b4a31a92952fd164b664a4341a47c95c7b408b85
Frank Niessink <frank@niessink.com>
2020-02-11 13:46:50 +0100

    Increase test coverage.

---
+++ components/frontend/src/source/SourceEntity.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+import { mount } from 'enzyme';
+import { SourceEntity } from './SourceEntity';
+
+describe('<Source />', () => {
+    it('render', () => {
+        const wrapper = mount(<Table><Table.Body><SourceEntity entity_attributes={["attr"]} entity={{key:"1"}} /></Table.Body></Table>);
+        expect(wrapper.find("TableRow").hasClass("status_unknown"))
+    });
+});
