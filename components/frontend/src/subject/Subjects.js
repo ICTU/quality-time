db1a0253b1a05c51c7f20b67262b9ce305194a58
Frank Niessink <frank@niessink.com>
2020-02-26 17:03:03 +0100

    Delay rendering of subjects lower on the page.

--- components/frontend/src/subject/Subjects.js
+++ components/frontend/src/subject/Subjects.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Segment } from 'semantic-ui-react';
 import { Subject } from './Subject';
 import { add_subject } from '../api/subject';
 import { ReadOnlyOrEditable } from '../context/ReadOnly';
 import { AddButton } from '../widgets/Button';
 
+function useDelayedRender() {
+  const [visible, setVisible] = useState(false);
+  useEffect(() => { setTimeout(setVisible, 50, true) }, []);
+  return visible;
+}
+
 export function Subjects(props) {
+  const visible = useDelayedRender();
   const [hideMetricsNotRequiringAction, setHideMetricsNotRequiringAction] = useState(false);
   const last_index = Object.keys(props.report.subjects).length - 1;
   return (
     <>
       {Object.keys(props.report.subjects).map((subject_uuid, index) =>
+        visible || index < 3 ?
         <Subject
           {...props}
           first_subject={index === 0}
@@ -19,7 +27,7 @@ export function Subjects(props) {
           last_subject={index === last_index}
           setHideMetricsNotRequiringAction={setHideMetricsNotRequiringAction}
           subject_uuid={subject_uuid}
-        />
+        /> : null
       )}
       <ReadOnlyOrEditable editableComponent={
         <Segment basic>
