31ab1b40a372ed3d5987d43aa87d131b77d7f785
Frank Niessink <frank@niessink.com>
2020-02-29 00:32:32 +0100

    Increase test coverage.

--- components/frontend/src/metric/MeasurementDetails.test.js
+++ components/frontend/src/metric/MeasurementDetails.test.js
@@ -3,12 +3,15 @@ import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import { ReadOnlyContext } from '../context/ReadOnly';
 import { MeasurementDetails } from './MeasurementDetails';
+import * as changelog_api from '../api/changelog';
 import * as metric_api from '../api/metric';
 import * as measurement_api from '../api/measurement';
 
+jest.mock("../api/changelog.js");
 jest.mock("../api/metric.js");
 jest.mock("../api/measurement.js");
 measurement_api.get_measurements.mockImplementation(() => Promise.resolve({ ok: true, measurements: [{ sources: [{}] }] }));
+changelog_api.get_changelog.mockImplementation(() => Promise.resolve({ changelog: [] }));
 
 const report = {
     report_uuid: "report_uuid",
@@ -22,6 +25,7 @@ const report = {
                     type: "violations",
                     sources: {
                         source_uuid: {
+                            type: "source_type",
                             entities: []
                         }
                     }
@@ -31,7 +35,30 @@ const report = {
     }
 };
 
+const data_model = {
+    sources: {source_type: {parameters: {}}},
+    metrics: { violations: { direction: "<", tags: [], sources: ["source_type"] } } }
+
 describe("<MeasurementDetails />", () => {
+    it('switches tabs', async () => {
+        let wrapper;
+        await act(async () => {
+            wrapper = mount(
+                <ReadOnlyContext.Provider value={false}>
+                    <MeasurementDetails
+                        datamodel={data_model}
+                        metric_uuid="metric_uuid"
+                        report={report}
+                        reports={[report]}
+                        subject_uuid="subject_uuid"
+                    />
+                </ReadOnlyContext.Provider>
+            );
+            wrapper.find("MenuItem").at(1).simulate('click');
+            wrapper.setProps({})  // rerender
+        });
+        expect(wrapper.find("a.active").text()).toBe("Sources");
+    });
     it('calls the callback on click', async () => {
         const mockCallBack = jest.fn();
         let wrapper;
@@ -39,7 +66,7 @@ describe("<MeasurementDetails />", () => {
             wrapper = mount(
                 <ReadOnlyContext.Provider value={false}>
                     <MeasurementDetails
-                        datamodel={{ metrics: { violations: { direction: "<", tags: [] } } }}
+                        datamodel={data_model}
                         metric_uuid="metric_uuid"
                         report={report}
                         reports={[report]}
@@ -48,7 +75,6 @@ describe("<MeasurementDetails />", () => {
                     />
                 </ReadOnlyContext.Provider>
             );
-            wrapper.update();
             wrapper.find({ icon: "angle double down" }).at(0).simulate("click");
         });
         expect(mockCallBack).toHaveBeenCalled();
@@ -60,8 +86,7 @@ describe("<MeasurementDetails />", () => {
             wrapper = mount(
                 <ReadOnlyContext.Provider value={false}>
                     <MeasurementDetails
-                        datamodel={{ metrics: { violations: { direction: "<", tags: [] } } }}
-                        measurements={[]}
+                        datamodel={data_model}
                         metric_uuid="metric_uuid"
                         report={report}
                         reports={[report]}
