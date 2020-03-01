65f266f4d51de6313ff5b6ef88f98f422288e60c
Frank Niessink <frank@niessink.com>
2020-03-01 21:24:45 +0100

    More async methods.

--- components/collector/src/metric_collectors/metrics_collector.py
+++ components/collector/src/metric_collectors/metrics_collector.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 import asyncio
 import logging
 import os
-import time
 from typing import cast, Any, Dict, Final, NoReturn
 
+import aiohttp
 import requests
 
 from collector_utilities.functions import timer
@@ -14,16 +14,18 @@ from collector_utilities.type import JSON, URL
 from .metric_collector import MetricCollector
 
 
-def get(api: URL) -> JSON:
+async def get(api: URL) -> JSON:
     """Get data from the API url."""
     try:
-        return cast(JSON, requests.get(api).json())
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api) as response:
+                return cast(JSON, await response.json())
     except Exception as reason:  # pylint: disable=broad-except
         logging.error("Getting data from %s failed: %s", api, reason)
         return {}
 
 
-def post(api: URL, data) -> None:
+async def post(api: URL, data) -> None:
     """Post the JSON data to the api url."""
     try:
         requests.post(api, json=data)
@@ -53,7 +55,7 @@ class MetricsCollector:
         """Start fetching measurements indefinitely."""
         max_sleep_duration = int(os.environ.get("COLLECTOR_SLEEP_DURATION", 60))
         measurement_frequency = int(os.environ.get("COLLECTOR_MEASUREMENT_FREQUENCY", 15 * 60))
-        self.data_model = self.fetch_data_model(max_sleep_duration)
+        self.data_model = await self.fetch_data_model(max_sleep_duration)
         while True:
             self.record_health()
             logging.info("Collecting...")
@@ -64,19 +66,19 @@ class MetricsCollector:
                 "Collecting took %.1f seconds. Sleeping %.1f seconds...", collection_timer.duration, sleep_duration)
             await asyncio.sleep(sleep_duration)
 
-    def fetch_data_model(self, sleep_duration: int) -> JSON:
+    async def fetch_data_model(self, sleep_duration: int) -> JSON:
         """Fetch the data model."""
         while True:
             self.record_health()
             logging.info("Loading data model...")
-            if data_model := get(URL(f"{self.server_url}/api/v2/datamodel")):
+            if data_model := await get(URL(f"{self.server_url}/api/v2/datamodel")):
                 return data_model
             logging.warning("Loading data model failed, trying again in %ss...", sleep_duration)
-            time.sleep(sleep_duration)
+            await asyncio.sleep(sleep_duration)
 
     async def fetch_measurements(self, measurement_frequency: int) -> None:
         """Fetch the metrics and their measurements."""
-        metrics = get(URL(f"{self.server_url}/api/v2/metrics"))
+        metrics = await get(URL(f"{self.server_url}/api/v2/metrics"))
         for metric_uuid, metric in metrics.items():
             if not (collector := MetricCollector(metric, self.data_model)).can_collect():
                 continue
@@ -86,7 +88,7 @@ class MetricsCollector:
             self.last_parameters[metric_uuid] = metric
             self.next_fetch[metric_uuid] = datetime.now() + timedelta(seconds=measurement_frequency)
             measurement["metric_uuid"] = metric_uuid
-            post(URL(f"{self.server_url}/api/v2/measurements"), measurement)
+            await post(URL(f"{self.server_url}/api/v2/measurements"), measurement)
 
     def __skip(self, metric_uuid: str, metric) -> bool:
         """Return whether the metric needs to be measured."""
--- components/collector/tests/metric_collectors/test_metrics_collector.py
+++ components/collector/tests/metric_collectors/test_metrics_collector.py
@@ -3,7 +3,7 @@
 import logging
 from datetime import datetime
 from typing import Tuple
-from unittest.mock import patch, mock_open, Mock
+from unittest.mock import patch, mock_open, AsyncMock, Mock
 
 import aiounittest
 
@@ -23,10 +23,7 @@ class CollectorTest(aiounittest.AsyncTestCase):
             def _parse_source_responses(self, responses: Responses) -> Tuple[Value, Value, Entities]:
                 return "42", "84", []
 
-        self.data_model_response = Mock()
         self.data_model = dict(sources=dict(source=dict(parameters=dict(url=dict(mandatory=True, metrics=["metric"])))))
-        self.data_model_response.json.return_value = self.data_model
-        self.metrics_response = Mock()
         self.metrics_collector = MetricsCollector()
         self.url = "https://url"
         self.measurement_api_url = "http://localhost:5001/api/v2/measurements"
@@ -36,31 +33,39 @@ class CollectorTest(aiounittest.AsyncTestCase):
         logging.disable(logging.NOTSET)
 
     @patch("requests.post")
-    @patch("requests.get")
+    @patch("aiohttp.ClientSession.get")
     async def test_fetch_without_sources(self, mocked_get, mocked_post):
         """Test fetching measurement for a metric without sources."""
-        self.metrics_response.json.return_value = dict(
-            metric_uuid=dict(type="metric", addition="sum", sources=dict()))
-        mocked_get.return_value = self.metrics_response
+        metrics = dict(metric_uuid=dict(type="metric", addition="sum", sources=dict()))
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(return_value=metrics)
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
         await self.metrics_collector.fetch_measurements(60)
         mocked_post.assert_not_called()
 
     @patch("requests.post")
-    @patch("requests.get", Mock(side_effect=RuntimeError))
+    @patch("aiohttp.ClientSession.get", Mock(side_effect=RuntimeError))
     async def test_fetch_with_get_error(self, mocked_post):
         """Test fetching measurement when getting fails."""
         await self.metrics_collector.fetch_measurements(60)
         mocked_post.assert_not_called()
 
+    @patch("requests.get", Mock())
     @patch("requests.post", side_effect=RuntimeError)
-    @patch("requests.get")
+    @patch("aiohttp.ClientSession.get")
     async def test_fetch_with_post_error(self, mocked_get, mocked_post):
         """Test fetching measurement when posting fails."""
-        self.metrics_response.json.return_value = dict(
+        metrics = dict(
             metric_uuid=dict(
                 addition="sum", type="metric",
                 sources=dict(source_id=dict(type="source", parameters=dict(url=self.url)))))
-        mocked_get.side_effect = [self.metrics_response, Mock()]
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(return_value=metrics)
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
         self.metrics_collector.data_model = self.data_model
         await self.metrics_collector.fetch_measurements(60)
         mocked_post.assert_called_once_with(
@@ -73,15 +78,20 @@ class CollectorTest(aiounittest.AsyncTestCase):
 
     @patch("asyncio.sleep", Mock(side_effect=RuntimeError))
     @patch("builtins.open", mock_open())
+    @patch("requests.get", Mock())
     @patch("requests.post")
-    @patch("requests.get")
+    @patch("aiohttp.ClientSession.get")
     async def test_collect(self, mocked_get, mocked_post):
         """Test the collect method."""
-        self.metrics_response.json.return_value = dict(
+        metrics = dict(
             metric_uuid=dict(
                 addition="sum", type="metric",
                 sources=dict(source_id=dict(type="source", parameters=dict(url=self.url)))))
-        mocked_get.side_effect = [self.data_model_response, self.metrics_response, Mock()]
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(side_effect=[self.data_model, metrics])
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
         with self.assertRaises(RuntimeError):
             await quality_time_collector.collect()
         mocked_post.assert_called_once_with(
@@ -92,26 +102,35 @@ class CollectorTest(aiounittest.AsyncTestCase):
                          connection_error=None, parse_error=None, source_uuid="source_id")],
                 metric_uuid="metric_uuid"))
 
-    @patch("requests.get")
+    @patch("aiohttp.ClientSession.get")
     async def test_missing_collector(self, mocked_get):
         """Test that an exception is thrown if there's no collector for the source and metric type."""
-        self.metrics_response.json.return_value = dict(
+        metrics = dict(
             metric_uuid=dict(
                 type="metric", addition="sum",
                 sources=dict(missing=dict(type="unknown_source", parameters=dict(url=self.url)))))
-        mocked_get.return_value = self.metrics_response
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(return_value=metrics)
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
         with self.assertRaises(LookupError):
             await self.metrics_collector.fetch_measurements(60)
 
+    @patch("requests.get", Mock())
     @patch("requests.post")
-    @patch("requests.get")
+    @patch("aiohttp.ClientSession.get")
     async def test_fetch_twice(self, mocked_get, mocked_post):
         """Test that the metric is skipped on the second fetch."""
-        self.metrics_response.json.return_value = dict(
+        metrics = dict(
             metric_uuid=dict(
                 addition="sum", type="metric",
                 sources=dict(source_id=dict(type="source", parameters=dict(url=self.url)))))
-        mocked_get.side_effect = [self.metrics_response, Mock(), self.metrics_response]
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(side_effect=[metrics, metrics])
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
         self.metrics_collector.data_model = self.data_model
         await self.metrics_collector.fetch_measurements(60)
         await self.metrics_collector.fetch_measurements(60)
@@ -124,23 +143,31 @@ class CollectorTest(aiounittest.AsyncTestCase):
                 metric_uuid="metric_uuid"))
 
     @patch("requests.post")
-    @patch("requests.get")
+    @patch("aiohttp.ClientSession.get")
     async def test_missing_mandatory_parameter(self, mocked_get, mocked_post):
         """Test that a metric with sources but without a mandatory parameter is skipped."""
-        self.metrics_response.json.return_value = dict(
+        metrics = dict(
             metric_uuid=dict(
                 type="metric", addition="sum", sources=dict(missing=dict(type="source", parameters=dict(url="")))))
-        mocked_get.return_value = self.metrics_response
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(return_value=metrics)
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
         self.metrics_collector.data_model = self.data_model
         await self.metrics_collector.fetch_measurements(60)
         mocked_post.assert_not_called()
 
     @patch("builtins.open", mock_open())
-    @patch("requests.get")
-    def test_fetch_data_model_after_failure(self, mocked_get):
+    @patch("aiohttp.ClientSession.get")
+    async def test_fetch_data_model_after_failure(self, mocked_get):
         """Test that the data model is fetched on the second try."""
-        mocked_get.side_effect = [None, self.data_model_response]
-        data_model = self.metrics_collector.fetch_data_model(0)
+        response_mock = AsyncMock()
+        response_mock.__aenter__.return_value = response_mock
+        response_mock.json = AsyncMock(side_effect=[RuntimeError, self.data_model])
+        mocked_get.__aenter__.return_value = mocked_get
+        mocked_get.return_value = response_mock
+        data_model = await self.metrics_collector.fetch_data_model(0)
         self.assertEqual(self.data_model, data_model)
 
     @patch("builtins.open", new_callable=mock_open)
