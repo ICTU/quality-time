71d0aeb489bfcb10fd930ec6a7552d32e66490d6
Frank Niessink <frank@niessink.com>
2020-03-28 16:55:54 +0100

    Remove cached client session.

--- components/collector/src/base_collectors/metrics_collector.py
+++ components/collector/src/base_collectors/metrics_collector.py
@@ -11,15 +11,14 @@ import aiohttp
 
 from collector_utilities.functions import timer
 from collector_utilities.type import JSON, URL
-from .cached_client_session import CachedClientSession
 from .source_collector import SourceCollector
 
 
 async def get(session: aiohttp.ClientSession, api: URL) -> JSON:
     """Get data from the API url."""
     try:
-        response = await session.get(api)
-        return cast(JSON, await response.json())
+        async with session.get(api) as response:
+            return cast(JSON, await response.json())
     except Exception as reason:  # pylint: disable=broad-except
         logging.error("Getting data from %s failed: %s", api, reason)
         logging.error(traceback.format_exc())
@@ -63,7 +62,7 @@ class MetricsCollector:
         while True:
             self.record_health()
             logging.info("Collecting...")
-            async with CachedClientSession(
+            async with aiohttp.ClientSession(
                     timeout=timeout, connector=aiohttp.TCPConnector(limit_per_host=20),
                     raise_for_status=True) as session:
                 with timer() as collection_timer:
