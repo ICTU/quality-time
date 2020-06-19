b3ada0668a83bd861a35dd562f303439b024bfd0
Frank Niessink <frank@niessink.com>
2020-06-19 22:41:53 +0200

    Add cloc (https://github.com/AlDanial/cloc) as source for the 'size' metric. As opposed to SonarQube, cloc makes it easy to exclude certain programming languages from the size measurement. Closes #460.

--- components/collector/src/source_collectors/file_source_collectors/cloc.py
+++ components/collector/src/source_collectors/file_source_collectors/cloc.py
@@ -2,6 +2,7 @@
 
 from typing import Tuple
 
+from collector_utilities.functions import match_string_or_regular_expression
 from collector_utilities.type import Entities, Responses, Value
 from base_collectors import JSONFileSourceCollector
 
@@ -12,9 +13,10 @@ class ClocLOC(JSONFileSourceCollector):
     async def _parse_source_responses(self, responses: Responses) -> Tuple[Value, Value, Entities]:
         loc = 0
         entities = []
+        languages_to_ignore = self._parameter("languages_to_ignore")
         for response in responses:
             for key, value in (await response.json()).items():
-                if key not in ("header", "SUM"):
+                if key not in ("header", "SUM") and not match_string_or_regular_expression(key, languages_to_ignore):
                     loc += value["code"]
                     entities.append(
                         dict(key=key, language=key, blank=str(value["blank"]), comment=str(value["comment"]),
