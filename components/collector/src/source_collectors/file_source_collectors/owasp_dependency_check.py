e1429c510854428ae779238d2f718196f0b52579
Frank Niessink <frank@niessink.com>
2020-06-20 11:45:36 +0200

    Remove duplication.

--- components/collector/src/source_collectors/file_source_collectors/owasp_dependency_check.py
+++ components/collector/src/source_collectors/file_source_collectors/owasp_dependency_check.py
@@ -19,52 +19,6 @@ class OWASPDependencyCheckBase(XMLFileSourceCollector, ABC):  # pylint: disable=
                          for version in ("2.0", "2.1", "2.2", "2.3")]
 
 
-class OWASPDependencyCheckSecurityWarnings(OWASPDependencyCheckBase):
-    """Collector to get security warnings from the OWASP Dependency Check XML report."""
-
-    async def _parse_source_responses(self, responses: Responses) -> Tuple[Value, Value, Entities]:
-        landing_url = await self._landing_url(responses)
-        entities = []
-        for response in responses:
-            tree, namespaces = await parse_source_response_xml_with_namespace(response, self.allowed_root_tags)
-            entities.extend(
-                [self.__parse_entity(dependency, index, namespaces, landing_url) for (index, dependency)
-                 in self.__vulnerable_dependencies(tree, namespaces)])
-        return str(len(entities)), "100", entities
-
-    def __vulnerable_dependencies(self, tree: Element, namespaces: Namespaces) -> List[Tuple[int, Element]]:
-        """Return the vulnerable dependencies."""
-        return [(index, dependency) for (index, dependency) in enumerate(tree.findall(".//ns:dependency", namespaces))
-                if self.__vulnerabilities(dependency, namespaces)]
-
-    def __parse_entity(
-            self, dependency: Element, dependency_index: int, namespaces: Namespaces, landing_url: str) -> Entity:
-        """Parse the entity from the dependency."""
-        file_path = dependency.findtext("ns:filePath", default="", namespaces=namespaces)
-        sha1 = dependency.findtext("ns:sha1", namespaces=namespaces)
-        # We can only generate a entity landing url if a sha1 is present in the XML, but unfortunately not all
-        # dependencies have one, so check for it:
-        entity_landing_url = f"{landing_url}#l{dependency_index + 1}_{sha1}" if sha1 else ""
-        key = sha1 if sha1 else sha1_hash(file_path)
-        vulnerabilities = self.__vulnerabilities(dependency, namespaces)
-        severities = set(vulnerability.findtext(".//ns:severity", default="", namespaces=namespaces).lower()
-                         for vulnerability in vulnerabilities)
-        highest_severity = "low"
-        for severity in ("critical", "high", "medium"):
-            if severity in severities:
-                highest_severity = severity
-                break
-        return dict(key=key, file_path=file_path, highest_severity=highest_severity.capitalize(),
-                    url=entity_landing_url, nr_vulnerabilities=str(len(vulnerabilities)))
-
-    def __vulnerabilities(self, element: Element, namespaces: Namespaces) -> List[Element]:
-        """Return the vulnerabilities that have one of the severities specified in the parameters."""
-        severities = self._parameter("severities")
-        vulnerabilities = element.findall(".//ns:vulnerabilities/ns:vulnerability", namespaces)
-        return [vulnerability for vulnerability in vulnerabilities if
-                vulnerability.findtext(".//ns:severity", default="", namespaces=namespaces).lower() in severities]
-
-
 class OWASPDependencyCheckDependencies(OWASPDependencyCheckBase):
     """Collector to get the dependencies from the OWASP Dependency Check XML report."""
 
@@ -74,12 +28,16 @@ class OWASPDependencyCheckDependencies(OWASPDependencyCheckBase):
         for response in responses:
             tree, namespaces = await parse_source_response_xml_with_namespace(response, self.allowed_root_tags)
             entities.extend(
-                [self.__parse_entity(dependency, index, namespaces, landing_url) for (index, dependency)
-                 in enumerate(tree.findall(".//ns:dependency", namespaces))])
+                [self._parse_entity(dependency, index, namespaces, landing_url) for (index, dependency)
+                 in enumerate(self._dependencies(tree, namespaces))])
         return str(len(entities)), "100", entities
 
-    @staticmethod
-    def __parse_entity(dependency: Element, dependency_index: int, namespaces: Namespaces, landing_url: str) -> Entity:
+    def _dependencies(self, tree: Element, namespaces: Namespaces) -> List[Element]:  # pylint: disable=no-self-use
+        """Return the enumerated dependencies."""
+        return tree.findall(".//ns:dependency", namespaces)
+
+    def _parse_entity(  # pylint: disable=no-self-use
+            self, dependency: Element, dependency_index: int, namespaces: Namespaces, landing_url: str) -> Entity:
         """Parse the entity from the dependency."""
         file_path = dependency.findtext("ns:filePath", default="", namespaces=namespaces)
         sha1 = dependency.findtext("ns:sha1", namespaces=namespaces)
@@ -88,6 +46,39 @@ class OWASPDependencyCheckDependencies(OWASPDependencyCheckBase):
         entity_landing_url = f"{landing_url}#l{dependency_index + 1}_{sha1}" if sha1 else ""
         key = sha1 if sha1 else sha1_hash(file_path)
         return dict(key=key, file_path=file_path, url=entity_landing_url)
+
+
+class OWASPDependencyCheckSecurityWarnings(OWASPDependencyCheckDependencies):
+    """Collector to get security warnings from the OWASP Dependency Check XML report."""
+
+    def _dependencies(self, tree: Element, namespaces: Namespaces) -> List[Element]:
+        """Return the vulnerable dependencies."""
+        return [
+            dependency for dependency in tree.findall(".//ns:dependency", namespaces)
+            if self.__vulnerabilities(dependency, namespaces)]
+
+    def _parse_entity(
+            self, dependency: Element, dependency_index: int, namespaces: Namespaces, landing_url: str) -> Entity:
+        """Parse the entity from the dependency."""
+        entity = super()._parse_entity(dependency, dependency_index, namespaces, landing_url)
+        vulnerabilities = self.__vulnerabilities(dependency, namespaces)
+        severities = set(vulnerability.findtext(".//ns:severity", default="", namespaces=namespaces).lower()
+                         for vulnerability in vulnerabilities)
+        highest_severity = "low"
+        for severity in ("critical", "high", "medium"):
+            if severity in severities:
+                highest_severity = severity
+                break
+        entity.update(
+            dict(highest_severity=highest_severity.capitalize(), nr_vulnerabilities=str(len(vulnerabilities))))
+        return entity
+
+    def __vulnerabilities(self, element: Element, namespaces: Namespaces) -> List[Element]:
+        """Return the vulnerabilities that have one of the severities specified in the parameters."""
+        severities = self._parameter("severities")
+        vulnerabilities = element.findall(".//ns:vulnerabilities/ns:vulnerability", namespaces)
+        return [vulnerability for vulnerability in vulnerabilities if
+                vulnerability.findtext(".//ns:severity", default="", namespaces=namespaces).lower() in severities]
 
 
 class OWASPDependencyCheckSourceUpToDateness(OWASPDependencyCheckBase, SourceUpToDatenessCollector):
