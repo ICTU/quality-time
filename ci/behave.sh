6fe601bb55a2e30e60f00c996e4ff1e01e52137b
Frank Niessink <frank@niessink.com>
2020-07-14 22:57:38 +0200

    Show server output.

--- ci/behave.sh
+++ ci/behave.sh
@@ -15,7 +15,7 @@ pip --quiet install --progress-bar off -r requirements.txt -r requirements-dev.t
 coverage erase
 export LOAD_EXAMPLE_REPORTS=False
 export COVERAGE_PROCESS_START=$COVERAGE_RCFILE
-python tests/quality_time_server_under_coverage.py > /tmp/quality_time_server.log 2>&1 &
+python tests/quality_time_server_under_coverage.py & # > /tmp/quality_time_server.log 2>&1 &
 deactivate
 cd ../..
 python3 -m venv venv
