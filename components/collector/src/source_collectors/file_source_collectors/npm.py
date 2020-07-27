e665b69421e454400ab835ffa9effd5af71aac3b
Commons Utilities <commonsutilities@cjib.minvenj.nl>
2020-07-27 21:11:20 +0200

    Invoke json.load() to avoid aiohttp Content-Type warnings (#1325)

--- components/collector/src/source_collectors/file_source_collectors/npm.py
+++ components/collector/src/source_collectors/file_source_collectors/npm.py
@@ -5,6 +5,7 @@ from typing import Dict, Tuple
 from collector_utilities.type import Entities, Responses, Value
 from base_collectors import JSONFileSourceCollector
 
+import json
 
 class NpmDependencies(JSONFileSourceCollector):
     """npm collector for dependencies."""
@@ -12,7 +13,10 @@ class NpmDependencies(JSONFileSourceCollector):
     async def _parse_source_responses(self, responses: Responses) -> Tuple[Value, Value, Entities]:
         installed_dependencies: Dict[str, Dict[str, str]] = {}
         for response in responses:
-            installed_dependencies.update(await response.json())
+
+            #1325: Don't use response.json since aiohttp warns when the Content-Type of the served JSON is incorrect:
+            data = await response.read()
+            installed_dependencies.update(json.loads(data))
         entities: Entities = [
             dict(
                 key=f'{dependency}@{versions.get("current", "?")}', name=dependency,
@@ -20,3 +24,4 @@ class NpmDependencies(JSONFileSourceCollector):
                 latest=versions.get("latest", "unknown"))
             for dependency, versions in installed_dependencies.items()]
         return str(len(entities)), "100", entities
+
