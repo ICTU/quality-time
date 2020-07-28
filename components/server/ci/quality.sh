48bc3a562c148ff403c0a4beda02bbec2bd09ef7
Frank Niessink <frank@niessink.com>
2020-07-28 11:48:35 +0200

    Update vulture ignore list.

--- components/server/ci/quality.sh
+++ components/server/ci/quality.sh
@@ -6,4 +6,4 @@ mypy src
 pylint src tests
 safety check --bare -r requirements.txt -r requirements-dev.txt
 bandit --quiet --recursive src/
-vulture --min-confidence 0 src/ tests/ .vulture_white_list.py
+vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
