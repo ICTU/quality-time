8a995e7304be72e882c9ffcd4535d074e92da8db
Frank Niessink <frank@niessink.com>
2020-07-28 16:34:40 +0200

    Ignore defensive code.

--- components/server/src/database/datamodels.py
+++ components/server/src/database/datamodels.py
@@ -18,7 +18,7 @@ def latest_datamodel(database: Database, max_iso_timestamp: str = ""):
 
 def insert_new_datamodel(database: Database, data_model):
     """Insert a new data model in the data models collection."""
-    if "_id" in data_model:
+    if "_id" in data_model:  # pragma: no cover-behave
         del data_model["_id"]
     data_model["timestamp"] = iso_timestamp()
     return database.datamodels.insert_one(data_model)
