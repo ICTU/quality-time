c0b63741c9d7bf725ce1eea1641fb205a7a3c025
Frank Niessink <frank@niessink.com>
2020-08-10 21:12:40 +0200

    Reduce complexity.

--- components/server/src/model/iterators.py
+++ components/server/src/model/iterators.py
@@ -3,8 +3,9 @@
 from typing import Iterator
 
 
-def sources(report) -> Iterator:
-    """Return all sources in the report."""
-    for subject in report.get("subjects", {}).values():
-        for metric in subject.get("metrics", {}).values():
-            yield from metric.get("sources", {}).items()
+def sources(reports) -> Iterator:
+    """Return all sources in the reports."""
+    for report in reports:
+        for subject in report.get("subjects", {}).values():
+            for metric in subject.get("metrics", {}).values():
+                yield from metric.get("sources", {}).values()
--- components/server/src/model/transformations.py
+++ components/server/src/model/transformations.py
@@ -11,37 +11,35 @@ from .queries import is_password_parameter
 
 def hide_credentials(data_model, *reports) -> None:
     """Hide the credentials in the reports."""
-    for report in reports:
-        for _, source in iter_sources(report):
-            for parameter_key, parameter_value in source.get("parameters", {}).items():
-                if parameter_value and is_password_parameter(data_model, source["type"], parameter_key):
-                    source["parameters"][parameter_key] = "this string replaces credentials"
+    for source in iter_sources(reports):
+        for parameter_key, parameter_value in source.get("parameters", {}).items():
+            if parameter_value and is_password_parameter(data_model, source["type"], parameter_key):
+                source["parameters"][parameter_key] = "this string replaces credentials"
 
 
 def change_source_parameter(data, parameter_key: str, old_value, new_value, scope: EditScope) -> List[ItemId]:
     """Change the parameter with the specified key of all sources of the specified type and with the same old value to
     the new value. Return the ids of the changed reports, subjects, metrics, and sources."""
-
-    def sources_to_change() -> Iterator:
-        """Return the sources to change."""
-        reports = data.reports if scope == "reports" else [data.report]
-        for report in reports:
-            subjects = {data.subject_uuid: data.subject} if scope in ["subject", "metric", "source"] \
-                else report["subjects"]
-            for subject_uuid, subject in subjects.items():
-                metrics = {data.metric_uuid: data.metric} if scope in ["metric", "source"] else subject["metrics"]
-                for metric_uuid, metric in metrics.items():
-                    sources = {data.source_uuid: data.source} if scope == "source" else metric["sources"]
-                    for source_uuid, source_to_change in sources.items():
-                        yield source_to_change, [report["report_uuid"], subject_uuid, metric_uuid, source_uuid]
-
     changed_ids: List[ItemId] = []
-    for source, uuids in sources_to_change():
+    for source, uuids in _sources_to_change(data, scope):
         if source["type"] == data.source["type"] and \
                 (source["parameters"].get(parameter_key) or None) == (old_value or None):
             source["parameters"][parameter_key] = new_value
             changed_ids.extend(uuids)
     return list(unique(changed_ids))
+
+
+def _sources_to_change(data, scope: EditScope) -> Iterator:
+    """Return the sources to change, given the scope."""
+    reports = data.reports if scope == "reports" else [data.report]
+    for report in reports:
+        subjects = {data.subject_uuid: data.subject} if scope in ("subject", "metric", "source") else report["subjects"]
+        for subject_uuid, subject in subjects.items():
+            metrics = {data.metric_uuid: data.metric} if scope in ("metric", "source") else subject["metrics"]
+            for metric_uuid, metric in metrics.items():
+                sources = {data.source_uuid: data.source} if scope == "source" else metric["sources"]
+                for source_uuid, source_to_change in sources.items():
+                    yield source_to_change, [report["report_uuid"], subject_uuid, metric_uuid, source_uuid]
 
 
 def summarize_report(report, recent_measurements, data_model) -> None:
