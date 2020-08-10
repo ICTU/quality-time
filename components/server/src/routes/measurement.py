e9363cd22826e7e92d26b525001a5dbbdcc74eab
Frank Niessink <frank@niessink.com>
2020-08-10 18:50:07 +0200

    Fix SonarQube issues.

--- components/server/src/routes/measurement.py
+++ components/server/src/routes/measurement.py
@@ -26,21 +26,24 @@ def post_measurement(database: Database) -> Dict:
         return dict(ok=False)  # Metric does not exist, must've been deleted while being measured
     data_model = latest_datamodel(database)
     if latest := latest_measurement(database, metric_uuid):
-        if latest_successful := latest_successful_measurement(database, metric_uuid):
-            latest_sources = latest_successful["sources"]
-        else:
-            latest_sources = latest["sources"]
-        for latest_source, new_source in zip(latest_sources, measurement["sources"]):
-            new_entity_keys = set(entity["key"] for entity in new_source.get("entities", []))
-            # Copy the user data of entities that still exist in the new measurement
-            for entity_key, attributes in latest_source.get("entity_user_data", {}).items():
-                if entity_key in new_entity_keys:
-                    new_source.setdefault("entity_user_data", {})[entity_key] = attributes
+        latest_successful = latest_successful_measurement(database, metric_uuid)
+        latest_sources = latest_successful["sources"] if latest_successful else latest["sources"]
+        copy_entity_user_data(latest_sources, measurement["sources"])
         if not debt_target_expired(data_model, metric, latest) and latest["sources"] == measurement["sources"]:
             # If the new measurement is equal to the previous one, merge them together
             update_measurement_end(database, latest["_id"])
             return dict(ok=True)
     return insert_new_measurement(database, data_model, metric, measurement)
+
+
+def copy_entity_user_data(old_sources, new_sources) -> None:
+    """Copy the entity user data from the old sources to the new sources."""
+    for old_source, new_source in zip(old_sources, new_sources):
+        new_entity_keys = set(entity["key"] for entity in new_source.get("entities", []))
+        # Copy the user data of entities that still exist in the new measurement:
+        for entity_key, attributes in old_source.get("entity_user_data", {}).items():
+            if entity_key in new_entity_keys:
+                new_source.setdefault("entity_user_data", {})[entity_key] = attributes
 
 
 def debt_target_expired(data_model, metric, measurement) -> bool:
