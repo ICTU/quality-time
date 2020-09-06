f023e468a2a7399f10668600ca7f22c30dd4a96a
Frank Niessink <frank@niessink.com>
2020-09-06 15:46:56 +0200

    When measuring 'velocity' with Jira as source, the metric can either report the number of points committed to or the number of points completed. Closes #1406.

--- components/collector/tests/source_collectors/api_source_collectors/test_jira.py
+++ components/collector/tests/source_collectors/api_source_collectors/test_jira.py
@@ -136,11 +136,13 @@ class JiraVelocityTest(JiraTestCase):
 
     METRIC_TYPE = "velocity"
 
-    async def test_velocity(self):
-        """Test that the velocity is returned."""
-        sprint_url = "https://jira/secure/RapidBoard.jspa?rapidView=2&view=reporting&chart=sprintRetrospective&sprint="
-        boards_json = dict(values=[dict(id=1, name="Board 1"), dict(id=2, name="Board 2")])
-        velocity_json = dict(
+    def setUp(self):
+        super().setUp()
+        base_url = "https://jira/secure/RapidBoard.jspa?rapidView=2&view=reporting&chart="
+        self.sprint_url = f"{base_url}sprintRetrospective&sprint="
+        self.landing_url = f"{base_url}velocityChart"
+        self.boards_json = dict(values=[dict(id=1, name="Board 1"), dict(id=2, name="Board 2")])
+        self.velocity_json = dict(
             sprints=[
                 dict(id=4, name="Sprint 4", goal="Goal 4"), dict(id=3, name="Sprint 3", goal="Goal 3"),
                 dict(id=2, name="Sprint 2", goal="")],
@@ -148,21 +150,38 @@ class JiraVelocityTest(JiraTestCase):
                 "2": dict(estimated=dict(value=65, text="65.0"), completed=dict(value=30, text="30.0")),
                 "3": dict(estimated=dict(value=62, text="62.0"), completed=dict(value=48, text="46.0")),
                 "4": dict(estimated=dict(value=40, text="40.0"), completed=dict(value=42, text="42.0"))})
-        response = await self.collect(
-            self.metric, get_request_json_side_effect=[boards_json, velocity_json])
+
+    def sprint_entity(self, key: str, points_completed: str, points_committed: str, goal: bool = True):
+        """Create an entity."""
+        velocity_type = self.sources["source_id"]["parameters"].get("velocity_type", "completed points")
+        points_measured = points_completed if velocity_type == "completed points" else points_committed
+        return dict(
+            key=key, name=f"Sprint {key}", goal=f"Goal {key}" if goal else "", points_completed=points_completed,
+            points_committed=points_committed, points_measured=points_measured, url=self.sprint_url + key)
+
+    async def test_completed_velocity(self):
+        """Test that the completed velocity is returned."""
+        response = await self.collect(self.metric, get_request_json_side_effect=[self.boards_json, self.velocity_json])
         self.assert_measurement(
-            response, value="40",
-            landing_url="https://jira/secure/RapidBoard.jspa?rapidView=2&view=reporting&chart=velocityChart",
+            response, value="40", landing_url=self.landing_url,
             entities=[
-                dict(key="4", name="Sprint 4", goal="Goal 4", points_completed="42.0", points_committed="40.0",
-                     url=sprint_url + "4"),
-                dict(key="3", name="Sprint 3", goal="Goal 3", points_completed="46.0", points_committed="62.0",
-                     url=sprint_url + "3"),
-                dict(key="2", name="Sprint 2", goal="", points_completed="30.0", points_committed="65.0",
-                     url=sprint_url + "2")])
+                self.sprint_entity(key="4", points_completed="42.0", points_committed="40.0"),
+                self.sprint_entity(key="3", points_completed="46.0", points_committed="62.0"),
+                self.sprint_entity(key="2", points_completed="30.0", points_committed="65.0", goal=False)])
+
+    async def test_committed_velocity(self):
+        """Test that the committed velocity is returned."""
+        self.sources["source_id"]["parameters"]["velocity_type"] = "committed points"
+        response = await self.collect(self.metric, get_request_json_side_effect=[self.boards_json, self.velocity_json])
+        self.assert_measurement(
+            response, value="56", landing_url=self.landing_url,
+            entities=[
+                self.sprint_entity(key="4", points_completed="42.0", points_committed="40.0"),
+                self.sprint_entity(key="3", points_completed="46.0", points_committed="62.0"),
+                self.sprint_entity(key="2", points_completed="30.0", points_committed="65.0", goal=False)])
 
     async def test_velocity_missing_board(self):
-        """Test that no velocity is returned if the board name or id is invalud."""
+        """Test that no velocity is returned if the board name or id is invalid."""
         boards_json = dict(values=[dict(id=1, name="Board 1")])
         response = await self.collect(self.metric, get_request_json_side_effect=[boards_json])
         self.assert_measurement(
--- docs/ci/create_datamodel_md.py
+++ docs/ci/create_datamodel_md.py
@@ -54,7 +54,11 @@ def metrics_table(dm, universal_sources: List[str]) -> str:
         unit = "% of the " + metric["unit"] if metric["default_scale"] == "percentage" else " " + metric["unit"]
         target = f"{direction} {metric['target']}{unit}"
         tags = ", ".join(metric['tags'])
-        sources = [dm["sources"][source]['name'] for source in metric["sources"] if source not in universal_sources]
+        sources = []
+        for source in metric["sources"]:
+            if source not in universal_sources:
+                source_name = dm['sources'][source]['name']
+                sources.append(f"[{source_name}]({metric_source_slug(dm, metric, source)})")
         markdown += markdown_table_row(metric['name'], metric['description'], target, tags, ", ".join(sorted(sources)))
     markdown += "\n"
     return markdown
@@ -69,10 +73,16 @@ def sources_table(dm, universal_sources: List[str]) -> str:
             metrics = "¹"
         else:
             metrics = ", ".join(
-                [metric["name"] for metric in dm["metrics"].values() if source_key in metric["sources"]])
+                [f"[{metric['name']}]({metric_source_slug(dm, metric, source_key)})" for metric in dm["metrics"].values() if source_key in metric["sources"]])
         markdown += markdown_table_row(name, source['description'], metrics)
     markdown += "\n"
     return markdown
+
+
+def metric_source_slug(dm, metric, source) -> str:
+    """Return a slug for the metric source combination."""
+    source_name = dm['sources'][source]['name']
+    return f"#{metric['name']} from {source_name}".lower().replace(" ", "-")
 
 
 def metric_source_table(dm, metric_key, source_key) -> str:
@@ -92,12 +102,16 @@ def metric_source_table(dm, metric_key, source_key) -> str:
 def data_model_as_table(dm) -> str:
     """Return the data model as Markdown table."""
     markdown = markdown_header("Quality-time metrics and sources")
+    markdown += "This document lists all [metrics](#metrics) that *Quality-time* can measure and all " \
+        "[sources](#sources) that *Quality-time* can use to measure the metrics. For each " \
+        "[supported combination of metric and source](#supported-metric-source-combinations), it lists the " \
+        "parameters that can be used to configure the source.\n\n"
     markdown += markdown_header("Metrics", 2)
     markdown += metrics_table(dm, universal_sources := ["manual_number", "random"])
     markdown += markdown_header("Sources", 2)
     markdown += sources_table(dm, universal_sources)
     markdown += "¹) All metrics can be measured using the 'Manual number' and the 'Random number' source.\n"
-    markdown += markdown_header("Supported metric/source combinations", 2)
+    markdown += markdown_header("Supported metric-source combinations", 2)
     for metric_key, metric in dm["metrics"].items():
         for source_key in metric["sources"]:
             if source_key not in universal_sources:
