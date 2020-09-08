c1312fa39bf7f07535ed5d8e237849c5c9ef421d
Frank Niessink <frank@niessink.com>
2020-09-08 18:05:29 +0200

    Increase test coverage.

--- components/frontend/src/metric/SourceStatus.test.js
+++ components/frontend/src/metric/SourceStatus.test.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { SourceStatus } from './SourceStatus';
 
+const metric = { sources: { source_uuid: { name: "Source name" } } };
+
 describe("<SourceStatus />", () => {
     it('renders the hyperlink label if the the source has a landing url', () => {
         const wrapper = mount(<SourceStatus
-            datamodel={{}} metric={{ sources: { source_uuid: { name: "Source name", landing_url: "https://landing" } } }}
-            source={{}} source_uuid="source_uuid"
+            datamodel={{}} metric={metric} source={{ landing_url: "https://landing"}} source_uuid="source_uuid"
         />);
-        expect(wrapper.text()).toBe("Source name");
+        expect(wrapper.find("HyperLink").text()).toBe("Source name");
     });
     it('renders the source label if there is no error', () => {
         const wrapper = mount(<SourceStatus
-            datamodel={{}} metric={{ sources: { source_uuid: { name: "Source name" } } }}
-            source={{}} source_uuid="source_uuid"
+            datamodel={{}} metric={metric} source={{}} source_uuid="source_uuid"
         />);
         expect(wrapper.text()).toBe("Source name");
     });
     it('renders the source label if there is an error', () => {
         const wrapper = mount(<SourceStatus
-            datamodel={{}} metric={{ sources: { source_uuid: { name: "Source name" } } }}
-            source={{ connection_error: "error" }} source_uuid="source_uuid"
+            datamodel={{}} metric={metric} source={{ connection_error: "error" }} source_uuid="source_uuid"
         />);
         expect(wrapper.find("Popup").text()).toBe("Source name");
     });
