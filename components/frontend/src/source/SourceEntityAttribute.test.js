0db75bc3e4fde2fc5b44eb60ee134c296a5f38dc
Frank Niessink <frank@niessink.com>
2020-09-08 17:33:09 +0200

    Increase test coverage.

--- components/frontend/src/source/SourceEntityAttribute.test.js
+++ components/frontend/src/source/SourceEntityAttribute.test.js
@@ -16,4 +16,9 @@ describe('<SourceEntityAttribute />', () => {
       <SourceEntityAttribute entity={{status: "target_met"}} entity_attribute={{key: "status", type: "status"}} />);
     expect(wrapper.find(StatusIcon).prop("status")).toBe("target_met");
   });
+  it('renders an url', () => {
+    const wrapper = shallow(
+      <SourceEntityAttribute entity={{status: "target_met", url: "https://url"}} entity_attribute={{key: "status", type: "status", url: "url"}} />);
+    expect(wrapper.find("HyperLink").prop("url")).toBe("https://url");
+  });
 });
