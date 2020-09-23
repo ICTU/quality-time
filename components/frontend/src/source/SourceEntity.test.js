a72a1ff2a82d42d302f75394b4a941b651fb7377
Frank Niessink <frank@niessink.com>
2020-09-23 23:09:45 +0200

    Increase test coverage.

--- components/frontend/src/source/SourceEntity.test.js
+++ components/frontend/src/source/SourceEntity.test.js
@@ -4,7 +4,7 @@ import { mount } from 'enzyme';
 import { SourceEntity } from './SourceEntity';
 
 describe('<SourceEntity />', () => {
-    it('render', () => {
+    it('renders', () => {
         const wrapper = mount(
             <Table>
                 <Table.Body>
@@ -13,5 +13,25 @@ describe('<SourceEntity />', () => {
             </Table>
         );
         expect(wrapper.find("TableRow").hasClass("status_unknown"))
+    });
+    it('renders a line through the entity if the the entity is marked as false positive', () => {
+        const wrapper = mount(
+            <Table>
+                <Table.Body>
+                    <SourceEntity entity_attributes={["attr"]} entity={{ key: "1" }} status={"false_positive"} />
+                </Table.Body>
+            </Table>
+        );
+        expect(wrapper.find("TableRow").prop("style")).toHaveProperty("textDecoration", "line-through");
+    });
+    it('does not render the entity if the the entity is marked as false positive and hidden', () => {
+        const wrapper = mount(
+            <Table>
+                <Table.Body>
+                    <SourceEntity entity_attributes={["attr"]} entity={{ key: "1" }} status={"false_positive"} hide_ignored_entities={true} />
+                </Table.Body>
+            </Table>
+        );
+        expect(wrapper.find("SourceEntity").isEmptyRender()).toBe(true);
     });
 });
