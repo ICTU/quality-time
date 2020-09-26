54008e7c6d35a5f923003df15397ab0be3e8a5bf
Frank Niessink <frank@niessink.com>
2020-09-26 23:45:29 +0200

    Remove duplication.

--- components/server/tests/routes/test_reports.py
+++ components/server/tests/routes/test_reports.py
@@ -15,11 +15,19 @@ class ReportsTest(unittest.TestCase):
         self.database = Mock()
         self.email = "jenny@example.org"
         self.database.sessions.find_one.return_value = dict(user="Jenny", email=self.email)
+        self.database.datamodels.find_one.return_value = dict(
+            _id="id",
+            sources=dict(source_type=dict(parameters=dict(url=dict(type="url"), password=dict(type="password")))),
+            metrics=dict(metric_type=dict(default_scale="count")))
+        self.database.reports_overviews.find_one.return_value = dict(_id="id", title="Reports", subtitle="")
+        self.database.measurements.find.return_value = [
+            dict(
+                _id="id", metric_uuid=METRIC_ID, status="red",
+                sources=[dict(source_uuid=SOURCE_ID, parse_error=None, connection_error=None, value="42")])]
 
     @patch("bottle.request")
     def test_post_reports_attribute_title(self, request):
         """Test that a reports (overview) attribute can be changed."""
-        self.database.reports_overviews.find_one.return_value = dict(_id="id", title="Reports")
         request.json = dict(title="All the reports")
         self.assertEqual(dict(ok=True), post_reports_attribute("title", self.database))
         inserted = self.database.reports_overviews.insert.call_args_list[0][0][0]
@@ -31,7 +39,6 @@ class ReportsTest(unittest.TestCase):
     @patch("bottle.request")
     def test_post_reports_attribute_title_unchanged(self, request):
         """Test that the reports (overview) attribute is not changed if the new value is equal to the old value."""
-        self.database.reports_overviews.find_one.return_value = dict(_id="id", title="Reports")
         request.json = dict(title="Reports")
         self.assertEqual(dict(ok=True), post_reports_attribute("title", self.database))
         self.database.reports_overviews.insert.assert_not_called()
@@ -39,7 +46,6 @@ class ReportsTest(unittest.TestCase):
     @patch("bottle.request")
     def test_post_reports_attribute_layout(self, request):
         """Test that a reports (overview) layout can be changed."""
-        self.database.reports_overviews.find_one.return_value = dict(_id="id", title="Reports")
         request.json = dict(layout=[dict(x=1, y=2)])
         self.assertEqual(dict(ok=True), post_reports_attribute("layout", self.database))
         inserted = self.database.reports_overviews.insert.call_args_list[0][0][0]
@@ -48,15 +54,6 @@ class ReportsTest(unittest.TestCase):
 
     def test_get_report(self):
         """Test that a report can be retrieved and credentials are hidden."""
-        self.database.datamodels.find_one.return_value = dict(
-            _id="id",
-            sources=dict(source_type=dict(parameters=dict(url=dict(type="url"), password=dict(type="password")))),
-            metrics=dict(metric_type=dict(default_scale="count")))
-        self.database.reports_overviews.find_one.return_value = dict(_id="id", title="Reports", subtitle="")
-        self.database.measurements.find.return_value = [
-            dict(
-                _id="id", metric_uuid=METRIC_ID, status="red",
-                sources=[dict(source_uuid=SOURCE_ID, parse_error=None, connection_error=None, value="42")])]
         report = create_report()
         self.database.reports.find.return_value = [report]
         report["summary"] = dict(red=0, green=0, yellow=0, grey=0, white=1)
@@ -68,15 +65,6 @@ class ReportsTest(unittest.TestCase):
     def test_get_old_report(self, request):
         """Test that an old report can be retrieved and credentials are hidden."""
         request.query = dict(report_date="2020-08-31T23:59:59.000Z")
-        self.database.datamodels.find_one.return_value = dict(
-            _id="id",
-            sources=dict(source_type=dict(parameters=dict(url=dict(type="url"), password=dict(type="password")))),
-            metrics=dict(metric_type=dict(default_scale="count")))
-        self.database.reports_overviews.find_one.return_value = dict(_id="id", title="Reports", subtitle="")
-        self.database.measurements.find.return_value = [
-            dict(
-                _id="id", metric_uuid=METRIC_ID, status="red",
-                sources=[dict(source_uuid=SOURCE_ID, parse_error=None, connection_error=None, value="42")])]
         report = create_report()
         self.database.reports.distinct.return_value = [REPORT_ID]
         self.database.reports.find_one.return_value = report
