ca338ded8e0bde4d561a9c2a14903a7408b14838
Frank Niessink <frank@niessink.com>
2020-09-27 22:16:27 +0200

    Add the total number of tests to the Jenkins test report results.

--- components/collector/tests/source_collectors/api_source_collectors/test_jenkins_test_report.py
+++ components/collector/tests/source_collectors/api_source_collectors/test_jenkins_test_report.py
@@ -23,7 +23,7 @@ class JenkinsTestReportTest(SourceCollectorTestCase):
         metric = dict(type="tests", addition="sum", sources=self.sources)
         response = await self.collect(metric, get_request_json_return_value=jenkins_json)
         self.assert_measurement(
-            response, value="2",
+            response, value="2", total="2",
             entities=[
                 dict(class_name="c1", key="tc1", name="tc1", test_result="failed", age="1"),
                 dict(class_name="c2", key="tc2", name="tc2", test_result="passed", age="0")])
@@ -42,7 +42,7 @@ class JenkinsTestReportTest(SourceCollectorTestCase):
         metric = dict(type="tests", addition="sum", sources=self.sources)
         response = await self.collect(metric, get_request_json_return_value=jenkins_json)
         self.assert_measurement(
-            response, value="2",
+            response, value="2", total="2",
             entities=[
                 dict(class_name="c1", key="tc1", name="tc1", test_result="failed", age="2"),
                 dict(class_name="c2", key="tc2", name="tc2", test_result="passed", age="0")])
@@ -57,12 +57,12 @@ class JenkinsTestReportTest(SourceCollectorTestCase):
         metric = dict(type="tests", addition="sum", sources=self.sources)
         response = await self.collect(metric, get_request_json_return_value=jenkins_json)
         expected_entities = [dict(class_name="c2", key="tc2", name="tc2", test_result="passed", age="0")]
-        self.assert_measurement(response, value="1", entities=expected_entities)
+        self.assert_measurement(response, value="1", total="2", entities=expected_entities)
 
     async def test_nr_of_failed_tests(self):
         """Test that the number of failed tests is returned."""
         jenkins_json = dict(
-            failCount=2, suites=[dict(
+            failCount=2, passCount=1, suites=[dict(
                 cases=[dict(status="FAILED", name="tc1", className="c1", age=1),
                        dict(status="FAILED", name="tc2", className="c2", age=2),
                        dict(status="PASSED", name="tc3", className="c3", age=0)])])
@@ -72,7 +72,7 @@ class JenkinsTestReportTest(SourceCollectorTestCase):
         expected_entities = [
             dict(class_name="c1", key="tc1", name="tc1", test_result="failed", age="1"),
             dict(class_name="c2", key="tc2", name="tc2", test_result="failed", age="2")]
-        self.assert_measurement(response, value="2", entities=expected_entities)
+        self.assert_measurement(response, value="2", total="3", entities=expected_entities)
 
     async def test_source_up_to_dateness(self):
         """Test that the source age in days is returned."""
