43e39e0a4ab5e598223c9426fb252f4a0b20171c
HHSCasRos <16094190@student.hhs.nl>
2020-09-30 14:00:48 +0200

    Fixed compatibility issue with unittests and Windows (#1512)

--- components/server/ci/unittest.sh
+++ components/server/ci/unittest.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export PYTHONDEVMODE=1
-export PYTHONPATH=src:$PYTHONPATH
+# For Windows compatibility; prevent path from ending with a ':'
+export PYTHONPATH=`python -c 'import sys;print(":".join(sys.argv[1:]))' src $PYTHONPATH`
 export COVERAGE_RCFILE=../../.coveragerc
 coverage run -m unittest --quiet
 coverage xml
