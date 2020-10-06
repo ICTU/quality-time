a3b18ec4f9423c8c7be01a97770a3da012781956
Ros, C. (16094190) <16094190@student.hhs.nl>
2020-10-06 16:33:14 +0200

    added the functionality to send notifications to teams in microsoft Teams r with '#' will be ignored, and an empty message aborts the commit.

--- components/notifier/src/quality_time_notifier.py
+++ components/notifier/src/quality_time_notifier.py
@@ -5,6 +5,10 @@ import logging
 import os
 
 import aiohttp
+import pymsteams
+
+from strategies.reds_per_report import reds_per_report
+from outbox import Outbox
 
 
 async def notify(log_level: int = None) -> None:
@@ -16,16 +20,20 @@ async def notify(log_level: int = None) -> None:
                 f"http://{os.environ.get('SERVER_HOST', 'localhost')}:"
                 f"{os.environ.get('SERVER_PORT', '5001')}/api/v3/reports")
             json = await response.json()
-            red_metrics = 0
-            for report in json["reports"]:
-                for subject in report["subjects"].values():
-                    for metric in subject["metrics"].values():
-                        print(metric["status"], flush=True)
-                        if metric["status"] == "target_met":
-                            red_metrics = red_metrics + 1
 
-            print(red_metrics, flush=True)
-            await asyncio.sleep(60)
+            reds_in_reports = reds_per_report(json)
+
+            notification = "number of red metrics in each report:"
+            for report in reds_in_reports:
+                notification += "\n\r - " + report[0] + ": " + str(report[1])
+            print(notification)
+
+            my_teams_message = pymsteams.connectorcard(
+                "https://outlook.office.com/webhook/0bc50e67-b5b0-4b07-b900-ffef52307ba0@6b1d3da2-3751-4e3d-b3c9-e6784c8bad70/IncomingWebhook/8deeb32e0e6a46a9b6c9c134ea279a10/ecf08313-c34b-4671-abe9-ea5e67981eda")
+            my_teams_message.text(notification)
+            my_teams_message.send()
+
+            await asyncio.sleep(os.environ.get('NOTIFIER_SLEEP_DURATION', 60))
 
 
 if __name__ == "__main__":
