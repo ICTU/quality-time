572d42991e4b3fd05ba4952b3122ce52c875447c
Frank Niessink <frank@niessink.com>
2020-10-14 23:01:24 +0200

    Increase test coverage.

--- components/frontend/src/metric/Measurement.test.js
+++ components/frontend/src/metric/Measurement.test.js
@@ -30,7 +30,33 @@ const data_model = {
   }
 };
 
-function measurement(visibleColumns=[]) {
+let report;
+
+beforeEach(() => {
+  report = {
+    report_uuid: "report_uuid",
+    subjects: {
+      subject_uuid: {
+        name: "Subject",
+        metrics: {
+          metric_uuid: {
+            name: "Metric",
+            accept_debt: false,
+            tags: [],
+            type: "violations",
+            sources: [],
+            status: "target_not_met",
+            scale: "count",
+            value: "50",
+            recent_measurements: [{ sources: [{ name: "Source", source_uuid: "1" }] }]
+          }
+        }
+      }
+    }
+  }
+});
+
+function measurement(visibleColumns = []) {
   return (
     mount(
       <Table>
@@ -39,6 +65,7 @@ function measurement(visibleColumns=[]) {
             datamodel={data_model}
             hiddenColumns={[]}
             metric_uuid="metric_uuid"
+            previousMeasurementDaysEarlier={3}
             report={report}
             reports={[report]}
             subject_uuid="subject_uuid"
@@ -74,7 +101,18 @@ it('renders the minutes as percentage', () => {
   expect(wrapper.find("TableCell").at(5).text()).toBe("≦ 0% minutes");
 });
 
-it('renders the previous measurement column', () => {
+it('renders the previous measurement column without measurement', () => {
   const wrapper = measurement(['prev1']);
-  expect(wrapper.find("TableCell").at(5).text()).toBe("?% minutes");
+  expect(wrapper.find("TableCell").at(5).text()).toBe("? violations");
+});
+
+it('renders the previous measurement column with measurement', () => {
+  let four_days_ago = new Date();
+  four_days_ago.setDate(four_days_ago.getDate() - 4);
+  const now = new Date();
+  report.subjects.subject_uuid.metrics.metric_uuid.recent_measurements[0].start = four_days_ago.toISOString();
+  report.subjects.subject_uuid.metrics.metric_uuid.recent_measurements[0].end = now.toISOString();
+  report.subjects.subject_uuid.metrics.metric_uuid.recent_measurements[0].count = { value: 50 };
+  const wrapper = measurement(['prev1']);
+  expect(wrapper.find("TableCell").at(5).text()).toBe("50 violations");
 });
