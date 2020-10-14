68978815f9172a67e8892702a3a3877458df4a7c
Frank Niessink <frank@niessink.com>
2020-10-14 12:10:52 +0200

    Ignored code that is sometimes reported as uncovered.

--- components/server/src/initialization/report.py
+++ components/server/src/initialization/report.py
@@ -13,22 +13,28 @@ from server_utilities.functions import uuid
 
 def initialize_reports_overview(database: Database) -> None:
     """Initialize the reports overview if not present in the database."""
-    if latest_reports_overview(database):
+    # The coverage measurement of the behave feature tests is unstable. Most of the time it reports the last two lines
+    # as covered, sometimes not. It's unclear why. To prevent needless checking of the coverage report coverage
+    # measurement of the last two lines and the if-statement has been turned off.
+    if latest_reports_overview(database):  # pragma: no-cover behave
         logging.info("Skipping initializing reports overview; it already exists")
     else:
-        logging.info("Initializing reports overview")
-        insert_new_reports_overview(database, dict(title="Reports", subtitle=""))
+        logging.info("Initializing reports overview")  # pragma: no-cover behave
+        insert_new_reports_overview(database, dict(title="Reports", subtitle=""))  # pragma: no-cover behave
 
 
 def import_report(database: Database, filename: pathlib.Path) -> None:
     """Read the report and store it in the database."""
+    # The coverage measurement of the behave feature tests is unstable. Most of the time it reports the last two lines
+    # as covered, sometimes not. It's unclear why. To prevent needless checking of the coverage report coverage
+    # measurement of the last two lines and the if-statement has been turned off.
     with filename.open() as json_report:
         imported_report = json.load(json_report)
-    if report_exists(database, imported_report["report_uuid"]):
+    if report_exists(database, imported_report["report_uuid"]):  # pragma: no-cover behave
         logging.info("Skipping import of %s; it already exists", filename)
-        return
-    import_json_report(database, imported_report)
-    logging.info("Report %s imported", filename)
+    else:
+        import_json_report(database, imported_report)  # pragma: no-cover behave
+        logging.info("Report %s imported", filename)  # pragma: no-cover behave
 
 
 def import_json_report(database: Database, imported_report):
