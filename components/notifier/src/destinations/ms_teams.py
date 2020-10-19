12d73973df90706f66b0500feea1fe9b826a8b3a
Ros, C. (16094190) <16094190@student.hhs.nl>
2020-10-19 14:56:10 +0200

    wip

--- components/notifier/src/destinations/ms_teams.py
+++ components/notifier/src/destinations/ms_teams.py
@@ -18,6 +18,15 @@ def build_notification_text(text_parameters) -> str:
     return f'{salutation}, {report_link} has {nr_red} metric{plural_s} that turned red.'
 
 
+def build_notification_detailed_text_partial(text_parameters) -> str:
+    """Create and format the contents of the notification."""
+    nr_red = text_parameters["new_red_metrics"]
+    plural_s = "s" if nr_red > 1 else ""
+    report_link = f'[{text_parameters["report_title"]}]({text_parameters["url"]})'
+    salutation = random.choice(SALUTATIONS)  # nosec, Not used for cryptography
+    return f'{salutation}, {report_link} has {nr_red} metric{plural_s} that turned red.'
+
+
 def send_notification_to_teams(destination: str, text: str) -> None:
     """Send notification to Microsoft Teams using a Webhook."""
     logging.info("Sending notification to configured webhook")
