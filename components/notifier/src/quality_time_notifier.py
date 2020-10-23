c320088f350efeb231dd6495f86aed112cf95096
Ros, C. (16094190) <16094190@student.hhs.nl>
2020-10-23 14:18:21 +0200

    reordered imports

--- components/notifier/src/quality_time_notifier.py
+++ components/notifier/src/quality_time_notifier.py
@@ -4,10 +4,10 @@ import asyncio
 import logging
 import os
 import traceback
+import aiohttp
 from datetime import datetime
 
 from typing import Final, cast
-import aiohttp
 
 from destinations.ms_teams import build_notification_text, send_notification_to_teams
 from strategies.reds_that_are_new import get_notable_metrics_from_json
