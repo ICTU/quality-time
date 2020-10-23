04a964698d4d641792c338635eb1e1a45b4c4012
Ros, C. (16094190) <16094190@student.hhs.nl>
2020-10-23 13:57:10 +0200

    fixed codesmell by splitting get_notable_metrics_from_json into searching for the notable metric, and building the returned dict

--- components/notifier/src/strategies/reds_that_are_new.py
+++ components/notifier/src/strategies/reds_that_are_new.py
@@ -12,22 +12,25 @@ def get_notable_metrics_from_json(json, most_recent_measurement_seen: str) -> Li
         for subject in report["subjects"].values():
             for metric in subject["metrics"].values():
                 if turned_red(metric, most_recent_measurement_seen):
-                    red_metrics.append(dict(
-                        metric_type=metric["type"],
-                        metric_name=metric["name"],
-                        metric_unit=metric["unit"],
-                        new_metric_status=metric["recent_measurements"][-1]["count"]["status"],
-                        new_metric_value=metric["recent_measurements"][-1]["count"]["value"]
-                    ))
-                    if len(metric["recent_measurements"]) > 1:
-                        red_metrics[-1]["old_metric_status"] = metric["recent_measurements"][-2]["count"]["status"]
-                        red_metrics[-1]["old_metric_value"] = metric["recent_measurements"][-2]["count"]["value"]
+                    red_metrics.append(create_metric(metric))
         if webhook and len(red_metrics) > 0:
             notifications.append(
                 dict(report_uuid=report["report_uuid"], report_title=report["title"], teams_webhook=webhook,
                      url=report.get("url"), new_red_metrics=len(red_metrics), metrics=red_metrics))
     return notifications
 
+def create_metric(metric):
+    result = dict(
+        metric_type=metric["type"],
+        metric_name=metric["name"],
+        metric_unit=metric["unit"],
+        new_metric_status=metric["recent_measurements"][-1]["count"]["status"],
+        new_metric_value=metric["recent_measurements"][-1]["count"]["value"]
+    )
+    if len(metric["recent_measurements"]) > 1:
+        result["old_metric_status"] = metric["recent_measurements"][-2]["count"]["status"]
+        result["old_metric_value"] = metric["recent_measurements"][-2]["count"]["value"]
+    return result
 
 def turned_red(metric, most_recent_measurement_seen: str) -> bool:
     """Determine if a metric turned red after the timestamp of the most recent measurement seen."""
