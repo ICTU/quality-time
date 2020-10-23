701386eb15530f99aab7f92c6fabd5f325882cca
Ros, C. (16094190) <16094190@student.hhs.nl>
2020-10-23 15:58:54 +0200

    order

--- components/notifier/tests/test_quality_time_notifier.py
+++ components/notifier/tests/test_quality_time_notifier.py
@@ -6,6 +6,7 @@ import unittest
 from datetime import datetime
 
 from unittest.mock import AsyncMock, mock_open, Mock, patch
+
 from quality_time_notifier import most_recent_measurement_timestamp, notify, record_health, retrieve_data_model
 
 
