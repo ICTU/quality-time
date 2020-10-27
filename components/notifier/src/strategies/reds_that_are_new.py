9b74291416e92e8855e547c444ea898b51a2dc28
HHSCasRos <16094190@student.hhs.nl>
2020-10-27 09:38:53 +0100

    Update components/notifier/src/strategies/reds_that_are_new.py
    Co-authored-by: Frank Niessink <frank@niessink.com>

--- components/notifier/src/strategies/reds_that_are_new.py
+++ components/notifier/src/strategies/reds_that_are_new.py
@@ -56,14 +56,13 @@ def turned_red(metric, most_recent_measurement_seen: str) -> bool:
         metric_is_red and recent_measurements and recent_measurements[-1]["start"] > most_recent_measurement_seen)
 
 
-def turned_white(metric, most_recent_measurement_seen: str) -> bool:
-    """Determine if a metric turned white after the timestamp of the most recent measurement seen."""
+def has_new_status(metric, new_status: str, most_recent_measurement_seen: str) -> bool:
+    """Determine if a metric got a new status after the timestamp of the most recent measurement seen."""
+    recent_measurements = metric.get("recent_measurements") or []
+    if len(recent_measurements) < 2:
+        return False  # If there are fewer than two measurements, the metric didn't recently change status
+    metric_has_status = metric["status"] == new_status
     scale = metric["scale"]
-    metric_is_white = metric["status"] == "unknown"
-    recent_measurements = metric.get("recent_measurements")
-    if recent_measurements and len(recent_measurements) > 1:
-        metric_was_not_white = recent_measurements[-2][scale]["status"] != "unknown"
-        return bool(
-            metric_is_white and metric_was_not_white and
-            recent_measurements[-1]["start"] > most_recent_measurement_seen)
-    return False
+    metric_had_other_status = recent_measurements[-2][scale]["status"] != new_status
+    change_was_recent = recent_measurements[-1]["start"] > most_recent_measurement_seen
+    return bool(metric_has_status and metric_had_other_status and change_was_recent)
