29f1e3bb18e0446eac84fc1dff354845fa9db1be
Frank Niessink <frank@niessink.com>
2020-10-30 16:25:12 +0100

    Increase test coverage.

--- components/frontend/src/metric/TrendTable.test.js
+++ components/frontend/src/metric/TrendTable.test.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { TrendTable } from './TrendTable';
 
-it('renders 8 columns with dates', () => {
-  render(<TrendTable measurements={[]} metric={{ direction: "<" }} report_date="2020-10-10" />);
-  expect(screen.getAllByText(/\d\d\d\d/).length).toBe(8)
-});
-
-it('renders past measurements', () => {
+it('renders the trend starting from the current date', () => {
   render(
     <TrendTable
-      measurements={[{ count: { value: "42" }, start: "2019-09-29", end: "2019-10-01" }]}
-      metric={{ direction: "<" }} report_date="2019-09-30" scale="count"
+      measurements={[
+        { count: { value: "33", target: "10", near_target: "20" }, start: "2019-09-25", end: "2019-09-30" },
+        { count: { value: "34", target: "10", near_target: "20" }, start: "2019-09-10", end: "2019-09-25" },
+      ]}
+      metric={{ direction: "<", value: "42", target: "10"}} report_date={null} trendTableInterval={1} trendTableNrDates={2}
+      scale="count"
     />
   );
   expect(screen.getAllByText(/42/).length).toBe(1)
+  expect(screen.getAllByText(/\?/).length).toBe(2)
+  expect(screen.getAllByText(/\d\d\d\d/).length).toBe(2)
 });
 
-it('renders current measurement', () => {
+it('renders the trend starting from a fixed date', () => {
   render(
     <TrendTable
-      measurements={[{ count: { value: "1", target: "10", near_target: "20" }, start: "2019-09-29", end: "2019-09-30" }]}
-      metric={{ direction: "<", value: 42 }} report_date={null} scale="count"
+      measurements={[
+        { count: { value: "33", target: "10", near_target: "20" }, start: "2019-09-25", end: "2019-09-30" },
+        { count: { value: "34", target: "10", near_target: "20" }, start: "2019-09-10", end: "2019-09-25" },
+      ]}
+      metric={{ direction: "<" }} report_date={"2019-09-29"} trendTableInterval={1} trendTableNrDates={2}
+      scale="count"
     />
   );
-  expect(screen.getAllByText(/42/).length).toBe(1)
+  expect(screen.getAllByText(/33/).length).toBe(1)
+  expect(screen.getAllByText(/34/).length).toBe(1)
+  expect(screen.getAllByText(/10/).length).toBe(2)
+  expect(screen.getAllByText(/\d\d\d\d/).length).toBe(2)
 });
