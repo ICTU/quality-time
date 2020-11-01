b1d0de1bb6dcfafb748d04f78f576b83a37fec4e
Frank Niessink <frank@niessink.com>
2020-11-01 19:28:44 +0100

    Increase test coverage.

--- components/frontend/src/header_footer/Menubar.test.js
+++ components/frontend/src/header_footer/Menubar.test.js
@@ -15,13 +15,9 @@ it('scrolls to the dashboard', async () => {
 });
 
 it('does not crash if there is no dashboard', async () => {
-  const scrollIntoView = jest.fn();
-  scrollIntoView.mockImplementation(() => { return { scrollIntoView: jest.fn() } });
   Object.defineProperty(global.document, 'getElementById', {});
-  Object.defineProperty(global.window, 'scrollBy', { value: jest.fn() });
   await act(async () => {
     render(<Menubar report_date_string="2019-10-10" onDate={console.log} />);
     fireEvent.click(screen.getByLabelText(/Scroll to dashboard/));
   });
-  expect(scrollIntoView).not.toHaveBeenCalled()
 });
