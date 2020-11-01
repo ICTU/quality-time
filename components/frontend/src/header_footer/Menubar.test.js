74957841ea8ea0f0b0f801f23d06c31718c6b447
Frank Niessink <frank@niessink.com>
2020-11-01 19:52:27 +0100

    Fix test.

--- components/frontend/src/header_footer/Menubar.test.js
+++ components/frontend/src/header_footer/Menubar.test.js
@@ -5,8 +5,8 @@ import { Menubar } from './Menubar';
 it('scrolls to the dashboard', async () => {
   const scrollIntoView = jest.fn();
   scrollIntoView.mockImplementation(() => { return { scrollIntoView: jest.fn() } });
-  Object.defineProperty(global.document, 'getElementById', { value: scrollIntoView });
-  Object.defineProperty(global.window, 'scrollBy', { value: jest.fn() });
+  Object.defineProperty(global.document, 'getElementById', { value: scrollIntoView, configurable: true });
+  Object.defineProperty(global.window, 'scrollBy', { value: jest.fn(), configurable: true });
   await act(async () => {
     render(<Menubar report_date_string="2019-10-10" onDate={console.log} />);
     fireEvent.click(screen.getByLabelText(/Scroll to dashboard/));
@@ -15,7 +15,7 @@ it('scrolls to the dashboard', async () => {
 });
 
 it('does not crash if there is no dashboard', async () => {
-  Object.defineProperty(global.document, 'getElementById', {value: null});
+  Object.defineProperty(global.document, 'getElementById', {value: null, configurable: true});
   await act(async () => {
     render(<Menubar report_date_string="2019-10-10" onDate={console.log} />);
     fireEvent.click(screen.getByLabelText(/Scroll to dashboard/));
