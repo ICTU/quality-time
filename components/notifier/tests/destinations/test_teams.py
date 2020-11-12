22cdafad0fc23d93219cac7e0f3d54618cea5754
HHSCasRos <16094190@student.hhs.nl>
2020-11-12 22:44:35 +0100

    iks355 fixed unknown values having an misleading appearance in the notification text (#1670)
    Fixed unknown values having an misleading appearance in the notification text
    
    Co-authored-by: Frank Niessink <frank@niessink.com>

--- components/notifier/tests/destinations/test_teams.py
+++ components/notifier/tests/destinations/test_teams.py
@@ -58,3 +58,22 @@ class BuildNotificationTextTests(TestCase):
             "* Metric status is red (target not met), was yellow (near target met). Value is 42 units, was 0 units.\n"
             "* Metric status is red (target not met), was green (target met). Value is 10 units, was 5 units.\n",
             text)
+
+    def test_unknown_text(self):
+        """Test that the text is correct."""
+        text = build_notification_text(
+            dict(
+                report_uuid="report1", report_title="Report 1", url="https://report1",
+                metrics=[
+                    dict(
+                        metric_type="metric_type",
+                        metric_name="Metric",
+                        metric_unit="units",
+                        old_metric_status="yellow (near target met)",
+                        old_metric_value=0,
+                        new_metric_status="white (unknown)",
+                        new_metric_value=None)]))
+        self.assertEqual(
+            "[Report 1](https://report1) has 1 metric that changed status:\n\n"
+            "* Metric status is white (unknown), was yellow (near target met). Value is ? units, was 0 units.\n",
+            text)
