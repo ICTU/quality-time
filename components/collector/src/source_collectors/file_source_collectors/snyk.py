09f5e111a4cbd52248a1cdd096f0e0ac86cabda9
bryan <b.soman@onvio.nl>
2020-11-15 17:27:38 +0100

    Snyk consolidated findings (#1641)
    * Consolidated Snyk Findings
    * Add filtering by severity.
    
    Co-authored-by: Frank Niessink <frank@niessink.com>

--- components/collector/src/source_collectors/file_source_collectors/snyk.py
+++ components/collector/src/source_collectors/file_source_collectors/snyk.py
@@ -1,30 +1,52 @@
 """Snyk metrics collector."""
 
+from typing import Collection, Dict, Literal, Set
+
 from base_collectors import JSONFileSourceCollector
-from collector_utilities.functions import md5_hash
 from source_model import Entity, SourceMeasurement, SourceResponses
+
+
+Severity = Literal["low", "medium", "high"]
 
 
 class SnykSecurityWarnings(JSONFileSourceCollector):
     """Snyk collector for security warnings."""
 
     async def _parse_source_responses(self, responses: SourceResponses) -> SourceMeasurement:
-        entities = []
-        severities = self._parameter("severities")
+        """Parse the direct dependencies with vulnerabilities from the responses."""
+        selected_severities = self._parameter("severities")
+        severities: Dict[str, Set[Severity]] = {}
+        nr_vulnerabilities: Dict[str, int] = {}
+        example_vulnerability = {}
         for response in responses:
             json = await response.json(content_type=None)
-            vulnerabilities = json.get("vulnerabilities", []) if isinstance(json, dict) else []
+            vulnerabilities = json.get("vulnerabilities", [])
             for vulnerability in vulnerabilities:
-                if vulnerability["severity"].lower() not in severities:
+                if (severity := vulnerability["severity"]) not in selected_severities:
                     continue
-                package_include = " ➜ ".join([str(package) for package in vulnerability["from"][1:]]) \
-                    if isinstance(vulnerability["from"], list) else vulnerability["from"]
-                fix = ", ".join([str(package) for package in vulnerability["fixedIn"]]) \
-                    if isinstance(vulnerability["fixedIn"], list) else vulnerability["fixedIn"]
-                key = md5_hash(f'{vulnerability["id"]}:{package_include}')
-                entities.append(
-                    Entity(
-                        key=key, cve=vulnerability["title"], package=vulnerability["packageName"],
-                        severity=vulnerability["severity"], version=vulnerability['version'],
-                        package_include=package_include, fix=fix, url=f"https://snyk.io/vuln/{vulnerability['id']}"))
+                dependency = vulnerability["from"][1] if len(vulnerability["from"]) > 1 else vulnerability["from"][0]
+                severities.setdefault(dependency, set()).add(severity)
+                nr_vulnerabilities[dependency] = nr_vulnerabilities.get(dependency, 0) + 1
+                path = " ➜ ".join(str(dependency) for dependency in vulnerability["from"])
+                example_vulnerability[dependency] = (vulnerability["id"], path)
+
+        entities = []
+        for dependency in severities:
+            entities.append(
+                Entity(
+                    key=dependency, dependency=dependency,
+                    nr_vulnerabilities=nr_vulnerabilities[dependency],
+                    example_vulnerability=example_vulnerability[dependency][0],
+                    url=f"https://snyk.io/vuln/{example_vulnerability[dependency][0]}",
+                    example_path=example_vulnerability[dependency][1],
+                    highest_severity=self.__highest_severity(severities[dependency])))
         return SourceMeasurement(entities=entities)
+
+    @staticmethod
+    def __highest_severity(severities: Collection[Severity]) -> Severity:
+        """Return the highest severity from a collection of severities."""
+        if "high" in severities:
+            return "high"
+        if "medium" in severities:
+            return "medium"
+        return "low"
--- components/collector/tests/source_collectors/file_source_collectors/test_snyk.py
+++ components/collector/tests/source_collectors/file_source_collectors/test_snyk.py
@@ -1,39 +1,95 @@
 """Unit tests for the Snyk source."""
 
+from collector_utilities.functions import safe_entity_key
+
 from tests.source_collectors.source_collector_test_case import SourceCollectorTestCase
 
 
 class SnykSecurityWarningsTest(SourceCollectorTestCase):
     """Unit tests for the security warning metric."""
 
-    async def test_warnings(self):
-        """Test the number of security warnings."""
-        css = "Cross Site Scripting"
-        package_name = "vulnerablepackage"
-        package_spec = "vulnerable_package@1.2.0"
-        sources = dict(source_id=dict(type="snyk", parameters=dict(url="snyk-vuln.json", severities=["high"])))
-        metric = dict(type="security_warnings", sources=sources, addition="sum")
-        vulnerabilities_json = dict(
+    def setUp(self):
+        """Prepare the security warnings metric and sources."""
+        super().setUp()
+        self.sources = dict(source_id=dict(type="snyk", parameters=dict(url="snyk.json")))
+        self.metric = dict(type="security_warnings", sources=self.sources, addition="sum")
+        self.direct_dependency = "laravel-mix@4.0.16"
+        self.direct_dependency_key = safe_entity_key(self.direct_dependency)
+        self.direct_dependency_path = ["package.json@*", self.direct_dependency]
+        self.vulnerabilities_json = dict(
             vulnerabilities=[
                 {
-                    'title': css, 'id': 'SNYK-JS-LODASH-590103', 'severity': 'high',
-                    'fixedIn': '3.0', 'packageName': package_name, 'version': '1.2',
-                    'from': ["mainpackage@0.0.0", package_spec]},
-                {
-                    'title': css, 'id': 'SNYK-JS-LODASH-590103', 'severity': 'high',
-                    'fixedIn': ['3.0', '3.1'], 'packageName': package_name, 'version': '1.2',
-                    'from': ["mainpackage@0.0.0", "intermediatepackage@1.0", package_spec]},
-                {
-                    'title': 'SQL Injection', 'id': 'SNYK-JS-LODASH-590104', 'severity': 'low',
-                    'fixedIn': ['3.0', '3.1'], 'packageName': package_name, 'version': '1.4.0',
-                    'from': ["mainpackage@1.0.0", "vulnerablepackage@1.4.0"]}])
+                    "id": "SNYK-JS-ACORN-559469",
+                    "severity": "low",
+                    "from": self.direct_dependency_path + ["webpack@4.41.4", "acorn@6.4.0"]
+                }
+            ]
+        )
+        self.expected_entity = dict(
+            key=self.direct_dependency_key,
+            dependency=self.direct_dependency,
+            nr_vulnerabilities=1, example_vulnerability='SNYK-JS-ACORN-559469',
+            url='https://snyk.io/vuln/SNYK-JS-ACORN-559469',
+            example_path='package.json@* ➜ laravel-mix@4.0.16 ➜ webpack@4.41.4 ➜ acorn@6.4.0',
+            highest_severity='low')
+
+    async def test_one_low_severity_warning(self):
+        """Test only low severity warnings."""
+        response = await self.collect(self.metric, get_request_json_return_value=self.vulnerabilities_json)
+        self.assert_measurement(response, value="1", entities=[self.expected_entity])
+
+    async def test_minimum_severity(self):
+        """Test that security warnings with a lower severity than selected are ignored."""
+        self.sources["source_id"]["parameters"]["severities"] = ["medium", "high"]
+        response = await self.collect(self.metric, get_request_json_return_value=self.vulnerabilities_json)
+        self.assert_measurement(response, value="0", entities=[])
+
+    async def test_one_warning_with_multiple_severities(self):
+        """Test that multiple warnings are combined for one top-level dependency."""
+        self.vulnerabilities_json["vulnerabilities"].extend([
+            {
+                "id": "SNYK-JS-AJV-584908",
+                "severity": "medium",
+                "from": self.direct_dependency_path + ["webpack@4.41.4", "ajv@6.10.2"]
+            },
+            {
+                "id": "SNYK-JS-AJV-584908",
+                "severity": "low",
+                "title": "Prototype Pollution",
+                "from": self.direct_dependency_path + [
+                    "extract-text-webpack-plugin@4.0.0-beta.0", "schema-utils@0.4.7", "ajv@6.10.2"]
+            }
+        ])
         expected_entities = [
             dict(
-                key='c6948b61d82118ab3438d4c847879a00', cve=css, url='https://snyk.io/vuln/SNYK-JS-LODASH-590103',
-                severity='high', fix='3.0', package=package_name, version='1.2', package_include=package_spec),
+                key=self.direct_dependency_key,
+                dependency=self.direct_dependency,
+                nr_vulnerabilities=3, example_vulnerability='SNYK-JS-AJV-584908',
+                url='https://snyk.io/vuln/SNYK-JS-AJV-584908',
+                example_path='package.json@* ➜ laravel-mix@4.0.16 ➜ extract-text-webpack-plugin@4.0.0-beta.0 ➜ '
+                             'schema-utils@0.4.7 ➜ ajv@6.10.2',
+                highest_severity='medium'
+            )
+        ]
+        response = await self.collect(self.metric, get_request_json_return_value=self.vulnerabilities_json)
+        self.assert_measurement(response, value="1", entities=expected_entities)
+
+    async def test_warning_without_indirect_dependencies(self):
+        """Test that warnings are also collected if they have no indirect dependencies."""
+        self.vulnerabilities_json["vulnerabilities"].append(
+            {
+                "id": "SNYK-PHP-LARAVELLARAVEL-609736", "severity": "high", "title": "Improper Input Validation",
+                "from": ["laravel/laravel@6.18.34"]
+            }
+        )
+        expected_entities = [
+            self.expected_entity,
             dict(
-                key='32317b40f5948e02fa530c5ab6380d3d', cve=css, url='https://snyk.io/vuln/SNYK-JS-LODASH-590103',
-                severity='high', fix='3.0, 3.1', package=package_name, version='1.2',
-                package_include=f"intermediatepackage@1.0 ➜ {package_spec}")]
-        response = await self.collect(metric, get_request_json_return_value=vulnerabilities_json)
+                key='laravel-laravel@6_18_34',
+                dependency='laravel/laravel@6.18.34',
+                nr_vulnerabilities=1, example_vulnerability='SNYK-PHP-LARAVELLARAVEL-609736',
+                url='https://snyk.io/vuln/SNYK-PHP-LARAVELLARAVEL-609736',
+                example_path='laravel/laravel@6.18.34',
+                highest_severity='high')]
+        response = await self.collect(self.metric, get_request_json_return_value=self.vulnerabilities_json)
         self.assert_measurement(response, value="2", entities=expected_entities)
