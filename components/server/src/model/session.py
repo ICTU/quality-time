c4e211196542e68c19ae5d71fe3210499d0985ad
Frank Niessink <frank@niessink.com>
2020-11-21 23:07:30 +0100

    Refactoring.

--- components/server/src/model/session.py
+++ components/server/src/model/session.py
@@ -1,23 +1,20 @@
 """Session model class."""
 
 from datetime import datetime
-from typing import List
+from typing import Dict, List, Union, cast
 
 
 class Session:
     """Class representing a user session."""
 
-    def __init__(self, session_data):
-        self.__session_data = session_data
+    def __init__(self, session_data: Dict[str, Union[datetime, str]]) -> None:
+        self.__session_data = session_data or {}
 
     def is_valid(self) -> bool:
         """Return whether the session is valid."""
-        if self.__session_data:
-            return bool(self.__session_data.get("session_expiration_datetime", datetime.min) > datetime.now())
-        return False  # No session exists
+        expiration_datetime = cast(datetime, self.__session_data.get("session_expiration_datetime", datetime.min))
+        return bool(expiration_datetime > datetime.now())
 
     def is_authorized(self, authorized_users: List[str]) -> bool:
         """Return whether the session's user is an authorized user."""
-        if authorized_users:
-            return self.__session_data["email"] in authorized_users
-        return True  # No authorized users specified, so any (logged in) user is authorized
+        return self.__session_data["email"] in authorized_users if authorized_users else True
