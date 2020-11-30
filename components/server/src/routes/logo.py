aab5040f1d57f208438f3c6874872176662c9474
Frank Niessink <frank@niessink.com>
2020-11-30 15:31:50 +0100

    Add checks for completeness of logos.

--- components/server/src/routes/logo.py
+++ components/server/src/routes/logo.py
@@ -9,6 +9,6 @@ LOGOS_ROOT = pathlib.Path(__file__).parent.absolute().joinpath(pathlib.Path("log
 
 
 @bottle.get("/api/v3/logo/<source_type>")
-def logo(source_type: str):
+def get_logo(source_type: str):
     """Return the logo for the source type."""
     return bottle.static_file(f"{source_type}.png", root=LOGOS_ROOT, mimetype="image/png")
