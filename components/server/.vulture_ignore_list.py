603996523353f526b471765490111547774bac7b
Frank Niessink <frank@niessink.com>
2020-11-30 15:40:06 +0100

    Suppress vulture false positive.

--- components/server/.vulture_ignore_list.py
+++ components/server/.vulture_ignore_list.py
@@ -1,5 +1,6 @@
-documentation  # unused import (src/initialization/bottle.py:7)
-notification  # unused import (src/initialization/bottle.py:7)
+documentation  # unused import (src/initialization/bottle.py:14)
+logo  # unused import (src/initialization/bottle.py:15)
+notification  # unused import (src/initialization/bottle.py:18)
 api  # unused variable (src/routes/plugins/authentication_plugin.py:13)
 _.apply  # unused method (src/routes/plugins/authentication_plugin.py:18)
 api  # unused variable (src/routes/plugins/injection_plugin.py:10)
