69c222342d72a3357b4b6f5420def1da895380a3
Frank Niessink <frank@niessink.com>
2020-12-03 23:37:49 +0100

    Add time-ago popup to status.

--- components/frontend/src/metric/StatusIcon.js
+++ components/frontend/src/metric/StatusIcon.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Icon } from 'semantic-ui-react';
+import TimeAgo from 'react-timeago';
+import { Icon, Popup } from 'semantic-ui-react';
 import './StatusIcon.css';
 
-export function StatusIcon({ status }) {
-  const status_icon = {
+export function StatusIcon({ status, status_start }) {
+  const icon_name = {
     target_met: 'check', near_target_met: 'warning', debt_target_met: 'money', target_not_met: 'x',
-    unknown: 'question'};
-  return <Icon className={status} inverted circular size='large' name={status_icon[status || 'unknown']} />
+    unknown: 'question'
+  }[status || "unknown"];
+  const status_name = {
+    target_met: 'Target met', near_target_met: 'Near target met', debt_target_met: 'Debt target met',
+    target_not_met: 'Target not met', unknown: 'Unknown'
+  }[status || "unknown"];
+  const status_start_date = status_start ? new Date(status_start) : new Date();
+  const icon = <Icon className={status} inverted circular size='large' name={icon_name} />;
+  return (status_start ? <Popup trigger={icon} flowing hoverable>{status_name} since <TimeAgo date={status_start}/> ({status_start_date.toLocaleString()})</Popup> : icon);
 }
