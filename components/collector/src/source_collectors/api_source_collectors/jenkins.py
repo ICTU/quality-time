4359c35e5bb6e59700a5cbeaa34146de2a6b1328
Frank Niessink <frank@niessink.com>
2020-12-07 23:10:31 +0100

    Issue 1680 jenkins source up to dateness (#1734)
    Allow using Jenkins jobs as source for the 'source up-to-dateness' metric. Closes #1680.

--- components/collector/src/source_collectors/api_source_collectors/jenkins.py
+++ components/collector/src/source_collectors/api_source_collectors/jenkins.py
@@ -1,6 +1,6 @@
 """Jenkins metric collector."""
 
-from datetime import datetime
+from datetime import datetime, date
 from typing import Iterator, cast
 
 from base_collectors import SourceCollector
@@ -20,9 +20,14 @@ class JenkinsJobs(SourceCollector):
     async def _parse_source_responses(self, responses: SourceResponses) -> SourceMeasurement:
         entities = [
             Entity(
-                key=job["name"], name=job["name"], url=job["url"], build_status=self._build_status(job),
-                build_date=str(self._build_datetime(job).date()) if self._build_datetime(job) > datetime.min else "")
-            for job in self.__jobs((await responses[0].json())["jobs"])]
+                key=job["name"],
+                name=job["name"],
+                url=job["url"],
+                build_status=self._build_status(job),
+                build_date=str(self._build_datetime(job).date()) if self._build_datetime(job) > datetime.min else "",
+            )
+            for job in self.__jobs((await responses[0].json())["jobs"])
+        ]
         return SourceMeasurement(entities=entities)
 
     def __jobs(self, jobs: Jobs, parent_job_name: str = "") -> Iterator[Job]:
@@ -30,47 +35,68 @@ class JenkinsJobs(SourceCollector):
         for job in jobs:
             if parent_job_name:
                 job["name"] = f"{parent_job_name}/{job['name']}"
-            if job.get("buildable") and self._count_job(job):
+            if job.get("buildable") and self._include_job(job):
                 yield job
             for child_job in self.__jobs(job.get("jobs", []), parent_job_name=job["name"]):
                 yield child_job
 
-    def _count_job(self, job: Job) -> bool:
+    def _include_job(self, job: Job) -> bool:
         """Return whether the job should be counted."""
         jobs_to_include = self._parameter("jobs_to_include")
         if len(jobs_to_include) > 0 and not match_string_or_regular_expression(job["name"], jobs_to_include):
             return False
         return not match_string_or_regular_expression(job["name"], self._parameter("jobs_to_ignore"))
 
-    @staticmethod
-    def _build_datetime(job: Job) -> datetime:
+    def _build_datetime(self, job: Job) -> datetime:
         """Return the date and time of the most recent build of the job."""
-        builds = job.get("builds")
-        return datetime.utcfromtimestamp(int(builds[0]["timestamp"]) / 1000.) if builds else datetime.min
+        builds = [build for build in job.get("builds", []) if self._include_build(build)]
+        return datetime.utcfromtimestamp(int(builds[0]["timestamp"]) / 1000.0) if builds else datetime.min
 
-    @staticmethod
-    def _build_status(job: Job) -> str:
-        """Return the build status of the job."""
-        for build in job.get("builds", []):
+    def _build_status(self, job: Job) -> str:
+        """Return the status of the most recent build of the job."""
+        builds = [build for build in job.get("builds", []) if self._include_build(build)]
+        for build in builds:
             if status := build.get("result"):
                 return str(status).capitalize().replace("_", " ")
         return "Not built"
+
+    def _include_build(  # pylint: disable=no-self-use,unused-argument # skipcq: PYL-W0613,PYL-R0201
+        self, build
+    ) -> bool:
+        """Return whether the include this build."""
+        return True
 
 
 class JenkinsFailedJobs(JenkinsJobs):
     """Collector to get failed jobs from Jenkins."""
 
-    def _count_job(self, job: Job) -> bool:
+    def _include_job(self, job: Job) -> bool:
         """Count the job if its build status matches the failure types selected by the user."""
-        return super()._count_job(job) and self._build_status(job) in self._parameter("failure_type")
+        return super()._include_job(job) and self._build_status(job) in self._parameter("failure_type")
 
 
 class JenkinsUnusedJobs(JenkinsJobs):
     """Collector to get unused jobs from Jenkins."""
 
-    def _count_job(self, job: Job) -> bool:
+    def _include_job(self, job: Job) -> bool:
         """Count the job if its most recent build is too old."""
-        if super()._count_job(job) and (build_datetime := self._build_datetime(job)) > datetime.min:
+        if super()._include_job(job) and (build_datetime := self._build_datetime(job)) > datetime.min:
             max_days = int(cast(str, self._parameter("inactive_days")))
             return days_ago(build_datetime) > max_days
         return False
+
+
+class JenkinsSourceUpToDateness(JenkinsJobs):
+    """Collector to get the last build date from Jenkins jobs."""
+
+    def _include_build(self, build) -> bool:
+        """Override to only include builds with an allowed result type."""
+        result_types = self._parameter("result_type")
+        return str(build.get("result", "Not built")).capitalize().replace("_", " ") in result_types
+
+    async def _parse_source_responses(self, responses: SourceResponses) -> SourceMeasurement:
+        """Extend to calculate how many days ago the jobs were built."""
+        measurement = await super()._parse_source_responses(responses)
+        build_dates = [entity["build_date"] for entity in measurement.entities if entity["build_date"]]
+        measurement.value = str((date.today() - date.fromisoformat(max(build_dates))).days) if build_dates else None
+        return measurement
