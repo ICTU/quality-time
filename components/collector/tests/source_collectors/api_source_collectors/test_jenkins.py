3ed8f9e8208ea62a1cc559f8fd43bf079986c93f
Frank Niessink <frank@niessink.com>
2020-12-07 22:24:00 +0100

    Remove duplication.

--- components/collector/tests/source_collectors/api_source_collectors/test_jenkins.py
+++ components/collector/tests/source_collectors/api_source_collectors/test_jenkins.py
@@ -26,6 +26,12 @@ class JenkinsFailedJobsTest(JenkinsTestCase):
         """Extend to set up the metric this class is testing."""
         super().setUp()
         self.metric = dict(type="failed_jobs", sources=self.sources, addition="sum")
+        self.jenkins_json = dict(
+            jobs=[
+                dict(name="job", url=self.job_url, buildable=True, color="red", builds=self.builds),
+                dict(name="job2", url=self.job2_url, buildable=True, color="red", builds=self.builds),
+            ]
+        )
 
     async def test_nr_of_failed_jobs(self):
         """Test that the number of failed jobs is returned."""
@@ -58,13 +64,7 @@ class JenkinsFailedJobsTest(JenkinsTestCase):
     async def test_include_jobs(self):
         """Test that any job that is not explicitly included fails if jobs_to_include is not empty."""
         self.sources["source_id"]["parameters"]["jobs_to_include"] = ["job"]
-        jenkins_json = dict(
-            jobs=[
-                dict(name="job", url=self.job_url, buildable=True, color="red", builds=self.builds),
-                dict(name="job2", url=self.job2_url, buildable=True, color="red", builds=self.builds),
-            ]
-        )
-        response = await self.collect(self.metric, get_request_json_return_value=jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         expected_entities = [
             dict(build_date="2019-03-15", build_status="Failure", key="job", name="job", url=self.job_url)
         ]
@@ -73,13 +73,7 @@ class JenkinsFailedJobsTest(JenkinsTestCase):
     async def test_include_jobs_by_regular_expression(self):
         """Test that any job that is not explicitly included fails if jobs_to_include is not empty."""
         self.sources["source_id"]["parameters"]["jobs_to_include"] = ["job."]
-        jenkins_json = dict(
-            jobs=[
-                dict(name="job", url=self.job_url, buildable=True, color="red", builds=self.builds),
-                dict(name="job2", url=self.job2_url, buildable=True, color="red", builds=self.builds),
-            ]
-        )
-        response = await self.collect(self.metric, get_request_json_return_value=jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         expected_entities = [
             dict(build_date="2019-03-15", build_status="Failure", key="job2", name="job2", url=self.job2_url)
         ]
@@ -88,13 +82,7 @@ class JenkinsFailedJobsTest(JenkinsTestCase):
     async def test_ignore_jobs(self):
         """Test that a failed job can be ignored."""
         self.sources["source_id"]["parameters"]["jobs_to_ignore"] = ["job2"]
-        jenkins_json = dict(
-            jobs=[
-                dict(name="job", url=self.job_url, buildable=True, color="red", builds=self.builds),
-                dict(name="job2", url=self.job2_url, buildable=True, color="red", builds=self.builds),
-            ]
-        )
-        response = await self.collect(self.metric, get_request_json_return_value=jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         expected_entities = [
             dict(build_date="2019-03-15", build_status="Failure", key="job", name="job", url=self.job_url)
         ]
@@ -103,13 +91,7 @@ class JenkinsFailedJobsTest(JenkinsTestCase):
     async def test_ignore_jobs_by_regular_expression(self):
         """Test that failed jobs can be ignored by regular expression."""
         self.sources["source_id"]["parameters"]["jobs_to_ignore"] = ["job."]
-        jenkins_json = dict(
-            jobs=[
-                dict(name="job", url=self.job_url, buildable=True, color="red", builds=self.builds),
-                dict(name="job2", url=self.job2_url, buildable=True, color="red", builds=self.builds),
-            ]
-        )
-        response = await self.collect(self.metric, get_request_json_return_value=jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         expected_entities = [
             dict(build_date="2019-03-15", build_status="Failure", key="job", name="job", url=self.job_url)
         ]
@@ -119,14 +101,10 @@ class JenkinsFailedJobsTest(JenkinsTestCase):
         """Test that jobs can be included and ignored."""
         self.sources["source_id"]["parameters"]["jobs_to_include"] = ["job."]
         self.sources["source_id"]["parameters"]["jobs_to_ignore"] = [".*2"]
-        jenkins_json = dict(
-            jobs=[
-                dict(name="job", url=self.job_url, buildable=True, color="red", builds=self.builds),
-                dict(name="job2", url=self.job2_url, buildable=True, color="red", builds=self.builds),
-                dict(name="job3", url="https://job3", buildable=True, color="red", builds=self.builds),
-            ]
+        self.jenkins_json["jobs"].append(
+            dict(name="job3", url="https://job3", buildable=True, color="red", builds=self.builds)
         )
-        response = await self.collect(self.metric, get_request_json_return_value=jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         expected_entities = [
             dict(build_date="2019-03-15", build_status="Failure", key="job3", name="job3", url="https://job3")
         ]
