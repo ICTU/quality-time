f283dfcdca2de362b66e6de4d6f4ea7fe055d1fa
Frank Niessink <frank@niessink.com>
2020-12-07 22:42:57 +0100

    Fix quality issues.

--- components/collector/src/source_collectors/api_source_collectors/jenkins.py
+++ components/collector/src/source_collectors/api_source_collectors/jenkins.py
@@ -60,7 +60,9 @@ class JenkinsJobs(SourceCollector):
                 return str(status).capitalize().replace("_", " ")
         return "Not built"
 
-    def _include_build(self, build) -> bool:
+    def _include_build(
+        self, build
+    ) -> bool:  # pylint: disable=no-self-use,unused-argument # skipcq: PYL-W0613,PYL-R0201
         """Return whether the include this build."""
         return True
 
