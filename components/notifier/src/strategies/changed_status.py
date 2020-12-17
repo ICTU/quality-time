38286702e359b0968bd0a6e72b1a0ec6a5906585
Ros, C. (16094190) <16094190@student.hhs.nl>
2020-12-17 16:46:43 +0100

    wip

--- components/notifier/src/strategies/changed_status.py
+++ components/notifier/src/strategies/changed_status.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 from models.notification import Notification
 from models.metric_notification_data import MetricNotificationData
 
+
 class NotableEvents:
     """Handle notification contents and status."""
 
@@ -12,7 +13,6 @@ class NotableEvents:
         """."""
         self.already_notified = []
         self.data_model = data_model
-
 
     def get_notable_metrics_from_json(self, json, most_recent_measurement_seen: str) -> List[Notification]:
         """Return the reports that have a webhook and metrics that require notifying."""
@@ -29,10 +29,10 @@ class NotableEvents:
                     notifications.append(Notification(report, notable_metrics, destination_uuid, destination))
         return notifications
 
-
     def check_if_metric_is_notable(self, metric, metric_uuid, most_recent_measurement_seen) -> Optional[str]:
         """Determine whether a notification should be generated for the given metric."""
-        if len(metric["recent_measurements"]) > 0 and "status_start" in metric["recent_measurements"][-1][metric["scale"]]:
+        if len(metric["recent_measurements"]) > 0 and \
+                "status_start" in metric["recent_measurements"][-1][metric["scale"]]:
             if self.long_unchanged_status(metric, metric_uuid, most_recent_measurement_seen):
                 return "status_long_unchanged"
         if self.status_changed(metric, most_recent_measurement_seen):
@@ -40,7 +40,6 @@ class NotableEvents:
                 self.already_notified.remove(metric_uuid)
             return "status_changed"
         return None
-
 
     @staticmethod
     def status_changed(metric, most_recent_measurement_seen: str) -> bool:
@@ -52,7 +51,6 @@ class NotableEvents:
         metric_had_other_status = recent_measurements[-2][scale]["status"] != recent_measurements[-1][scale]["status"]
         change_was_recent = recent_measurements[-1]["start"] > most_recent_measurement_seen
         return bool(metric_had_other_status and change_was_recent)
-
 
     def long_unchanged_status(self, metric, metric_uuid, most_recent_measurement_seen: str) -> bool:
         """Determine if a metric has been red for 3 weeks."""
