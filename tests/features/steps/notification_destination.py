3d2c7f4eafa626a94bfec0a913892c5b0cb695f8
Frank Niessink <frank@niessink.com>
2020-12-20 16:25:01 +0100

    Refactor.

--- tests/features/steps/notification_destination.py
+++ tests/features/steps/notification_destination.py
@@ -3,10 +3,9 @@
 from behave import given, when
 
 
-@given('a notification destination')
-@when('the client adds a new notification destination')
+@given("a notification destination")
+@when("the client adds a new notification destination")
 def add_notification_destination(context):
     """Add a notification destination to the report."""
-    report_uuid = context.uuid["report"]
-    context.uuid["notification_destination"] = context.post(
-        f"report/{report_uuid}/notification_destination/new")["new_destination_uuid"]
+    api = f"report/{context.uuid['report']}/notification_destination/new"
+    context.uuid["notification_destination"] = context.post(api)["new_destination_uuid"]
