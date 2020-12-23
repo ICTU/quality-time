bf969670bf16113b37616f0315b66c4fd1eb15e1
Frank Niessink <frank@niessink.com>
2020-12-23 11:09:13 +0100

    Increase test coverage.

--- components/frontend/src/source/Source.test.js
+++ components/frontend/src/source/Source.test.js
@@ -8,13 +8,13 @@ import * as fetch_server_api from '../api/fetch_server_api';
 jest.mock("../api/fetch_server_api.js")
 
 const datamodel = {
-    metrics: { metric_type: { sources: ["source_type"] } },
+    metrics: { metric_type: { sources: ["source_type1", "source_type2"] } },
     sources: { 
-        source_type: { name: "Source type", parameters: {} },
+        source_type1: { name: "Source type 1", parameters: {} },
         source_type2: { name: "Source type 2", parameters: {} }
     }
 };
-const source = { type: "source_type" };
+const source = { type: "source_type1" };
 const report = { report_uuid: "report_uuid", subjects: {} };
 
 function render_source() {
@@ -39,6 +39,15 @@ it('invokes the callback on clicking delete', async () => {
         fireEvent.click(screen.getByText(/Delete source/));
     })
     expect(fetch_server_api.fetch_server_api).toHaveBeenNthCalledWith(1, "delete", "source/source_uuid", {});
+});
+
+it('changes the source type', async () => {
+    await act(async () => {
+        render_source();
+        fireEvent.click(screen.getAllByText(/Source type 1/)[0]);
+    })
+    fireEvent.click(screen.getByText(/Source type 2/));
+    expect(fetch_server_api.fetch_server_api).toHaveBeenLastCalledWith("post", "source/source_uuid/attribute/type", {type: "source_type2"});
 });
 
 it('changes the source name', async () => {
