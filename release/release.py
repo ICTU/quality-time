52bffca0816be0bba78ad56595eac8652d0931df
Frank Niessink <frank@niessink.com>
2020-12-23 17:38:18 +0100

    Move release script and supporting files/docs to separate release folder.

--- release/release.py
+++ release/release.py
@@ -31,7 +31,8 @@ def parse_arguments() -> Tuple[str, str, bool]:
     bumps = allowed_bumps_in_rc_mode if "rc" in current_version else allowed_bumps
     parser.add_argument("bump", choices=bumps)
     parser.add_argument(
-        "-c", "--check-preconditions-only", action="store_true", help="only check the preconditions and then exit")
+        "-c", "--check-preconditions-only", action="store_true", help="only check the preconditions and then exit"
+    )
     arguments = parser.parse_args()
     return current_version, arguments.bump, arguments.check_preconditions_only
 
@@ -39,23 +40,25 @@ def parse_arguments() -> Tuple[str, str, bool]:
 def check_preconditions(bump: str):
     """Check preconditions for version bump."""
     messages = []
-    repo = git.Repo(".")
+    repo = git.Repo("..")
     if repo.active_branch.name != "master":
         messages.append("The current branch is not the master branch.")
     if repo.is_dirty():
         messages.append("The workspace has uncommitted changes.")
-    subprocess.run(["python3", "docs/ci/create_metrics_and_sources_md.py"], check=True)  # skipcq: BAN-B603
+    subprocess.run(["python3", "../docs/ci/create_metrics_and_sources_md.py"], check=True)  # skipcq: BAN-B603
     if repo.is_dirty(path="docs/METRICS_AND_SOURCES.md"):
         messages.append(
-            "The generated data model documentation is not up-to-date, please commit docs/METRICS_AND_SOURCES.md.")
-    with pathlib.Path("docs/CHANGELOG.md").open() as changelog:
+            "The generated data model documentation is not up-to-date, please commit ../docs/METRICS_AND_SOURCES.md."
+        )
+    with pathlib.Path("../docs/CHANGELOG.md").open() as changelog:
         changelog_text = changelog.read()
     if "[Unreleased]" not in changelog_text:
-        messages.append("The change log (docs/CHANGELOG.md) has no '[Unreleased]' header.")
+        messages.append("The change log (../docs/CHANGELOG.md) has no '[Unreleased]' header.")
     if bump == "drop-rc" and "-rc." in changelog_text:
         messages.append(
-            "The change log (docs/CHANGELOG.md) still contains release candidates; remove "
-            "the release candidates and move their changes under the '[Unreleased]' header.")
+            "The change log (../docs/CHANGELOG.md) still contains release candidates; remove "
+            "the release candidates and move their changes under the '[Unreleased]' header."
+        )
     if messages:
         formatted_messages = "\n".join([f"- {message}" for message in messages])
         sys.exit(f"Please fix these issues before releasing Quality-time:\n{formatted_messages}\n")
@@ -75,8 +78,18 @@ def main() -> None:
         message = f"Bump version: {current_version} → {{new_version}}"
         # Previous command makes the work space dirty, so allow dirty in the next version bump
         commands.append(
-            ["bump2version", "--config-file", ".bumpversion-rc.cfg", "--message", message, "--tag-message", message,
-             "--allow-dirty", rc_bump])
+            [
+                "bump2version",
+                "--config-file",
+                ".bumpversion-rc.cfg",
+                "--message",
+                message,
+                "--tag-message",
+                message,
+                "--allow-dirty",
+                rc_bump,
+            ]
+        )
     elif bump == "drop-rc":
         new_version = current_version.split("-")[0]
         commands.append(["bump2version", "--new-version", new_version, PART])
