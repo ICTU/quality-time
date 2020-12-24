f3d6092db34a9191583bd5db7972d5640d8c4879
Frank Niessink <frank@niessink.com>
2020-12-24 16:50:01 +0100

    Don't use venv.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 cd tests/application_tests
-python3 -m venv venv
-. venv/bin/activate
-pip --quiet install --progress-bar off -r requirements.txt
+pip --quiet install --progress-bar off --user -r requirements.txt
 python -m unittest discover --start-directory .
