b5f29afea3f8238c11df688e5dbcd4b39c2b4084
Frank Niessink <frank@niessink.com>
2020-12-28 18:07:47 +0100

    Don't crash the notifier when a metric has an unknown (white) status. Fixes #1802.

--- components/notifier/tests/models/test_metric_notification_data.py
+++ components/notifier/tests/models/test_metric_notification_data.py
@@ -10,21 +10,42 @@ class MetricNotificationDataModelTestCase(unittest.TestCase):
 
     def setUp(self):
         """Set variables for the other testcases."""
-        self.data_model = dict(metrics=dict(metric_type=dict(name="type")),
-                               sources=dict(quality_time=dict(parameters=dict(status=dict(api_values={
-                                   "target met (green)": "target_met",
-                                   "target not met (red)": "target_not_met",
-                               })))))
-        self.metric1 = dict(
+        self.data_model = dict(
+            metrics=dict(metric_type=dict(name="type")),
+            sources=dict(
+                quality_time=dict(
+                    parameters=dict(
+                        status=dict(
+                            api_values={
+                                "target met (green)": "target_met",
+                                "target not met (red)": "target_not_met",
+                            }
+                        )
+                    )
+                )
+            ),
+        )
+        self.metric = dict(
             type="metric_type",
             name="default metric 1",
             unit="units",
             scale="count",
-            recent_measurements=[dict(count=dict(value=10,
-                                                 status="target_met")),
-                                 dict(count=dict(value=20,
-                                                 status="target_not_met"))])
+            recent_measurements=[
+                dict(count=dict(value=10, status="target_met")),
+                dict(count=dict(value=20, status="target_not_met")),
+            ],
+        )
 
     def test_initialisation(self):
         """Test that an object can be initialised."""
-        MetricNotificationData(self.metric1, self.data_model, "status_changed")
+        self.assertEqual(
+            "red (target not met)",
+            MetricNotificationData(self.metric, self.data_model, "status_changed").new_metric_status,
+        )
+
+    def test_unknown_status(self):
+        """Test that a metric without recent measurements works."""
+        self.metric["recent_measurements"][-1]["count"]["status"] = None
+        self.assertEqual(
+            "white (unknown)", MetricNotificationData(self.metric, self.data_model, "status_changed").new_metric_status
+        )
