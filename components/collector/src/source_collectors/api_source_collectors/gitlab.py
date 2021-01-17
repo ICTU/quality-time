b4c16feed6c6255aa90ad6e5fcd7f4a49e06cef7
Frank Niessink <frank@niessink.com>
2021-01-17 18:45:34 +0100

    WIP.

--- components/collector/src/source_collectors/api_source_collectors/gitlab.py
+++ components/collector/src/source_collectors/api_source_collectors/gitlab.py
@@ -29,10 +29,16 @@ class GitLabBase(SourceCollector, ABC):  # pylint: disable=abstract-method
 
     async def _get_source_responses(self, *urls: URL) -> SourceResponses:
         """Extend to support GitLab pagination. See https://docs.gitlab.com/ce/api/README.html#pagination."""
+        from logging import warning
+
+        warning(">1> %s", urls)
         responses = await super()._get_source_responses(*urls)
+        warning(">2> %s", len(responses))
         # For each response, get the link to the next page, if any:
-        if next_links := [link for response in responses if (link := response.links.get("next"))]:
+        if next_links := [link for response in responses if (link := response.links.get("next", {}).get("url"))]:
+            warning(">3> %s", next_links)
             responses.extend(await self._get_source_responses(*next_links))
+            warning(">4> %s", len(responses))
         return responses
 
     def _basic_auth_credentials(self) -> Optional[Tuple[str, str]]:
