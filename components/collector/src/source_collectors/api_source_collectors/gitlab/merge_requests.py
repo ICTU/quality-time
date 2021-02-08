62cddc6eb6dc9005b38c6e821e0e6e12a0f8f0b1
Frank Niessink <frank@niessink.com>
2021-02-08 22:24:26 +0100

    Allow for filtering by target branch.

--- components/collector/src/source_collectors/api_source_collectors/gitlab/merge_requests.py
+++ components/collector/src/source_collectors/api_source_collectors/gitlab/merge_requests.py
@@ -2,6 +2,7 @@
 
 from typing import cast
 
+from collector_utilities.functions import match_string_or_regular_expression
 from collector_utilities.type import URL
 from source_model import Entity, SourceMeasurement, SourceResponses
 
@@ -21,6 +22,7 @@ class GitLabMergeRequests(GitLabBase):
 
     async def _parse_source_responses(self, responses: SourceResponses) -> SourceMeasurement:
         """Override to get the merge requests."""
+        merge_requests = await self._merge_requests(responses)
         entities = [
             Entity(
                 key=merge_request["id"],
@@ -35,10 +37,10 @@ class GitLabMergeRequests(GitLabBase):
                 downvotes=str(merge_request.get("downvotes", 0)),
                 upvotes=str(merge_request.get("upvotes", 0)),
             )
-            for merge_request in await self._merge_requests(responses)
+            for merge_request in merge_requests
             if self._include_merge_request(merge_request)
         ]
-        return SourceMeasurement(entities=entities)
+        return SourceMeasurement(entities=entities, total=str(len(merge_requests)))
 
     @staticmethod
     async def _merge_requests(responses: SourceResponses):
@@ -48,6 +50,9 @@ class GitLabMergeRequests(GitLabBase):
 
     def _include_merge_request(self, merge_request) -> bool:
         """Return whether the merge request should be counted."""
-        min_upvotes = int(cast(str, self._parameter("upvotes")))
-        states = self._parameter("merge_request_state")
-        return merge_request["state"] in states and int(merge_request["upvotes"]) < min_upvotes
+        request_has_fewer_than_min_upvotes = int(merge_request["upvotes"]) < int(cast(str, self._parameter("upvotes")))
+        request_matches_state = merge_request["state"] in self._parameter("merge_request_state")
+        branches = self._parameter("target_branches_to_include")
+        target_branch = merge_request["target_branch"]
+        request_matches_branches = branches and match_string_or_regular_expression(target_branch, branches)
+        return request_has_fewer_than_min_upvotes and request_matches_state and request_matches_branches
--- components/collector/tests/source_collectors/api_source_collectors/gitlab/test_merge_requests.py
+++ components/collector/tests/source_collectors/api_source_collectors/gitlab/test_merge_requests.py
@@ -10,6 +10,7 @@ class GitLabMergeRequestsTest(GitLabTestCase):
         """Test that the number of merge requests can be measured."""
         self.sources["source_id"]["parameters"]["merge_request_state"] = ["opened", "closed", "merged"]
         self.sources["source_id"]["parameters"]["upvotes"] = "2"  # Require at least two upvotes
+        self.sources["source_id"]["parameters"]["target_branches_to_include"] = ["default"]
         metric = dict(type="merge_requests", sources=self.sources, addition="sum")
         gitlab_json = [
             dict(
@@ -27,16 +28,27 @@ class GitLabMergeRequestsTest(GitLabTestCase):
             ),
             dict(
                 id=2,
-                title="Merge request 2",
+                title="Merge request 2: excluded because of state",
                 state="locked",
+                target_branch="default",
+                upvotes=1,
                 web_url="https://gitlab/mr2",
             ),
             dict(
                 id=3,
-                title="Merge request 3",
+                title="Merge request 3: excluded because of upvotes",
                 state="merged",
-                web_url="https://gitlab/mr3",
+                target_branch="default",
                 upvotes=2,
+                web_url="https://gitlab/mr3",
+            ),
+            dict(
+                id=4,
+                title="Merge request 4: excluded because of target branch",
+                state="merged",
+                web_url="https://gitlab/mr4",
+                upvotes=1,
+                target_branch="dev",
             ),
         ]
         response = await self.collect(metric, get_request_json_return_value=gitlab_json)
@@ -58,6 +70,7 @@ class GitLabMergeRequestsTest(GitLabTestCase):
         self.assert_measurement(
             response,
             value="1",
+            total="4",
             entities=expected_entities,
             landing_url="https://gitlab/namespace/project/-/merge_requests",
         )
