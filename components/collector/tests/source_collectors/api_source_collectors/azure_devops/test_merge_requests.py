daa63e364d566a24d5c5eeaec1fcc246aec78e00
Frank Niessink <frank@niessink.com>
2021-02-10 22:33:22 +0100

    Fix SonarQube code smells.

--- components/collector/tests/source_collectors/api_source_collectors/azure_devops/test_merge_requests.py
+++ components/collector/tests/source_collectors/api_source_collectors/azure_devops/test_merge_requests.py
@@ -12,6 +12,19 @@ class AzureDevopsMergeRequestsTest(AzureDevopsTestCase):
         self.metric = dict(type="merge_requests", sources=self.sources, addition="sum")
         self.repositories = dict(value=[dict(id="id", name="project")])
 
+    @staticmethod
+    def create_merge_request(number: int, status: str = "active", vote: int = 0, branch: str = "main"):
+        """Create a merge request fixture."""
+        return dict(
+            pullRequestId=number,
+            title=f"Pull request {number}",
+            targetRefName=f"refs/heads/{branch}",
+            status=status,
+            url=f"https://azure/pr{number}",
+            creationDate="2021-02-09T17:10:11.0326704Z",
+            reviewers=[dict(vote=10), dict(vote=vote)],
+        )
+
     async def test_merge_requests(self):
         """Test that the number of merge requests can be measured."""
         self.sources["source_id"]["parameters"]["merge_request_state"] = ["active"]
@@ -19,42 +32,10 @@ class AzureDevopsMergeRequestsTest(AzureDevopsTestCase):
         self.sources["source_id"]["parameters"]["target_branches_to_include"] = ["refs/heads/main"]
         azure_devops_json = dict(
             value=[
-                dict(
-                    pullRequestId=1,
-                    title="Pull request 1",
-                    targetRefName="refs/heads/main",
-                    status="active",
-                    url="https://azure/pr1",
-                    creationDate="2021-02-09T17:10:11.0326704Z",
-                    reviewers=[dict(vote=10), dict(vote=0)],
-                ),
-                dict(
-                    pullRequestId=2,
-                    title="Pull request 2",
-                    targetRefName="refs/heads/main",
-                    status="abandoned",
-                    url="https://azure/pr2",
-                    creationDate="2021-02-10T10:10:11.0326432Z",
-                    reviewers=[dict(vote=10), dict(vote=0)],
-                ),
-                dict(
-                    pullRequestId=3,
-                    title="Pull request 3",
-                    targetRefName="refs/heads/main",
-                    status="active",
-                    url="https://azure/pr2",
-                    creationDate="2021-02-10T10:10:11.0326432Z",
-                    reviewers=[dict(vote=10), dict(vote=10)],
-                ),
-                dict(
-                    pullRequestId=4,
-                    title="Pull request 4",
-                    targetRefName="refs/heads/dev",
-                    status="active",
-                    url="https://azure/pr2",
-                    creationDate="2021-02-10T10:10:11.0326432Z",
-                    reviewers=[dict(vote=10), dict(vote=0)],
-                ),
+                self.create_merge_request(1),
+                self.create_merge_request(2, status="abandoned"),
+                self.create_merge_request(3, vote=10),
+                self.create_merge_request(4, branch="dev"),
             ]
         )
         response = await self.collect(self.metric, get_request_json_side_effect=[self.repositories, azure_devops_json])
