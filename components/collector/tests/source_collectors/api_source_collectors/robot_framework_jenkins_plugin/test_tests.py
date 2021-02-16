c04e0c1e2d4ace11f0e31d8f06825a352de3da41
Frank Niessink <frank@niessink.com>
2021-02-16 23:57:22 +0100

    Remove duplication.

--- components/collector/tests/source_collectors/api_source_collectors/robot_framework_jenkins_plugin/test_tests.py
+++ components/collector/tests/source_collectors/api_source_collectors/robot_framework_jenkins_plugin/test_tests.py
@@ -9,26 +9,24 @@ class RobotFrameworkJenkinsPluginTest(JenkinsPluginTestCase, JenkinsPluginTestsM
     source_type = "robot_framework_jenkins_plugin"
 
     def setUp(self):
-        """Extend to set up Jenkins data."""
+        """Extend to set up the metric and Jenkins data."""
         super().setUp()
+        self.metric = dict(type="tests", addition="sum", sources=self.sources)
         self.jenkins_json = dict(overallTotal=2, overallFailed=1, overallPassed=1)
 
     async def test_nr_of_tests(self):
         """Test that the number of tests is returned."""
-        metric = dict(type="tests", addition="sum", sources=self.sources)
-        response = await self.collect(metric, get_request_json_return_value=self.jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         self.assert_measurement(response, value="2", total="2")
 
     async def test_failed_tests(self):
         """Test that the number of failed tests is returned."""
         self.sources["source_id"]["parameters"]["test_result"] = ["fail"]
-        metric = dict(type="tests", addition="sum", sources=self.sources)
-        response = await self.collect(metric, get_request_json_return_value=self.jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         self.assert_measurement(response, value="1", total="2")
 
     async def test_passed_tests(self):
         """Test that the number of passed tests is returned."""
         self.sources["source_id"]["parameters"]["test_result"] = ["pass"]
-        metric = dict(type="tests", addition="sum", sources=self.sources)
-        response = await self.collect(metric, get_request_json_return_value=self.jenkins_json)
+        response = await self.collect(self.metric, get_request_json_return_value=self.jenkins_json)
         self.assert_measurement(response, value="1", total="2")
