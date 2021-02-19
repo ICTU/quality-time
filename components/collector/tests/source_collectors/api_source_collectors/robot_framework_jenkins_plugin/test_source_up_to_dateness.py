27f17c63746720830bca67fa13c5520ce46b8e36
Frank Niessink <frank@niessink.com>
2021-02-20 00:25:41 +0100

    Remove duplication.

--- components/collector/tests/source_collectors/api_source_collectors/robot_framework_jenkins_plugin/test_source_up_to_dateness.py
+++ components/collector/tests/source_collectors/api_source_collectors/robot_framework_jenkins_plugin/test_source_up_to_dateness.py
@@ -1,10 +1,11 @@
 """Unit tests for the Robot Framework Jenkins plugin source up-to-dateness collector."""
 
-from ...source_collector_test_case import SourceCollectorTestCase
 from ..jenkins_plugin_test_case import JenkinsPluginSourceUpToDatenessMixin
 
+from .base import RobotFrameworkJenkinsPluginTestCase
 
-class RobotFrameworkJenkinsPluginSourceUpToDatenessTest(JenkinsPluginSourceUpToDatenessMixin, SourceCollectorTestCase):
+
+class RobotFrameworkJenkinsPluginSourceUpToDatenessTest(
+    JenkinsPluginSourceUpToDatenessMixin, RobotFrameworkJenkinsPluginTestCase
+):
     """Unit tests for the Robot Framework Jenkins plugin source up-to-dateness collector."""
-
-    SOURCE_TYPE = "robot_framework_jenkins_plugin"
