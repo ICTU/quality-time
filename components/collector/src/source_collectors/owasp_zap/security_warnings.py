fa3877014e54dfe445988a2f45dcc7cfdf7d9c58
Frank Niessink <frank@niessink.com>
2021-03-04 18:12:33 +0100

    When measuring security warnings with OWASP ZAP as source, allow for … (#1966)
    When measuring security warnings with OWASP ZAP as source, allow for counting alert types as security warnings as opposed to alert instances. Closes #1902.

--- components/collector/src/source_collectors/owasp_zap/security_warnings.py
+++ components/collector/src/source_collectors/owasp_zap/security_warnings.py
@@ -14,10 +14,11 @@ class OWASPZAPSecurityWarnings(XMLFileSourceCollector):
     """Collector to get security warnings from OWASP ZAP."""
 
     async def _parse_entities(self, responses: SourceResponses) -> Entities:
-        """Override to parse the securty warnings from the XML."""
+        """Override to parse the security warnings from the XML."""
         entities: dict[str, Entity] = {}
         tag_re = re.compile(r"<[^>]*>")
         risks = cast(list[str], self._parameter("risks"))
+        count_alert_instances = cast(str, self._parameter("alerts")) == "alert instances"
         for alert in await self.__alerts(responses, risks):
             ids = [
                 alert.findtext(id_tag, default="") for id_tag in ("alert", "pluginid", "cweid", "wascid", "sourceid")
@@ -25,22 +26,31 @@ class OWASPZAPSecurityWarnings(XMLFileSourceCollector):
             name = alert.findtext("name", default="")
             description = tag_re.sub("", alert.findtext("desc", default=""))
             risk = alert.findtext("riskdesc", default="")
-            for alert_instance in alert.findall("./instances/instance"):
-                method = alert_instance.findtext("method", default="")
-                uri = self.__stable(hashless(URL(alert_instance.findtext("uri", default=""))))
-                key = md5_hash(f"{':'.join(ids)}:{method}:{uri}")
-                entities[key] = Entity(
-                    key=key,
-                    old_key=md5_hash(f"{':'.join(ids[1:])}:{method}:{uri}"),
-                    name=name,
-                    description=description,
-                    uri=uri,
-                    location=f"{method} {uri}",
-                    risk=risk,
-                )
+            entity_kwargs = dict(name=name, description=description, risk=risk)
+            if count_alert_instances:
+                for alert_instance in alert.findall("./instances/instance"):
+                    entity = self.__alert_instance_entity(ids, entity_kwargs, alert_instance)
+                    entities[entity["key"]] = entity
+            else:
+                entity = self.__alert_type_entity(ids, entity_kwargs)
+                entities[entity["key"]] = entity
         return list(entities.values())
 
-    def __stable(self, url: URL) -> URL:
+    def __alert_instance_entity(self, ids, entity_kwargs, alert_instance) -> Entity:
+        """Create an alert instance entity."""
+        method = alert_instance.findtext("method", default="")
+        uri = self.__stable_url(hashless(URL(alert_instance.findtext("uri", default=""))))
+        key = md5_hash(f"{':'.join(ids)}:{method}:{uri}")
+        old_key = md5_hash(f"{':'.join(ids[1:])}:{method}:{uri}")
+        location = f"{method} {uri}"
+        return Entity(key=key, old_key=old_key, uri=uri, location=location, **entity_kwargs)
+
+    @staticmethod
+    def __alert_type_entity(ids, entity_kwargs) -> Entity:
+        """Create an alert type entity."""
+        return Entity(key=md5_hash(f"{':'.join(ids)}"), **entity_kwargs)
+
+    def __stable_url(self, url: URL) -> URL:
         """Return the url without the variable parts."""
         reg_exps = self._parameter("variable_url_regexp")
         stable_url = cast(str, url)
