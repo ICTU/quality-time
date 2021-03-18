38e9277adf41ca35c6f0013cdbd662842aa35a14
Erik <emdverboom@gmail.com>
2021-03-18 16:53:09 +0100

    remove dummy functions

--- components/server/src/server_utilities/functions.py
+++ components/server/src/server_utilities/functions.py
@@ -159,11 +159,3 @@ def asymmetric_decrypt(private_key: str, fernet_key_message: Tuple[str, str]) ->
     message = symmetric_decrypt(decrypted_key, message_bytes)
 
     return message.decode()
-
-
-def test_how_many_functions_i_need_for_bch():
-    print("test")
-
-
-def second_function():
-    print("test")
