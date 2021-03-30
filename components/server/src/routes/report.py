58cc7de87e2b9e117c1c2c2922f6619eba6140c5
emdv88 <emdverboom@gmail.com>
2021-03-30 15:46:44 +0200

    Update components/server/src/routes/report.py
    Co-authored-by: Frank Niessink <frank@niessink.com>

--- components/server/src/routes/report.py
+++ components/server/src/routes/report.py
@@ -114,7 +114,7 @@ def export_report_as_pdf(report_uuid: ReportId):
 
 @bottle.get("/api/v3/report/<report_uuid>/json", authentication_required=True)
 def export_report_as_json(database: Database, report_uuid: ReportId):
-    """Return the quality-time report, including iencrypted credentials for api access to the sources."""
+    """Return the quality-time report, including encrypted credentials for api access to the sources."""
     date_time = report_date_time()
     data_model = latest_datamodel(database, date_time)
     report = latest_report(database, report_uuid)
