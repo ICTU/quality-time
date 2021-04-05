15e956e073bbcbe8b8839009d39165aa563a176a
Frank Niessink <frank@niessink.com>
2021-04-05 16:02:38 +0200

    Suppress DeepSource issue.

--- components/collector/tests/source_collectors/anchore_jenkins_plugin/test_source_up_to_dateness.py
+++ components/collector/tests/source_collectors/anchore_jenkins_plugin/test_source_up_to_dateness.py
@@ -4,7 +4,9 @@ from ..jenkins_plugin_test_case import JenkinsPluginSourceUpToDatenessMixin
 from ..source_collector_test_case import SourceCollectorTestCase
 
 
-class AnchoreJenkinsPluginSourceUpToDatenessTest(JenkinsPluginSourceUpToDatenessMixin, SourceCollectorTestCase):
+class AnchoreJenkinsPluginSourceUpToDatenessTest(
+    JenkinsPluginSourceUpToDatenessMixin, SourceCollectorTestCase
+):  # skipcq: PTC-W0046
     """Unit tests for the Anchore Jenkins plugin source up-to-dateness collector."""
 
     SOURCE_TYPE = "anchore_jenkins_plugin"
