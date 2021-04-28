aabdb1d5c3f11a76d7911aef6135af1126b025fc
Frank Niessink <frank@niessink.com>
2021-04-28 09:47:07 +0200

    Move black and pip dependencies to top-level requirements file and in… (#2099)
    Move black and pip dependencies to top-level requirements file and include that file in the requirements files of the components and test folders.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd tests/application_tests
-pip --quiet install --progress-bar off --user -r requirements.txt
+pip --quiet install --progress-bar off --user -r requirements-dev.txt
 python -m unittest discover --start-directory .
