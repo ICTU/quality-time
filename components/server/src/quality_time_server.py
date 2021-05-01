45db886e00aa846a38492044a5c316407e52aa92
Frank Niessink <frank@niessink.com>
2021-05-01 17:47:51 +0200

    Test with reloader=False.

--- components/server/src/quality_time_server.py
+++ components/server/src/quality_time_server.py
@@ -16,13 +16,13 @@ import bottle  # skipcq: FLK-E402
 from initialization import init_bottle, init_database  # skipcq: FLK-E402
 
 
-def serve(server: str = "gevent") -> None:  # pragma: no cover-behave
+def serve(reloader: bool = True) -> None:  # pragma: no cover-behave
     """Connect to the database and start the application server."""
     logging.getLogger().setLevel(logging.INFO)
     database = init_database()
     init_bottle(database)
     server_port = os.environ.get("SERVER_PORT", "5001")
-    bottle.run(server=server, host="0.0.0.0", port=server_port, reloader=True, log=logging.getLogger())  # nosec
+    bottle.run(server="gevent", host="0.0.0.0", port=server_port, reloader=reloader, log=logging.getLogger())  # nosec
 
 
 if __name__ == "__main__":  # pragma: no cover-behave
