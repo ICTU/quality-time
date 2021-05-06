65bd9c79d8dbcc81c4d22931ac2a3342eaaa441b
Frank Niessink <frank@niessink.com>
2021-05-07 00:40:30 +0200

    Start coverage before importing the server code.

--- components/server/tests/quality_time_server_under_coverage.py
+++ components/server/tests/quality_time_server_under_coverage.py
@@ -4,13 +4,15 @@ import sys
 
 import coverage
 
+
 sys.path.insert(0, "src")
-from quality_time_server import serve
 
 
 if __name__ == "__main__":
     cov = coverage.Coverage()
     cov.start()
+    from quality_time_server import serve
+
     try:
         serve()
     finally:
