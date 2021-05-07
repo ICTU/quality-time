bf6adf59f407c26c8bcaeda263387856153b74e0
Frank Niessink <frank@niessink.com>
2021-05-07 11:18:14 +0200

    Save coverage on SIGTERM.

--- components/server/tests/quality_time_server_under_coverage.py
+++ components/server/tests/quality_time_server_under_coverage.py
@@ -1,5 +1,6 @@
 """Quality-time server, with coverage measurement."""
 
+import signal
 import sys
 
 import coverage
@@ -7,14 +8,28 @@ import coverage
 
 sys.path.insert(0, "src")
 
+cov = coverage.Coverage()
+
+
+def stop_coverage():
+    """Stop and save the coverage."""
+    if cov:
+        cov.stop()
+        cov.save()
+
+
+def signal_handler(*args):  # pylint: disable=unused-argument
+    """Save the coverage data on receiving a SIGTERM."""
+    stop_coverage()
+    sys.exit()
+
 
 if __name__ == "__main__":
-    cov = coverage.Coverage()
+    signal.signal(signal.SIGTERM, signal_handler)
     cov.start()
     from quality_time_server import serve
 
     try:
         serve()
     finally:
-        cov.stop()
-        cov.save()
+        stop_coverage()
