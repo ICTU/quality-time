a1d80e723ce93f72b7b9b4f3c88e6fad7ea2f93b
Frank Niessink <frank@niessink.com>
2021-05-16 00:00:48 +0200

    Fix SonarQube issue.

--- components/collector/tests/source_collectors/robot_framework/test_tests.py
+++ components/collector/tests/source_collectors/robot_framework/test_tests.py
@@ -7,6 +7,7 @@ class RobotFrameworkTestReportTest(RobotFrameworkTestCase):
     """Unit tests for the Robot Framework XML test report metrics."""
 
     METRIC_TYPE = "tests"
+    LANDING_URL = "https://robot_framework"
 
     async def test_tests(self):
         """Test that the number of tests is returned."""
@@ -18,7 +19,7 @@ class RobotFrameworkTestReportTest(RobotFrameworkTestCase):
                     dict(key="s1-t2", name="Test 2", test_result="pass"),
                 ]
                 self.assert_measurement(
-                    response, value="2", total="2", entities=expected_entities, landing_url="https://robot_framework"
+                    response, value="2", total="2", entities=expected_entities, landing_url=self.LANDING_URL
                 )
 
     async def test_failed_tests(self):
@@ -29,7 +30,7 @@ class RobotFrameworkTestReportTest(RobotFrameworkTestCase):
                 response = await self.collect(get_request_text=self.ROBOT_FRAMEWORK_XML_V3)
                 expected_entities = [dict(key="s1-t1", name="Test 1", test_result="fail")]
                 self.assert_measurement(
-                    response, value="1", total="2", entities=expected_entities, landing_url="https://robot_framework"
+                    response, value="1", total="2", entities=expected_entities, landing_url=self.LANDING_URL
                 )
 
     async def test_skipped_tests(self):
@@ -38,5 +39,5 @@ class RobotFrameworkTestReportTest(RobotFrameworkTestCase):
         response = await self.collect(get_request_text=self.ROBOT_FRAMEWORK_XML_V4_WITH_SKIPPED_TESTS)
         expected_entities = [dict(key="s1-t3", name="Test 3", test_result="skip")]
         self.assert_measurement(
-            response, value="1", total="3", entities=expected_entities, landing_url="https://robot_framework"
+            response, value="1", total="3", entities=expected_entities, landing_url=self.LANDING_URL
         )
