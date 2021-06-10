07ec059458e9c6f7959037cc5dacf4666e1f81e2
Frank Niessink <frank@niessink.com>
2021-06-10 19:02:03 +0200

    Fix Pylint issue.

--- components/collector/src/source_collectors/cxsast/security_warnings.py
+++ components/collector/src/source_collectors/cxsast/security_warnings.py
@@ -14,9 +14,7 @@ class CxSASTSecurityWarnings(CxSASTScanBase):
         """Extend to get the scan results."""
         await super()._get_source_responses(*urls, **kwargs)  # Get scan id
         stats_api = URL(f"{await self._api_url()}/cxrestapi/sast/scans/{self._scan_id}/resultsStatistics")
-        return await SourceCollector._get_source_responses(
-            self, stats_api, **kwargs
-        )  # pylint: disable=protected-access
+        return await SourceCollector._get_source_responses(self, stats_api)  # pylint: disable=protected-access
 
     async def _parse_value(self, responses: SourceResponses) -> Value:
         """Override to parse the number of security warnings from the scan results."""
