247410c48004717e33bbe072c3249e96ffdcc688
Frank Niessink <frank@niessink.com>
2021-06-14 14:17:34 +0200

    More concise logging.

--- components/server/src/routes/measurement.py
+++ components/server/src/routes/measurement.py
@@ -31,36 +31,34 @@ from server_utilities.type import MetricId, SourceId
 @bottle.post("/internal-api/v3/measurements", authentication_required=False)
 def post_measurement(database: Database) -> None:
     """Put the measurement in the database."""
+    times = [time.time()]
     measurement_data = dict(bottle.request.json)
     metric_uuid = measurement_data["metric_uuid"]
-    logging.info("Received a measurement for metric %s", metric_uuid)
     if (metric := latest_metric(database, metric_uuid)) is None:
-        logging.info("Metric %s does not exist, not storing the measurement", metric_uuid)
         return  # Metric does not exist, must've been deleted while being measured
-    logging.info("Retrieved metric %s", metric_uuid)
+    times.append(time.time())
     latest = latest_measurement(database, metric)
-    logging.info("Retrieved latest measurement for metric %s", metric_uuid)
+    times.append(time.time())
     measurement = Measurement(metric, measurement_data, previous_measurement=latest)
+    times.append(time.time())
     if latest:
-        logging.info("Metric %s has measurements, so will try to find latest successful measurement", metric_uuid)
         latest_successful = latest_successful_measurement(database, metric)
-        if latest_successful is None:
-            logging.info("Metric %s has no successful measurements", metric_uuid)
-        else:
-            logging.info("Retrieved latest successful measurement for metric %s", metric_uuid)
+        times.append(time.time())
         measurement.copy_entity_user_data(latest if latest_successful is None else latest_successful)
-        if latest_successful is None:
-            logging.info("Copied entity user data from latest measurement for metric %s", metric_uuid)
-        else:
-            logging.info("Copied entity user data from latest successful measurement for metric %s", metric_uuid)
+        times.append(time.time())
         if not latest.debt_target_expired() and latest.sources() == measurement.sources():
-            logging.info("Updating latest measurement of metric %s because value didn't change", metric_uuid)
             # If the new measurement is equal to the previous one, merge them together
             update_measurement_end(database, latest["_id"])
+            times.append(time.time())
+            if times[-1] - times[0] > 5:  # pragma: no cover
+                times = [round(t - times[index - 1], 1) for index, t in list(enumerate(times))[1:]]
+                logging.info("Updating latest measurement for %s took %ss: %s", metric_uuid, sum(times), times)
             return
-    logging.info("Inserting a new measurement for metric %s", metric_uuid)
     insert_new_measurement(database, measurement)
-    logging.info("Done processing the measurement received for metric %s", metric_uuid)
+    times.append(time.time())
+    if times[-1] - times[0] > 5:  # pragma: no cover
+        times = [round(t - times[index - 1], 1) for index, t in list(enumerate(times))[1:]]
+        logging.info("Inserting new measurement for %s took %ss: %s", metric_uuid, sum(times), times)
 
 
 @bottle.post(
