4d228fc6347cede98bd297c4387aa1575d21ffa5
Frank Niessink <frank@niessink.com>
2021-06-18 14:53:32 +0200

    Install markdownlint.

--- docs/ci/quality.sh
+++ docs/ci/quality.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-markdownlint CHANGELOG.md  # TODO: add all markdown files
+./node_modules/markdownlint-cli/markdownlint.js CHANGELOG.md  # TODO: add all markdown files
 mypy src
 pylint src tests
 python -m flake8 --select=DUO src
