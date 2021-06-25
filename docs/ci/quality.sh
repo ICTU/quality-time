6e2a58f730e4abbce4570598ab51d5f2cf58c90b
Frank Niessink <frank@niessink.com>
2021-06-25 12:04:37 +0200

    Use one .pylintrc.

--- docs/ci/quality.sh
+++ docs/ci/quality.sh
@@ -4,7 +4,7 @@ set -e
 
 ./node_modules/markdownlint-cli/markdownlint.js *.md
 mypy src
-pylint src tests
+pylint --rcfile=../.pylintrc src tests
 python -m flake8 --select=DUO src
 isort **/*.py --check-only
 safety check --bare -r requirements-dev.txt
