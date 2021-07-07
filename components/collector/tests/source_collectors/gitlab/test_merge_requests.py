6be3efb5ebda8ce05283a54b5d4fde6923328143
Frank Niessink <frank@niessink.com>
2021-07-07 14:32:12 +0200

    Fix SonarQube code smell.

--- components/collector/tests/source_collectors/gitlab/test_merge_requests.py
+++ components/collector/tests/source_collectors/gitlab/test_merge_requests.py
@@ -11,16 +11,10 @@ class GitLabMergeRequestsTest(GitLabTestCase):
     """Unit tests for the merge requests metric."""
 
     METRIC_TYPE = "merge_requests"
-
-    def setUp(self):
-        """Extend to set up the metric under test."""
-        super().setUp()
-        self.landing_url = "https://gitlab/namespace/project/-/merge_requests"
-        self.collector = MetricsCollector()
-        self.collector.data_model = self.data_model
+    LANDING_URL = "https://gitlab/namespace/project/-/merge_requests"
 
     @staticmethod
-    def create_merge_request(
+    def merge_request_json(
         number: int, branch: str = "default", state: str = "merged", upvotes: int = 1, approved: bool = None
     ):
         """Create a merge request."""
@@ -39,7 +33,7 @@ class GitLabMergeRequestsTest(GitLabTestCase):
         )
 
     @staticmethod
-    def create_merge_requests(nodes, count: int = None, has_next_page: bool = False):
+    def merge_requests_json(nodes, count: int = None, has_next_page: bool = False):
         """Create the GraphQL merge request response JSON."""
         return dict(
             data=dict(
@@ -54,8 +48,8 @@ class GitLabMergeRequestsTest(GitLabTestCase):
         )
 
     @staticmethod
-    def create_merge_request_fields(has_approved_field: bool = False):
-        """Create the GraphQL merge request fields response JSON."""
+    def merge_request_fields_json(has_approved_field: bool = False):
+        """Return the GraphQL merge request fields response JSON."""
         fields = [dict(name="approved")] if has_approved_field else []
         return dict(data=dict(__type=dict(fields=fields)))
 
@@ -76,57 +70,61 @@ class GitLabMergeRequestsTest(GitLabTestCase):
             downvotes="0",
         )
 
+    async def collect_merge_requests(self, execute_mock):
+        """Return the source responses."""
+        collector = MetricsCollector()
+        collector.data_model = self.data_model
+        with patch("aiogqlc.GraphQLClient.execute", execute_mock):
+            return await collector.collect_sources(None, self.metric)
+
     async def test_merge_requests(self):
         """Test that the number of merge requests can be measured."""
         self.set_source_parameter("merge_request_state", ["opened", "closed", "merged"])
         self.set_source_parameter("upvotes", "2")  # Require at least two upvotes
         self.set_source_parameter("target_branches_to_include", ["default"])
-        merge_requests_json = self.create_merge_requests(
+        merge_requests_json = self.merge_requests_json(
             [
-                self.create_merge_request(1),
-                self.create_merge_request(2, state="locked", upvotes=2),  # Excluded because of state
-                self.create_merge_request(3, upvotes=2),  # Excluded because of upvotes
-                self.create_merge_request(4, branch="dev"),  # Excluded because of target branch
+                self.merge_request_json(1),
+                self.merge_request_json(2, state="locked", upvotes=2),  # Excluded because of state
+                self.merge_request_json(3, upvotes=2),  # Excluded because of upvotes
+                self.merge_request_json(4, branch="dev"),  # Excluded because of target branch
             ]
         )
         merge_request_fields_response = AsyncMock()
         merge_requests_response = AsyncMock()
         execute = AsyncMock(side_effect=[merge_request_fields_response, merge_requests_response])
-        merge_request_fields_response.json = AsyncMock(return_value=self.create_merge_request_fields())
+        merge_request_fields_response.json = AsyncMock(return_value=self.merge_request_fields_json())
         merge_requests_response.json = AsyncMock(return_value=merge_requests_json)
-        with patch("aiogqlc.GraphQLClient.execute", execute):
-            response = await self.collector.collect_sources(None, self.metric)
         entities = [self.create_entity(1)]
-        self.assert_measurement(response, value="1", total="4", entities=entities, landing_url=self.landing_url)
+        response = await self.collect_merge_requests(execute)
+        self.assert_measurement(response, value="1", total="4", entities=entities, landing_url=self.LANDING_URL)
 
     async def test_pagination(self):
         """Test that pagination works."""
-        merge_requests_json1 = self.create_merge_requests([self.create_merge_request(1)], count=2, has_next_page=True)
-        merge_requests_json2 = self.create_merge_requests([self.create_merge_request(2)], count=2)
+        merge_requests_json1 = self.merge_requests_json([self.merge_request_json(1)], count=2, has_next_page=True)
+        merge_requests_json2 = self.merge_requests_json([self.merge_request_json(2)], count=2)
         merge_request_fields_response = AsyncMock()
         merge_requests_page1 = AsyncMock()
         merge_requests_page2 = AsyncMock()
         execute = AsyncMock(side_effect=[merge_request_fields_response, merge_requests_page1, merge_requests_page2])
-        merge_request_fields_response.json = AsyncMock(return_value=self.create_merge_request_fields())
+        merge_request_fields_response.json = AsyncMock(return_value=self.merge_request_fields_json())
         merge_requests_page1.json = AsyncMock(return_value=merge_requests_json1)
         merge_requests_page2.json = AsyncMock(return_value=merge_requests_json2)
-        with patch("aiogqlc.GraphQLClient.execute", execute):
-            response = await self.collector.collect_sources(None, self.metric)
         entities = [self.create_entity(1), self.create_entity(2)]
-        self.assert_measurement(response, value="2", total="2", entities=entities, landing_url=self.landing_url)
+        response = await self.collect_merge_requests(execute)
+        self.assert_measurement(response, value="2", total="2", entities=entities, landing_url=self.LANDING_URL)
 
     async def test_approval(self):
         """Test that merge requests can be filtered by approval state."""
         self.set_source_parameter("approval_state", ["approved"])
-        merge_requests_json = self.create_merge_requests(
-            [self.create_merge_request(1, approved=True), self.create_merge_request(2)]
+        merge_requests_json = self.merge_requests_json(
+            [self.merge_request_json(1, approved=True), self.merge_request_json(2)]
         )
         merge_request_fields_response = AsyncMock()
         merge_requests_response = AsyncMock()
         execute = AsyncMock(side_effect=[merge_request_fields_response, merge_requests_response])
-        merge_request_fields_response.json = AsyncMock(return_value=self.create_merge_request_fields(True))
+        merge_request_fields_response.json = AsyncMock(return_value=self.merge_request_fields_json(True))
         merge_requests_response.json = AsyncMock(return_value=merge_requests_json)
-        with patch("aiogqlc.GraphQLClient.execute", execute):
-            response = await self.collector.collect_sources(None, self.metric)
         entities = [self.create_entity(1, approved="yes")]
-        self.assert_measurement(response, value="1", total="2", entities=entities, landing_url=self.landing_url)
+        response = await self.collect_merge_requests(execute)
+        self.assert_measurement(response, value="1", total="2", entities=entities, landing_url=self.LANDING_URL)
