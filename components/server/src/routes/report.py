d4e26aeb7adfc3d8f16845d5f668fe4007ea1c30
Frank Niessink <frank@niessink.com>
2021-07-23 14:25:35 +0200

    Fix spelling and docstring layout.

--- components/server/src/routes/report.py
+++ components/server/src/routes/report.py
@@ -52,8 +52,10 @@ def get_report(database: Database, report_uuid: ReportId = None):
 
 @bottle.get("/api/v3/tagreport/<tag>", authentication_required=False)
 def get_tag_report_api(tag: str, database: Database):  # pragma: no cover
-    """DEPRICATED use /api/v3/report/<report_uuid> instead.
-    Get a report with all metrics that have the specified tag."""
+    """Get a report with all metrics that have the specified tag.
+
+    DEPRECATED use /api/v3/report/<report_uuid> instead.
+    """
     date_time = report_date_time()
     reports = latest_reports(database, date_time)
     data_model = latest_datamodel(database, date_time)
