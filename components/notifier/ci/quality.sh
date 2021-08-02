a04ebc3000bdd45fc6045ec691d2b630141a9635
Frank Niessink <frank@niessink.com>
2021-08-02 16:41:50 +0200

    Ignore false positive (?) safety issue.

--- components/notifier/ci/quality.sh
+++ components/notifier/ci/quality.sh
@@ -6,6 +6,6 @@ mypy src
 pylint --rcfile=../../.pylintrc src tests
 isort **/*.py --check-only
 python -m flake8 --select=DUO src
-safety check --bare -r requirements.txt -r requirements-dev.txt
+safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
 bandit --quiet --recursive src/
 vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
--- components/server/ci/quality.sh
+++ components/server/ci/quality.sh
@@ -5,6 +5,6 @@ set -e
 mypy src
 pylint --rcfile=../../.pylintrc src tests
 python -m flake8 --select=DUO src
-safety check --bare -r requirements.txt -r requirements-dev.txt
+safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
 bandit --quiet --recursive src/
 vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
