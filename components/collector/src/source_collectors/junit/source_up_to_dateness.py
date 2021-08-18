69f5a1ea3d3a7948aeef2d14d90f4b1a9d1f1bf6
Frank Niessink <frank@niessink.com>
2021-08-18 22:34:38 +0200

    JUnit XML files may have empty test suites, be prepared. Fixes #2507.

--- components/collector/src/source_collectors/junit/source_up_to_dateness.py
+++ components/collector/src/source_collectors/junit/source_up_to_dateness.py
@@ -15,5 +15,11 @@ class JUnitSourceUpToDateness(XMLFileSourceCollector, SourceUpToDatenessCollecto
     async def _parse_source_response_date_time(self, response: Response) -> datetime:
         """Override to parse the timestamp from the response."""
         tree = await parse_source_response_xml(response)
-        test_suite = tree if tree.tag == "testsuite" else tree.findall("testsuite")[0]
-        return parse(test_suite.get("timestamp", ""))
+        test_suites = [tree] if tree.tag == "testsuite" else tree.findall("testsuite")
+        # If <testsuites> has at least one <testsuite> get the timestamp from the first <testsuite>. Since we're only
+        # interested in the date that the test ran, and not the exact time, we're assuming we don't need to look for
+        # the oldest testsuite. If there's no <testsuite> within the <testsuites>, we can't get a timestamp and return
+        # datetime.now(). We don't return datetime.min because there might be more Junit XML files being parsed that do
+        # have a timestamp. If we'd return datetime.min, SourceUpToDatenessCollector._parse_source_responses() would
+        # always return datetime.min as it returns the oldest timestamp it sees.
+        return parse(test_suites[0].get("timestamp", "")) if test_suites else datetime.now()
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -17,6 +17,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 - Add subject name to metrics in MS Teams notifications, so it's clear which metric changed status when different subjects have metrics with the same name. Fixes [#2353](https://github.com/ICTU/quality-time/issues/2353).
 - After reloading a report, edit controls are shown even when the user has no edit permission. Fixes [#2373](https://github.com/ICTU/quality-time/issues/2373).
 - The security warnings in OWASP ZAP reports do not have unique keys. However, *Quality-time* needs security warnings to be uniquely identifiable to detect whether the list of warnings changes between measurements. Therefore, *Quality-time* generates keys for OWASP ZAP security warnings itself. Unfortunately, the key that *Quality-time* generated, was not guaranteed to be unique. Fixes [#2429](https://github.com/ICTU/quality-time/issues/2429).
+- JUnit XML files may have empty test suites, be prepared. Fixes [#2507](https://github.com/ICTU/quality-time/issues/2507).
 
 ### Added
 
