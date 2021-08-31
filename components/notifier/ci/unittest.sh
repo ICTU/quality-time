594627484a7ab9e7c51758ea1daad95614377411
Frank Niessink <frank@niessink.com>
2021-08-31 14:54:36 +0200

    Remove set -e so that coverage reports are generated even if the target is not met. (#2564)

--- components/notifier/ci/unittest.sh
+++ components/notifier/ci/unittest.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 # We currently get one warning when running Python in dev mode:
 # aiohttp/helpers.py:107: DeprecationWarning: "@coroutine" decorator is deprecated since Python 3.8, use "async def" instead
 # Turn off dev mode until aiohttp gets fixed or there's a way to suppress warnings in third party code
