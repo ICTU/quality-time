12838795636b39ef312bffbe72ece497f6e758e7
Frank Niessink <frank@niessink.com>
2021-09-07 21:47:08 +0200

    Refactor props.

--- components/frontend/src/metric/Target.js
+++ components/frontend/src/metric/Target.js
@@ -4,24 +4,24 @@ import { StringInput } from '../fields/StringInput';
 import { set_metric_attribute } from '../api/metric';
 import { EDIT_REPORT_PERMISSION } from '../context/Permissions';
 
-export function Target(props) {
-    const metric_type = props.datamodel.metrics[props.metric.type];
-    const metric_scale = props.metric.scale || metric_type.default_scale || "count";
+export function Target({ datamodel, metric, metric_uuid, target_type, label, reload }) {
+    const metric_type = datamodel.metrics[metric.type];
+    const metric_scale = metric.scale || metric_type.default_scale || "count";
     // Old versions of the datamodel may contain the unicode version of the direction, be prepared:
-    const metric_direction = { "≦": "<", "≧": ">", "<": "<", ">": ">" }[props.metric.direction || metric_type.direction];
+    const metric_direction = { "≦": "<", "≧": ">", "<": "<", ">": ">" }[metric.direction || metric_type.direction];
     const metric_direction_prefix = { "<": "≦", ">": "≧" }[metric_direction];
-    const default_target = metric_type[props.target_type];
-    const measurement_value = props.metric[props.target_type];
-    const metric_unit_without_percentage = props.metric.unit || metric_type.unit;
+    const default_target = metric_type[target_type];
+    const measurement_value = metric[target_type];
+    const metric_unit_without_percentage = metric.unit || metric_type.unit;
     const metric_unit = `${metric_scale === "percentage" ? "% " : ""}${metric_unit_without_percentage}`;
-    const label = props.label + (metric_type[props.target_type] === props.metric[props.target_type] || default_target === undefined ? '' : ` (default: ${default_target} ${metric_unit})`);
+    const inputLabel = label + (metric_type[target_type] === metric[target_type] || default_target === undefined ? '' : ` (default: ${default_target} ${metric_unit})`);
     if (metric_scale === "version_number") {
         return (
             <StringInput
-                label={label}
+                label={inputLabel}
                 prefix={metric_direction_prefix}
                 requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                set_value={(value) => set_metric_attribute(props.metric_uuid, props.target_type, value, props.reload)}
+                set_value={(value) => set_metric_attribute(metric_uuid, target_type, value, reload)}
                 value={measurement_value}
             />
         )
@@ -29,12 +29,12 @@ export function Target(props) {
         const max = metric_scale === "percentage" ? "100" : null;
         return (
             <IntegerInput
-                label={label}
+                label={inputLabel}
                 max={max}
                 min="0"
                 prefix={metric_direction_prefix}
                 requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                set_value={(value) => set_metric_attribute(props.metric_uuid, props.target_type, value, props.reload)}
+                set_value={(value) => set_metric_attribute(metric_uuid, target_type, value, reload)}
                 unit={metric_unit}
                 value={measurement_value}
             />
