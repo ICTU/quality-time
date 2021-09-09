a5e753ce916866e504a5ac2e8cc2265fd15c3471
Frank Niessink <frank@niessink.com>
2021-09-09 16:48:05 +0200

    Refactor props (#2589)
    * Refactor Metric props.
    * Refactor MetricDetails props.

--- components/frontend/src/metric/Metric.js
+++ components/frontend/src/metric/Metric.js
@@ -10,7 +10,21 @@ import { SourceStatus } from './SourceStatus';
 import { StatusIcon } from './StatusIcon';
 import { TrendSparkline } from './TrendSparkline';
 
-export function Metric(props) {
+export function Metric({
+  datamodel,
+  reports_overview,
+  report,
+  report_date,
+  subject_uuid,
+  metric_uuid,
+  first_metric,
+  last_metric,
+  stop_sort,
+  changed_fields,
+  visibleDetailsTabs,
+  toggleVisibleDetailsTab,
+  hiddenColumns
+}) {
   function MeasurementValue() {
     const value = metric.value && metric_type.unit === "minutes" && metric.scale !== "percentage" ? format_minutes(metric.value) : metric.value || "?";
     const now = new Date();
@@ -26,7 +40,7 @@ export function Metric(props) {
     )
   }
   function measurement_target() {
-    const metric_direction = get_metric_direction(metric, props.datamodel)
+    const metric_direction = get_metric_direction(metric, datamodel)
     let debt_end = "";
     if (metric.debt_end_date) {
       const end_date = new Date(metric.debt_end_date);
@@ -41,37 +55,57 @@ export function Metric(props) {
   }
   function measurement_sources() {
     return sources.map((source, index) => [index > 0 && ", ", <SourceStatus key={source.source_uuid} source_uuid={source.source_uuid}
-      metric={metric} source={source} datamodel={props.datamodel} />])
+      metric={metric} source={source} datamodel={datamodel} />])
   }
-  const metric = props.report.subjects[props.subject_uuid].metrics[props.metric_uuid];
-  const metric_type = props.datamodel.metrics[metric.type];
+  const metric = report.subjects[subject_uuid].metrics[metric_uuid];
+  const metric_type = datamodel.metrics[metric.type];
   const latest_measurements = metric.recent_measurements;
   const latest_measurement = latest_measurements.length > 0 ? latest_measurements[latest_measurements.length - 1] : null;
   const sources = (latest_measurement && latest_measurement.sources) || [];
   const metric_unit = formatMetricScaleAndUnit(metric_type, metric);
-  const metric_name = get_metric_name(metric, props.datamodel);
-  const details = <MetricDetails measurement={latest_measurement} metric_name={metric_name} scale={metric.scale} unit={formatMetricScaleAndUnit(metric_type, metric, false)} {...props} />
-  const expanded = props.visibleDetailsTabs.filter((tab) => tab?.startsWith(props.metric_uuid)).length > 0;
+  const metric_name = get_metric_name(metric, datamodel);
+  const details = (
+    <MetricDetails 
+      measurement={latest_measurement}
+      metric_name={metric_name}
+      scale={metric.scale}
+      unit={formatMetricScaleAndUnit(metric_type, metric, false)}
+      datamodel={datamodel}
+      report_date={report_date}
+      reports={reports_overview}
+      report={report}
+      subject_uuid={subject_uuid}
+      metric_uuid={metric_uuid}
+      metric_unit={metric_unit}
+      first_metric={first_metric}
+      last_metric={last_metric}
+      stop_sort={stop_sort}
+      changed_fields={changed_fields}
+      visibleDetailsTabs={visibleDetailsTabs}
+      toggleVisibleDetailsTab={toggleVisibleDetailsTab}
+      reload />
+  )
+  const expanded = visibleDetailsTabs.filter((tab) => tab?.startsWith(metric_uuid)).length > 0;
   function onExpand(expand) {
     if (expand) {
-      props.toggleVisibleDetailsTab(`${props.metric_uuid}:0`)
+      toggleVisibleDetailsTab(`${metric_uuid}:0`)
     } else {
-      const tabs = props.visibleDetailsTabs.filter((each) => each?.startsWith(props.metric_uuid));
+      const tabs = visibleDetailsTabs.filter((each) => each?.startsWith(metric_uuid));
       if (tabs.length > 0) {
-        props.toggleVisibleDetailsTab(tabs[0])
+        toggleVisibleDetailsTab(tabs[0])
       }
     }
   }
   return (
-    <TableRowWithDetails id={props.metric_uuid} className={metric.status} details={details} expanded={expanded} onExpand={(state) => onExpand(state)}>
+    <TableRowWithDetails id={metric_uuid} className={metric.status} details={details} expanded={expanded} onExpand={(state) => onExpand(state)}>
       <Table.Cell>{metric_name}</Table.Cell>
-      {!props.hiddenColumns.includes("trend") && <Table.Cell><TrendSparkline measurements={latest_measurements} report_date={props.report_date} scale={metric.scale} /></Table.Cell>}
-      {!props.hiddenColumns.includes("status") && <Table.Cell textAlign='center'><StatusIcon status={metric.status} status_start={metric.status_start} /></Table.Cell>}
-      {!props.hiddenColumns.includes("measurement") && <Table.Cell><MeasurementValue /></Table.Cell>}
-      {!props.hiddenColumns.includes("target") && <Table.Cell>{measurement_target()}</Table.Cell>}
-      {!props.hiddenColumns.includes("source") && <Table.Cell>{measurement_sources()}</Table.Cell>}
-      {!props.hiddenColumns.includes("comment") && <Table.Cell><div dangerouslySetInnerHTML={{ __html: metric.comment }} /></Table.Cell>}
-      {!props.hiddenColumns.includes("tags") && <Table.Cell>{get_metric_tags(metric).map((tag) => <Tag key={tag} tag={tag} />)}</Table.Cell>}
+      {!hiddenColumns.includes("trend") && <Table.Cell><TrendSparkline measurements={latest_measurements} report_date={report_date} scale={metric.scale} /></Table.Cell>}
+      {!hiddenColumns.includes("status") && <Table.Cell textAlign='center'><StatusIcon status={metric.status} status_start={metric.status_start} /></Table.Cell>}
+      {!hiddenColumns.includes("measurement") && <Table.Cell><MeasurementValue /></Table.Cell>}
+      {!hiddenColumns.includes("target") && <Table.Cell>{measurement_target()}</Table.Cell>}
+      {!hiddenColumns.includes("source") && <Table.Cell>{measurement_sources()}</Table.Cell>}
+      {!hiddenColumns.includes("comment") && <Table.Cell><div dangerouslySetInnerHTML={{ __html: metric.comment }} /></Table.Cell>}
+      {!hiddenColumns.includes("tags") && <Table.Cell>{get_metric_tags(metric).map((tag) => <Tag key={tag} tag={tag} />)}</Table.Cell>}
     </TableRowWithDetails>
   )
 }
