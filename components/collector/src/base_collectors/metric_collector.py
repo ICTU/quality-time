9349dde76c3dd1e0ae3a7d9d0be13ab85ef19eb7
Frank Niessink <frank@niessink.com>
2021-09-14 20:47:45 +0200

    Need both a tracker type and a URL.

--- components/collector/src/base_collectors/metric_collector.py
+++ components/collector/src/base_collectors/metric_collector.py
@@ -61,9 +61,10 @@ class MetricCollector:
 
     def __issue_status_collector(self) -> Optional[Coroutine]:
         """Create the issue status collector for the metric."""
+        issue = self._metric.get("tracker_issue")
         tracker = self._metric.get("issue_tracker", {})
         tracker_type = tracker.get("type")
-        issue = self._metric.get("tracker_issue")
-        if issue and tracker_type and (collector_class := SourceCollector.get_subclass(tracker_type, "issue_status")):
+        has_tracker = bool(tracker_type and tracker.get("url"))
+        if issue and has_tracker and (collector_class := SourceCollector.get_subclass(tracker_type, "issue_status")):
             return collector_class(self.__session, tracker, self.__data_model).collect_issue_status(issue)
         return None
