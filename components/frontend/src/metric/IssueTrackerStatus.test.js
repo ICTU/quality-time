798b1449c81809de147e859877179e28bf04d47a
Frank Niessink <frank@niessink.com>
2021-09-18 17:37:29 +0200

    Improve tests.

--- components/frontend/src/metric/IssueTrackerStatus.test.js
+++ components/frontend/src/metric/IssueTrackerStatus.test.js
@@ -5,7 +5,7 @@ import { IssueTrackerStatus } from './IssueTrackerStatus';
 it("displays a link with correct content and popup", async () => {
     const metric = {
         issue_id: "123",
-        issue_status: {issue: "123", name: "in progress", description: "doing", landing_url: "https://test"}
+        issue_status: { issue: "123", name: "in progress", description: "doing", landing_url: "https://test" }
     }
     const statusText = metric.issue_id + ": " + metric.issue_status.name
     const { queryByText } = render(<IssueTrackerStatus metric={metric} />)
@@ -14,27 +14,25 @@ it("displays a link with correct content and popup", async () => {
 });
 
 it("displays a connection error", async () => {
-    const metric = {issue_id: "123", issue_status: {issue: "123", connection_error: "error"}}
+    const metric = { issue_id: "123", issue_status: { issue: "123", connection_error: "error" } }
     const { queryByTestId } = render(<IssueTrackerStatus metric={metric} />)
     expect(queryByTestId("errorlabel")).not.toBe(null)
 });
 
 it("displays a parse error", async () => {
-    const metric = {issue_id: "123", issue_status: {issue: "123", parse_error: "error"}}
+    const metric = { issue_id: "123", issue_status: { issue: "123", parse_error: "error" } }
     const { queryByTestId } = render(<IssueTrackerStatus metric={metric} />)
     expect(queryByTestId("errorlabel")).not.toBe(null)
 });
 
 it("displays nothing if the metric has no metric id", async () => {
-    const metric = {
-        issue_status: {issue: "123", name: "in progress", description: "doing", landing_url: "https://test"}
-    }
-    const { queryByTestId } = render(<IssueTrackerStatus metric={metric} />)
-    expect(queryByTestId("errorlabel")).toBe(null)
+    const metric = { issue_status: { issue: "123", name: "in progress", landing_url: "https://test" } }
+    const { queryByText } = render(<IssueTrackerStatus metric={metric} />)
+    expect(queryByText(/in progress/)).toBe(null)
 });
 
 it("displays nothing if the metric has no issue startus", async () => {
     const metric = { issue_id: "123" }
-    const { queryByTestId } = render(<IssueTrackerStatus metric={metric} />)
-    expect(queryByTestId("errorlabel")).toBe(null)
+    const { queryByText } = render(<IssueTrackerStatus metric={metric} />)
+    expect(queryByText(/123/)).toBe(null)
 });
