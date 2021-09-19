29399e165e98a344da1abd4770ed6c4074ffe227
Frank Niessink <frank@niessink.com>
2021-09-19 17:16:19 +0200

    Prepare for multiple issues per metric.

--- components/frontend/src/metric/IssueTrackerStatus.js
+++ components/frontend/src/metric/IssueTrackerStatus.js
@@ -4,14 +4,17 @@ import { HyperLink } from '../widgets/HyperLink';
 
 export function IssueTrackerStatus({ metric }) {
     if (!metric.issue_id || !metric.issue_status) { return null }
-    const label_text = metric.issue_id + ": " + (metric.issue_status.name || "?");
-    const label = metric.issue_status.landing_url ? <HyperLink url={metric.issue_status.landing_url}>{label_text}</HyperLink> : label_text;
-    const error = metric.issue_status.connection_error ? "Connection error" : "Parse error";
-    return (
-        <Popup
-            flowing hoverable
-            trigger={metric.issue_status.connection_error || metric.issue_status.parse_error ? <Label data-testid="errorlabel" color='red'>{label}</Label> : <div>{label}</div>}>
-            {metric.issue_status.description || error}
-        </Popup>
-    )
+    let issue_statuses = [metric.issue_status]
+    return issue_statuses.map((issue_status) => {
+        const label_text = metric.issue_id + ": " + (issue_status.name || "?");
+        const label = issue_status.landing_url ? <HyperLink url={issue_status.landing_url}>{label_text}</HyperLink> : label_text;
+        const error = issue_status.connection_error ? "Connection error" : "Parse error";
+        return (
+            <Popup
+                flowing hoverable
+                trigger={issue_status.connection_error || issue_status.parse_error ? <Label data-testid="errorlabel" color='red'>{label}</Label> : <div>{label}</div>}>
+                {issue_status.description || error}
+            </Popup>
+        )
+    })
 }
