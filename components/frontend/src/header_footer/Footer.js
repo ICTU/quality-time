ac78d737340f2376dd9c9ea595f6105525bb9588
Frank Niessink <frank@niessink.com>
2021-09-20 18:18:42 +0200

    Remove divider and add icons.

--- components/frontend/src/header_footer/Footer.js
+++ components/frontend/src/header_footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Container, Segment, Grid, Header, List, Divider, Image } from 'semantic-ui-react';
+import { Container, Divider, Grid, Header, Icon, Image, List, Segment } from 'semantic-ui-react';
 
-function FooterColumn(props) {
+function FooterColumn({ children, header, textAlign, width }) {
     return (
-        <Grid.Column width={props.width || 5}>
-            {props.header ? <Header inverted as='h4'>{props.header}</Header> : null}
+        <Grid.Column width={width || 3} textAlign={textAlign || "left"}>
+            {header ? <Header inverted as='h4'>{header}</Header> : null}
             <List link inverted>
-                {props.children}
+                {children}
             </List>
         </Grid.Column>
     )
@@ -14,55 +14,67 @@ function FooterColumn(props) {
 
 function AboutAppColumn() {
     return (
-        <FooterColumn header={<><em>Quality-time</em> v{process.env.REACT_APP_VERSION}</>} >
-            <List.Item as='a' href="https://www.ictu.nl/about-us">Developed by ICTU</List.Item>
-            <List.Item as='a' href='https://github.com/ICTU/quality-time/blob/master/LICENSE'>License</List.Item>
-            <List.Item as='a' href={`https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/changelog.html`}>Changelog</List.Item>
-            <List.Item as='a' href="https://github.com/ICTU/quality-time">Source code</List.Item>
-        </FooterColumn>
+        <>
+            <Grid.Column width={1} />
+            <FooterColumn header={<><em>Quality-time</em> v{process.env.REACT_APP_VERSION}</>} >
+                <List.Item as='a' href="https://www.ictu.nl/about-us"><Icon name="flask"/> Developed by ICTU</List.Item>
+                <List.Item as='a' href='https://github.com/ICTU/quality-time/blob/master/LICENSE'><Icon name="copyright outline" /> License</List.Item>
+                <List.Item as='a' href={`https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/changelog.html`}><Icon name="history" /> Changelog</List.Item>
+                <List.Item as='a' href="https://github.com/ICTU/quality-time"><Icon name="code" /> Source code</List.Item>
+            </FooterColumn>
+        </>
     )
 }
 
-function AboutReportColumn(props) {
+function AboutReportColumn({ report, last_update }) {
     return (
-        <FooterColumn width={6} header="About this report">
-            <List.Item>{props.report.title}</List.Item>
-            <List.Item>{props.report.subtitle || <span>&nbsp;</span>}</List.Item>
-            <List.Item>{props.last_update.toLocaleDateString()}</List.Item>
-            <List.Item>{props.last_update.toLocaleTimeString()}</List.Item>
+        <FooterColumn width={8} textAlign="center" header="About this report">
+            <List.Item>{report.title}</List.Item>
+            <List.Item>{report.subtitle || <span>&nbsp;</span>}</List.Item>
+            <List.Item>{last_update.toLocaleDateString()}</List.Item>
+            <List.Item>{last_update.toLocaleTimeString()}</List.Item>
         </FooterColumn>
     )
 }
 
 function QuoteColumn() {
+    /* Use the zero-width non-joiner character to make this column the same height as the other two columns 
+       so that the two dividers between the three columns are equal height as well
+    */
     return (
-        <FooterColumn width={6}>
+        <FooterColumn textAlign='center' width={8} header="&zwnj;">
             <List.Item><em>Quality without results is pointless.</em></List.Item>
             <List.Item><em>Results without quality is boring.</em></List.Item>
             <List.Item>Johan Cruyff</List.Item>
+            <List.Item>&zwnj;</List.Item>
         </FooterColumn>
     )
 }
 
 function SupportColumn() {
     return (
-        <FooterColumn header='Support'>
-            <List.Item as='a' href={`https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/`}>Documentation</List.Item>
-            <List.Item as='a' href={`https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/usage.html`}>User manual</List.Item>
-            <List.Item as='a' href="https://github.com/ICTU/quality-time/issues">Known issues</List.Item>
-            <List.Item as='a' href="https://github.com/ICTU/quality-time/issues/new">Report an issue</List.Item>
-        </FooterColumn>
+        <>
+            <Grid.Column width={1} />
+            <FooterColumn header='Support'>
+                <List.Item as='a' href={`https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/`}><Icon name="book" /> Documentation</List.Item>
+                <List.Item as='a' href={`https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/usage.html`}><Icon name="user outline" /> User manual</List.Item>
+                <List.Item as='a' href="https://github.com/ICTU/quality-time/issues"><Icon name="bug" /> Known issues</List.Item>
+                <List.Item as='a' href="https://github.com/ICTU/quality-time/issues/new"><Icon name="comment" /> Report an issue</List.Item>
+            </FooterColumn>
+        </>
     )
 }
 
-export function Footer(props) {
+export function Footer({ report, last_update }) {
     return (
         <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em 3em' }}>
             <Container>
-                <Grid divided inverted stackable textAlign='center' verticalAlign='middle'>
-                    <AboutAppColumn />
-                    {props.report ? <AboutReportColumn {...props} /> : <QuoteColumn />}
-                    <SupportColumn />
+                <Grid inverted stackable verticalAlign='middle' columns={16}>
+                    <Grid.Row>
+                        <AboutAppColumn />
+                        {report ? <AboutReportColumn report={report} last_update={last_update} /> : <QuoteColumn />}
+                        <SupportColumn />
+                    </Grid.Row>
                 </Grid>
                 <Divider inverted section />
                 <Image centered size='mini' src='./favicon.ico' />
