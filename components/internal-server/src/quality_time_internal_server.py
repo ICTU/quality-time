8675216651830cc6cea43003d9dd72691719a5ea
Frank Niessink <frank@niessink.com>
2021-09-21 17:08:10 +0200

    Suppress DeepSourc issue.

--- components/internal-server/src/quality_time_internal_server.py
+++ components/internal-server/src/quality_time_internal_server.py
@@ -8,7 +8,9 @@ from routes import data_model, health, measurements, metrics
 
 app = FastAPI()
 for module in data_model, health, measurements, metrics:
-    app.include_router(getattr(module, "router"))
+    # DeepSource complains that getattr doesn't receive a default value and will cause an AttributeError if the
+    # attribute does not exist. As we do want to fail if the module has no router, we suppress the rule here.
+    app.include_router(getattr(module, "router"))  # skipcq: PTC-W0034
 
 
 @app.get("/")
