bbba566695bb5665a1fd29dc5f6bd892ad062100
Frank Niessink <frank@niessink.com>
2021-09-24 13:13:51 +0200

    Use labels.

--- components/frontend/src/metric/IssueStatus.js
+++ components/frontend/src/metric/IssueStatus.js
@@ -6,13 +6,23 @@ import { HyperLink } from '../widgets/HyperLink';
 export function IssueStatus({ metric }) {
     const issueStatuses = metric.issue_status || [];
     const statuses = issueStatuses.map((issueStatus) => {
-        let label = issueStatus.issue_id + ": " + (issueStatus.name || "?"); 
-        if (issueStatus.landing_url) { label = <HyperLink url={issueStatus.landing_url}>{label}</HyperLink> }
-        if (issueStatus.created) { label = <>{label} (created <TimeAgo date={issueStatus.created}/>)</>}
-        let error = "";
-        if (issueStatus.connection_error) { error = "Connection error" }
-        if (issueStatus.parse_error) { error = "Parse error" }
-        if (error) { label = <Popup content={error} flowing hoverable trigger={<Label color="red">{label}</Label>} /> }
+        let popupContent = "";
+        if (issueStatus.connection_error) { popupContent = "Connection error" }
+        if (issueStatus.parse_error) { popupContent = "Parse error" }
+        const color = popupContent ? "red" : "grey";
+        const basic = popupContent ? false : true;
+        let label = <Label basic={basic} color={color}>{issueStatus.issue_id}<Label.Detail>{issueStatus.name || "?"}</Label.Detail></Label>
+        if (issueStatus.landing_url) {
+            // Without the span, the popup doesn't work
+            label = <span><HyperLink url={issueStatus.landing_url}>{label}</HyperLink></span>
+        }
+        if (!popupContent && issueStatus.created) {
+            const creationDate = new Date(issueStatus.created);
+            popupContent = <>Created <TimeAgo date={issueStatus.created}/> ({creationDate.toLocaleString()})</>
+        }
+        if (popupContent){
+            label = <Popup content={popupContent} flowing hoverable trigger={label} />
+        }
         return <div key={issueStatus.issue_id}>{label}</div>
     });
     return <>{statuses}</>
--- components/frontend/src/metric/IssueStatus.test.js
+++ components/frontend/src/metric/IssueStatus.test.js
@@ -5,10 +5,9 @@ import { IssueStatus } from './IssueStatus';
 
 it("displays a link with correct content", async () => {
     const metric = { issue_status: [{ issue_id: "123", name: "in progress", landing_url: "https://test" }] }
-    const label = metric.issue_status[0].issue_id + ": " + metric.issue_status[0].name
     const { queryByText } = render(<IssueStatus metric={metric} />)
-    expect(queryByText(label)).not.toBe(null)
-    expect(queryByText(label).closest("a").href).toBe("https://test/")
+    expect(queryByText(/123/)).not.toBe(null)
+    expect(queryByText(/123/).closest("a").href).toBe("https://test/")
 });
 
 it("displays the creation date", async () => {
@@ -16,21 +15,21 @@ it("displays the creation date", async () => {
     date.setDate(date.getDate() - 2);
     const metric = { issue_status: [{ issue_id: "123", name: "done", created: date.toISOString()}] }
     const { queryByText } = render(<IssueStatus metric={metric} />)
-    userEvent.hover(queryByText(/123: ?/))
+    userEvent.hover(queryByText(/123/))
     await waitFor(() => { expect(queryByText("2 days ago")).not.toBe(null) })
 });
 
 it("displays a connection error", async () => {
     const metric = { issue_status: [{ issue_id: "123", connection_error: "error" }] }
     const { queryByText } = render(<IssueStatus metric={metric} />)
-    userEvent.hover(queryByText(/123: ?/))
+    userEvent.hover(queryByText(/123/))
     await waitFor(() => { expect(queryByText("Connection error")).not.toBe(null) })
 });
 
 it("displays a parse error", async () => {
     const metric = { issue_status: [{ issue_id: "123", parse_error: "error" }] }
     const { queryByText } = render(<IssueStatus metric={metric} />)
-    userEvent.hover(queryByText(/123: ?/))
+    userEvent.hover(queryByText(/123/))
     await waitFor(() => { expect(queryByText("Parse error")).not.toBe(null) })
 });
 
