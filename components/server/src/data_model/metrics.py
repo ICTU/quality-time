73483cecce14a7a951f8479d7e619b86f4dc3c01
Frank Niessink <frank@niessink.com>
2021-09-25 16:54:29 +0200

    Sort metrics alphabetically. Remove sentiment tag. Specify addition as MIN (adding sentiments doesn't make sense).

--- components/server/src/data_model/metrics.py
+++ components/server/src/data_model/metrics.py
@@ -267,6 +267,16 @@ METRICS = Metrics.parse_obj(
             ],
             tags=[Tag.SECURITY],
         ),
+        sentiment=dict(
+            name="Sentiment",
+            description="How are the team members feeling?",
+            unit=Unit.NONE,
+            addition=Addition.MIN,
+            direction=Direction.MORE_IS_BETTER,
+            target="10",
+            near_target="8",
+            sources=["manual_number"],
+        ),
         suppressed_violations=dict(
             name="Suppressed violations",
             description="The amount of violations suppressed in the source.",
@@ -389,16 +399,6 @@ METRICS = Metrics.parse_obj(
             default_source="sonarqube",
             sources=["manual_number", "ojaudit", "sonarqube"],
             tags=[Tag.MAINTAINABILITY],
-        ),
-        sentiment=dict(
-            name="Sentiment",
-            description="How are the team members feeling?",
-            unit=Unit.NONE,
-            direction=Direction.MORE_IS_BETTER,
-            target="10",
-            near_target="8",
-            sources=["manual_number"],
-            tags=[Tag.PROCESS_EFFICIENCY],
         ),
     )
 )
