7a27faea4b44603379c6df904082c77ed3bf2194
Frank Niessink <frank@niessink.com>
2021-10-06 23:42:29 +0200

    Prepare for running on Ubuntu VM.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd tests/application_tests
-pip --quiet install --progress-bar off --user -r requirements-dev.txt
-python -m unittest discover --start-directory .
+python3 -m pip --quiet install --progress-bar off --user -r requirements-dev.txt
+python3 -m unittest discover --start-directory .
