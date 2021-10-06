670e7f02ac370dce74912032ee6aa0723a2c4018
Frank Niessink <frank@niessink.com>
2021-10-06 23:52:57 +0200

    Revert usage of cimg for application tests.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd tests/application_tests
-python3 -m pip --quiet install --progress-bar off --user -r requirements-dev.txt
-python3 -m unittest discover --start-directory .
+pip --quiet install --progress-bar off --user -r requirements-dev.txt
+python -m unittest discover --start-directory .
