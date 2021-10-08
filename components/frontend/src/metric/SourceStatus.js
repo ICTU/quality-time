a0ed49b8c74d3ad8f66fd0ef19c6bc5af165f04b
Frank Niessink <frank@niessink.com>
2021-10-08 23:30:37 +0200

    Refactoring.

--- components/frontend/src/metric/SourceStatus.js
+++ components/frontend/src/metric/SourceStatus.js
@@ -4,11 +4,11 @@ import { DataModel } from '../context/DataModel';
 import { HyperLink } from '../widgets/HyperLink';
 import { get_source_name } from '../utils';
 
-export function SourceStatus({metric, measurement_source, source_uuid}) {
+export function SourceStatus({metric, measurement_source }) {
     const dataModel = useContext(DataModel)
     // Source may be deleted from report but still referenced in the latest measurement, be prepared:
-    if (!Object.keys(metric.sources).includes(source_uuid)) { return null }
-    const source = metric.sources[source_uuid];
+    if (!Object.keys(metric.sources).includes(measurement_source.source_uuid)) { return null }
+    const source = metric.sources[measurement_source.source_uuid];
     const source_name = get_source_name(source, dataModel);
     function source_label() {
         return (measurement_source.landing_url ? <HyperLink url={measurement_source.landing_url}>{source_name}</HyperLink> : source_name)
--- components/frontend/src/metric/SourceStatus.test.js
+++ components/frontend/src/metric/SourceStatus.test.js
@@ -6,26 +6,26 @@ import { SourceStatus } from './SourceStatus';
 
 const metric = { sources: { source_uuid: { name: "Source name" } } };
 
-function render_source_status(measurement_source, source_uuid) {
+function render_source_status(measurement_source) {
     return render(
-        <DataModel.Provider value={{}}><SourceStatus
-            metric={metric} measurement_source={measurement_source} source_uuid={source_uuid || "source_uuid"}
-        /></DataModel.Provider>
+        <DataModel.Provider value={{}}>
+            <SourceStatus metric={metric} measurement_source={measurement_source} />
+        </DataModel.Provider>
     )
 }
 
 it('renders the hyperlink label if the the source has a landing url', () => {
-    render_source_status({ landing_url: "https://landing" });
+    render_source_status({ landing_url: "https://landing", source_uuid: "source_uuid" });
     expect(screen.getAllByRole("link").length).toBe(1)
 });
 
 it('renders the source label if there is no error', () => {
-    render_source_status({});
+    render_source_status({source_uuid: "source_uuid"});
     expect(screen.getAllByText(/Source name/).length).toBe(1)
 });
 
 it('renders the source label and the popup if there is an connection error', async () => {
-    render_source_status({ connection_error: "error" })
+    render_source_status({source_uuid: "source_uuid", connection_error: "error" })
     expect(screen.getAllByText(/Source name/).length).toBe(1)
     userEvent.hover(screen.queryByText(/Source name/))
     await waitFor(() => {
@@ -34,7 +34,7 @@ it('renders the source label and the popup if there is an connection error', asy
 });
 
 it('renders the source label and the popup if there is a parse error', async () => {
-    render_source_status({ parse_error: "error" })
+    render_source_status({source_uuid: "source_uuid", parse_error: "error" })
     expect(screen.getAllByText(/Source name/).length).toBe(1)
     userEvent.hover(screen.queryByText(/Source name/))
     await waitFor(() => {
@@ -43,6 +43,6 @@ it('renders the source label and the popup if there is a parse error', async () 
 });
 
 it('renders nothing if the source has been deleted', () => {
-    render_source_status({}, "other_source_uuid");
+    render_source_status({source_uuid: "other_source_uuid"});
     expect(screen.queryByText(/Source name/)).toBe(null)
 });
