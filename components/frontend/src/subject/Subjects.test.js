0fef7b467aaef3b036b5c788e433c62164dc3cf3
Erik <emdverboom@gmail.com>
2021-10-14 12:16:08 +0200

    add subject buttons test

--- components/frontend/src/subject/Subjects.test.js
+++ components/frontend/src/subject/Subjects.test.js
@@ -5,6 +5,36 @@ import { mount } from 'enzyme';
 import { DataModel } from '../context/DataModel';
 import { EDIT_REPORT_PERMISSION, Permissions } from '../context/Permissions';
 import { Subjects } from './Subjects';
+import { add_subject, copy_subject, move_subject } from '../api/subject';
+import { subject_options } from '../widgets/menu_options';
+
+jest.mock('../api/subject', () => {
+    const originalModule = jest.requireActual('../api/subject');
+  
+    //Mock the default export and named export 'foo'
+    return {
+        __esModule: true,
+        ...originalModule,
+        add_subject: jest.fn(() => {}),
+        copy_subject: jest.fn(() => {}),
+        move_subject: jest.fn(() => {}),
+    };
+});
+
+jest.mock('../widgets/menu_options', () => {
+    const originalModule = jest.requireActual('../api/subject');
+  
+    //Mock the default export and named export 'foo'
+    return {
+        __esModule: true,
+        ...originalModule,
+        subject_options: jest.fn(() => [{text: "dummy option 1"}, {text: "dummy option 2"}]),
+    };
+});
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 
 const datamodel = {
     subjects: {
@@ -86,8 +116,30 @@ it("shows the add subject button when editable", () => {
 
 it("does not show the add subject button when not editable", () => {
     render_subjects();
-    expect(screen.queryByText(/Add subject/)).toBeNull();
+    expect(screen.queryByText(/Add[ \r\n]+subject/)).toBeNull();
 })
+
+it("adds a subject", () => {
+    render_subjects([EDIT_REPORT_PERMISSION]);
+    userEvent.click(screen.getByText(/Add[ \r\n]+subject/))
+    expect(add_subject.mock.calls.length).toBe(1);
+});
+
+it("copies a subject", () => {
+    render_subjects([EDIT_REPORT_PERMISSION]);
+    userEvent.click(screen.getByText("Copy subject"))
+    expect(subject_options.mock.calls.length).toBe(1);
+    userEvent.click(screen.getByText("dummy option 1"))
+    expect(copy_subject.mock.calls.length).toBe(1)
+});
+
+it("moves a subject", () => {
+    render_subjects([EDIT_REPORT_PERMISSION]);
+    userEvent.click(screen.getByText("Move subject"))
+    expect(subject_options.mock.calls.length).toBe(1);
+    userEvent.click(screen.getByText("dummy option 2"))
+    expect(move_subject.mock.calls.length).toBe(1)
+});
 
 it("hides metrics not requiring action", () => {
     render_subjects();
