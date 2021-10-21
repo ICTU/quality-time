cdada42ede33b212db3c9276ef40dcd751b25647
Frank Niessink <frank@niessink.com>
2021-10-21 19:24:04 +0200

    Remove the unused serviceWorker.js.

--- components/frontend/src/index.js
+++ components/frontend/src/index.js
@@ -4,11 +4,5 @@ import 'semantic-ui-css/semantic.min.css'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
