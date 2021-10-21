54563c7480da97eef727960bea978c49470cc4f9
Frank Niessink <frank@niessink.com>
2021-10-21 17:39:31 +0200

    2663 console log (#2737)
    * Prevent 'Warning: Infinity is an invalid value for the width css style property.' message in the console log.
    
    * Prevent 'Error: Problem parsing d=M-2592.670630208333,NaNL-2592.670630208333,...' messages in the console log. These messages were caused by trying to create a sparkline graph for the source version metric. Fixes #2663.

--- components/frontend/src/metric/TrendSparkline.js
+++ components/frontend/src/metric/TrendSparkline.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { VictoryGroup, VictoryLine, VictoryTheme } from 'victory';
 
-export function TrendSparkline(props) {
-    let measurements = [];
-    for (let measurement of props.measurements) {
-        const value = (measurement[props.scale] && measurement[props.scale].value) || null;
+export function TrendSparkline({ measurements, scale, report_date }) {
+    if (scale === "version_number") { return null }
+    let points = [];
+    for (let measurement of measurements) {
+        const value = (measurement[scale] && measurement[scale].value) || null;
         const y = value !== null ? Number(value) : null;
         const x1 = new Date(measurement.start);
         const x2 = new Date(measurement.end);
-        measurements.push({ y: y, x: x1 });
-        measurements.push({ y: y, x: x2 });
+        points.push({ y: y, x: x1 });
+        points.push({ y: y, x: x2 });
     }
-    const now = props.report_date ? new Date(props.report_date) : new Date();
-    let week_ago = props.report_date ? new Date(props.report_date) : new Date();
+    const now = report_date ? new Date(report_date) : new Date();
+    let week_ago = report_date ? new Date(report_date) : new Date();
     week_ago.setDate(week_ago.getDate() - 7);
+    // The width property below is not used according to https://formidable.com/open-source/victory/docs/common-props#width,
+    // but setting it prevents these messages in the console: "Warning: `Infinity` is an invalid value for the `width` css style property.""
     return (
         <VictoryGroup theme={VictoryTheme.material} scale={{ x: "time", y: "linear" }} domain={{ x: [week_ago, now] }} height={30} padding={0}>
-            <VictoryLine data={measurements} interpolation="stepBefore" style={{
+            <VictoryLine data={points} interpolation="stepBefore" style={{
                 data: {
-                    stroke: "black", strokeWidth: 3
+                    stroke: "black", strokeWidth: 3, width: "100%"
                 }
             }} />
         </VictoryGroup>
