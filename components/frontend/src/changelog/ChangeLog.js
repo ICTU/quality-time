f365db3709ecb6418cc99daa09a729d03221677e
Frank Niessink <frank@niessink.com>
2021-10-30 18:46:21 +0200

    Expand ChangeLogWithoutMemo props.

--- components/frontend/src/changelog/ChangeLog.js
+++ components/frontend/src/changelog/ChangeLog.js
@@ -12,36 +12,36 @@ function Event({ description, email, timestamp }) {
             <Feed.Content>
                 <Feed.Summary>
                     {description}
-                    <Feed.Date><TimeAgoWithDate date={timestamp}/></Feed.Date>
+                    <Feed.Date><TimeAgoWithDate date={timestamp} /></Feed.Date>
                 </Feed.Summary>
             </Feed.Content>
         </Feed.Event>
     )
 }
 
-function ChangeLogWithoutMemo(props) {
+function ChangeLogWithoutMemo({ report_uuid, subject_uuid, metric_uuid, source_uuid, timestamp }) {
     const [changes, setChanges] = useState([]);
     const [nrChanges, setNrChanges] = useState(5);
     useEffect(() => {
         let didCancel = false;
         let uuids = {};
-        if (props.report_uuid) { uuids.report_uuid = props.report_uuid }
-        if (props.subject_uuid) { uuids.subject_uuid = props.subject_uuid }
-        if (props.metric_uuid) { uuids.metric_uuid = props.metric_uuid }
-        if (props.source_uuid) { uuids.source_uuid = props.source_uuid }
+        if (report_uuid) { uuids.report_uuid = report_uuid }
+        if (subject_uuid) { uuids.subject_uuid = subject_uuid }
+        if (metric_uuid) { uuids.metric_uuid = metric_uuid }
+        if (source_uuid) { uuids.source_uuid = source_uuid }
         get_changelog(nrChanges, uuids).then(function (json) {
             if (!didCancel) {
                 setChanges(json.changelog || []);
             }
         });
         return () => { didCancel = true; };
-    }, [props.report_uuid, props.subject_uuid, props.metric_uuid, props.source_uuid, props.timestamp, nrChanges]);
+    }, [report_uuid, subject_uuid, metric_uuid, source_uuid, timestamp, nrChanges]);
 
     let scope = "Changes in this instance of Quality-time";
-    if (props.report_uuid) { scope = "Changes in this report" }
-    if (props.subject_uuid) { scope = "Changes to this subject" }
-    if (props.metric_uuid) { scope = "Changes to this metric and its sources" }
-    if (props.source_uuid) { scope = "Changes to this source" }
+    if (report_uuid) { scope = "Changes in this report" }
+    if (subject_uuid) { scope = "Changes to this subject" }
+    if (metric_uuid) { scope = "Changes to this metric and its sources" }
+    if (source_uuid) { scope = "Changes to this source" }
 
     return (
         <Form>
@@ -53,7 +53,7 @@ function ChangeLogWithoutMemo(props) {
             <Segment>
                 <Feed size="small">
                     {changes.map(change =>
-                       <Event key={change.timestamp + change.delta} description={change.delta} email={change.email} timestamp={change.timestamp} />)}
+                        <Event key={change.timestamp + change.delta} description={change.delta} email={change.email} timestamp={change.timestamp} />)}
                 </Feed>
                 <Button basic icon primary size='small' onClick={() => setNrChanges(nrChanges + 10)}>
                     <Icon name="refresh" /> Load more changes
@@ -63,5 +63,5 @@ function ChangeLogWithoutMemo(props) {
     )
 }
 
-// Use React.memo so the ChangeLog is not re-rendered on reload, but only when one of its props change 
+// Use React.memo so the ChangeLog is not re-rendered on reload, but only when one of its props change
 export const ChangeLog = React.memo(props => <ChangeLogWithoutMemo {...props} />)
