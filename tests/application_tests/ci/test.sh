f31073e71791fa2416234dfc923faa99d623d572
Frank Niessink <frank@niessink.com>
2021-11-30 14:44:02 +0100

    Prevent dependency conflict.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd tests/application_tests
-pip --quiet install --progress-bar off --user -r requirements-dev.txt
+pip --quiet install --progress-bar off --user --upgrade pip
+pip --quiet install --progress-bar off --user --require-hashes -r requirements.txt
 python -m unittest discover --start-directory .
