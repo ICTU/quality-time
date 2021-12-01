3d71b0996982bad60959d118b10cc6308c83b1b3
Frank Niessink <frank@niessink.com>
2021-12-01 23:18:47 +0100

    Suppress false positive DeepSource issue.

--- components/collector/tests/source_collectors/jmeter_json/base.py
+++ components/collector/tests/source_collectors/jmeter_json/base.py
@@ -3,7 +3,7 @@
 from ..source_collector_test_case import SourceCollectorTestCase
 
 
-class JMeterJSONTestCase(SourceCollectorTestCase):
+class JMeterJSONTestCase(SourceCollectorTestCase):  # skipcq: PTC-W0046
     """Base class for JMeter JSON collector unit tests."""
 
     SOURCE_TYPE = "jmeter_json"
