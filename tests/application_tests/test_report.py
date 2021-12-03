e4652e6f4c264d219856d69749d6f71073f79416
Frank Niessink <frank@niessink.com>
2021-12-03 23:20:47 +0100

    Run application tests with a separate selenium container  (#2973)
    Run application tests with a separate selenium container so we don't need the obsolete circleci/python:3.10.0-browsers image anymore.

--- tests/application_tests/test_report.py
+++ tests/application_tests/test_report.py
@@ -39,25 +39,24 @@ class OpenReportTest(unittest.TestCase):
 
     def setUp(self):
         """Override to setup the driver."""
-        chrome_options = webdriver.ChromeOptions()
-        for argument in "headless no-sandbox single-process disable-dev-shm-usage disable-gpu".split(" "):
-            chrome_options.add_argument(f"--{argument}")
-        self.driver = webdriver.Chrome(options=chrome_options)
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.add_argument("--headless")
+        self.driver = webdriver.Remote("http://selenium:4444", options=firefox_options)
         self.driver.implicitly_wait(10)
         self.wait = WebDriverWait(self.driver, 10)
         self.driver.get("http://www:80")
 
     def tearDown(self):
         """Override to close the driver."""
-        self.driver.close()
+        self.driver.quit()
 
     def login(self):
         """Login a user."""
-        self.driver.find_element_by_xpath('//button[text()="Login"]').click()
-        login_form = self.driver.find_element_by_class_name("form")
-        login_form.find_element_by_name("username").send_keys("jadoe")
-        login_form.find_element_by_name("password").send_keys("secret")
-        login_form.find_element_by_class_name("button").click()
+        self.driver.find_element(By.XPATH, '//button[text()="Login"]').click()
+        login_form = self.driver.find_element(By.CLASS_NAME, "form")
+        login_form.find_element(By.NAME, "username").send_keys("jadoe")
+        login_form.find_element(By.NAME, "password").send_keys("secret")
+        login_form.find_element(By.CLASS_NAME, "button").click()
         self.wait.until(element_has_no_css_class((By.TAG_NAME, "body")))  # Wait for body dimmer to disappear
 
     def test_title(self):
@@ -66,25 +65,25 @@ class OpenReportTest(unittest.TestCase):
 
     def test_open_report(self):
         """Test that the first report can be opened."""
-        report = self.driver.find_elements_by_class_name("card")[0]
-        report_title = report.find_element_by_class_name("header")
+        report = self.driver.find_elements(By.CLASS_NAME, "card")[0]
+        report_title = report.find_element(By.CLASS_NAME, "header")
         report.click()
         self.assertTrue(
-            expect.text_to_be_present_in_element(self.driver.find_element_by_class_name("header"), report_title)
+            expect.text_to_be_present_in_element(self.driver.find_element(By.CLASS_NAME, "header"), report_title)
         )
 
     def test_login_and_logout(self):
         """Test that a user can login and logout."""
         self.login()
-        logout_dropdown = self.driver.find_element_by_class_name("dropdown")
+        logout_dropdown = self.driver.find_element(By.CLASS_NAME, "dropdown")
         logout_dropdown.click()
-        logout_menu_item = self.driver.find_element_by_class_name("selected.item")
+        logout_menu_item = self.driver.find_element(By.CLASS_NAME, "selected.item")
         logout_menu_item.click()
-        self.assertTrue(self.driver.find_element_by_xpath('//button[text()="Login"]'))
+        self.assertTrue(self.driver.find_element(By.XPATH, '//button[text()="Login"]'))
 
     def test_add_report(self):
         """Test that a logged in user can add a report."""
         self.login()
-        nr_reports = len(self.driver.find_elements_by_class_name("card"))
-        self.driver.find_element_by_class_name("button.primary").click()
+        nr_reports = len(self.driver.find_elements(By.CLASS_NAME, "card"))
+        self.driver.find_element(By.CLASS_NAME, "button.primary").click()
         self.wait.until(nr_elements((By.CLASS_NAME, "card"), nr_reports + 1))
