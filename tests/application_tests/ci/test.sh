6c0d41021460bb9c235d73d11bd3b9eda443c8fa
Frank Niessink <frank@niessink.com>
2021-12-03 22:20:13 +0100

    Use Firefox and update deprecated method calls.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cd tests/application_tests
-pip --quiet install --progress-bar off --user -r requirements-dev.txt
+python3 -m venv venv
+. venv/bin/activate
+pip --quiet install --upgrade pip
+pip --quiet install --progress-bar off --require-hashes -r requirements.txt
 python -m unittest discover --start-directory .
