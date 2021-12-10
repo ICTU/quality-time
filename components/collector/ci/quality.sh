9e64abd23665c932c94686d30d5d4fac2295e106
Frank Niessink <frank@niessink.com>
2021-12-10 22:50:45 +0100

    Run pip-audit on the Python components.

--- components/collector/ci/quality.sh
+++ components/collector/ci/quality.sh
@@ -12,6 +12,7 @@ mypy src
 pylint --rcfile=../../.pylintrc src tests
 isort **/*.py --check-only
 python -m flake8 --select=DUO src  # Dlint
+pip-audit --strict --progress-spinner=off
 safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
 bandit --quiet --recursive src/
 NAMES_TO_IGNORE='Anchore*,Axe*,AzureDevops*,Bandit*,Calendar*,Cloc*,Cobertura*,Composer*,CxSAST*,Generic*,GitLab*,Jacoco*,Jenkins*,Jira*,JMeter*,JUnit*,ManualNumber*,NCover*,Npm*,OJAudit*,OpenVAS*,OWASPDependencyCheck*,OWASPZAP*,PerformanceTestRunner*,Pip*,PyupioSafety*,QualityTime*,RobotFramework*,Snyk*,SonarQube*,Trello*'
--- components/notifier/ci/quality.sh
+++ components/notifier/ci/quality.sh
@@ -12,6 +12,7 @@ mypy src
 pylint --rcfile=../../.pylintrc src tests
 isort **/*.py --check-only
 python -m flake8 --select=DUO src
+pip-audit --strict --progress-spinner=off
 safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
 bandit --quiet --recursive src/
 vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
