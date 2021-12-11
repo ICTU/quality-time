74c66980ce48c5227c6ff7c9d19efdfbbe4496cd
Frank Niessink <frank@niessink.com>
2021-12-11 12:24:38 +0100

    Run pip-audit on the Python components. (#3016)

--- components/server/ci/quality.sh
+++ components/server/ci/quality.sh
@@ -11,6 +11,7 @@ set -e
 mypy src
 pylint --rcfile=../../.pylintrc src tests
 python -m flake8 --select=DUO src
+pip-audit --strict --progress-spinner=off
 safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
 bandit --quiet --recursive src/
 vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
