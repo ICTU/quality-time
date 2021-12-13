3b262e247991d3b73522447e6204ebc7c8ef53ef
Frank Niessink <frank@niessink.com>
2021-12-13 22:16:23 +0100

    Use testing library instead of enzyme.

--- components/frontend/src/fields/StringInput.test.js
+++ components/frontend/src/fields/StringInput.test.js
@@ -1,46 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { mount, shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Permissions } from '../context/Permissions';
 import { StringInput } from './StringInput';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<StringInput />, div);
-    ReactDOM.unmountComponentAtNode(div);
+function renderStringInput(set_value) {
+    return render(
+        <Permissions.Provider value={false}>
+            <StringInput
+                options={["Option 1", "Option 2"]}
+                set_value={set_value}
+                value="Option 1"
+            />
+        </Permissions.Provider>
+    )
+}
+
+it('renders the value of the input', () => {
+    renderStringInput();
+    expect(screen.getByDisplayValue(/Option 1/)).not.toBe(null)
 });
 
-it('renders the value of the Input', () => {
-    const wrapper = shallow(<StringInput value="Hello" />);
-    expect(wrapper.find('Input').prop('value')).toBe("Hello");
+it('invokes the callback on change', () => {
+    const mockCallback = jest.fn();
+    renderStringInput(mockCallback);
+    userEvent.type(screen.getByDisplayValue(/Option 1/), '{selectall}Option 2{enter}')
+    expect(screen.getByDisplayValue(/Option 2/)).not.toBe(null)
+    expect(mockCallback).toHaveBeenCalledWith("Option 2")
 });
 
-it('renders the value of the StringInputWithSuggestions', () => {
-    const wrapper = mount(<Permissions.Provider value={false}><StringInput value="Hello" options='x' /></Permissions.Provider>);
-    expect(wrapper.find('StringInputWithSuggestions').prop('value')).toBe("Hello");
-});
-
-it('renders error prop to true when warning is set to true', () => {
-    const wrapper = mount(<Permissions.Provider value={false}><StringInput warning={true} options='x' /></Permissions.Provider>);
-    expect(wrapper.find('FormDropdown').prop('error')).toBe(true);
-});
-
-it('renders error prop to true when the field is required and empty and the warning is undefined', () => {
-    const wrapper = mount(<Permissions.Provider value={false}><StringInput required={true} value='' options='x' /></Permissions.Provider>);
-    expect(wrapper.find('FormDropdown').prop('error')).toBe(true);
-});
-
-it('renders error prop to false when the field is required and not empty and the warning is undefined', () => {
-    const wrapper = mount(<Permissions.Provider value={false}><StringInput required={true} value='x' options='x' /></Permissions.Provider>);
-    expect(wrapper.find('FormDropdown').prop('error')).toBe(false);
-});
-
-it('renders error prop to true when the field is required and empty and the warning is false', () => {
-    const wrapper = mount(<Permissions.Provider value={false}><StringInput required={true} warning={false} value='' options='x' /></Permissions.Provider>);
-    expect(wrapper.find('FormDropdown').prop('error')).toBe(true);
-});
-
-it('renders error prop to false when the field is required and not empty and the warning is false', () => {
-    const wrapper = mount(<Permissions.Provider value={false}><StringInput required={true} warning={false} value='x' options='x' /></Permissions.Provider>);
-    expect(wrapper.find('FormDropdown').prop('error')).toBe(false);
+it('invokes the callback on add', () => {
+    const mockCallback = jest.fn();
+    renderStringInput(mockCallback);
+    userEvent.type(screen.getByDisplayValue(/Option 1/), '{selectall}Option 3{enter}')
+    expect(screen.getByDisplayValue(/Option 3/)).not.toBe(null)
+    expect(mockCallback).toHaveBeenCalledWith("Option 3")
 });
