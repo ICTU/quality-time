5adea6d98abba81e9b55ad647c82da74e4636d97
Frank Niessink <frank@niessink.com>
2021-12-16 22:45:07 +0100

    Remove isort. Add some progress info to the ci/quality.sh scripts.

--- components/server/ci/quality.sh
+++ components/server/ci/quality.sh
@@ -7,20 +7,18 @@ set -e
 # by 'astroid.nodes' in astroid 3.0.0
 # Turn off dev mode until astroid gets fixed or there's a way to suppress warnings in third party code
 #export PYTHONDEVMODE=1
-HEADER='\033[95m'
-ENDSTYLE='\033[0m'
 
-echo "${HEADER}mypy${ENDSTYLE}"
-mypy src
-echo "${HEADER}pylint${ENDSTYLE}"
-pylint --rcfile=../../.pylintrc src tests
-echo "${HEADER}flake8${ENDSTYLE}"
-python -m flake8 --select=DUO src
-echo "${HEADER}pip-audit${ENDSTYLE}"
-pip-audit --strict --progress-spinner=off
-echo "${HEADER}safety${ENDSTYLE}"
-safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
-echo "${HEADER}bandit${ENDSTYLE}"
-bandit --quiet --recursive src/
-echo "${HEADER}vulture${ENDSTYLE}"
-vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
+run () {
+    header='\033[95m'
+    endstyle='\033[0m'
+    echo "${header}$*${endstyle}"
+    eval "$*"
+}
+
+run mypy src
+run pylint --rcfile=../../.pylintrc src tests
+run python -m flake8 --select=DUO src
+run pip-audit --strict --progress-spinner=off
+run safety check --bare --ignore 41002 -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
+run bandit --quiet --recursive src/
+run vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
