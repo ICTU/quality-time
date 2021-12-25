63ab0871d2dd07a2ba25590e1fe3b756e27b4f8d
Frank Niessink <frank@niessink.com>
2021-12-25 17:04:03 +0100

    Increase coverage.

--- components/server/src/internal/database/datamodels.py
+++ components/server/src/internal/database/datamodels.py
@@ -6,6 +6,6 @@ from pymongo.database import Database
 
 def latest_datamodel(database: Database):
     """Return the latest data model."""
-    if data_model := database.datamodels.find_one(sort=[("timestamp", pymongo.DESCENDING)]):
+    if data_model := database.datamodels.find_one(sort=[("timestamp", pymongo.DESCENDING)]):  # pragma: no cover-behave
         data_model["_id"] = str(data_model["_id"])
     return data_model or {}
