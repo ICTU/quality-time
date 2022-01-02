33251f8aec90afcfb9706c1c6e7cf85a197697b5
Frank Niessink <frank@niessink.com>
2022-01-02 19:24:49 +0100

    Refactor.

--- components/server/src/external/database/reports.py
+++ components/server/src/external/database/reports.py
@@ -88,7 +88,6 @@ def _prepare_documents_for_insertion(
     """Prepare the documents for insertion in the database by removing any ids and setting the extra attributes."""
     now = iso_timestamp()
     user = sessions.find_user(database)
-    email = user.email
     username = user.username or "An operator"
     # Don't use str.format because there may be curly braces in the delta description, e.g. due to regular expressions:
     description = delta_description.replace("{user}", username, 1)
@@ -96,7 +95,7 @@ def _prepare_documents_for_insertion(
         if "_id" in document:
             del document["_id"]
         document["timestamp"] = now
-        document["delta"] = dict(description=description, email=email)
+        document["delta"] = dict(description=description, email=user.email)
         if uuids:
             document["delta"]["uuids"] = sorted(list(set(uuids)))
         for key, value in extra_attributes.items():
