e7531d3a60dac38cbc9fa582015d49d62d313be5
Erik <emdverboom@gmail.com>
2022-01-06 15:39:51 +0100

    also make external latest_report function crash safe

--- components/server/src/internal/database/reports.py
+++ components/server/src/internal/database/reports.py
@@ -16,7 +16,7 @@ def latest_reports(database: Database, data_model: dict) -> list[Report]:
     return [Report(data_model, report_dict) for report_dict in report_dicts]
 
 
-def latest_report(database: Database, data_model, report_uuid: str) -> Report:
+def latest_report(database: Database, data_model, report_uuid: str) -> Report | None:
     """Get latest report with this uuid."""
     report_dict = database.reports.find_one({"report_uuid": report_uuid, "last": True, "deleted": DOES_NOT_EXIST})
     if report_dict:
