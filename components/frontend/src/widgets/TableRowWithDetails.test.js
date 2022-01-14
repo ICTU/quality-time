829e8399bd09c1def11614173e14c1d7758c3d64
Frank Niessink <frank@niessink.com>
2022-01-14 22:44:55 +0100

    Increase coverage.

--- components/frontend/src/widgets/TableRowWithDetails.test.js
+++ components/frontend/src/widgets/TableRowWithDetails.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Table } from 'semantic-ui-react'
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import { TableRowWithDetails } from './TableRowWithDetails';
 
-it('renders without crashing', () => {
-    const tbody = document.createElement('tbody');
-    ReactDOM.render(<TableRowWithDetails />, tbody);
-    ReactDOM.unmountComponentAtNode(tbody);
+function renderTableRowWithDetails(expanded, onExpand) {
+    render(
+        <Table>
+            <Table.Body>
+                <TableRowWithDetails expanded={expanded} onExpand={onExpand} details={"Details"}/>
+            </Table.Body>
+        </Table>
+    )
+}
+
+it('shows the details when expanded', () => {
+    renderTableRowWithDetails(true)
+    expect(screen.queryAllByText(/Details/).length).toBe(1)
+});
+
+it('does not show the details when collapsed', () => {
+    renderTableRowWithDetails(false)
+    expect(screen.queryAllByText(/Details/).length).toBe(0)
+});
+
+it('calls the expand callback when clicked', () => {
+    const onExpand = jest.fn()
+    renderTableRowWithDetails(false, onExpand)
+    fireEvent.click(screen.getByRole("button"))
+    expect(onExpand).toHaveBeenCalledWith(true)
+});
+
+it('calls the expand callback on keypress', () => {
+    const onExpand = jest.fn()
+    renderTableRowWithDetails(false, onExpand)
+    userEvent.type(screen.getByRole("button"), " ")
+    expect(onExpand).toHaveBeenCalledWith(true)
 });
