8d173ada778e4dc4636e7482de72903334a79fe9
Frank Niessink <frank@niessink.com>
2022-01-16 15:37:43 +0100

    Increase coverage.

--- components/frontend/src/measurement/TrendSparkline.js
+++ components/frontend/src/measurement/TrendSparkline.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { VictoryGroup, VictoryLine, VictoryTheme } from 'victory';
+import { pluralize } from '../utils';
 
 export function TrendSparkline({ measurements, scale, report_date }) {
     if (scale === "version_number") { return null }
     let points = [];
+    let yvalues = new Set();
     for (let measurement of measurements) {
         const value = (measurement[scale] && measurement[scale].value) || null;
         const y = value !== null ? Number(value) : null;
+        if (y !== null) { yvalues.add(y) }
         const x1 = new Date(measurement.start);
         const x2 = new Date(measurement.end);
         points.push({ y: y, x: x1 });
@@ -15,15 +18,18 @@ export function TrendSparkline({ measurements, scale, report_date }) {
     const now = report_date ? new Date(report_date) : new Date();
     let week_ago = report_date ? new Date(report_date) : new Date();
     week_ago.setDate(week_ago.getDate() - 7);
+    const ariaLabel = `sparkline graph showing ${yvalues.size} different measurement ${pluralize("value", yvalues.size)} in the week before ${report_date ? now.toLocaleDateString() : "today"}`
     // The width property below is not used according to https://formidable.com/open-source/victory/docs/common-props#width,
     // but setting it prevents these messages in the console: "Warning: `Infinity` is an invalid value for the `width` css style property.""
     return (
-        <VictoryGroup theme={VictoryTheme.material} scale={{ x: "time", y: "linear" }} domain={{ x: [week_ago, now] }} height={30} padding={0}>
-            <VictoryLine data={points} interpolation="stepBefore" style={{
-                data: {
-                    stroke: "black", strokeWidth: 3, width: "100%"
-                }
-            }} />
-        </VictoryGroup>
+        <div aria-label={ariaLabel}>
+            <VictoryGroup theme={VictoryTheme.material} scale={{ x: "time", y: "linear" }} domain={{ x: [week_ago, now] }} height={30} padding={0}>
+                <VictoryLine data={points} interpolation="stepBefore" style={{
+                    data: {
+                        stroke: "black", strokeWidth: 3, width: "100%"
+                    }
+                }} />
+            </VictoryGroup>
+        </div>
     )
 }
--- components/frontend/src/measurement/TrendSparkline.test.js
+++ components/frontend/src/measurement/TrendSparkline.test.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import { TrendSparkline } from './TrendSparkline';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TrendSparkline measurements={[]} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it('returns null when the metric scale is version number', () => {
+    render(<TrendSparkline scale="version_number"/>)
+    expect(screen.queryAllByLabelText(/sparkline graph/).length).toBe(0)
+})
+
+it('renders an empty sparkline if there are no measurements', () => {
+    render(<TrendSparkline measurements={[]} />)
+    expect(screen.queryAllByLabelText(/sparkline graph showing 0 different measurement values/).length).toBe(1)
 });
 
-it('renders measurements without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TrendSparkline measurements={[{ count: { value: "1" }, start: "2019-09-29", end: "2019-09-30" }]} scale="count" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it('renders a recent measurement', () => {
+    render(<TrendSparkline measurements={[{ count: { value: "1" }, start: "2019-09-29", end: "2019-09-30" }]} scale="count" />);
+    expect(screen.queryAllByLabelText(/sparkline graph showing 1 different measurement value in the week before today/).length).toBe(1)
 });
 
-it('renders old measurements without crashing', () => {
-    const div = document.createElement('div');
-    const now = new Date("2020-01-01");
-    ReactDOM.render(<TrendSparkline measurements={[{ count: { value: "1" }, start: "2019-09-29", end: "2019-09-30" }]} report_date={now} scale="count" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it('renders multiple recent measurements', () => {
+    render(
+        <TrendSparkline
+            measurements={[
+                { count: { value: "1" }, start: "2019-09-28", end: "2019-09-29" },
+                { count: { value: "2" }, start: "2019-09-29", end: "2019-09-30" }
+            ]}
+            scale="count"
+        />
+    );
+    expect(screen.queryAllByLabelText(/sparkline graph showing 2 different measurement values in the week before today/).length).toBe(1)
+});
+
+it('renders old measurements', () => {
+    const date = new Date("2020-01-01");
+    render(<TrendSparkline measurements={[{ count: { value: "1" }, start: "2019-09-29", end: "2019-09-30" }]} report_date={date} scale="count" />);
+    expect(screen.queryAllByLabelText(`sparkline graph showing 1 different measurement value in the week before ${date.toLocaleDateString()}`).length).toBe(1)
 });
