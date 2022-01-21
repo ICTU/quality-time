bae0a2631473010ce64c716df5e0288b190a1321
Erik <emdverboom@gmail.com>
2022-01-21 10:22:39 +0100

    fix backend unittests

--- components/server/ci/unittest.sh
+++ components/server/ci/unittest.sh
@@ -2,7 +2,7 @@
 
 export PYTHONDEVMODE=1
 # For Windows compatibility; prevent path from ending with a ':'
-export PYTHONPATH=`python -c 'import sys;print(":".join(sys.argv[1:]))' src $PYTHONPATH`
+export PYTHONPATH=`python -c 'import sys;print(":".join(sys.argv[1:]))' src ../shared_python $PYTHONPATH`
 export COVERAGE_RCFILE=../../.coveragerc
 
 coverage run -m unittest --quiet
