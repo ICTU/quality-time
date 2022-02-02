cc772d3afc1e34b9a8b823a92e3a23518f722d0c
Frank Niessink <frank@niessink.com>
2022-02-02 21:42:49 +0100

    Better calendar date help text.

--- components/server/src/external/data_model/sources/calendar.py
+++ components/server/src/external/data_model/sources/calendar.py
@@ -6,9 +6,9 @@ from ..parameters import DateParameter
 
 CALENDAR = Source(
     name="Calendar date",
-    description="Warn when the date is too long ago. Can be used to, for example, warn when it is time for the next "
+    description="Return a manually set date. Can be used to, for example, warn when it is time for the next "
     "security test.",
     parameters=dict(
-        date=DateParameter(name="Date", mandatory=True, default_value="2021-01-01", metrics=["source_up_to_dateness"]),
+        date=DateParameter(name="Date", mandatory=True, default_value="2022-01-01", metrics=["source_up_to_dateness"]),
     ),
 )
