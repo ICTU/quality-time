60d2c2d09834d8d2ab6e53a03b590e07c35968af
Frank Niessink <frank@niessink.com>
2022-02-05 19:01:25 +0100

    Suppress DeepSource issue.

--- components/collector/tests/source_collectors/calendar/base.py
+++ components/collector/tests/source_collectors/calendar/base.py
@@ -3,7 +3,7 @@
 from ..source_collector_test_case import SourceCollectorTestCase
 
 
-class CalendarTestCase(SourceCollectorTestCase):
+class CalendarTestCase(SourceCollectorTestCase):  # skipcq: PTC-W0046
     """Base class for calendar collectors."""
 
     SOURCE_TYPE = "calendar"
