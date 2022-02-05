f48ccabe3a6da99f395b6228d512edea5190f98a
Frank Niessink <frank@niessink.com>
2022-02-05 15:47:45 +0100

    Add 'time remaining' metric.

--- components/server/src/external/data_model/sources/calendar.py
+++ components/server/src/external/data_model/sources/calendar.py
@@ -6,9 +6,11 @@ from ..parameters import DateParameter
 
 CALENDAR = Source(
     name="Calendar date",
-    description="Warn when the date is too long ago. Can be used to, for example, warn when it is time for the next "
-    "security test.",
+    description="Specify a specific date in the past or the future. Can be used to, for example, warn when it is time "
+    "for the next security test.",
     parameters=dict(
-        date=DateParameter(name="Date", mandatory=True, default_value="2021-01-01", metrics=["source_up_to_dateness"]),
+        date=DateParameter(
+            name="Date", mandatory=True, default_value="2021-01-01", metrics=["source_up_to_dateness", "time_remaining"]
+        ),
     ),
 )
