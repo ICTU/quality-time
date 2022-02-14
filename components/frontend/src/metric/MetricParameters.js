572d4333d5d8704d6353e45a5335b911191ed7b6
Frank Niessink <frank@niessink.com>
2022-02-15 00:20:34 +0100

    Refactor parameters.

--- components/frontend/src/metric/MetricParameters.js
+++ components/frontend/src/metric/MetricParameters.js
@@ -30,17 +30,86 @@ function metric_scale_options(metric_scales, datamodel) {
     return scale_options;
 }
 
+function MetricName({ metric, metricType, metric_uuid, reload }) {
+    return (
+        <StringInput
+            requiredPermissions={[EDIT_REPORT_PERMISSION]}
+            id="metric-name"
+            label="Metric name"
+            placeholder={metricType.name}
+            set_value={(value) => set_metric_attribute(metric_uuid, "name", value, reload)}
+            value={metric.name ?? ""}
+        />
+    )
+}
+
+function Tags({ metric, metric_uuid, reload, tags }) {
+    return (
+        <MultipleChoiceInput
+            requiredPermissions={[EDIT_REPORT_PERMISSION]}
+            allowAdditions
+            label="Tags"
+            options={[...tags]}
+            set_value={(value) => set_metric_attribute(metric_uuid, "tags", value, reload)}
+            value={get_metric_tags(metric)}
+        />
+    )
+}
+
+function Scale({ metricType, metric_scale, metric_uuid, reload }) {
+    const dataModel = useContext(DataModel)
+    const scale_options = metric_scale_options(metricType.scales || ["count"], dataModel);
+    return (
+        <SingleChoiceInput
+            requiredPermissions={[EDIT_REPORT_PERMISSION]}
+            label="Metric scale"
+            options={scale_options}
+            placeholder={metricType.default_scale || "Count"}
+            set_value={(value) => set_metric_attribute(metric_uuid, "scale", value, reload)}
+            value={metric_scale}
+        />
+    )
+}
+
+function Direction({ metric, metric_scale, metric_uuid, metricType, reload }) {
+    const dataModel = useContext(DataModel)
+    const metricUnitWithoutPercentage = metric.unit || metricType.unit;
+    const metricUnit = `${metric_scale === "percentage" ? "% " : ""}${metricUnitWithoutPercentage}`;
+    const fewer = { count: `Fewer ${metricUnit}`, percentage: `A lower percentage of ${metricUnitWithoutPercentage}`, version_number: "A lower version number" }[metric_scale];
+    const more = { count: `More ${metricUnit}`, percentage: `A higher percentage of ${metricUnitWithoutPercentage}`, version_number: "A higher version number" }[metric_scale];
+    const metricDirection = getMetricDirection(metric, dataModel) ?? "<"
+    return (
+        <SingleChoiceInput
+            requiredPermissions={[EDIT_REPORT_PERMISSION]}
+            label="Metric direction"
+            options={[
+                { key: "0", text: `${fewer} is better`, value: "<" },
+                { key: "1", text: `${more} is better`, value: ">" }]}
+            set_value={(value) => set_metric_attribute(metric_uuid, "direction", value, reload)}
+            value={metricDirection}
+        />
+    )
+}
+
+function Unit({ metric, metric_scale, metric_uuid, metricType, reload }) {
+    return (
+        <StringInput
+            id="metric-unit"
+            label="Metric unit"
+            placeholder={metricType.unit}
+            prefix={metric_scale === "percentage" ? "%" : ""}
+            requiredPermissions={[EDIT_REPORT_PERMISSION]}
+            set_value={(value) => set_metric_attribute(metric_uuid, "unit", value, reload)}
+            value={metric.unit ?? ""}
+        />
+    )
+}
+
 export function MetricParameters({ report, metric, metric_uuid, reload }) {
     const dataModel = useContext(DataModel)
     const metricType = dataModel.metrics[metric.type];
     const metric_scale = getMetricScale(metric, dataModel);
-    const metric_unit_without_percentage = metric.unit || metricType.unit;
-    const metric_unit = `${metric_scale === "percentage" ? "% " : ""}${metric_unit_without_percentage}`;
-    const fewer = { count: `Fewer ${metric_unit}`, percentage: `A lower percentage of ${metric_unit_without_percentage}`, version_number: "A lower version number" }[metric_scale];
-    const more = { count: `More ${metric_unit}`, percentage: `A higher percentage of ${metric_unit_without_percentage}`, version_number: "A higher version number" }[metric_scale];
-    const metric_direction = getMetricDirection(metric, dataModel)
     const tags = Object.keys(report.summary_by_tag || {});
-    const scale_options = metric_scale_options(metricType.scales || ["count"], dataModel);
     const issue_tracker_instruction = "Please configure an issue tracker by expanding the report title and selecting the 'Issue tracker' tab."
     const issue_status_help = "Identifiers of issues in the configured issue tracker that track the progress of fixing this metric." + (report.issue_tracker ? "" : ` ${issue_tracker_instruction}`);
     const metric_issue_ids = get_metric_issue_ids(metric);
@@ -48,66 +117,25 @@ export function MetricParameters({ report, metric, metric_uuid, reload }) {
         <Grid stackable columns={3}>
             <Grid.Row>
                 <Grid.Column>
-                    <MetricType
-                        metricType={metric.type}
-                        metric_uuid={metric_uuid}
-                        reload={reload}
-                    />
+                    <MetricType metricType={metric.type} metric_uuid={metric_uuid} reload={reload} />
                 </Grid.Column>
                 <Grid.Column>
-                    <StringInput
-                        requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                        id="metric-name"
-                        label="Metric name"
-                        placeholder={metricType.name}
-                        set_value={(value) => set_metric_attribute(metric_uuid, "name", value, reload)}
-                        value={metric.name ?? ""}
-                    />
+                    <MetricName metric={metric} metricType={metricType} metric_uuid={metric_uuid} reload={reload} />
                 </Grid.Column>
                 <Grid.Column>
-                    <MultipleChoiceInput
-                        requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                        allowAdditions
-                        label="Tags"
-                        options={[...tags]}
-                        set_value={(value) => set_metric_attribute(metric_uuid, "tags", value, reload)}
-                        value={get_metric_tags(metric)}
-                    />
+                    <Tags metric={metric} metric_uuid={metric_uuid} reload={reload} tags={tags} />
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column>
-                    <SingleChoiceInput
-                        requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                        label="Metric scale"
-                        options={scale_options}
-                        placeholder={metricType.default_scale || "Count"}
-                        set_value={(value) => set_metric_attribute(metric_uuid, "scale", value, reload)}
-                        value={metric_scale}
-                    />
+                    <Scale metricType={metricType} metric_scale={metric_scale} metric_uuid={metric_uuid} reload={reload} />
                 </Grid.Column>
                 <Grid.Column>
-                    <SingleChoiceInput
-                        requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                        label="Metric direction"
-                        options={[
-                            { key: "0", text: `${fewer} is better`, value: "<" },
-                            { key: "1", text: `${more} is better`, value: ">" }]}
-                        set_value={(value) => set_metric_attribute(metric_uuid, "direction", value, reload)}
-                        value={metric_direction ?? "<"}
-                    />
+                    <Direction metric={metric} metric_scale={metric_scale} metric_uuid={metric_uuid} metricType={metricType} reload={reload} />
                 </Grid.Column>
                 {metric_scale !== "version_number" &&
                     <Grid.Column>
-                        <StringInput
-                            id="metric-unit"
-                            label="Metric unit"
-                            placeholder={metricType.unit}
-                            prefix={metric_scale === "percentage" ? "%" : ""}
-                            requiredPermissions={[EDIT_REPORT_PERMISSION]}
-                            set_value={(value) => set_metric_attribute(metric_uuid, "unit", value, reload)}
-                            value={metric.unit ?? ""}
-                        />
+                        <Unit metric={metric} metric_scale={metric_scale} metric_uuid={metric_uuid} metricType={metricType} reload={reload} />
                     </Grid.Column>}
             </Grid.Row>
             <Grid.Row>
