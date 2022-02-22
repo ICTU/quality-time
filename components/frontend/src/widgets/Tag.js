ed2ed0d6455a9cddcd23dcec82335e106d0599fb
Frank Niessink <frank@niessink.com>
2022-02-22 15:48:29 +0100

    Fix bug.

--- components/frontend/src/widgets/Tag.js
+++ components/frontend/src/widgets/Tag.js
@@ -3,7 +3,7 @@ import { Label } from 'semantic-ui-react';
 import "./Tag.css";
 
 export function Tag({ selected, tag }) {
-    const color = selected ? "blue" : "gainsboro"
+    const color = selected ? "blue" : null
     return (
         <Label color={color} tag>{tag}</Label>
     )
