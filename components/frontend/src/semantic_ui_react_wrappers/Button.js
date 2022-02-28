52392ce604a2b389ceeaf60efc58c11c213a8d5a
Frank Niessink <frank@niessink.com>
2022-02-28 11:53:17 +0100

    Color negative button red on hover in dark mode.

--- components/frontend/src/semantic_ui_react_wrappers/Button.js
+++ components/frontend/src/semantic_ui_react_wrappers/Button.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Button as SemanticUIButton } from 'semantic-ui-react';
 import { DarkMode } from '../context/DarkMode';
+import "./Button.css";
 
 export function Button(props) {
     return (
