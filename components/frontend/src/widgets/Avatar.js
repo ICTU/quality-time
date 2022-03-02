d9f80478c3ecb8f606b9c4def9cdc2aaa0258755
Frank Niessink <frank@niessink.com>
2022-03-02 12:55:37 +0100

    Fox SonarQube violation.

--- components/frontend/src/widgets/Avatar.js
+++ components/frontend/src/widgets/Avatar.js
@@ -4,8 +4,8 @@ import md5 from 'md5';
 import { DarkMode } from '../context/DarkMode';
 
 export function Avatar({ email }) {
-    const darkMode = useContext(DarkMode)
+    const color = useContext(DarkMode) ? "grey" : null;
     return (
-        email ? <Image avatar src={`https://www.gravatar.com/avatar/${md5(email)}?d=identicon`} alt="Avatar" /> : <Icon color={darkMode ? "grey": null} name="user" />
+        email ? <Image avatar src={`https://www.gravatar.com/avatar/${md5(email)}?d=identicon`} alt="Avatar" /> : <Icon color={color} name="user" />
     )
 }
