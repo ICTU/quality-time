9766e1d9824eed0883631dbb4c655a752b9ec8b4
Frank Niessink <frank@niessink.com>
2022-03-03 22:45:08 +0100

    Several improvements.

--- components/frontend/src/semantic_ui_react_wrappers/Popup.js
+++ components/frontend/src/semantic_ui_react_wrappers/Popup.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Popup as SemanticUIPopup } from 'semantic-ui-react';
 import { DarkMode } from '../context/DarkMode';
+import './Popup.css';
 
 export function Popup(props) {
     return (
