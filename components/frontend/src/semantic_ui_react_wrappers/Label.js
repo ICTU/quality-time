e46927f97a4a07d7fc2385cc0d40028c85eef465
Frank Niessink <frank@niessink.com>
2022-03-15 17:08:45 +0100

    Make contrast of red labels better in dark mode.

--- components/frontend/src/semantic_ui_react_wrappers/Label.js
+++ components/frontend/src/semantic_ui_react_wrappers/Label.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Label as SemanticUILabel } from 'semantic-ui-react';
 import { DarkMode } from '../context/DarkMode';
+import './Label.css';
 
 export function Label(props) {
     return (
