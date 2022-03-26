cac1214c94132ccadbe3b513f0bae9148ca67f0e
Frank Niessink <frank@niessink.com>
2022-03-26 17:22:03 +0100

    Pull up the table cells.

--- components/frontend/src/subject/SubjectTable.js
+++ components/frontend/src/subject/SubjectTable.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Table } from '../semantic_ui_react_wrappers';
+import { DataModel } from "../context/DataModel";
+import { DarkMode } from "../context/DarkMode";
+import { IssueStatus } from '../measurement/IssueStatus';
+import { MeasurementSources } from '../measurement/MeasurementSources';
+import { MeasurementTarget } from '../measurement/MeasurementTarget';
+import { MeasurementValue } from '../measurement/MeasurementValue';
+import { TrendSparkline } from '../measurement/TrendSparkline';
+import { StatusIcon } from '../measurement/StatusIcon';
+import { Tag } from '../widgets/Tag';
+import { formatMetricScale, format_minutes, get_metric_name, get_metric_tags, getMetricUnit } from '../utils';
 import { SubjectTableFooter } from './SubjectTableFooter';
 import { SubjectTableRow } from './SubjectTableRow';
 import { SubjectTableHeader } from './SubjectTableHeader';
@@ -44,8 +54,12 @@ export function SubjectTable({
     const dates = getColumnDates(reportDate, dateInterval, dateOrder, nrDates)
     const last_index = Object.entries(subject.metrics).length - 1;
     const className = "stickyHeader" + (subject.subtitle ? " subjectHasSubTitle" : "")
+    const dataModel = useContext(DataModel)
+    const darkMode = useContext(DarkMode)
+    // Sort measurements in reverse order so that if there multiple measurements on a day, we find the most recent one:
+    measurements.sort((m1, m2) => m1.start < m2.start ? 1 : -1)
     return (
-        <Table sortable className={className} style={{marginTop: "0px"}}>
+        <Table sortable className={className} style={{ marginTop: "0px" }}>
             <SubjectTableHeader
                 columnDates={dates}
                 handleSort={handleSort}
@@ -56,6 +70,19 @@ export function SubjectTable({
             />
             <Table.Body>
                 {metricEntries.map(([metric_uuid, metric], index) => {
+                    const metricType = dataModel.metrics[metric.type];
+                    const measurementCells = []
+                    dates.forEach((date) => {
+                        const iso_date_string = date.toISOString().split("T")[0];
+                        const measurement = measurements?.find((m) => { return m.metric_uuid === metric_uuid && m.start.split("T")[0] <= iso_date_string && iso_date_string <= m.end.split("T")[0] })
+                        let metric_value = measurement?.[metric.scale]?.value ?? "?";
+                        metric_value = metric_value !== "?" && metricType.unit === "minutes" && metric.scale !== "percentage" ? format_minutes(metric_value) : metric_value;
+                        const status = measurement?.[metric.scale]?.status ?? "unknown";
+                        measurementCells.push(<Table.Cell className={status} key={date} textAlign="right">{metric_value}{formatMetricScale(metric)}</Table.Cell>)
+                    })
+                    const metricName = get_metric_name(metric, dataModel);
+                    const unit = getMetricUnit(metric, dataModel)
+                    const style = nrDates > 1 ? { background: darkMode ? "rgba(60, 60, 60, 1)" : "#f9fafb" } : {}
                     return (
                         <SubjectTableRow key={metric_uuid}
                             changed_fields={changed_fields}
@@ -63,22 +90,36 @@ export function SubjectTable({
                             last_metric={index === last_index}
                             metric_uuid={metric_uuid}
                             metric={metric}
-                            dates={dates}
                             reportDate={reportDate}
                             report={report}
                             reports={reports}
                             subject_uuid={subject_uuid}
-                            measurements={measurements.filter((measurement) => measurement.metric_uuid === metric_uuid)}
-                            hiddenColumns={hiddenColumns}
                             visibleDetailsTabs={visibleDetailsTabs}
                             toggleVisibleDetailsTab={toggleVisibleDetailsTab}
                             nrDates={nrDates}
                             reload={reload}
-                            showIssueCreationDate={showIssueCreationDate}
-                            showIssueSummary={showIssueSummary}
-                            showIssueUpdateDate={showIssueUpdateDate}
                             stopSorting={() => handleSort(null)}
-                        />
+                        >
+                            <Table.Cell style={style}>{metricName}</Table.Cell>
+                            {nrDates > 1 && measurementCells}
+                            {nrDates === 1 && !hiddenColumns.includes("trend") && <Table.Cell><TrendSparkline measurements={metric.recent_measurements} report_date={reportDate} scale={metric.scale} /></Table.Cell>}
+                            {nrDates === 1 && !hiddenColumns.includes("status") && <Table.Cell textAlign='center'><StatusIcon status={metric.status} status_start={metric.status_start} /></Table.Cell>}
+                            {nrDates === 1 && !hiddenColumns.includes("measurement") && <Table.Cell textAlign="right"><MeasurementValue metric={metric} /></Table.Cell>}
+                            {nrDates === 1 && !hiddenColumns.includes("target") && <Table.Cell textAlign="right"><MeasurementTarget metric={metric} /></Table.Cell>}
+                            {!hiddenColumns.includes("unit") && <Table.Cell style={style}>{unit}</Table.Cell>}
+                            {!hiddenColumns.includes("source") && <Table.Cell style={style}><MeasurementSources metric={metric} /></Table.Cell>}
+                            {!hiddenColumns.includes("comment") && <Table.Cell style={style}><div dangerouslySetInnerHTML={{ __html: metric.comment }} /></Table.Cell>}
+                            {!hiddenColumns.includes("issues") && <Table.Cell style={style}>
+                                <IssueStatus
+                                    metric={metric}
+                                    issueTrackerMissing={!report.issue_tracker && !report.report_uuid.startsWith("tag-")}
+                                    showIssueCreationDate={showIssueCreationDate}
+                                    showIssueSummary={showIssueSummary}
+                                    showIssueUpdateDate={showIssueUpdateDate}
+                                />
+                            </Table.Cell>}
+                            {!hiddenColumns.includes("tags") && <Table.Cell style={style}>{get_metric_tags(metric).map((tag) => <Tag key={tag} tag={tag} />)}</Table.Cell>}
+                        </SubjectTableRow>
                     )
                 })
                 }
--- components/frontend/src/subject/SubjectTableRow.test.js
+++ components/frontend/src/subject/SubjectTableRow.test.js
@@ -5,34 +5,6 @@ import { DataModel } from '../context/DataModel';
 
 describe("MeasurementRow", () => {
 
-    const measurements = [
-        {
-            metric_uuid: "1",
-            start: "2020-01-06T00:00:00+00:00",
-            end: "2020-01-09T00:00:00+00:00",
-            count: {
-                value: "1",
-                status: "near_target_met",
-                target: "1",
-            }
-        },
-        {
-            metric_uuid: "0",
-            start: "2020-01-04T00:00:00+00:00",
-            end: "2020-01-06T00:00:00+00:00",
-            count: {
-                value: "0",
-                status: "near_target_met",
-                target: "0",
-            }
-        },
-    ]
-    const dates = [
-        new Date("2020-01-09T00:00:00+00:00"),
-        new Date("2020-01-05T00:00:00+00:00"),
-        new Date("2020-01-03T00:00:00+00:00"),
-    ]
-
     const dataModel = { metrics: { metricType: { name: "testName", unit: "" } } }
     const visibleDetailsTabs = [];
     const toggleVisibleDetailsTab = jest.fn();
@@ -47,11 +19,8 @@ describe("MeasurementRow", () => {
                             metric={metric}
                             report={{report_uuid: "report_uuid", subjects: {subject_uuid: {metrics: {metric_uuid: {type: "metricType"}}}}}}
                             subject_uuid="subject_uuid"
-                            measurements={measurements}
-                            dates={dates}
                             reportDate={null}
                             nrDates={1}
-                            hiddenColumns={[]}
                             toggleVisibleDetailsTab={(tab) => toggleVisibleDetailsTab(tab)}
                             visibleDetailsTabs={visibleDetailsTabs}
                         />
@@ -60,20 +29,6 @@ describe("MeasurementRow", () => {
             </table>
         );
     }
-
-    it('renders one single row with metric name, measurement values and unit', () => {
-        const { queryAllByText } = render_measurements_row({ type: "metricType", unit: "testUnit", scale: "count", recent_measurements: [] })
-        expect(queryAllByText("testName").length).toBe(1) // measurement name cell
-        expect(queryAllByText("?").length).toBe(1) // measurement value
-        expect(queryAllByText("testUnit").length).toBe(1) // measurement unit
-    });
-
-    it('renders one single row with metric name, measurement values and minutes unit', () => {
-        dataModel.metrics.metricType.unit = "minutes"
-        render_measurements_row({ type: "metricType", unit: "", scale: "count", recent_measurements: [] })
-        expect(screen.queryAllByText(/0:00/).length).toBe(1)
-        expect(screen.queryAllByText(/hours/).length).toBe(1)
-    });
 
     it('expands and collapses the metric via the props', () => {
         render_measurements_row({ type: "metricType", unit: "testUnit", scale: "count", recent_measurements: [] })
