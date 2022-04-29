580820cf099dedff35851447ab07327e0626fb0d
Frank Niessink <frank@niessink.com>
2022-04-29 23:18:36 +0200

    Add rationale urls to the popup.

--- components/frontend/src/metric/MetricTypeHeader.js
+++ components/frontend/src/metric/MetricTypeHeader.js
@@ -3,14 +3,33 @@ import { Icon } from 'semantic-ui-react';
 import { Header, Popup } from '../semantic_ui_react_wrappers';
 
 export function MetricTypeHeader({ metricType }) {
-    const rationale = metricType.rationale ? <Popup
-        on={['hover', 'focus']}
-        wide="very"
-        trigger={
-            <span>&nbsp;<Icon role="tooltip" aria-label="help" tabIndex="0" name="info circle" /></span>
+    let rationale = null;
+    if (metricType.rationale) {
+        let rationaleUrls = null;
+        if (metricType.rationale_urls) {
+            rationaleUrls = <>
+                <Header>See also</Header>
+                <ul>
+                    {metricType.rationale_urls.map((url) => <li><a href={url}>{url}</a></li>)}
+                </ul>
+            </>
         }
-        content={<><Header>Why measure {metricType.name.toLowerCase()}?</Header>{metricType.rationale}</>}
-    /> : null;
+        rationale = <Popup
+            content={
+                <>
+                    <Header>Why measure {metricType.name.toLowerCase()}?</Header>
+                    {metricType.rationale}
+                    {rationaleUrls}
+                </>
+            }
+            hoverable
+            on={['hover', 'focus']}
+            trigger={
+                <span>&nbsp;<Icon role="tooltip" aria-label="help" tabIndex="0" name="info circle" /></span>
+            }
+            wide="very"
+        />
+    }
     return (
         <Header>
             <Header.Content>
