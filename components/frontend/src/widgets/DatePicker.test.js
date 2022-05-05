43843ca7bf5e0008b1a10bfcab34e589982b6ed5
Frank Niessink <frank@niessink.com>
2022-05-05 23:44:47 +0200

    Replace enzyme with testing library.

--- components/frontend/src/widgets/DatePicker.test.js
+++ components/frontend/src/widgets/DatePicker.test.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { DatePicker } from './DatePicker';
 
-let mockCallback;
-let wrapper;
+it('calls the callback on date pick', async () => {
+    const mockCallback = jest.fn();
+    render(<DatePicker onDate={mockCallback} name="report_date_string" value="20-07-2020" label="Report date" />)
+    await userEvent.type(screen.getByLabelText("Report date"), "21-07-2020", {initialSelectionStart: 0, initialSelectionEnd: 10})
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+})
 
-describe('<DatePicker />', () => {
-    beforeEach(() => {
-        mockCallback = jest.fn();
-        wrapper = mount(<DatePicker onDate={mockCallback} name="report_date_string" value={"20-07-2020"} label="Report date" />);
-    });
+it('does not call the callback on an invalid date', async () => {
+    const mockCallback = jest.fn();
+    render(<DatePicker onDate={mockCallback} name="report_date_string" value="20-07-2020" label="Report date" />)
+    await userEvent.type(screen.getByLabelText("Report date"), "21-21-2020", {initialSelectionStart: 0, initialSelectionEnd: 10})
+    expect(mockCallback).not.toHaveBeenCalled()
+})
 
-    it('calls the callback on date pick', () => {
-        wrapper.find("input").simulate("change", { target: { name: "report_date_string", value: "21-07-2020" } });
-        wrapper.find("input").simulate("keydown", { key: "Enter" });
-        expect(mockCallback).toHaveBeenCalled();
-    });
-    it('does not call the callback on an invalid date', () => {
-        wrapper.find("input").simulate("change", { target: { name: "report_date_string", value: "21-21-2020" } });
-        wrapper.find("input").simulate("keydown", { key: "Enter" });
-        expect(mockCallback).not.toHaveBeenCalled();
-    });
-    it('does not call the callback when the value is not a date', () => {
-        wrapper.find("input").simulate("change", { target: { name: "report_date_string", value: "not a date" } });
-        wrapper.find("input").simulate("keydown", { key: "Enter" });
-        expect(mockCallback).not.toHaveBeenCalled();
-    });
-    it('calls the callback on clear', () => {
-        wrapper.find("Icon").simulate("click")
-        expect(mockCallback).toHaveBeenCalled();
-    });
-});
+it('does not call the callback when the value is not a date', async () => {
+    const mockCallback = jest.fn();
+    render(<DatePicker onDate={mockCallback} name="report_date_string" value="20-07-2020" label="Report date" />)
+    await userEvent.type(screen.getByLabelText("Report date"), "not a date", {initialSelectionStart: 0, initialSelectionEnd: 10})
+    expect(mockCallback).not.toHaveBeenCalled()
+})
+
+it('calls the callback on clear', async () => {
+    const mockCallback = jest.fn();
+    const { container } = render(<DatePicker onDate={mockCallback} name="report_date_string" value="20-07-2020" label="Report date" />)
+    fireEvent.click(container.querySelector('.remove'))
+    expect(mockCallback).toHaveBeenCalledTimes(1)
+})
