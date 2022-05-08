9f760d45e38e735b80d910d7bcaa979ff3cf9784
Frank Niessink <frank@niessink.com>
2022-05-08 16:29:40 +0200

    Replace enzyme with testing library.

--- components/frontend/src/context/Permissions.test.js
+++ components/frontend/src/context/Permissions.test.js
@@ -1,43 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { Permissions, ReadOnlyOrEditable } from './Permissions';
 
-function MockComponent() { return ("Hi") }
+function MockComponent1() { return ("One") }
 function MockComponent2() { return ("Two") }
 
-describe("<Permissions />", () => {
-  it('provides the permissions context to components', () => {
-    const wrapper = shallow(
-      <Permissions.Provider value={['mockPermission']}>
-        <Permissions.Consumer>{(permissions) => (<MockComponent permissions={permissions} />)}</Permissions.Consumer>
-      </Permissions.Provider>);
-    expect(wrapper.dive().find('MockComponent').prop("permissions")).toStrictEqual(['mockPermission']);
-  });
+it('shows the read only component if no permissions are present', () => {
+    render(
+        <Permissions.Provider value={[]}>
+            <ReadOnlyOrEditable
+                requiredPermissions={['mockPermission']}
+                readOnlyComponent={<MockComponent1 />}
+                editableComponent={<MockComponent2 />}
+            />
+        </Permissions.Provider>)
+    expect(screen.queryAllByText("One").length).toBe(1)
+    expect(screen.queryAllByText("Two").length).toBe(0)
 });
 
-describe("<ReadOnlyOrEditable />", () => {
-  it('shows the read only component if no permissions are present', () => {
-    const wrapper = shallow(
-      <Permissions.Provider value={[]}>
-        <ReadOnlyOrEditable requiredPermissions={['mockPermission']} readOnlyComponent={<MockComponent/>} editableComponent={<MockComponent2/>} />
-      </Permissions.Provider>)
-    expect(wrapper.dive().dive().name()).toBe("MockComponent");
-  });
-  it('shows the read only component if not all permissions are present', () => {
-    const wrapper = shallow(
-      <Permissions.Provider value={['mockPermission']}>
-        <ReadOnlyOrEditable 
-          requiredPermissions={['mockPermission', 'mockPermission1']}
-          readOnlyComponent={<MockComponent/>}
-          editableComponent={<MockComponent2/>} />
-      </Permissions.Provider>)
-    expect(wrapper.dive().dive().name()).toBe("MockComponent");
-  });
-  it('shows the editable only component', () => {
-    const wrapper = shallow(
-      <Permissions.Provider value={['mockPermission']}>
-        <ReadOnlyOrEditable requiredPermissions={['mockPermission']} readOnlyComponent={<MockComponent/>} editableComponent={<MockComponent2/>} />
-      </Permissions.Provider>);
-    expect(wrapper.dive().dive().name()).toBe("MockComponent2");
-  });
+it('shows the read only component if not all permissions are present', () => {
+    render(
+        <Permissions.Provider value={['mockPermission']}>
+            <ReadOnlyOrEditable
+                requiredPermissions={['mockPermission', 'mockPermission1']}
+                readOnlyComponent={<MockComponent1 />}
+                editableComponent={<MockComponent2 />} />
+        </Permissions.Provider>)
+    expect(screen.queryAllByText("One").length).toBe(1)
+    expect(screen.queryAllByText("Two").length).toBe(0)
+});
+
+it('shows the editable only component', () => {
+    render(
+        <Permissions.Provider value={['mockPermission']}>
+            <ReadOnlyOrEditable
+                requiredPermissions={['mockPermission']}
+                readOnlyComponent={<MockComponent1 />}
+                editableComponent={<MockComponent2 />}
+            />
+        </Permissions.Provider>);
+    expect(screen.queryAllByText("One").length).toBe(0)
+    expect(screen.queryAllByText("Two").length).toBe(1)
 });
