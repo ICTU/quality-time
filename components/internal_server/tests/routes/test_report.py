0a63c0d8a1b3c9bc3f500747088a12b42101beff
Erik <emdverboom@gmail.com>
2022-05-13 14:28:49 +0200

    giving up on circleci applicationtests

---
+++ components/internal_server/tests/routes/test_report.py
@@ -0,0 +1,39 @@
+"""Unit tests for the report routes."""
+
+import unittest
+from unittest.mock import Mock
+
+from shared.model.report import Report
+
+from routes import get_report
+
+from ..fixtures import create_report
+
+
+class ReportTest(unittest.TestCase):
+    """Unit tests for getting reports."""
+
+    PAST_DATE = "2021-08-31T23:59:59.000Z"
+
+    def setUp(self):
+        """Extend to set up a database with a report and a user session."""
+        self.database = Mock()
+        self.database.datamodels.find_one.return_value = dict(
+            _id="id",
+            scales=["count", "percentage"],
+            subjects=dict(subject_type=dict(name="Subject type")),
+            metrics=dict(
+                metric_type=dict(
+                    name="Metric type",
+                    scales=["count", "percentage"],
+                )
+            ),
+            sources=dict(source_type=dict(name="Source type", parameters={"url": {"type": "not a password"}})),
+        )
+        self.database.measurements.find.return_value = []
+        self.report = Report(self.database.datamodels.find_one(), create_report())
+        self.database.reports.find.return_value = [self.report]
+
+    def test_get_all_reports(self):
+        """Test that all reports can be retrieved."""
+        self.assertEqual(1, len(get_report(self.database)["reports"]))
