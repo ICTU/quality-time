1169bc4a3cdf59c280516642300e62421be123e6
Erik <emdverboom@gmail.com>
2022-05-13 12:54:11 +0200

    longer waiting time

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-sleep 60 # give time to start up docker containers
 
 cd tests/application_tests
 python3 -m venv venv
 . venv/bin/activate
 ci/pip-install.sh
 python -m unittest discover --start-directory .
-
