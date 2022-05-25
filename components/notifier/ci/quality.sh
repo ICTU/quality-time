78dc184b70752397b723773e8b900534fb2cc727
Frank Niessink <frank@niessink.com>
2022-05-25 20:10:23 +0200

    Fix requirements paths.

--- components/notifier/ci/quality.sh
+++ components/notifier/ci/quality.sh
@@ -18,7 +18,7 @@ run () {
 run mypy src
 run pylint --rcfile=../../.pylintrc src tests
 run python -m flake8 --select=DUO src
-run pip-audit --strict --progress-spinner=off -r requirements-base.txt -r requirements.txt -r requirements-dev.txt
-run safety check --bare --ignore 41002 -r requirements-base.txt -r requirements.txt -r requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
+run pip-audit --strict --progress-spinner=off -r requirements/requirements-base.txt -r requirements/requirements.txt -r requirements/requirements-dev.txt
+run safety check --bare --ignore 41002 -r requirements/requirements-base.txt -r requirements/requirements.txt -r requirements/requirements-dev.txt  # See https://github.com/nedbat/coveragepy/issues/1200
 run bandit --quiet --recursive src/
 run vulture --min-confidence 0 src/ tests/ .vulture_ignore_list.py
