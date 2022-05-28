d6f305a0589d60647d339f0015ce5454c297e519
Frank Niessink <frank@niessink.com>
2022-05-28 21:48:27 +0200

    Add api function for the issue id suggestions endpoint.

--- components/frontend/src/api/report.js
+++ components/frontend/src/api/report.js
@@ -36,6 +36,10 @@ function get_report_pdf(report_uuid, query_string) {
   return fetch_server_api('get', `report/${report_uuid}/pdf${query_string}`, {}, 'application/pdf')
 }
 
+function get_report_issue_tracker_suggestions(report_uuid, query) {
+    return fetch_server_api('get', `report/${report_uuid}/issue_tracker/suggestions/${query}`)
+}
+
 export {
   add_report, copy_report, delete_report, get_reports, get_reports_overview, get_report_pdf, set_report_attribute,
-  set_report_issue_tracker_attribute, set_reports_attribute }
+  set_report_issue_tracker_attribute, set_reports_attribute, get_report_issue_tracker_suggestions }
