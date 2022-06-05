f64d7eb505f26b300141356d322958cb58afe4d6
Frank Niessink <frank@niessink.com>
2022-06-05 13:49:37 +0200

    Fix SonarCloud issues.

--- components/frontend/src/subject/SubjectTableFooter.test.js
+++ components/frontend/src/subject/SubjectTableFooter.test.js
@@ -12,7 +12,7 @@ jest.mock("../api/fetch_server_api.js")
 fetch_server_api.fetch_server_api = jest.fn().mockResolvedValue({ ok: true });
 
 it('shows the add metric button and adds a metric when clicked', async () => {
-    const { queryAllByText, getByText } = render(
+    const { getByText } = render(
         <Permissions.Provider value={[EDIT_REPORT_PERMISSION]}>
             <DataModel.Provider value={datamodel}>
                 <Table>
