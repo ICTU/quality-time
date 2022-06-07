bafb8e7f6f8495dda1c594e754c9ef1560e717d7
Frank Niessink <frank@niessink.com>
2022-06-07 10:52:12 +0200

    Remove default source from data model. Update change log.

--- components/shared_python/src/shared/data_model/meta/metric.py
+++ components/shared_python/src/shared/data_model/meta/metric.py
@@ -48,7 +48,6 @@ class Metric(DescribedModel):
     target: str = "0"
     near_target: str = "10"
     sources: list[str] = Field(..., min_items=1)
-    default_source: Optional[str] = None
     tags: list[Tag] = []
     rationale: Optional[str] = ""
     rationale_urls: list[str] = []
@@ -58,19 +57,6 @@ class Metric(DescribedModel):
         """If the metric supports just one scale, make that scale the default scale."""
         scales = values.get("scales", [])
         return scales[0] if len(scales) == 1 else default_scale
-
-    @validator("default_source", always=True)
-    def set_default_source(cls, default_source, values):  # pylint: disable=no-self-argument
-        """If the metric is supported by just one source besides manual_number, make that source the default source."""
-        sources = values.get("sources", [])
-        sources_except_manual_number = [source for source in sources if source != "manual_number"]
-        if default_source is None and len(sources_except_manual_number) == 1:
-            default_source = sources_except_manual_number[0]
-        if default_source is None and len(sources) == 1:
-            default_source = sources[0]
-        if default_source not in sources:
-            raise ValueError(f"Default source '{default_source}' is not listed as source")
-        return default_source
 
 
 class Metrics(BaseModel):  # pylint: disable=too-few-public-methods
--- components/shared_python/tests/shared/data_model/meta/test_metric.py
+++ components/shared_python/tests/shared/data_model/meta/test_metric.py
@@ -18,24 +18,3 @@ class MetricTest(MetaModelTestCase):
     def test_direction(self):
         """Test that an invalid direction value throws a validation error."""
         self.check_validation_error("value is not a valid enumeration member; permitted: '<', '>'", direction="<>")
-
-    def test_default_source(self):
-        """Test that the default source is set when the metric has one source."""
-        metric = Metric.parse_obj(dict(name="Name", description="Description.", sources=["source"]))
-        self.assertEqual("source", metric.default_source)
-
-    def test_default_source_none(self):
-        """Test that the default source can't be None."""
-        metric = Metric.parse_obj(
-            dict(name="Name", description="Description.", default_source=None, sources=["source"])
-        )
-        self.assertEqual("source", metric.default_source)
-
-    def test_default_source_not_listed_as_source(self):
-        """Test that the default source must be listed as source."""
-        message = "Default source 'missing' is not listed as source"
-        self.check_validation_error(message, default_source="missing", sources=["source"])
-
-    def test_default_source_mandatory_on_many_sources(self):
-        """Test that a default source is mandatory when there is more than one source != the manual number source."""
-        self.check_validation_error("Default source 'None' is not listed as source", sources=["source1", "source2"])
