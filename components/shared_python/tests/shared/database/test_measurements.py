f7ce08c0f7da104e92e264c69001ef3a32113f4e
Frank Niessink <frank@niessink.com>
2022-06-08 17:06:27 +0200

    Move recent_measurements() to shared python component.

--- components/shared_python/tests/shared/database/test_measurements.py
+++ components/shared_python/tests/shared/database/test_measurements.py
@@ -4,10 +4,12 @@ import json
 import unittest
 from unittest.mock import Mock
 
+from shared.database.measurements import insert_new_measurement, latest_measurement, recent_measurements
 from shared.data_model import DATA_MODEL_JSON
-from shared.database.measurements import insert_new_measurement, latest_measurement
 from shared.model.measurement import Measurement
 from shared.model.metric import Metric
+
+from tests.fixtures import METRIC_ID, METRIC_ID2
 
 
 class MeasurementsTest(unittest.TestCase):
@@ -18,6 +20,14 @@ class MeasurementsTest(unittest.TestCase):
         self.database = Mock()
         self.database.measurements.insert_one = self.insert_one_measurement
         self.metric = Metric(json.loads(DATA_MODEL_JSON), dict(type="violations"), "metric_uuid")
+        self.measurements = [
+            {"_id": 1, "start": "0", "end": "1", "sources": [], "metric_uuid": METRIC_ID},
+            {"_id": 2, "start": "3", "end": "4", "sources": [], "metric_uuid": METRIC_ID},
+            {"_id": 3, "start": "6", "end": "7", "sources": [], "metric_uuid": METRIC_ID},
+            {"_id": 4, "start": "1", "end": "2", "sources": [], "metric_uuid": METRIC_ID2},
+            {"_id": 5, "start": "4", "end": "5", "sources": [], "metric_uuid": METRIC_ID2},
+            {"_id": 6, "start": "7", "end": "8", "sources": [], "metric_uuid": METRIC_ID2},
+        ]
 
     @staticmethod
     def insert_one_measurement(measurement):
@@ -45,3 +55,15 @@ class MeasurementsTest(unittest.TestCase):
         measurement = Measurement(self.metric, {"_id": "measurement_id"})
         inserted_measurement = insert_new_measurement(self.database, measurement)
         self.assertFalse("_id" in inserted_measurement)
+
+    def test_recent_measurements_by_uuid_uuid_filter(self):
+        """Test that we get all measurements with all metric ids."""
+        self.database.measurements.find.return_value = self.measurements[0:6]
+        metric_1 = Metric({}, {}, METRIC_ID)
+        metric_2 = Metric({}, {}, METRIC_ID2)
+        measurements = recent_measurements(self.database, metrics_dict={METRIC_ID: metric_1, METRIC_ID2: metric_2})
+        self.assertEqual(len(measurements), 2)
+        self.assertIn(METRIC_ID, measurements)
+        self.assertEqual(len(measurements[METRIC_ID]), 3)
+        self.assertIn(METRIC_ID2, measurements)
+        self.assertEqual(len(measurements[METRIC_ID2]), 3)
