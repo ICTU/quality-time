389efd2dd106211fdb55a10b2d850641331e46d9
Frank Niessink <frank@niessink.com>
2022-06-08 20:34:33 +0200

    Increase test coverage.

---
+++ components/shared_python/tests/shared/database/test_reports.py
@@ -0,0 +1,27 @@
+"""Unit tests for the reports collection."""
+
+import unittest
+from unittest.mock import Mock
+
+from shared.database.reports import insert_new_report
+
+from ...fixtures import JOHN, REPORT_ID, REPORT_ID2
+
+
+class ReportsTest(unittest.TestCase):
+    """Unit tests for the reports collection."""
+
+    def setUp(self):
+        """Override to setup the database."""
+        self.database = Mock()
+        self.database.sessions.find_one.return_value = JOHN
+
+    def test_insert_one_report(self):
+        """Test that a report can be inserted into the reports collection."""
+        report = dict(report_uuid=REPORT_ID)
+        self.assertEqual(dict(ok=True), insert_new_report(self.database, "delta", [REPORT_ID], report))
+
+    def test_insert_multiple_reports(self):
+        """Test that multiple reports can be inserted into the reports collection."""
+        reports = [dict(report_uuid=REPORT_ID), dict(report_uuid=REPORT_ID2, _id="id")]
+        self.assertEqual(dict(ok=True), insert_new_report(self.database, "delta", [REPORT_ID], *reports))
