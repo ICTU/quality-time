824083bd07569d0f1bdbf7eb12e37b3e53d035e1
Frank Niessink <frank@niessink.com>
2022-06-10 17:12:10 +0200

    Fix service name.

--- tests/feature_tests/ci/test.sh
+++ tests/feature_tests/ci/test.sh
@@ -7,7 +7,7 @@
 
 mkdir -p build
 export COVERAGE_RCFILE="$(pwd)"/tests/feature_tests/.coveragerc
-docker compose build --progress quiet database server renderer frontend www
+docker compose build --progress quiet database external_server renderer frontend www
 docker compose up --detach database ldap
 cd components/external_server || exit
 python3 -m venv venv
