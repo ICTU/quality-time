f3488de934d8b40033a1716257a524ee73770ccf
Frank Niessink <frank@niessink.com>
2022-06-27 16:55:02 +0200

    Add error message to measurement value popup if the measurement value is older than one hour.

--- components/frontend/src/measurement/MeasurementValue.test.js
+++ components/frontend/src/measurement/MeasurementValue.test.js
@@ -90,3 +90,15 @@ it('shows when the last measurement attempt was', async () => {
         expect(screen.queryByText(/Last measurement attempt/)).not.toBe(null)
     })
 })
+
+it('shows when the last measurement attempt was long ago', async () => {
+    render(
+        <DataModel.Provider value={{ metrics: { violations: { unit: "violations" } } }}>
+            <MeasurementValue metric={{ status: null, type: "violations", scale: "count", unit: null, latest_measurement: { start: "2022-01-16T00:31:00", end: "2022-01-16T00:51:00", count: { value: null } } }} />
+        </DataModel.Provider>
+    )
+    await userEvent.hover(screen.queryByText(/\?/))
+    await waitFor(() => {
+        expect(screen.queryByText(/This metric was not recently measured/)).not.toBe(null)
+    })
+})
