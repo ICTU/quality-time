3fb8ed526d8b0ded8335ad7059ed78fd3c4650c0
Erik <emdverboom@gmail.com>
2022-06-27 18:15:58 +0200

    add settings endpoints

--- components/external_server/src/database/users.py
+++ components/external_server/src/database/users.py
@@ -8,9 +8,13 @@ from shared.utils.type import User
 def upsert_user(database: Database, user: User) -> None:
     """Update the existing user or insert a new one."""
     database.users.replace_one(
-        dict(user=user.username),
-        dict(
-            user.to_dict()
-        ),
+        dict(username=user.username),
+        dict(user.to_dict()),
         upsert=True,
     )
+
+
+def get_user(database: Database, username: str) -> None:
+    """Update the existing user or insert a new one."""
+    user_dict = database.users.find_one({"username": username})
+    return User(**user_dict)
---
+++ components/external_server/src/routes/settings.py
@@ -0,0 +1,31 @@
+"""Define all routes about settings."""
+from typing import cast
+import bottle
+from pymongo.database import Database
+from shared.utils.type import SessionId
+
+from database.users import upsert_user, get_user
+
+
+def find_user(database: Database) -> str:
+    """Find the username belonging to a session_id"""
+    session_id = cast(SessionId, str(bottle.request.get_cookie("session_id")))
+    username = database.sessions.find_one({"session_id": session_id})
+    user = get_user(database, username)
+    return user
+
+
+@bottle.get("/api/v3/settings", authentication_required=True)
+def get_settings(database: Database) -> dict[str, bool]:
+    """Log the user out."""
+    user = find_user(database)
+    return dict(settings=user.settings)
+
+
+@bottle.post("/api/v3/settings", authentication_required=True)
+def update_settings(database: Database, settings: dict) -> dict:
+    """Update the settings for the logged-in user."""
+    user = find_user(database)
+    user.settings = settings
+    upsert_user(database, user)
+    return dict(ok=True)
