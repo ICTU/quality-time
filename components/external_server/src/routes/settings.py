f8712306b542b54e0bb0cd46915f45c2476b9b35
Erik <emdverboom@gmail.com>
2022-06-27 18:20:56 +0200

    fixing quality

--- components/external_server/src/routes/settings.py
+++ components/external_server/src/routes/settings.py
@@ -2,28 +2,28 @@
 from typing import cast
 import bottle
 from pymongo.database import Database
-from shared.utils.type import SessionId
+from shared.utils.type import SessionId, User
 
 from database.users import upsert_user, get_user
 
 
-def find_user(database: Database) -> str:
+def find_user(database: Database) -> User:
     """Find the username belonging to a session_id"""
     session_id = cast(SessionId, str(bottle.request.get_cookie("session_id")))
-    username = database.sessions.find_one({"session_id": session_id})
+    username = cast(str, database.sessions.find_one({"session_id": session_id}))
     user = get_user(database, username)
     return user
 
 
 @bottle.get("/api/v3/settings", authentication_required=True)
-def get_settings(database: Database) -> dict[str, bool]:
+def get_settings(database: Database) -> dict:
     """Log the user out."""
     user = find_user(database)
     return dict(settings=user.settings)
 
 
 @bottle.post("/api/v3/settings", authentication_required=True)
-def update_settings(database: Database, settings: dict) -> dict:
+def update_settings(database: Database, settings: dict) -> dict[str, bool]:
     """Update the settings for the logged-in user."""
     user = find_user(database)
     user.settings = settings
