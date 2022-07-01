31c4f1562deec7e13ad4992edb4d9a159b5a9acf
Frank Niessink <frank@niessink.com>
2022-07-01 20:50:20 +0200

    Increase test coverage.

--- components/frontend/src/__fixtures__/fixtures.js
+++ components/frontend/src/__fixtures__/fixtures.js
@@ -15,10 +15,27 @@ export const report = {
         subject_uuid: {
             type: "subject_type", name: "Subject 1 title", metrics: {
                 metric_uuid: {
-                    name: "M1", type: "metric_type", tags: [], sources: {}, status: "informative", recent_measurements: []
+                    name: "M1",
+                    type: "metric_type",
+                    tags: [],
+                    target: "1",
+                    sources: {},
+                    status: "informative",
+                    recent_measurements: [],
+                    latest_measurement: {count: 1},
+                    comment: "Comment 1"
                 },
                 metric_uuidi2: {
-                    name: "M2", type: "metric_type", tags: ["tag"], issue_ids: ["ABC-42"], sources: {source_uuid: {name: "Source"}}, recent_measurements: []
+                    name: "M2",
+                    type: "metric_type",
+                    tags: ["tag"],
+                    target: "2",
+                    issue_ids: ["ABC-42"],
+                    sources: {source_uuid: {name: "Source"}},
+                    status: "target_not_met",
+                    recent_measurements: [],
+                    latest_measurement: {count: 2},
+                    comment: "Comment 2"
                 }
             }
         },
--- components/frontend/src/subject/Subject.test.js
+++ components/frontend/src/subject/Subject.test.js
@@ -1,15 +1,18 @@
-import { act, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import { Subject } from "./Subject";
 import * as fetch_server_api from '../api/fetch_server_api';
 import { DataModel } from "../context/DataModel";
 import { datamodel, report } from "../__fixtures__/fixtures";
 
-function renderSubject(dates, hideMetricsNotRequiringAction) {
+function renderSubject(dates, hideMetricsNotRequiringAction, sortColumn, sortDirection) {
     render(
         <DataModel.Provider value={datamodel}>
             <Subject
                 dates={dates}
+                handleSort={() => { /* Dummy implementation */ }}
                 report={report}
+                sortColumn={sortColumn}
+                sortDirection={sortDirection}
                 subject_uuid="subject_uuid"
                 tags={[]}
                 hiddenColumns={[]}
@@ -41,4 +44,98 @@ it('shows the subject title', async () => {
 it('hides metrics not requiring action', async () => {
     await act(async () => { renderSubject([new Date(2022, 3, 26)], true) });
     expect(screen.queryAllByText(/M\d/).length).toBe(1);
+})
+
+function expectOrder(metricNames) {
+    expect(screen.getAllByText(/M\d/).map((element) => element.innerHTML)).toStrictEqual(metricNames)
+}
+
+it('sorts metrics by name ascending', async () => {
+    await act(async () => { renderSubject([], false, "name", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by name descending', async () => {
+    await act(async () => { renderSubject([], false, "name", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by measurement ascending', async () => {
+    await act(async () => { renderSubject([], false, "measurement", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by measurement descending', async () => {
+    await act(async () => { renderSubject([], false, "measurement", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by target ascending', async () => {
+    await act(async () => { renderSubject([], false, "target", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by target descending', async () => {
+    await act(async () => { renderSubject([], false, "target", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by comment ascending', async () => {
+    await act(async () => { renderSubject([], false, "comment", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by comment descending', async () => {
+    await act(async () => { renderSubject([], false, "comment", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by status ascending', async () => {
+    await act(async () => { renderSubject([], false, "status", "ascending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by status descending', async () => {
+    await act(async () => { renderSubject([], false, "status", "descending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by source ascending', async () => {
+    await act(async () => { renderSubject([], false, "source", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by source descending', async () => {
+    await act(async () => { renderSubject([], false, "source", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by issues ascending', async () => {
+    await act(async () => { renderSubject([], false, "issues", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by issues descending', async () => {
+    await act(async () => { renderSubject([], false, "issues", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by tags ascending', async () => {
+    await act(async () => { renderSubject([], false, "tags", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by tags descending', async () => {
+    await act(async () => { renderSubject([], false, "tags", "descending") });
+    expectOrder(["M2", "M1"])
+})
+
+it('sorts metrics by unit ascending', async () => {
+    await act(async () => { renderSubject([], false, "unit", "ascending") });
+    expectOrder(["M1", "M2"])
+})
+
+it('sorts metrics by unit descending', async () => {
+    await act(async () => { renderSubject([], false, "unit", "descending") });
+    expectOrder(["M2", "M1"])
 })
