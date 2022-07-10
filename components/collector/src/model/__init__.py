209bb4a19d93538f8510985b78336226f911a30a
Frank Niessink <frank@niessink.com>
2022-07-10 12:33:25 +0200

    Collect the issue status categeroy.

--- components/collector/src/model/__init__.py
+++ components/collector/src/model/__init__.py
@@ -1,7 +1,7 @@
 """Model package."""
 
 from .entity import Entities, Entity
-from .issue_status import IssueStatus
+from .issue_status import IssueStatus, IssueStatusCategory
 from .measurement import SourceMeasurement, MetricMeasurement
 from .parameters import SourceParameters
 from .responses import SourceResponses
--- components/collector/src/source_collectors/jira/issue_status.py
+++ components/collector/src/source_collectors/jira/issue_status.py
@@ -1,13 +1,17 @@
 """Jira issue status collector."""
 
+from typing import cast
+
 from collector_utilities.type import URL
-from model import IssueStatus, SourceResponses
+from model import IssueStatus, IssueStatusCategory, SourceResponses
 
 from .base import JiraBase
 
 
 class JiraIssueStatus(JiraBase):
     """Jira issue status collector."""
+
+    STATUS_CATEGORY_MAPPING = dict(done="done", indeterminate="doing", new="todo")
 
     async def _api_url(self) -> URL:
         """Override to get the issue, including the status field, from Jira."""
@@ -23,7 +27,16 @@ class JiraIssueStatus(JiraBase):
         """Override to get the issue status from the responses."""
         json = await responses[0].json()
         name = json["fields"]["status"]["name"]
+        jira_status_category = json["fields"]["status"]["statusCategory"]["key"]
+        status_category = cast(IssueStatusCategory, self.STATUS_CATEGORY_MAPPING.get(jira_status_category, "todo"))
         created = json["fields"]["created"]
         updated = json["fields"].get("updated")
         summary = json["fields"].get("summary")
-        return IssueStatus(self._issue_id, name=name, created=created, updated=updated, summary=summary)
+        return IssueStatus(
+            self._issue_id,
+            name=name,
+            status_category=status_category,
+            created=created,
+            updated=updated,
+            summary=summary,
+        )
