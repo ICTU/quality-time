eeea790ff7080319c55f9997d2ec0331c5bb3194
Frank Niessink <frank@niessink.com>
2022-07-12 15:42:45 +0200

    Only show target with grey label if technical debt target is not applied.

--- components/frontend/src/measurement/MeasurementTarget.js
+++ components/frontend/src/measurement/MeasurementTarget.js
@@ -3,19 +3,15 @@ import { Label, Popup } from '../semantic_ui_react_wrappers';
 import { DataModel } from "../context/DataModel";
 import { formatMetricDirection, formatMetricScale, formatMetricScaleAndUnit, get_metric_target } from '../utils';
 
-function popupText(metric, dataModel) {
+function popupText(metric, debtEndDateInThePast, allIssuesDone, dataModel) {
     const unit = formatMetricScaleAndUnit(dataModel.metrics[metric.type], metric)
     const metricDirection = formatMetricDirection(metric, dataModel)
-    let debtEndDateInThePast = false;
     let debtEndDateText = ""
     let endDate;
     if (metric.debt_end_date) {
         endDate = new Date(metric.debt_end_date);
-        const today = new Date();
-        debtEndDateInThePast = endDate.toISOString().split("T")[0] < today.toISOString().split("T")[0];
         debtEndDateText = debtEndDateInThePast ? "" : ` until ${endDate.toLocaleDateString()}`
     }
-    const allIssuesDone = metric.issue_status?.length > 0 ? metric.issue_status.every((status) => status.status_category === "done") : false
     let text = `Measurements ${metricDirection} ${metric.debt_target ?? 0}${unit} are accepted as technical debt${debtEndDateText}.`
     if (allIssuesDone || debtEndDateInThePast) {
         text += " However, this technical debt target is not applied because"
@@ -37,14 +33,18 @@ export function MeasurementTarget({ metric }) {
     if (metric?.evaluate_targets === false) { return null }
     const metricDirection = formatMetricDirection(metric, dataModel)
     const target = `${metricDirection} ${get_metric_target(metric)}${formatMetricScale(metric)}`
+    const allIssuesDone = metric.issue_status?.length > 0 ? metric.issue_status.every((status) => status.status_category === "done") : false
+    let debtEndDateInThePast = false
+    if (metric.debt_end_date) {
+        const endDate = new Date(metric.debt_end_date);
+        const today = new Date();
+        debtEndDateInThePast = endDate.toISOString().split("T")[0] < today.toISOString().split("T")[0];
+    }
+    const label = allIssuesDone || debtEndDateInThePast ? <Label color="grey">{target}</Label> : <span>{target}</span>
     if (metric.accept_debt) {
         return (
-            <Popup
-                hoverable
-                on={['hover', 'focus']}
-                trigger={<Label color="grey">{target}</Label>}
-            >
-                {popupText(metric, dataModel)}
+            <Popup hoverable on={['hover', 'focus']} trigger={label}>
+                {popupText(metric, debtEndDateInThePast, allIssuesDone, dataModel)}
             </Popup>
         )
     }
