35cf4c7ec78a3109b467bcaf39f777f77027d8bf
Frank Niessink <frank@niessink.com>
2022-07-15 22:05:39 +0200

    Increase test coverage.

--- components/frontend/src/measurement/IssueStatus.test.js
+++ components/frontend/src/measurement/IssueStatus.test.js
@@ -15,8 +15,11 @@ function renderIssueStatus(
         status = "in progress",
         statusCategory = "",
         release = false,
+        releaseName = "1.0",
         releaseReleased = false,
+        releaseDate = "3000-01-02",
         sprint = false,
+        sprintEndDate = "3000-01-01",
         updated = false,
     } = {}
 ) {
@@ -36,12 +39,12 @@ function renderIssueStatus(
         landing_url: landingUrl,
         connection_error: connectionError ? "error" : null,
         parse_error: parseError ? "error" : null,
-        release_name: release ? "Release 1.0" : null,
+        release_name: release ? releaseName : null,
         release_released: release ? releaseReleased : null,
-        release_date: release ? "3000-01-02" : null,
+        release_date: release ? releaseDate : null,
         sprint_name: sprint ? "Sprint 42" : null,
         sprint_state: sprint ? "active" : null,
-        sprint_enddate: sprint ? "3000-01-01" : null
+        sprint_enddate: sprint ? sprintEndDate : null
     }
     if (statusCategory) {
         issueStatus["status_category"] = statusCategory
@@ -171,14 +174,14 @@ it("displays the due date in the popup", async () => {
 
 it("displays the planned release in the label if configured", async () => {
     const { queryByText } = renderIssueStatus({ release: true, issueSettings: {showIssueRelease: true }})
-    expect(queryByText(/Release 1.0/)).not.toBe(null)
+    expect(queryByText(/1.0/)).not.toBe(null)
     expect(queryByText(/planned/)).not.toBe(null)
     expect(queryByText(/from now/)).not.toBe(null)
 });
 
 it("displays the released release in the label if configured", async () => {
     const { queryByText } = renderIssueStatus({ release: true, releaseReleased: true, issueSettings: {showIssueRelease: true }})
-    expect(queryByText(/Release 1.0/)).not.toBe(null)
+    expect(queryByText(/1.0/)).not.toBe(null)
     expect(queryByText(/released/)).not.toBe(null)
     expect(queryByText(/from now/)).not.toBe(null)
 });
@@ -191,7 +194,7 @@ it("displays the release without doubling release in the label", async () => {
 
 it("does not display the release in the label if not configured", async () => {
     const { queryByText } = renderIssueStatus({ release: true })
-    expect(queryByText(/Release 1.0/)).toBe(null)
+    expect(queryByText(/1.0/)).toBe(null)
     expect(queryByText(/planned/)).toBe(null)
     expect(queryByText(/from now/)).toBe(null)
 });
@@ -200,9 +203,19 @@ it("displays the release in the popup", async () => {
     const { queryByText } = renderIssueStatus({ release: true })
     await userEvent.hover(queryByText(/123/))
     await waitFor(() => {
-        expect(queryByText(/Release 1.0/)).not.toBe(null)
+        expect(queryByText(/1.0/)).not.toBe(null)
         expect(queryByText(/planned/)).not.toBe(null)
         expect(queryByText(/from now/)).not.toBe(null)
+    })
+});
+
+it("displays the release in the popup without release date", async () => {
+    const { queryByText } = renderIssueStatus({ release: true, releaseDate: null })
+    await userEvent.hover(queryByText(/123/))
+    await waitFor(() => {
+        expect(queryByText(/1.0/)).not.toBe(null)
+        expect(queryByText(/planned/)).toBe(null)
+        expect(queryByText(/from now/)).toBe(null)
     })
 });
 
@@ -227,6 +240,16 @@ it("displays the sprint in the popup", async () => {
         expect(queryByText(/Sprint 42/)).not.toBe(null)
         expect(queryByText(/active/)).not.toBe(null)
         expect(queryByText(/from now/)).not.toBe(null)
+    })
+});
+
+it("displays the sprint in the popup without sprint end date", async () => {
+    const { queryByText } = renderIssueStatus({ sprint: true, sprintEndDate: null })
+    await userEvent.hover(queryByText(/123/))
+    await waitFor(() => {
+        expect(queryByText(/Sprint 42/)).not.toBe(null)
+        expect(queryByText(/active/)).not.toBe(null)
+        expect(queryByText(/from now/)).toBe(null)
     })
 });
 
