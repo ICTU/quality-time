4a65eefd26f950ae0e2f5b18f79863ffcb4bb990
Frank Niessink <frank@niessink.com>
2022-07-25 15:25:49 +0200

    Replace md5 with crypto-js because md5 has had no releases in two years (#4284)
    * Replace md5 with blueimp-md5 because md5 has had no releases in two years.
    
    * Replace blueimp-md5 with crypto-js because blueimp-md5 is also not supported.

--- components/frontend/src/widgets/Avatar.js
+++ components/frontend/src/widgets/Avatar.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { Image, Icon } from 'semantic-ui-react';
-import md5 from 'md5';
+import MD5 from 'crypto-js/md5';
 import { DarkMode } from '../context/DarkMode';
 
 export function Avatar({ email }) {
     const color = useContext(DarkMode) ? "grey" : null;
     return (
-        email ? <Image avatar src={`https://www.gravatar.com/avatar/${md5(email)}?d=identicon`} alt="Avatar" /> : <Icon color={color} name="user" />
+        email ? <Image avatar src={`https://www.gravatar.com/avatar/${MD5(email)}?d=identicon`} alt="Avatar" /> : <Icon color={color} name="user" />
     )
 }
