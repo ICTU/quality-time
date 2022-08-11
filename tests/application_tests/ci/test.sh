3f00cdc35180de0e8a779bc93cd8f72fbb7e8b20
Frank Niessink <frank@niessink.com>
2022-08-11 12:17:36 +0200

    Revert changes to test.sh.

--- tests/application_tests/ci/test.sh
+++ tests/application_tests/ci/test.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cd tests/application_tests
-python3 -m venv /tmp/venv-quality-time-application-tests
-. /tmp/venv-quality-time-application-tests/bin/activate
+python3 -m venv venv
+. venv/bin/activate
 ci/pip-install.sh
 python -m unittest discover --start-directory .
-deactivate
-rm -rf /tmp/venv-quality-time-application-tests
+
