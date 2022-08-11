5bbe2f1754b60fcdefc7f9fc00af63950d901c3e
Frank Niessink <frank@niessink.com>
2022-08-11 23:25:39 +0200

    Add more column help popups.

--- components/frontend/src/subject/SubjectTableHeader.js
+++ components/frontend/src/subject/SubjectTableHeader.js
@@ -1,22 +1,61 @@
 import { Table } from "semantic-ui-react";
+import { Label } from "../semantic_ui_react_wrappers";
 import { SortableTableHeaderCell } from '../widgets/SortableTableHeaderCell';
+import { StatusIcon } from '../measurement/StatusIcon';
+
+const statusHelp = <>
+    <p>
+        The current status of the metric.
+    </p>
+    <p>
+        Hover over the status to see how long the metric has had the current status.
+    </p>
+    <p>
+        If the status is <StatusIcon status="informative" size="tiny" /> the measurement value is not evaluated against a target value.
+    </p>
+    <p>
+        If the status is <StatusIcon status="target_met" size="tiny" /> the measurement value meets the target value.
+    </p>
+    <p>
+        If the status is <StatusIcon status="near_target_met" size="tiny" /> the measurement value is close to the target value.
+    </p>
+    <p>
+        If the status is <StatusIcon status="target_not_met" size="tiny" /> the measurement value does not meet the target value.
+    </p>
+    <p>
+        If the status is <StatusIcon status="debt_target_met" size="tiny" /> the measurement value does not meet the target value,
+        but this is accepted as technical debt. The measurement value does meet the technical debt target.
+    </p>
+    <p>
+        If the status is <StatusIcon status="unknown" size="tiny" /> the status could not be determined because
+        no sources have been configured for the metric yet or the measurement data could not be collected.
+    </p>
+</>
 
 const measurementHelp = <>
     <p>
-        The latest measurement value.
+        The latest measurement value. Metrics are measured periodically.
     </p>
     <p>
-        If the measurement value is '?', no sources have been configured for the metric.
-        Expand the metric and navigate to the sources tab to add sources.
+        Hover over the measurement value to see when the metric was last measured.
     </p>
     <p>
-        If the measurement value has a red background, the metric has not been measured recently.
+        If the measurement value is '?', no sources have been configured for the metric yet or the measurement
+        data could not be collected.
+        Expand the metric and navigate to the sources tab to add sources or see the error details.
+    </p>
+    <p>
+        If the measurement value has a <Label horizontal color="red">red background</Label>, the metric has not been measured recently.
+        This indicates a problem with <em>Quality-time</em> itself, and a system administrator should be notified.
     </p>
 </>
 
 const timeLeftHelp = <>
     <p>
         The number of days left to address the metric.
+    </p>
+    <p>
+        Hover of the number of days to see the exact deadline.
     </p>
     <p>
         If the metric needs action, the time left is based on the desired reaction times.
@@ -31,8 +70,11 @@ const sourcesHelp = <>
         The tools and reports accessed to collect the measurement data.
     </p>
     <p>
-        If a source has a red background, the source could not be accessed or the data could not be parsed.
-        Expand the metric and navigate to the source to see the error details.
+        Click a source to open the tool or report in a new tab.
+    </p>
+    <p>
+        If a source has a <Label horizontal color="red">red background</Label>, the source could not be accessed or the
+        data could not be parsed. Expand the metric and navigate to the source to see the error details.
     </p>
 </>
 
@@ -41,8 +83,14 @@ const issuesHelp = <>
         Links to issues, opened in an issue tracker such as Jira, to track progress of addressing the metric.
     </p>
     <p>
-        If an issue has a red background, the issue tracker could not be accessed or the data could not be parsed.
-        Expand the metric and navigate to the technical debt tab to see the error details.
+        Hover over an issue to see more information about the issue.
+    </p>
+    <p>
+        Click an issue to open the issue in a new tab.
+    </p>
+    <p>
+        If an issue has a <Label horizontal color="red">red background</Label>, the issue tracker could not be accessed
+        or the data could not be parsed. Expand the metric and navigate to the technical debt tab to see the error details.
     </p>
 </>
 
@@ -62,7 +110,7 @@ export function SubjectTableHeader(
                 <SortableTableHeaderCell colSpan="2" column='name' label='Metric' {...sortProps} />
                 {nrDates > 1 && columnDates.map(date => <Table.HeaderCell key={date} className="unsortable" textAlign="right">{date.toLocaleDateString()}</Table.HeaderCell>)}
                 {nrDates === 1 && !hiddenColumns.includes("trend") && <Table.HeaderCell className="unsortable" width="2">Trend (7 days)</Table.HeaderCell>}
-                {nrDates === 1 && !hiddenColumns.includes("status") && <SortableTableHeaderCell column='status' label='Status' textAlign='center' {...sortProps} />}
+                {nrDates === 1 && !hiddenColumns.includes("status") && <SortableTableHeaderCell column='status' label='Status' textAlign='center' help={statusHelp} {...sortProps} />}
                 {nrDates === 1 && !hiddenColumns.includes("measurement") && <SortableTableHeaderCell column='measurement' label='Measurement' textAlign="right" help={measurementHelp} {...sortProps} />}
                 {nrDates === 1 && !hiddenColumns.includes("target") && <SortableTableHeaderCell column='target' label='Target' textAlign="right" {...sortProps} />}
                 {!hiddenColumns.includes("unit") && <SortableTableHeaderCell column="unit" label="Unit" {...sortProps} />}
--- components/frontend/src/widgets/SortableTableHeaderCell.js
+++ components/frontend/src/widgets/SortableTableHeaderCell.js
@@ -3,7 +3,7 @@ import { Popup, Table } from '../semantic_ui_react_wrappers';
 
 export function SortableTableHeaderCell({ colSpan, column, sortColumn, sortDirection, handleSort, label, textAlign, help }) {
     const sorted = sortColumn === column ? sortDirection : null;
-    const children = help ? <Popup wide trigger={<span>{label}</span>} header={label} content={help} /> : label
+    const children = help ? <Popup wide="very" trigger={<span>{label}</span>} header={label} content={help} /> : label
     return (
         <Table.HeaderCell colSpan={colSpan} onClick={() => handleSort(column)} sorted={sorted} textAlign={textAlign || 'left'}>
             {children}
