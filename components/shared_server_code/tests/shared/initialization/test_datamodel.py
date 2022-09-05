880c358641acb8f3c5b29a5a81792f18c187d14f
Frank Niessink <frank@niessink.com>
2022-09-05 11:12:27 +0200

    Remove duplication.

--- components/shared_server_code/tests/shared/initialization/test_datamodel.py
+++ components/shared_server_code/tests/shared/initialization/test_datamodel.py
@@ -12,7 +12,7 @@ class DataModelImportTest(DataModelTestCase):
 
     def setUp(self):
         """Override to set up the database."""
-        self.data_model = self.DATA_MODEL.copy()
+        self.data_model = self.DATA_MODEL.copy()  # Copy the data model as instance attribute so we can safely change it
         self.database = Mock()
         self.database.datamodels.find_one.return_value = self.data_model
 
@@ -24,15 +24,11 @@ class DataModelImportTest(DataModelTestCase):
 
     def test_import(self):
         """Test that a data model can be imported."""
-        self.data_model["_id"] = "id"
-        self.data_model["timestamp"] = "timestamp"
         self.data_model["changed"] = "make sure the data model is changed"
         import_datamodel(self.database)
         self.database.datamodels.insert_one.assert_called_once()
 
     def test_skip_import(self):
         """Test that a data model is not imported if it's unchanged."""
-        self.data_model["_id"] = "id"
-        self.data_model["timestamp"] = "timestamp"
         import_datamodel(self.database)
         self.database.datamodels.insert_one.assert_not_called()
