81e30b583b82b85f866ffa10ce708acc597e84e2
Frank Niessink <frank@niessink.com>
2022-10-10 18:02:16 +0200

    Increase feature test coverage.

--- tests/feature_tests/steps/subject.py
+++ tests/feature_tests/steps/subject.py
@@ -8,5 +8,5 @@ def get_measurements(context, has_or_had, expected_number):
     if has_or_had == "had":
         context.report_date = "2020-11-17T10:00:00Z"
 
-    response = context.get(f"subject/{context.uuid['subject']}/measurements")
+    response = context.get(f"subject/{context.uuid['subject']}/measurements?min_report_date=2020-11-17")
     assert_equal(int(expected_number), len(response["measurements"]))
