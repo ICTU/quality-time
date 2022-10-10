d7301835daf817886a754deb76ec921ae7bfeb0e
Frank Niessink <frank@niessink.com>
2022-10-10 18:03:10 +0200

    Fix timestamp.

--- tests/feature_tests/steps/subject.py
+++ tests/feature_tests/steps/subject.py
@@ -8,5 +8,5 @@ def get_measurements(context, has_or_had, expected_number):
     if has_or_had == "had":
         context.report_date = "2020-11-17T10:00:00Z"
 
-    response = context.get(f"subject/{context.uuid['subject']}/measurements?min_report_date=2020-11-17")
+    response = context.get(f"subject/{context.uuid['subject']}/measurements?min_report_date=2020-11-16T00:00:00Z")
     assert_equal(int(expected_number), len(response["measurements"]))
