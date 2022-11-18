4b9c56d4ffdf5f8d114c35aeabbcba09d74d6f76
Frank Niessink <frank@niessink.com>
2022-11-18 13:13:25 +0100

    Fix SonarQube issues.

--- components/collector/tests/source_collectors/gitlab/test_source_up_to_dateness.py
+++ components/collector/tests/source_collectors/gitlab/test_source_up_to_dateness.py
@@ -17,12 +17,16 @@ class GitLabSourceUpToDatenessTest(GitLabTestCase):
         super().setUp()
         self.commit_json = dict(committed_date="2019-01-01T09:06:12+00:00")
         self.expected_age = (datetime.now(timezone.utc) - datetime(2019, 1, 1, 9, 6, 9, tzinfo=timezone.utc)).days
-        self.head_response = Mock()
-        self.head_response.headers = {"X-Gitlab-Last-Commit-Id": "commit-sha"}
+
+    def patched_client_session_head(self):
+        """Return a patched version of the client session head method."""
+        head_response = Mock()
+        head_response.headers = {"X-Gitlab-Last-Commit-Id": "commit-sha"}
+        return patch("aiohttp.ClientSession.head", AsyncMock(side_effect=[head_response, head_response]))
 
     async def test_source_up_to_dateness_file(self):
         """Test that the age of a file in a repo can be measured."""
-        with patch("aiohttp.ClientSession.head", AsyncMock(return_value=self.head_response)):
+        with self.patched_client_session_head():
             response = await self.collect(
                 get_request_json_side_effect=[[], self.commit_json, dict(web_url="https://gitlab.com/project")]
             )
@@ -32,7 +36,7 @@ class GitLabSourceUpToDatenessTest(GitLabTestCase):
 
     async def test_source_up_to_dateness_folder(self):
         """Test that the age of a folder in a repo can be measured."""
-        with patch("aiohttp.ClientSession.head", AsyncMock(side_effect=[self.head_response, self.head_response])):
+        with self.patched_client_session_head():
             response = await self.collect(
                 get_request_json_side_effect=[
                     [dict(type="blob", path="file.txt"), dict(type="tree", path="folder")],
@@ -51,7 +55,7 @@ class GitLabSourceUpToDatenessTest(GitLabTestCase):
         self.set_source_parameter("file_path", "")
         build_date = datetime.fromisoformat(self.gitlab_jobs_json[0]["created_at"].strip("Z")).date()
         expected_age = (date.today() - build_date).days
-        with patch("aiohttp.ClientSession.head", AsyncMock(return_value=self.head_response)):
+        with self.patched_client_session_head():
             response = await self.collect(get_request_json_return_value=self.gitlab_jobs_json)
         self.assert_measurement(response, value=str(expected_age), landing_url="https://gitlab/project/-/pipelines/1")
 
