e6526ef491a801e2769a92010f5cc3a999f25f3e
Frank Niessink <frank@niessink.com>
2023-01-05 16:59:35 +0100

    Make the description of subjects, metrics, and sources in the UI of Quality-time link to the relevant documentation on Read-The-Docs. Closes #5121.

--- components/frontend/src/metric/MetricTypeHeader.js
+++ components/frontend/src/metric/MetricTypeHeader.js
@@ -1,52 +1,17 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
-import { Header, Popup } from '../semantic_ui_react_wrappers';
-
-function seeAlso(key, urls) {
-    return (
-        <div key={key}>
-            <Header>See also</Header>
-            <ol>
-                {urls.map((url) => <li key={url}><a href={url}>{url}</a></li>)}
-            </ol>
-        </div>
-    )
-}
+import { Header } from '../semantic_ui_react_wrappers';
+import { HyperLink } from '../widgets/HyperLink';
+import { slugify } from '../utils';
 
 export function MetricTypeHeader({ metricType }) {
-    let popup = null;
-    const popupContents = [];
-    if (metricType.rationale) {
-        popupContents.push(<Header key="rationale-header">Why measure {metricType.name.toLowerCase()}?</Header>)
-        popupContents.push(<p key="rationale-text">{metricType.rationale}</p>)
-        if (metricType.rationale_urls?.length > 0) {
-            popupContents.push(seeAlso("rationale-urls", metricType.rationale_urls))
-        }
-    }
-    if (metricType.explanation) {
-        popupContents.push(<Header key="explanation=header">More information</Header>)
-        popupContents.push(<p key="explanation-text">{metricType.explanation}</p>)
-        if (metricType.explanation_urls?.length > 0) {
-            popupContents.push(seeAlso("explanation-urls", metricType.explanation_urls))
-        }
-    }
-    if (popupContents.length > 0) {
-        popup = <Popup
-            content={<>{popupContents}</>}
-            hoverable
-            on={['hover', 'focus']}
-            trigger={
-                <span>&nbsp;<Icon role="tooltip" aria-label="help" tabIndex="0" name="info circle" /></span>
-            }
-            wide="very"
-        />
-    }
+    const url = `https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/reference.html${slugify(metricType.name)}`
     return (
         <Header>
             <Header.Content>
                 {metricType.name}
                 <Header.Subheader>
-                    {metricType.description}{popup}
+                    {metricType.description} <HyperLink url={url}>Read the Docs <Icon name="external" link /></HyperLink>
                 </Header.Subheader>
             </Header.Content>
         </Header>
--- components/frontend/src/metric/MetricTypeHeader.test.js
+++ components/frontend/src/metric/MetricTypeHeader.test.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import { MetricTypeHeader } from "./MetricTypeHeader";
 
-function renderMetricTypeHeader(rationale, rationaleUrls, explanation, explanationUrls) {
+function renderMetricTypeHeader() {
     render(
         <MetricTypeHeader
             metricType={
                 {
                     name: "Metric type",
                     description: "Description",
-                    rationale: rationale,
-                    rationale_urls: rationaleUrls,
-                    explanation: explanation,
-                    explanation_urls: explanationUrls
                 }
             }
         />
@@ -23,28 +18,4 @@ function renderMetricTypeHeader(rationale, rationaleUrls, explanation, explanati
 it('shows the header', async () => {
     renderMetricTypeHeader();
     expect(screen.getAllByText("Metric type").length).toBe(1)
-});
-
-it('shows the rationale', async () => {
-    renderMetricTypeHeader("Rationale");
-    await userEvent.hover(screen.queryByRole("tooltip"))
-    await waitFor(() => { expect(screen.queryAllByText(/Rationale/).length).toBe(1) });
-});
-
-it('shows the rationale URLs', async () => {
-    renderMetricTypeHeader("Rationale", ["https://rationale"]);
-    await userEvent.hover(screen.queryByRole("tooltip"))
-    await waitFor(() => { expect(screen.queryAllByText(/https:\/\/rationale/).length).toBe(1) });
-});
-
-it('shows the explanation', async () => {
-    renderMetricTypeHeader("", [], "Explanation.");
-    await userEvent.hover(screen.queryByRole("tooltip"))
-    await waitFor(() => { expect(screen.queryAllByText(/Explanation/).length).toBe(1) });
-});
-
-it('shows the explanation URLs', async () => {
-    renderMetricTypeHeader("", [], "Explanation.", ["https://explanation"]);
-    await userEvent.hover(screen.queryByRole("tooltip"))
-    await waitFor(() => { expect(screen.queryAllByText(/https:\/\/explanation/).length).toBe(1) });
 });
