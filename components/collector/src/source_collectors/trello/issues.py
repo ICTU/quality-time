914a6dd1fb34ddde8fcef706a6fe6d8d7332257a
wkoot <3715211+wkoot@users.noreply.github.com>
2023-01-24 21:43:19 +0100

    Update TrelloIssues _include_entity

--- components/collector/src/source_collectors/trello/issues.py
+++ components/collector/src/source_collectors/trello/issues.py
@@ -19,30 +19,31 @@ class TrelloIssues(TrelloBase):
         json = await responses[0].json()
         cards = json["cards"]
         lists = {lst["id"]: lst["name"] for lst in json["lists"]}
-        return Entities(self.__card_to_entity(card, lists) for card in cards if not self.__ignore_card(card, lists))
+        return Entities(self.__card_to_entity(card, lists) for card in cards)
 
-    def __ignore_card(self, card, lists) -> bool:
-        """Return whether the card should be ignored."""
+    def _include_entity(self, entity: Entity) -> bool:
+        """Return whether the card should be included."""
 
         def card_is_inactive() -> bool:
             """Return whether the card is inactive."""
-            date_last_activity = parse(card["dateLastActivity"])
+            date_last_activity = parse(entity["date_last_activity"])
             return days_ago(date_last_activity) > int(cast(int, self._parameter("inactive_days")))
 
         def card_is_overdue() -> bool:
             """Return whether the card is overdue."""
-            due_date = parse(card["due"]) if card.get("due") else datetime.max
+            due_date = parse(entity["due_date"]) if entity["due_date"] else datetime.max
             return due_date < datetime.now(tz=due_date.tzinfo)
 
         lists_to_ignore = self._parameter("lists_to_ignore")
-        if card["idList"] in lists_to_ignore or lists[card["idList"]] in lists_to_ignore:
-            return True
-        cards_to_count = self._parameter("cards_to_count")
+        if entity["id_list"] in lists_to_ignore or entity["list"] in lists_to_ignore:
+            return False
+
+        cards_to_count = self._parameter("cards_to_count")  # this parameter is not inclusive; it is "if and only if"
         if "overdue" in cards_to_count and card_is_overdue():
-            return False
+            return True
         if "inactive" in cards_to_count and card_is_inactive():
-            return False
-        return bool(cards_to_count)
+            return True
+        return not bool(cards_to_count)  # only include if the parameter is empty, i.e. when selecting "all cards"
 
     @staticmethod
     def __card_to_entity(card, lists) -> Entity:
@@ -51,7 +52,8 @@ class TrelloIssues(TrelloBase):
             key=card["id"],
             title=card["name"],
             url=card["url"],
+            id_list=card["idList"],
             list=lists[card["idList"]],
-            due_date=card["due"],
+            due_date=card.get("due", ""),
             date_last_activity=card["dateLastActivity"],
         )
