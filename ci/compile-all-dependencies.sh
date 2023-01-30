56d1259051b1d8d7109d75fe52ba0b2441ce5621
Frank Niessink <frank@niessink.com>
2023-01-30 14:11:23 +0100

    Bump types-ldap3, puppeteer, react-focus-lock, gevent, sbom-action.

---
+++ ci/compile-all-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+# Update the compiled requirements and package-lock files
+(cd components/shared_data_model; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/shared_server_code; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/shared_server_code; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/external_server; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/internal_server; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/collector; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/notifier; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd components/frontend; rm package-lock.json; npm install) &
+(cd components/renderer; rm package-lock.json; npm install) &
+(cd docs; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd release; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd tests/application_tests; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+(cd tests/feature_tests; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh) &
+wait
