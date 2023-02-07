9285f030c570b083a700a333574b2ba439b7acba
wkoot <3715211+wkoot@users.noreply.github.com>
2023-02-07 22:17:00 +0100

    Update GitLabJobsBase _include_entity

--- components/collector/src/source_collectors/gitlab/job_runs_within_time_period.py
+++ components/collector/src/source_collectors/gitlab/job_runs_within_time_period.py
@@ -1,13 +1,12 @@
 """GitLab job runs within time period collector."""
 
-from collections.abc import Sequence
 from typing import cast
 
 from dateutil.parser import parse
 
 from collector_utilities.functions import days_ago
 from collector_utilities.type import Job
-from model import SourceResponses
+from model import SourceResponses, Entity
 
 from .base import GitLabJobsBase
 
@@ -15,15 +14,16 @@ from .base import GitLabJobsBase
 class GitLabJobRunsWithinTimePeriod(GitLabJobsBase):
     """Collector class to measure the number of GitLab CI builds run within a specified time period."""
 
-    async def _jobs(self, responses: SourceResponses) -> Sequence[Job]:
+    @staticmethod
+    async def _jobs(responses: SourceResponses) -> list[Job]:
         """Return the jobs to count, not deduplicated to latest branch or tag run."""
         jobs: list[Job] = []
         for response in responses:
             jobs.extend(list(await response.json()))
-        return [job for job in jobs if self._count_job(job)]
+        return jobs
 
-    def _count_job(self, job: Job) -> bool:
+    def _include_entity(self, entity: Entity) -> bool:
         """Return whether the job was run within the specified time period."""
-        within_time_period = days_ago(parse(job["created_at"])) <= int(
+        within_time_period = days_ago(parse(entity["build_date"])) <= int(
             cast(str, self._parameter("lookback_days")))
-        return within_time_period and super()._count_job(job)
+        return within_time_period and super()._include_entity(entity)
--- components/collector/src/source_collectors/gitlab/unused_jobs.py
+++ components/collector/src/source_collectors/gitlab/unused_jobs.py
@@ -5,7 +5,7 @@ from typing import cast
 from dateutil.parser import parse
 
 from collector_utilities.functions import days_ago
-from collector_utilities.type import Job
+from model import Entity
 
 from .base import GitLabJobsBase
 
@@ -13,7 +13,7 @@ from .base import GitLabJobsBase
 class GitLabUnusedJobs(GitLabJobsBase):
     """Collector class to get unused job counts from GitLab."""
 
-    def _count_job(self, job: Job) -> bool:
+    def _include_entity(self, entity: Entity) -> bool:
         """Return whether the job is unused."""
         max_days = int(cast(str, self._parameter("inactive_job_days")))
-        return super()._count_job(job) and days_ago(parse(job["created_at"])) > max_days
+        return super()._include_entity(entity) and days_ago(parse(entity["build_date"])) > max_days
