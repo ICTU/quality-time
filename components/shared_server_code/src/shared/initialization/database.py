a1e07caf20db3d713663ef5467db48825b379d97
Peter de Ruijter <ptrr@pm.me>
2023-03-16 12:31:42 +0100

    Changes the database code so that it's more flexible

--- components/shared_server_code/src/shared/initialization/database.py
+++ components/shared_server_code/src/shared/initialization/database.py
@@ -11,19 +11,39 @@ from shared.initialization.report import import_example_reports, initialize_repo
 from shared.initialization.secrets import initialize_secrets
 
 
+def get_client() -> pymongo.MongoClient:
+    database_url = os.environ.get(
+        "DATABASE_URL", "mongodb://root:root@localhost:27017")
+    return pymongo.MongoClient(database_url)
+
+
+def get_database(client: pymongo.MongoClient = get_client()) -> Database:
+    """ Get the Quality Time database connection.
+
+    Returns:
+        Database: pointer to the Quality Time database.
+    """
+
+    set_feature_compatibility_version(client.admin)
+
+    return client.quality_time_db
+
 # For some reason the init_database() function gets reported as partially uncovered by the feature tests. Ignore.
 
 
 def init_database() -> Database:  # pragma: no feature-test-cover
-    """Initialize the database connection and contents."""
-    database_url = os.environ.get("DATABASE_URL", "mongodb://root:root@localhost:27017")
-    client: pymongo.MongoClient = pymongo.MongoClient(database_url)
-    set_feature_compatibility_version(client.admin)
-    database = client.quality_time_db
+    """Initialize the database, create indexes, import the datamodel and import example reports.
+
+    Returns:
+        Database: Pointer to the Quality Time database.
+    """
+    database = get_database()
+
     logging.info("Connected to database: %s", database)
     nr_reports = database.reports.count_documents({})
     nr_measurements = database.measurements.count_documents({})
-    logging.info("Database has %d report documents and %d measurement documents", nr_reports, nr_measurements)
+    logging.info("Database has %d report documents and %d measurement documents",
+                 nr_reports, nr_measurements)
     create_indexes(database)
     import_datamodel(database)
     initialize_secrets(database)
@@ -47,8 +67,11 @@ def create_indexes(database: Database) -> None:
     database.reports.create_index("timestamp")
     database.users.create_index("username", unique=True)
     start_index = pymongo.IndexModel([("start", pymongo.ASCENDING)])
-    latest_measurement_index = pymongo.IndexModel([("metric_uuid", pymongo.ASCENDING), ("start", pymongo.DESCENDING)])
+    latest_measurement_index = pymongo.IndexModel(
+        [("metric_uuid", pymongo.ASCENDING), ("start", pymongo.DESCENDING)])
     latest_successful_measurement_index = pymongo.IndexModel(
-        [("metric_uuid", pymongo.ASCENDING), ("has_error", pymongo.ASCENDING), ("start", pymongo.DESCENDING)]
+        [("metric_uuid", pymongo.ASCENDING), ("has_error",
+                                              pymongo.ASCENDING), ("start", pymongo.DESCENDING)]
     )
-    database.measurements.create_indexes([start_index, latest_measurement_index, latest_successful_measurement_index])
+    database.measurements.create_indexes(
+        [start_index, latest_measurement_index, latest_successful_measurement_index])
