2ed404ac472aca0f5c9fba09cfc18617b9842171
Frank Niessink <frank@niessink.com>
2023-06-19 14:04:52 +0200

    Update puppeteer, markdownlint-cli, and indirect dependencies.

--- ci/update-all-dependencies.sh
+++ ci/update-all-dependencies.sh
@@ -9,6 +9,7 @@ set -e
 (cd components/notifier; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh; ci/pip-install.sh) &
 (cd components/frontend; rm package-lock.json; npm install) &
 (cd components/renderer; rm package-lock.json; npm install) &
+(cd docs; rm package-lock.json; npm install) &
 (cd docs; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh; ci/pip-install.sh) &
 (cd release; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh; ci/pip-install.sh) &
 (cd tests/application_tests; PATH="$(pwd)/venv/bin:$PATH"; ci/pip-compile.sh; ci/pip-install.sh) &
