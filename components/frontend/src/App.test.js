fdb2d69241a2743b67a7ba2a206b117323d5c9db
Frank Niessink <frank@niessink.com>
2023-07-03 09:47:31 +0200

    Show warning when user session expires. Show warning if session has expired while user was away. Closes #5327.

--- components/frontend/src/App.test.js
+++ components/frontend/src/App.test.js
@@ -4,19 +4,9 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 import * as fetch_server_api from './api/fetch_server_api';
 
-jest.mock('./api/fetch_server_api', () => {
-    const originalModule = jest.requireActual('./api/fetch_server_api');
-
-    return {
-        __esModule: true,
-        ...originalModule,
-        fetch_server_api: jest.fn().mockResolvedValue({ ok: true, measurements: [] }),
-    };
-});
-
-function set_user_in_local_storage(session_expiration_datetime) {
+function set_user_in_local_storage(session_expiration_datetime, email) {
     localStorage.setItem("user", "admin");
-    localStorage.setItem("email", "admin@example.org");
+    localStorage.setItem("email", email ?? "admin@example.org");
     localStorage.setItem("session_expiration_datetime", session_expiration_datetime);
 }
 
@@ -45,6 +35,14 @@ it('sets the user from local storage', () => {
     set_user_in_local_storage("3000-02-23T22:00:50.945Z");
     render(<App />);
     expect(screen.getAllByText(/admin/).length).toBe(1)
+    expect(screen.getAllByAltText(/Avatar/).length).toBe(1)
+});
+
+it('does not set invalid email addresses', () => {
+    set_user_in_local_storage("3000-02-23T22:00:50.945Z", "admin at example.org");
+    render(<App />);
+    expect(screen.getAllByText(/admin/).length).toBe(1)
+    expect(screen.queryAllByAltText(/Avatar/).length).toBe(0)
 });
 
 it('resets the user when the session is expired on mount', () => {
@@ -52,6 +50,14 @@ it('resets the user when the session is expired on mount', () => {
     render(<App />);
     expect(screen.queryAllByText(/admin/).length).toBe(0)
 });
+
+it('resets the user when the user clicks logout', async () => {
+    set_user_in_local_storage("3000-02-23T22:00:50.945Z");
+    render(<App />);
+    await act(async () => { fireEvent.click(screen.getByText(/admin/)) })
+    await act(async () => { fireEvent.click(screen.getByText(/Logout/)) })
+    expect(screen.queryAllByText(/admin/).length).toBe(0)
+})
 
 it('handles a date change', async () => {
     fetch_server_api.fetch_server_api = jest.fn().mockReturnValue({ then: jest.fn().mockReturnValue({ finally: jest.fn() }) });
