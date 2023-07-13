f341f96792390d1f8cd0c89256a8287ea23207f7
Frank Niessink <frank@niessink.com>
2023-07-13 15:14:58 +0200

    Use another third-party package for the data picker. Fixes #5466.

--- components/frontend/src/fields/DateInput.js
+++ components/frontend/src/fields/DateInput.js
@@ -1,31 +1,35 @@
-import React, { useContext } from 'react';
-import { Form } from '../semantic_ui_react_wrappers';
-import SemanticDatepicker from 'react-semantic-ui-datepickers';
-import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
+import React from 'react';
+import { Icon, Label, Form } from '../semantic_ui_react_wrappers';
+import { DatePicker } from '../widgets/DatePicker';
 import { ReadOnlyOrEditable } from '../context/Permissions';
 import { ReadOnlyInput } from './ReadOnlyInput';
-import { DarkMode } from '../context/DarkMode';
+import { toISODateStringInCurrentTZ } from '../utils';
 import './DateInput.css';
 
 function EditableDateInput({ ariaLabelledBy, label, placeholder, required, set_value, value }) {
     value = value ? new Date(value) : null
     return (
-        <SemanticDatepicker
+        <Form.Input
             aria-labelledby={ariaLabelledBy}
-            clearable={!required}
             error={(required && !value)}
-            inverted={useContext(DarkMode)}
             label={label}
-            onChange={(event, { value: newDate }) => {
-                let dateValue = null
-                if (!!newDate) {
-                    dateValue = `${String(newDate.getFullYear())}-${String(newDate.getMonth() + 1).padStart(2, '0')}-${String(newDate.getDate()).padStart(2, '0')}`;
-                }
-                set_value(dateValue)
-            }}
-            placeholder={placeholder}
-            value={value}
-        />
+            labelPosition="left"
+            required={required}
+        >
+            <Label><Icon fitted name="calendar" /></Label>
+            <DatePicker
+                selected={value}
+                isClearable={!required}
+                onChange={(newDate) => {
+                    let dateValue = null
+                    if (newDate !== null) {
+                        dateValue = toISODateStringInCurrentTZ(newDate)
+                    }
+                    set_value(dateValue)
+                }}
+                placeholderText={placeholder}
+            />
+        </Form.Input>
     )
 }
 
--- components/frontend/src/widgets/DatePicker.js
+++ components/frontend/src/widgets/DatePicker.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import SemanticDatepicker from 'react-semantic-ui-datepickers';
-import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
+import { default as ReactDatePicker } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { isValidDate_YYYYMMDD } from '../utils';
+import "./DatePicker.css";
 
 export function DatePicker(props) {
+    const { onChange, ...otherProps } = props
     return (
-        <SemanticDatepicker
-            filterDate={(date) => {return (date.getTime() < (new Date()).getTime())}}
-            inverted
-            onChange={props.onDate}
-            value={props.value}
+        <ReactDatePicker
+            dateFormat="yyyy-MM-dd"
+            dropdownMode="select"
+            onChange={(date) => { if (date === null) { onChange(null) } }}  // See https://github.com/Hacker0x01/react-datepicker/discussions/3636
+            onChangeRaw={(event) => { if (isValidDate_YYYYMMDD(event.target.value)) { onChange(new Date(event.target.value)) } }}
+            onSelect={onChange}
+            placeholderText="YYYY-MM-DD"
+            showIcon={false}
+            showMonthDropdown
+            showPopperArrow={false}
+            showYearDropdown
+            todayButton="Today"
+            {...otherProps}
         />
     )
 }
