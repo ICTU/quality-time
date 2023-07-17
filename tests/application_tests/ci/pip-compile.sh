700b1825487e8c0ea0612aa1ef6d7a12cd86e733
Frank Niessink <frank@niessink.com>
2023-07-17 10:33:41 +0200

    Bump pip-tools from 6.14.0 to 7.0.0.

--- tests/application_tests/ci/pip-compile.sh
+++ tests/application_tests/ci/pip-compile.sh
@@ -13,5 +13,5 @@ run () {
 # Use --strip-extras to prevent:
 # ERROR: In --require-hashes mode, all requirements must have their versions pinned with ==. These do not:
 #    urllib3<3,>=1.21.1 from https://files.pythonhosted.org/packages/4b/1d/f8383ef593114755429c307449e7717b87044b3bcd5f7860b89b1f759e34/urllib3-2.0.2-py3-none-any.whl (from requests==2.30.0->-r requirements/requirements.txt (line 123))
-run pip-compile --strip-extras --allow-unsafe --generate-hashes --quiet --upgrade --resolver=backtracking --output-file requirements/requirements.txt pyproject.toml
-run pip-compile --strip-extras --allow-unsafe --generate-hashes --quiet --upgrade --resolver=backtracking --extra dev --output-file requirements/requirements-dev.txt pyproject.toml
+run pip-compile --strip-extras --allow-unsafe --generate-hashes --quiet --upgrade --output-file requirements/requirements.txt pyproject.toml
+run pip-compile --strip-extras --allow-unsafe --generate-hashes --quiet --upgrade --extra dev --output-file requirements/requirements-dev.txt pyproject.toml
