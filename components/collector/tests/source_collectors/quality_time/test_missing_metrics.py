9593ce7ef0adc81e56b99c3fd6f98f95924c1ba1
Frank Niessink <frank@niessink.com>
2023-09-04 18:09:04 +0200

    Bump several dependencies.

--- components/collector/tests/source_collectors/quality_time/test_missing_metrics.py
+++ components/collector/tests/source_collectors/quality_time/test_missing_metrics.py
@@ -59,7 +59,7 @@ class QualityTimeMissingMetricsTest(QualityTimeTestCase):
             for subject_uuid, subject in report.get("subjects", {}).items():
                 for metric_type in self.data_model["subjects"]["software"]["metrics"]:
                     if metric_type not in ["violations", "accessibility", "loc"]:
-                        self.entities.append(  # noqa: PERF401
+                        self.entities.append(
                             {
                                 "key": f"{report['report_uuid']}:{subject_uuid}:{metric_type}",
                                 "report": report["title"],
