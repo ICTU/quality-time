159eb6335a75428579bfd319129bc5cd85113ca3
Frank Niessink <frank@niessink.com>
2023-11-24 15:19:15 +0100

    Newly added subjects would not be visible. Fixes #7552.

--- components/frontend/src/subject/Subject.test.js
+++ components/frontend/src/subject/Subject.test.js
@@ -9,6 +9,7 @@ function renderSubject(
         atReportsOverview = false,
         dates = [new Date()],
         reportDate = null,
+        reportToRender = null,
     } = {}
 ) {
     const settings = createTestableSettings()
@@ -19,7 +20,7 @@ function renderSubject(
                 dates={dates}
                 handleSort={() => jest.fn()}
                 measurements={[]}
-                report={report}
+                report={reportToRender || report}
                 report_date={reportDate}
                 settings={settings}
                 subject_uuid="subject_uuid"
@@ -52,6 +53,27 @@ it('hides metrics not requiring action', async () => {
 it('hides the subject if all metrics are hidden', async () => {
     history.push("?hidden_tags=tag,other tag")
     renderSubject();
+    expect(screen.queryAllByText("Subject 1 title").length).toBe(0);
+})
+
+const reportWithEmptySubject = {
+    reportToRender: { subjects: { subject_uuid: { name: "Subject 1 title", metrics: {}, type: "subject_type" } } }
+}
+
+it('does not hide an empty subject if no metrics are hidden', async () => {
+    renderSubject(reportWithEmptySubject);
+    expect(screen.queryAllByText("Subject 1 title").length).toBe(1);
+})
+
+it('hides an empty subject if metrics that require action are hidden', async () => {
+    history.push("?metrics_to_hide=no_action_needed")
+    renderSubject(reportWithEmptySubject);
+    expect(screen.queryAllByText("Subject 1 title").length).toBe(0);
+})
+
+it('hides an empty subject if metrics with tags are hidden', async () => {
+    history.push("?hidden_tags=tag,other tag")
+    renderSubject(reportWithEmptySubject);
     expect(screen.queryAllByText("Subject 1 title").length).toBe(0);
 })
 
