233fa0b77b734125455b447d57796e59a2fbd871
Frank Niessink <frank@niessink.com>
2023-11-24 16:39:07 +0100

    Replace  by .

--- components/collector/ci/quality.sh
+++ components/collector/ci/quality.sh
@@ -3,7 +3,8 @@
 source ../../ci/base.sh
 
 # Ruff
-run pipx run `spec ruff` .
+run pipx run `spec ruff` check .
+run pipx run `spec ruff` format --check .
 
 # Mypy
 run pipx run `spec mypy` --python-executable=$(which python) src tests
@@ -23,6 +24,3 @@ run pipx run `spec bandit` --quiet --recursive src/
 # Vulture
 NAMES_TO_IGNORE='Anchore*,Axe*,AzureDevops*,Bandit*,Calendar*,CargoAudit*,Cloc*,Cobertura*,Composer*,CxSAST*,Gatling*,Generic*,GitLab*,Harbor*,Jacoco*,Jenkins*,Jira*,JMeter*,JUnit*,ManualNumber*,NCover*,Npm*,OJAudit*,OpenVAS*,OWASPDependencyCheck*,OWASPZAP*,PerformanceTestRunner*,Pip*,PyupioSafety*,QualityTime*,RobotFramework*,SARIF*,Snyk*,SonarQube*,Trello*,TrivyJSON*'
 run pipx run `spec vulture` --min-confidence 0 --ignore-names $NAMES_TO_IGNORE src/ tests/ .vulture_ignore_list.py
-
-# Black
-run pipx run `spec black` --check src tests
--- components/shared_code/ci/quality.sh
+++ components/shared_code/ci/quality.sh
@@ -3,7 +3,8 @@
 source ../../ci/base.sh
 
 # Ruff
-run pipx run `spec ruff` .
+run pipx run `spec ruff` check .
+run pipx run `spec ruff` format --check .
 
 # Mypy
 # pipx run can't be used because mypy needs the pydantic plugin to be installed in the same venv (using pipx inject)
@@ -24,6 +25,3 @@ run pipx run `spec bandit` --quiet --recursive src/
 
 # Vulture
 run pipx run `spec vulture` --min-confidence 0 src/ tests/ .vulture_ignore_list.py
-
-# Black
-run pipx run `spec black` --check src tests
