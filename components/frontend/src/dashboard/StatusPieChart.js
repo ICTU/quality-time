d47a009b61e8105568d408448d83480a58d5d2cb
Frank Niessink <frank@niessink.com>
2024-02-16 14:08:33 +0100

    In the popups in the dashboard, show the percentage next to the number of metrics with a specific status. Closes #7946.

--- components/frontend/src/dashboard/StatusPieChart.js
+++ components/frontend/src/dashboard/StatusPieChart.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { VictoryPie } from 'victory';
 import { pluralize, STATUSES, STATUS_COLORS, STATUS_NAME, sum } from '../utils';
 
 function nrMetricsLabel(nrMetrics) {
     return nrMetrics === 0 ? "No metrics" : nrMetrics + pluralize(" metric", nrMetrics)
+}
+nrMetricsLabel.PropTypes = {
+    nrMetrics: PropTypes.number
 }
 
 export function StatusPieChart({ animate, colors, label, tooltip, summary, style, maxY, width, height }) {
@@ -14,7 +18,8 @@ export function StatusPieChart({ animate, colors, label, tooltip, summary, style
     const innerRadius = maxInnerRadius - (maxInnerRadius - minInnerRadius) * (nrMetrics / maxY)
     const data = STATUSES.map((status) => {
         const y = summary[STATUS_COLORS[status]]
-        return { y: y, label: `${STATUS_NAME[status]}: ${nrMetricsLabel(y)}` }
+        const yPercentage = Math.round(y / nrMetrics * 100)
+        return { y: y, label: `${STATUS_NAME[status]}: ${nrMetricsLabel(y)} (${yPercentage}%)` }
     })
     return (
         <>
@@ -36,4 +41,15 @@ export function StatusPieChart({ animate, colors, label, tooltip, summary, style
             }
         </>
     )
+}
+StatusPieChart.propTypes = {
+    animate: PropTypes.object,
+    colors: PropTypes.arrayOf(PropTypes.string),
+    height: PropTypes.number,
+    label: PropTypes.object,
+    maxY: PropTypes.number,
+    style: PropTypes.object,
+    summary: PropTypes.object,
+    tooltip: PropTypes.element,
+    width: PropTypes.number,
 }
