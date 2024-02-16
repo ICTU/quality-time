49bab05d1c9aa6e3a6f91ec38fc0eb287abefcdf
Frank Niessink <frank@niessink.com>
2024-02-16 17:25:21 +0100

    If the documentation at Read-the-Docs has specific information on configuring a metric or source, point this out in Quality-time itself. Closes #4445.

--- components/frontend/src/metric/MetricTypeHeader.js
+++ components/frontend/src/metric/MetricTypeHeader.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import { Header } from '../semantic_ui_react_wrappers';
 import { HyperLink } from '../widgets/HyperLink';
+import { metricTypePropType } from '../sharedPropTypes';
 import { slugify } from '../utils';
 
 export function MetricTypeHeader({ metricType }) {
     const url = `https://quality-time.readthedocs.io/en/v${process.env.REACT_APP_VERSION}/reference.html${slugify(metricType.name)}`
+    const howToConfigure = metricType.documentation ? " for specific information on how to configure this metric type." : ""
     return (
         <Header>
             <Header.Content>
                 {metricType.name}
                 <Header.Subheader>
-                    {metricType.description} <HyperLink url={url}>Read the Docs <Icon name="external" link /></HyperLink>
+                    {metricType.description} <HyperLink url={url}>Read the Docs <Icon name="external" link /></HyperLink>{howToConfigure}
                 </Header.Subheader>
             </Header.Content>
         </Header>
     )
+}
+MetricTypeHeader.propTypes = {
+    metricType: metricTypePropType,
 }
--- docs/src/changelog.md
+++ docs/src/changelog.md
@@ -8,6 +8,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 <!-- The line "## <square-bracket>Unreleased</square-bracket>" is replaced by the release/release.py script with the new release version and release date. -->
 
+## [Unreleased]
+
+### Deployment notes
+
+If your currently installed *Quality-time* version is v4.10.0 or older, please read the v5.0.0 deployment notes first.
+
+### Added
+
+- If the documentation at Read-the-Docs has specific information on configuring a metric or source, point this out in Quality-time itself. Closes [#4445](https://github.com/ICTU/quality-time/issues/4445).
+
 ## v5.8.0 - 2024-02-16
 
 ### Deployment notes
