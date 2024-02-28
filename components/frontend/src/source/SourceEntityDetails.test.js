87a19cbd6e3d40d75f88a5475196b18fb4ad4240
Frank Niessink <frank@niessink.com>
2024-02-28 16:45:54 +0100

    Suppress "Warning: An update to t inside a test was not wrapped in act(...)." caused by interacting with the date picker.

--- components/frontend/src/source/SourceEntityDetails.test.js
+++ components/frontend/src/source/SourceEntityDetails.test.js
@@ -35,12 +35,17 @@ it('changes the entity status', () => {
 })
 
 it('changes the entity status end date', async () => {
+    // Suppress "Warning: An update to t inside a test was not wrapped in act(...)." caused by interacting with
+    // the date picker.
+    const consoleLog = console.log;
+    console.error = jest.fn();
     source.set_source_entity_attribute = jest.fn()
     renderSourceEntityDetails()
     await userEvent.type(screen.getByPlaceholderText(/YYYY-MM-DD/), '2222-01-01{Tab}', { initialSelectionStart: 0, initialSelectionEnd: 10 });
     expect(source.set_source_entity_attribute).toHaveBeenCalledWith(
         "metric_uuid", "source_uuid", "key", "status_end_date", "2222-01-01", reload
     );
+    console.log = consoleLog;
 })
 
 it('changes the rationale', async () => {
