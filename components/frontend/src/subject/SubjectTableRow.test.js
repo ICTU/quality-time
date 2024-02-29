ac26fea27a1cfeeda560d3de83bf9ffd295cbf52
Frank Niessink <frank@niessink.com>
2024-02-29 16:47:19 +0100

    Improve the delta column header popup and add popups to the delta values.

--- components/frontend/src/subject/SubjectTableRow.test.js
+++ components/frontend/src/subject/SubjectTableRow.test.js
@@ -58,6 +58,7 @@ function renderSubjectTableRow(
                                 recent_measurements: [],
                                 scale: scale,
                                 type: "metric_type",
+                                unit: "things",
                             }
                         }
                         metric_uuid="metric_uuid"
@@ -75,9 +76,9 @@ it('shows the delta column', () => {
     history.push("?nr_dates=3&date_interval=1")
     renderSubjectTableRow()
     expect(screen.getAllByText("+2").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value worsened by +2").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type worsened from 10 to 12 things by +2 things").length).toBe(1);
     expect(screen.getAllByText("-4").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value improved by -4").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type improved from 12 to 8 things by -4 things").length).toBe(1);
 })
 
 it('hides the delta column', () => {
@@ -90,34 +91,34 @@ it('takes the metric direction into account', () => {
     history.push("?nr_dates=3&date_interval=1")
     renderSubjectTableRow({ direction: ">" })
     expect(screen.getAllByText("+2").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value improved by +2").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type improved from 10 to 12 things by +2 things").length).toBe(1);
     expect(screen.getAllByText("-4").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value worsened by -4").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type worsened from 12 to 8 things by -4 things").length).toBe(1);
 })
 
 it('works for informative metrics', () => {
     history.push("?nr_dates=3&date_interval=1")
     renderSubjectTableRow({ evaluate_targets: false })
     expect(screen.getAllByText("+2").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value changed by +2").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type changed from 10 to 12 things by +2 things").length).toBe(1);
     expect(screen.getAllByText("-4").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value changed by -4").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type changed from 12 to 8 things by -4 things").length).toBe(1);
 })
 
 it('takes the date order into account', () => {
     history.push("?nr_dates=3&date_interval=1&date_order=ascending")
     renderSubjectTableRow({ ascending: true })
     expect(screen.getAllByText("+2").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value worsened by +2").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type worsened from 10 to 12 things by +2 things").length).toBe(1);
     expect(screen.getAllByText("-4").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value improved by -4").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type improved from 12 to 8 things by -4 things").length).toBe(1);
 })
 
 it('shows the delta column for the version scale', () => {
     history.push("?nr_dates=3&date_interval=1")
     renderSubjectTableRow({ scale: "version_number" })
     expect(screen.getAllByText("+").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value worsened").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type worsened from 1.0 to 1.2").length).toBe(1);
     expect(screen.getAllByText("-").length).toBe(1);
-    expect(screen.getAllByLabelText("The measurement value improved").length).toBe(1);
+    expect(screen.getAllByLabelText("Metric type improved from 1.2 to 0.8").length).toBe(1);
 })
