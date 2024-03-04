7f7225247b7e896374ee655206bffd1b63e2ee02
Frank Niessink <frank@niessink.com>
2024-03-04 15:00:13 +0100

    Refactor SubjectTableFooter to prepare for adding a summary row to the footer.

--- components/frontend/src/subject/SubjectTableFooter.js
+++ components/frontend/src/subject/SubjectTableFooter.js
@@ -9,47 +9,53 @@ import { metric_options } from "../widgets/menu_options";
 import { metricTypeOptions } from "../metric/MetricType";
 import { reportsPropType, subjectPropType } from '../sharedPropTypes';
 
-export function SubjectTableFooter({ subject, subjectUuid, reload, reports, stopFilteringAndSorting }) {
+function SubjectTableFooterButtonRow({ subject, subjectUuid, reload, reports, stopFilteringAndSorting }) {
     const dataModel = useContext(DataModel)
     return (
-        <ReadOnlyOrEditable requiredPermissions={[EDIT_REPORT_PERMISSION]} editableComponent={
-            <Table.Footer>
-                <Table.Row>
-                    <Table.HeaderCell colSpan='99'>
-                        <AddDropdownButton
-                            itemType="metric"
-                            itemSubtypes={metricTypeOptions(dataModel, subject.type)}
-                            onClick={(subtype) => {
-                                stopFilteringAndSorting()
-                                add_metric(subjectUuid, subtype, reload);
-                            }}
-                        />
-                        <CopyButton
-                            itemType="metric"
-                            onChange={(source_metric_uuid) => {
-                                stopFilteringAndSorting()
-                                copy_metric(source_metric_uuid, subjectUuid, reload);
-                            }}
-                            get_options={() => metric_options(reports, dataModel, subject.type)}
-                        />
-                        <MoveButton
-                            itemType="metric"
-                            onChange={(source_metric_uuid) => {
-                                stopFilteringAndSorting()
-                                move_metric(source_metric_uuid, subjectUuid, reload);
-                            }}
-                            get_options={() => metric_options(reports, dataModel, subject.type, subjectUuid)}
-                        />
-                    </Table.HeaderCell>
-                </Table.Row>
-            </Table.Footer>}
-        />
+        <Table.Row>
+            <Table.HeaderCell colSpan="99">
+                <AddDropdownButton
+                    itemType="metric"
+                    itemSubtypes={metricTypeOptions(dataModel, subject.type)}
+                    onClick={(subtype) => {
+                        stopFilteringAndSorting()
+                        add_metric(subjectUuid, subtype, reload);
+                    }}
+                />
+                <CopyButton
+                    itemType="metric"
+                    onChange={(source_metric_uuid) => {
+                        stopFilteringAndSorting()
+                        copy_metric(source_metric_uuid, subjectUuid, reload);
+                    }}
+                    get_options={() => metric_options(reports, dataModel, subject.type)}
+                />
+                <MoveButton
+                    itemType="metric"
+                    onChange={(source_metric_uuid) => {
+                        stopFilteringAndSorting()
+                        move_metric(source_metric_uuid, subjectUuid, reload);
+                    }}
+                    get_options={() => metric_options(reports, dataModel, subject.type, subjectUuid)}
+                />
+            </Table.HeaderCell>
+        </Table.Row>
     )
 }
-SubjectTableFooter.propTypes = {
+SubjectTableFooterButtonRow.propTypes = {
     subject: subjectPropType,
     subjectUuid: string,
     reload: func,
     reports: reportsPropType,
     stopFilteringAndSorting: func,
+}
+
+export function SubjectTableFooter(props) {
+    return (
+        <ReadOnlyOrEditable requiredPermissions={[EDIT_REPORT_PERMISSION]} editableComponent={
+            <Table.Footer>
+                <SubjectTableFooterButtonRow {...props} />
+            </Table.Footer>}
+        />
+    )
 }
