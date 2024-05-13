08c9f8115798016974fee1877d4a7e4ef86d6783
Frank Niessink <frank@niessink.com>
2024-05-13 09:20:54 +0200

    Ignore CVE-2018-20225 reported by Safety
    Vulnerability ID: 67599
    ADVISORY: ** DISPUTED ** An issue was discovered in pip (all versions) because it installs the version with the
    highest version number, even if the user had intended to obtain a private package from a private index. This only
    affects use of the --extra-index-url option, and exploitation requires that the...
    CVE-2018-20225
    For more information about this vulnerability, visit https://data.safetycli.com/v/67599/97c

--- components/api_server/ci/quality.sh
+++ components/api_server/ci/quality.sh
@@ -15,7 +15,13 @@ run pipx run `spec pip-audit` --strict --progress-spinner=off -r requirements/re
 export PYTHONDEVMODE=1
 
 # Safety
-run pipx run `spec safety` check --bare -r requirements/requirements.txt -r requirements/requirements-dev.txt
+# Vulnerability ID: 67599
+# ADVISORY: ** DISPUTED ** An issue was discovered in pip (all versions) because it installs the version with the
+# highest version number, even if the user had intended to obtain a private package from a private index. This only
+# affects use of the --extra-index-url option, and exploitation requires that the...
+# CVE-2018-20225
+# For more information about this vulnerability, visit https://data.safetycli.com/v/67599/97c
+run pipx run `spec safety` check --bare --ignore 67599 -r requirements/requirements.txt -r requirements/requirements-dev.txt
 
 # Bandit
 run pipx run `spec bandit` --quiet --recursive src/
