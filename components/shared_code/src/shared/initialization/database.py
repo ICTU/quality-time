b0b5df04097796da1d430a7d7962522f539d8ca1
wkoot <3715211+wkoot@users.noreply.github.com>
2024-05-16 12:48:59 +0200

    Allow specifying partial database connection parameters (#8669)
    Closes #8668

--- components/shared_code/src/shared/initialization/database.py
+++ components/shared_code/src/shared/initialization/database.py
@@ -5,12 +5,17 @@ import os
 import pymongo
 from pymongo import database
 
-DEFAULT_DATABASE_URL = "mongodb://root:root@localhost:27017"
-
 
 def client() -> pymongo.MongoClient:  # pragma: no feature-test-cover
     """Return a pymongo client."""
-    database_url = os.environ.get("DATABASE_URL", DEFAULT_DATABASE_URL)
+    database_url = os.environ.get("DATABASE_URL")
+    if not database_url:
+        db_user = os.environ.get("DATABASE_USERNAME", "root")
+        db_pass = os.environ.get("DATABASE_PASSWORD", "root")
+        db_host = os.environ.get("DATABASE_HOST", "localhost")
+        db_port = os.environ.get("DATABASE_PORT", "27017")
+        database_url = f"mongodb://{db_user}:{db_pass}@{db_host}:{db_port}"
+
     return pymongo.MongoClient(database_url)
 
 
