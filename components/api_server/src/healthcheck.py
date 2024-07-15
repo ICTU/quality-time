ff0b222cb8f7ad3b424f310f28c0b95cab50b517
Frank Niessink <frank@niessink.com>
2024-07-15 11:30:12 +0200

    Update several dependencies.

--- components/api_server/src/healthcheck.py
+++ components/api_server/src/healthcheck.py
@@ -8,7 +8,7 @@ import sys
 api_server_port = getenv("API_SERVER_PORT", "5001")
 exit_code = 1
 try:
-    with urlopen(f"http://localhost:{api_server_port}/api/health") as response:  # nosec B310 # noqa: S310
+    with urlopen(f"http://localhost:{api_server_port}/api/health") as response:  # nosec B310
         exit_code = 0 if response.status == HTTPStatus.OK else 1
 finally:
     sys.exit(exit_code)
--- components/frontend/src/widgets/WarningMessage.js
+++ components/frontend/src/widgets/WarningMessage.js
@@ -5,7 +5,7 @@ import { Message } from "../semantic_ui_react_wrappers"
 export function WarningMessage(props) {
     // Show a warning message if showIf is true or undefined
     const { showIf, ...messageProps } = props
-    return showIf ?? true ? <Message warning {...messageProps} /> : null
+    return (showIf ?? true) ? <Message warning {...messageProps} /> : null
 }
 WarningMessage.propTypes = {
     showIf: bool,
