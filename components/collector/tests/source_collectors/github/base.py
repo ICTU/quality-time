d24fc68fd600119022ce00e50e2ac3bc9dfadb50
termeczky <tobias.termeczky@the-experts.nl>
2024-07-26 10:41:51 +0200

    Added unit test for GitHub Merge Request collector

---
+++ components/collector/tests/source_collectors/github/base.py
@@ -0,0 +1,16 @@
+"""GitHub unit test base classes."""
+
+from tests.source_collectors.source_collector_test_case import SourceCollectorTestCase
+
+
+class GitHubTestCase(SourceCollectorTestCase):
+    """Base class for testing GitHub collectors."""
+
+    SOURCE_TYPE = "github"
+
+    def setUp(self):
+        """Extend to add generic test fixtures."""
+        super().setUp()
+        self.set_source_parameter("branch", "branch")
+        self.set_source_parameter("owner", "owner")
+        self.set_source_parameter("repository", "repository")
