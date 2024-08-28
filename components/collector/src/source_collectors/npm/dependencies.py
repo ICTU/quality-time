e4614c41dd85dc27a58cc71c6161427d77d01d42
Frank Niessink <frank@niessink.com>
2024-08-28 16:17:34 +0200

    Don't fail on parsing npm outdated output with multiple dependents per dependency.
    Fixes #9566.

--- components/collector/src/source_collectors/npm/dependencies.py
+++ components/collector/src/source_collectors/npm/dependencies.py
@@ -1,10 +1,20 @@
 """npm dependencies collector."""
 
-from typing import cast
+from typing import TypedDict, cast
 
 from base_collectors import JSONFileSourceCollector
 from collector_utilities.type import JSON, JSONDict
 from model import Entities, Entity
+
+
+class DependencyVersionInfo(TypedDict):
+    """Dependency version information."""
+
+    current: str  # Current version
+    dependent: str  # Dependent component, i.e. user of the dependency
+    latest: str  # Latest version
+    location: str  # Location of the package
+    wanted: str  # Wanted version
 
 
 class NpmDependencies(JSONFileSourceCollector):
@@ -12,13 +22,24 @@ class NpmDependencies(JSONFileSourceCollector):
 
     def _parse_json(self, json: JSON, filename: str) -> Entities:
         """Override to parse the dependencies from the JSON."""
-        return Entities(
-            Entity(
-                key=f'{dependency}@{versions.get("current", "?")}',
-                name=dependency,
-                current=versions.get("current", "unknown"),
-                wanted=versions.get("wanted", "unknown"),
-                latest=versions.get("latest", "unknown"),
-            )
-            for dependency, versions in cast(JSONDict, json).items()
+        entities = Entities()
+        for dependency, version_info in cast(JSONDict, json).items():
+            versions = version_info if isinstance(version_info, list) else [version_info]
+            for version in versions:
+                entities.append(self._create_entity(dependency, version))
+        return entities
+
+    def _create_entity(self, dependency: str, version: DependencyVersionInfo) -> Entity:
+        """Create an entity for the dependency."""
+        key = f'{dependency}@{version.get("current", "?")}'
+        name = dependency
+        if "dependent" in version:
+            key += f"@{version['dependent']}"
+            name += f" ({version['dependent']})"
+        return Entity(
+            key=key,
+            name=name,
+            current=version.get("current", "unknown"),
+            wanted=version.get("wanted", "unknown"),
+            latest=version.get("latest", "unknown"),
         )
--- components/collector/tests/source_collectors/npm/test_dependencies.py
+++ components/collector/tests/source_collectors/npm/test_dependencies.py
@@ -27,3 +27,30 @@ class NpmDependenciesTest(SourceCollectorTestCase):
         ]
         response = await self.collect(get_request_json_return_value=npm_json)
         self.assert_measurement(response, value="2", total="100", entities=expected_entities)
+
+    async def test_multiple_dependents_per_dependency(self):
+        """Test that the number of dependencies is returned if a dependency has multiple dependents."""
+        npm_json = {
+            "@testing-library/jest-dom": [
+                {"current": "6.4.6", "wanted": "6.5.0", "latest": "6.5.0", "dependent": "foo", "location": ""},
+                {"current": "6.4.6", "wanted": "6.5.0", "latest": "6.5.0", "dependent": "bar", "location": ""},
+            ]
+        }
+        expected_entities = [
+            {
+                "key": "@testing-library-jest-dom@6_4_6@foo",
+                "name": "@testing-library/jest-dom (foo)",
+                "current": "6.4.6",
+                "wanted": "6.5.0",
+                "latest": "6.5.0",
+            },
+            {
+                "key": "@testing-library-jest-dom@6_4_6@bar",
+                "name": "@testing-library/jest-dom (bar)",
+                "current": "6.4.6",
+                "wanted": "6.5.0",
+                "latest": "6.5.0",
+            },
+        ]
+        response = await self.collect(get_request_json_return_value=npm_json)
+        self.assert_measurement(response, value="2", total="100", entities=expected_entities)
