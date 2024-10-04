1b9be9ff5858db7945d1479c5d2fb6a2939ab619
Frank Niessink <frank@niessink.com>
2024-10-04 18:00:47 +0200

    Use uv.

--- ci/pip-base.sh
+++ ci/pip-base.sh
@@ -3,15 +3,15 @@
 source base.sh
 
 # Insert a custom compile command in generated requirements file so it is clear how they are generated:
-export CUSTOM_COMPILE_COMMAND="ci/pip-compile.sh"
+export UV_CUSTOM_COMPILE_COMMAND="ci/pip-compile.sh"
 
 run_pip_compile() {
-    for requirements_file in $(python $(script_dir)/requirements_files.py); do
+    for requirements_file in $(.venv/bin/python $(script_dir)/requirements_files.py); do
         extra=$([[ "$requirements_file" == *"-dev"* ]] && echo "--extra dev" || echo "")
-        run pip-compile $extra --output-file $requirements_file pyproject.toml
+        run uv pip compile --quiet --generate-hashes $extra --output-file $requirements_file pyproject.toml
     done
 }
 
 run_pip_install() {
-    run pip install --ignore-installed --quiet --use-pep517 $@
+    run uv pip install --quiet $@
 }
--- ci/quality-base.sh
+++ ci/quality-base.sh
@@ -1,55 +1,54 @@
 #!/bin/bash
 
-source pipx-base.sh
+source uvx-base.sh
 
 python_files_and_folders() {
-    echo $(python $(script_dir)/python_files_and_folders.py)
+    echo $(.venv/bin/python $(script_dir)/python_files_and_folders.py)
 }
 
 run_ruff() {
     PYTHON_FILES_AND_FOLDERS=$(python_files_and_folders)
-    run_pipx ruff check $PYTHON_FILES_AND_FOLDERS
-    run_pipx ruff format --check $PYTHON_FILES_AND_FOLDERS
+    run_uvx ruff check $PYTHON_FILES_AND_FOLDERS
+    run_uvx ruff format --check $PYTHON_FILES_AND_FOLDERS
 }
 
 run_fixit() {
-    run_pipx fixit lint $(python_files_and_folders)
+    run_uvx fixit lint $(python_files_and_folders)
 }
 
 run_mypy() {
     PYTHON_FILES_AND_FOLDERS=$(python_files_and_folders)
     # Run mypy with or without pydantic plugin depending on whether pydantic is listed as dependency in the tools
     # section of the optional dependencies in the pyproject.toml file.
-    pydantic_spec=$(spec pydantic)
+    pydantic_spec=$(spec pydantic ==)
     if [[ "$pydantic_spec" == "" ]]; then
-        run_pipx mypy --python-executable=$(which python) $PYTHON_FILES_AND_FOLDERS
+        run_uvx mypy --python-executable=.venv/bin/python $PYTHON_FILES_AND_FOLDERS
     else
-        # To use the pydantic plugin, we need to first install mypy and then inject pydantic
-        run pipx install --force $(spec mypy) # --force works around this bug: https://github.com/pypa/pipx/issues/795
-        run pipx inject mypy $pydantic_spec
-        run $PIPX_BIN_DIR/mypy --python-executable=$(which python) $PYTHON_FILES_AND_FOLDERS
+        run uvx --with $pydantic_spec $(spec mypy @) --python-executable=.venv/bin/python $PYTHON_FILES_AND_FOLDERS
     fi
 }
 
 run_pyproject_fmt() {
-    run_pipx pyproject-fmt --check pyproject.toml
+    run_uvx pyproject-fmt --check pyproject.toml
 }
 
 run_bandit() {
-    run_pipx bandit --configfile pyproject.toml --quiet --recursive $(python_files_and_folders)
+    run_uvx bandit --configfile pyproject.toml --quiet --recursive $(python_files_and_folders)
 }
 
 run_pip_audit() {
-    run_pipx pip-audit --strict --progress-spinner=off $(python $(script_dir)/requirements_files.py "-r %s")
+    for requirements_file in $(python $(script_dir)/requirements_files.py); do
+        run_uvx pip-audit --disable-pip --progress-spinner=off --requirement $requirements_file
+    done
 }
 
 run_vulture() {
-    run_pipx vulture --min-confidence 0 $(python_files_and_folders) .vulture_ignore_list.py $@
+    run_uvx vulture --min-confidence 0 $(python_files_and_folders) .vulture_ignore_list.py $@
 }
 
 run_vale() {
-    run_pipx vale sync
-    run_pipx vale --no-wrap --glob "*.md" src
+    run_uvx vale sync
+    run_uvx vale --no-wrap --glob "*.md" src
 }
 
 run_markdownlint() {
