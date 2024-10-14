ad2e8d104c9676b550dcfa30dd4b183c491be46b
Frank Niessink <frank@niessink.com>
2024-10-14 15:12:32 +0200

    Finish migration to uv.
    - Remove pip-tools, pipx, and pip from the dev dependencies
    - Remove the pip-tools settings from the pyproject.toml files
    - Run uv pip compile with the --update flag as we did with pip compile
    - Update indirect dependencies

--- ci/pip-base.sh
+++ ci/pip-base.sh
@@ -8,7 +8,7 @@ export UV_CUSTOM_COMPILE_COMMAND="ci/pip-compile.sh"
 run_pip_compile() {
     for requirements_file in $(.venv/bin/python $(script_dir)/requirements_files.py); do
         extra=$([[ "$requirements_file" == *"-dev"* ]] && echo "--extra dev" || echo "")
-        run uv pip compile --quiet --generate-hashes $extra --output-file $requirements_file pyproject.toml
+        run uv pip compile --quiet --upgrade --generate-hashes $extra --output-file $requirements_file pyproject.toml
     done
 }
 
