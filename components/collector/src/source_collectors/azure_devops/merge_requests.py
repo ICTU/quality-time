86d2c8fb4fb8e654d1842895c8fcee89d46d6256
tguler <tunahan.guler@sogeti.com>
2025-01-21 11:38:11 +0100

    change in down- and upvotes

--- components/collector/src/source_collectors/azure_devops/merge_requests.py
+++ components/collector/src/source_collectors/azure_devops/merge_requests.py
@@ -76,9 +76,9 @@ class AzureDevopsMergeRequests(AzureDevopsRepositoryBase):
     @staticmethod
     def _downvotes(merge_request) -> int:
         """Return the number of downvotes the merge request has."""
-        return len([r for r in merge_request.get("reviewers", []) if r.get("vote", 0) < 0])
+        return len([r for r in merge_request.get("reviewers", []) if r.get("approved") is False])
 
     @staticmethod
     def _upvotes(merge_request) -> int:
         """Return the number of upvotes the merge request has."""
-        return len([r for r in merge_request.get("reviewers", []) if r.get("vote", 0) > 0])
+        return len([r for r in merge_request.get("reviewers", []) if r.get("approved") is True])
