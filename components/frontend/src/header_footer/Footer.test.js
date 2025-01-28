c4dcc8bcd1c2b814ba38c2e4a09b031afffa832c
Frank Niessink <frank@niessink.com>
2025-01-28 09:25:39 +0100

    Update several dependencies.

--- components/frontend/src/header_footer/Footer.test.js
+++ components/frontend/src/header_footer/Footer.test.js
@@ -6,13 +6,13 @@ import { Footer } from "./Footer"
 it("renders the report title when there is a report", async () => {
     const lastUpdate = new Date()
     const { container } = render(<Footer lastUpdate={lastUpdate} report={{ title: "Report title" }} />)
-    expect(screen.findByText("Report")).not.toBe(null)
+    expect(screen.findByText("Report title")).not.toBe(null)
     await expectNoAccessibilityViolations(container)
 })
 
 it("renders a quote when there is no report", async () => {
     const { container } = render(<Footer />)
-    expect(screen.queryByText("Report")).toBe(null)
+    expect(screen.queryByText(/Jez Humble|Johan Cruyff|DeMarco and Lister|Henry Ford|Robert M. Pirsig/)).not.toBe(null)
     await expectNoAccessibilityViolations(container)
 })
 
