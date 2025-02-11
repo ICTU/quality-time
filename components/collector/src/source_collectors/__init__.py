8f36720c29fc3a2324f8415f94cedc524ece8982
Frank Niessink <frank@niessink.com>
2025-02-11 08:33:24 +0100

    Update several dependencies.

--- components/collector/src/source_collectors/__init__.py
+++ components/collector/src/source_collectors/__init__.py
@@ -23,8 +23,8 @@ from .azure_devops.user_story_points import AzureDevopsUserStoryPoints
 from .bandit.security_warnings import BanditSecurityWarnings
 from .bandit.source_up_to_dateness import BanditSourceUpToDateness
 from .bitbucket.inactive_branches import BitbucketInactiveBranches
-from .calendar.source_up_to_dateness import CalendarSourceUpToDateness
-from .calendar.time_remaining import CalendarTimeRemaining
+from .calendar_date.source_up_to_dateness import CalendarSourceUpToDateness
+from .calendar_date.time_remaining import CalendarTimeRemaining
 from .cargo_audit.security_warnings import CargoAuditSecurityWarnings
 from .cloc.loc import ClocLOC
 from .cloc.source_version import ClocSourceVersion
