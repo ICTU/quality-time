9ecda5f9303dc8da64568373baa8110162116758
Frank Niessink <frank@niessink.com>
2025-02-24 09:58:58 +0100

    Update several dependencies.

--- ci/update-indirect-dependencies.sh
+++ ci/update-indirect-dependencies.sh
@@ -3,11 +3,12 @@
 set -e
 
 # Update the compiled requirements and package-lock files
+# Remove node_modules in the frontend component as a work-around for this npm bug: https://github.com/npm/cli/issues/4828
 (cd components/shared_code; ci/pip-compile.sh; ci/pip-install.sh) &
 (cd components/api_server; ci/pip-compile.sh; ci/pip-install.sh) &
 (cd components/collector; ci/pip-compile.sh; ci/pip-install.sh) &
 (cd components/notifier;  ci/pip-compile.sh; ci/pip-install.sh) &
-(cd components/frontend; rm -f package-lock.json; npm install --ignore-scripts) &
+(cd components/frontend; rm -f package-lock.json; rm -rf node_modules; npm install --ignore-scripts) &
 (cd components/renderer; rm -f package-lock.json; npm install --ignore-scripts) &
 (cd docs; rm -f package-lock.json; npm install --ignore-scripts) &
 (cd docs; ci/pip-compile.sh; ci/pip-install.sh) &
