8b2cd5a8e4d72e94d051e328ccd1016f8e73beea
Frank Niessink <frank@niessink.com>
2025-03-18 19:16:00 +0100

    Remove suppression of console logging messages.
    Remove suppression of console log messages in the frontend that have disappeared after upgrading to React 19:
    - components/frontend/src/index.js
    - components/frontend/src/setupTests.js
    
    Closes #8974.

--- components/frontend/src/index.jsx
+++ components/frontend/src/index.jsx
@@ -4,12 +4,5 @@ import { createRoot } from "react-dom/client"
 
 import App from "./App"
 
-const error = console.error
-console.error = (...args) => {
-    if (/Support for defaultProps will be removed from function components/.test(args[0])) return
-    if (/findDOMNode is deprecated and will be removed in the next major release./.test(args[0])) return
-    error(...args)
-}
-
 const root = createRoot(document.getElementById("root"))
 root.render(<App />)
