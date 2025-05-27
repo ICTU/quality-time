999ff2bf4c77cd2d1f8bb28dbd9ad9f6e37e0a41
Frank Niessink <frank@niessink.com>
2025-05-27 20:15:17 +0200

    Add panning and zooming to metric trend graphs
    Closes #3587.

--- components/frontend/src/metric/TrendGraph.jsx
+++ components/frontend/src/metric/TrendGraph.jsx
@@ -1,10 +1,18 @@
 import { useTheme } from "@mui/material"
-import { useContext } from "react"
-import { VictoryAxis, VictoryChart, VictoryLabel, VictoryLine, VictoryTheme } from "victory"
+import { useContext, useState } from "react"
+import {
+    VictoryAxis,
+    VictoryBrushContainer,
+    VictoryChart,
+    VictoryLabel,
+    VictoryLine,
+    VictoryTheme,
+    VictoryZoomContainer,
+} from "victory"
 
 import { DataModel } from "../context/DataModel"
 import { loadingPropType, measurementsPropType, metricPropType } from "../sharedPropTypes"
-import { capitalize, formatMetricScaleAndUnit, getMetricName, getMetricScale, niceNumber, scaledNumber } from "../utils"
+import { capitalize, formatMetricScaleAndUnit, getMetricName, getMetricScale, scaledNumber } from "../utils"
 import { LoadingPlaceHolder } from "../widgets/Placeholder"
 import { FailedToLoadMeasurementsWarningMessage, InfoMessage, WarningMessage } from "../widgets/WarningMessage"
 
@@ -16,10 +24,23 @@ function measurementAttributeAsNumber(metric, measurement, field, dataModel) {
 
 export function TrendGraph({ metric, measurements, loading }) {
     const dataModel = useContext(DataModel)
+    const [state, setState] = useState({})
+
+    function handleZoom(domain) {
+        setState({
+            selectedDomain: domain,
+        })
+    }
+
+    function handleBrush(domain) {
+        setState({ zoomDomain: domain })
+    }
+
     const color = useTheme().palette.text.secondary
     const bgcolor = useTheme().palette.background.secondary
     const fontFamily = useTheme().typography.fontFamily
     const chartHeight = 250
+    const chartWidth = 750
     const estimatedTotalChartHeight = chartHeight + 200 // Estimate of the height including title and axis
     if (getMetricScale(metric, dataModel) === "version_number") {
         return (
@@ -46,7 +67,7 @@ export function TrendGraph({ metric, measurements, loading }) {
     const measurementValues = measurements.map((measurement) =>
         measurementAttributeAsNumber(metric, measurement, "value", dataModel),
     )
-    let maxY = niceNumber(Math.max(...measurementValues))
+    let maxY = Math.max(...measurementValues)
     let measurementPoints = [] // The measurement values as (x, y) coordinates
     measurements.forEach((measurement, index) => {
         measurementPoints.push(
@@ -59,35 +80,79 @@ export function TrendGraph({ metric, measurements, loading }) {
         tickLabels: { fontSize: 8, fill: color, fontFamily: fontFamily },
     }
     return (
-        <VictoryChart
-            aria-label={`Trend graph for the metric ${metricName}`}
-            height={chartHeight}
-            scale={{ x: "time", y: "linear" }}
-            style={{ parent: { height: "100%", background: bgcolor }, fontFamily: fontFamily }}
-            theme={VictoryTheme.material}
-            width={750}
-        >
-            <VictoryLabel
-                x={375}
-                y={20}
-                style={{ fill: color, fontFamily: fontFamily }}
-                text={metricName}
-                textAnchor="middle"
-            />
-            <VictoryAxis label={"Time"} style={axisStyle} />
-            <VictoryAxis
-                dependentAxis
-                domain={[0, maxY]}
-                label={unit}
-                style={axisStyle}
-                tickFormat={(t) => `${scaledNumber(t)}`}
-            />
-            <VictoryLine
-                data={measurementPoints}
-                interpolation="stepBefore"
-                style={{ data: { stroke: color, strokeWidth: 2 } }}
-            />
-        </VictoryChart>
+        <div>
+            <VictoryChart
+                aria-label={`Trend graph for the metric ${metricName}`}
+                containerComponent={
+                    <VictoryZoomContainer
+                        responsive={true}
+                        zoomDimension="x"
+                        zoomDomain={state.zoomDomain}
+                        onZoomDomainChange={handleZoom}
+                    />
+                }
+                height={chartHeight}
+                padding={{
+                    top: 50,
+                    left: 50,
+                    right: 30,
+                    bottom: 30,
+                }}
+                scale={{ x: "time", y: "linear" }}
+                style={{ parent: { height: "100%", background: bgcolor }, fontFamily: fontFamily }}
+                theme={VictoryTheme.material}
+                width={chartWidth}
+            >
+                <VictoryLabel
+                    x={375}
+                    y={20}
+                    style={{ fill: color, fontFamily: fontFamily }}
+                    text={metricName}
+                    textAnchor="middle"
+                />
+                <VictoryAxis style={axisStyle} />
+                <VictoryAxis
+                    dependentAxis
+                    domain={[0, maxY]}
+                    label={unit}
+                    style={axisStyle}
+                    tickFormat={(t) => `${scaledNumber(t)}`}
+                />
+                <VictoryLine
+                    data={measurementPoints}
+                    interpolation="stepBefore"
+                    style={{ data: { stroke: color, strokeWidth: 2 } }}
+                />
+            </VictoryChart>
+            <VictoryChart
+                aria-label={`Brush graph for the metric ${metricName}`}
+                containerComponent={
+                    <VictoryBrushContainer
+                        brushDimension="x"
+                        brushDomain={state.selectedDomain}
+                        onBrushDomainChange={handleBrush}
+                    />
+                }
+                height={50}
+                padding={{
+                    top: 0,
+                    left: 50,
+                    right: 30,
+                    bottom: 30,
+                }}
+                scale={{ x: "time", y: "linear" }}
+                style={{ parent: { height: "100%", background: bgcolor }, fontFamily: fontFamily }}
+                theme={VictoryTheme.material}
+                width={chartWidth}
+            >
+                <VictoryAxis label={"Time"} style={axisStyle} />
+                <VictoryLine
+                    data={measurementPoints}
+                    interpolation="stepBefore"
+                    style={{ data: { stroke: color, strokeWidth: 1 } }}
+                />
+            </VictoryChart>
+        </div>
     )
 }
 TrendGraph.propTypes = {
