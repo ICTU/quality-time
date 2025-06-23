182972e8c1b66a4982239e65d4def27560ac402d
RicardoDeden <r.d.deden@hotmail.nl>
2025-06-23 17:39:45 +0200

    - move to using fixtures in SubjectTableBody.test.jsx to decrease duplication

--- components/frontend/src/subject/SubjectTableBody.test.jsx
+++ components/frontend/src/subject/SubjectTableBody.test.jsx
@@ -2,7 +2,7 @@ import { createEvent, fireEvent, render, screen } from "@testing-library/react"
 import history from "history/browser"
 import { vi } from "vitest"
 
-import { createTestableSettings } from "../__fixtures__/fixtures"
+import { createTestableSettings, dataModel, report } from "../__fixtures__/fixtures"
 import { setMetricAttribute } from "../api/metric"
 import { DataModel } from "../context/DataModel"
 import { EDIT_REPORT_PERMISSION, Permissions } from "../context/Permissions"
@@ -25,32 +25,6 @@ vi.mock("../api/metric", async () => {
     }
 })
 
-const metric = {
-    unit: "testUnit",
-    scale: "count",
-    type: "metric_type",
-    name: "name_1",
-    sources: {},
-    tags: ["Tag 1"],
-}
-const metric2 = {
-    unit: "tests",
-    scale: "percentage",
-    name: "name_2",
-    type: "metric_type",
-}
-const dataModel = {
-    metrics: {
-        metric_type: { name: "Metric type", sources: ["source_type"], tags: [] },
-    },
-    sources: {
-        source_type: { name: "Source type" },
-    },
-    subjects: {
-        subject_type: { metrics: ["metric_type"] },
-    },
-}
-
 function renderSubjectTableBody({ dates = [], expandedItems = null, settings = null } = {}) {
     settings = settings ?? createTestableSettings()
     if (expandedItems) {
@@ -66,41 +40,11 @@ function renderSubjectTableBody({ dates = [], expandedItems = null, settings = n
                         reportDate={new Date("2020-01-15T00:00:00+00:00")}
                         reload={vi.fn()}
                         reversedMeasurements={[]}
-                        report={{
-                            subjects: {
-                                subject_uuid: {
-                                    type: "subject_type",
-                                    name: "Subject 1 title",
-                                    metrics: {
-                                        metric_uuid: {
-                                            name: "M1",
-                                            type: "metric_type",
-                                            tags: ["other tag"],
-                                            target: "1",
-                                            sources: { source_uuid: { name: "Source" } },
-                                            status: "target_not_met",
-                                            recent_measurements: [],
-                                            latest_measurement: { count: 1 },
-                                            comment: "Comment 1",
-                                        },
-                                        metric_uuid2: {
-                                            name: "M2",
-                                            type: "metric_type",
-                                            tags: ["tag"],
-                                            target: "2",
-                                            issue_ids: ["ABC-42"],
-                                            sources: { source_uuid2: { name: "Source 2" } },
-                                            status: "informative",
-                                            recent_measurements: [],
-                                            latest_measurement: { count: 2 },
-                                            comment: "Comment 2",
-                                        },
-                                    },
-                                },
-                            },
-                            title: "Report title",
-                        }}
-                        metricEntries={Object.entries({ 1: metric, 2: metric2 })}
+                        report={report}
+                        metricEntries={Object.entries({
+                            1: report.subjects.subject_uuid.metrics.metric_uuid,
+                            2: report.subjects.subject_uuid.metrics.metric_uuid2,
+                        })}
                         reports={[]}
                         settings={settings}
                         subject_uuid="subject_uuid"
@@ -193,6 +137,6 @@ it("handles drop by reordering metrics", () => {
     const firstRowText = rows[0].textContent
     const secondRowText = rows[1].textContent
 
-    expect(firstRowText).toContain("name_2")
-    expect(secondRowText).toContain("name_1")
+    expect(firstRowText).toContain("M2")
+    expect(secondRowText).toContain("M1")
 })
