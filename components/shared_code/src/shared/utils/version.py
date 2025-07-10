82fad7e92ecef387149cc3e9b2ba924675a1a772
Frank Niessink <frank@niessink.com>
2025-07-10 10:22:32 +0200

    Bump version: 5.35.0 → 5.36.0-rc.0

--- components/shared_code/src/shared/utils/version.py
+++ components/shared_code/src/shared/utils/version.py
@@ -1,4 +1,4 @@
 """Quality-time version information."""
 
-QUALITY_TIME_VERSION = "5.35.0"
+QUALITY_TIME_VERSION = "5.36.0-rc.0"
 REFERENCE_DOCUMENTATION_URL = f"https://quality-time.readthedocs.io/en/v{QUALITY_TIME_VERSION}/reference.html"
