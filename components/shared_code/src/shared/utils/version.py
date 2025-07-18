89086880ad0c9f2a9a8547c8072c84d8b5ba38c1
wkoot <3715211+wkoot@users.noreply.github.com>
2025-07-18 09:11:32 +0200

    Bump version: 5.36.0 → 5.36.1-rc.0

--- components/shared_code/src/shared/utils/version.py
+++ components/shared_code/src/shared/utils/version.py
@@ -1,4 +1,4 @@
 """Quality-time version information."""
 
-QUALITY_TIME_VERSION = "5.36.0"
+QUALITY_TIME_VERSION = "5.36.1-rc.0"
 REFERENCE_DOCUMENTATION_URL = f"https://quality-time.readthedocs.io/en/v{QUALITY_TIME_VERSION}/reference.html"
