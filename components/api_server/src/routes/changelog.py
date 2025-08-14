08c8008107e3e27634bca19dee39dd97ca80d7d1
Frank Niessink <frank@niessink.com>
2025-08-14 14:52:44 +0200

    Refactor: better typing

--- components/api_server/src/routes/changelog.py
+++ components/api_server/src/routes/changelog.py
@@ -1,6 +1,6 @@
 """Changelog routes."""
 
-from typing import cast
+from typing import TypedDict
 
 import bottle
 from pymongo.database import Database
@@ -10,46 +10,59 @@ from shared.utils.type import MetricId, ReportId, SourceId, SubjectId
 from database import measurements, reports
 
 
-def _get_changelog(database: Database, nr_changes: str, **uuids: str) -> dict[str, list[dict[str, str]]]:
+class ChangelogEntry(TypedDict):
+    """Changelog entry."""
+
+    delta: str
+    email: str
+    timestamp: str
+
+
+class ChangelogJSON(TypedDict):
+    """Changelog JSON."""
+
+    changelog: list[ChangelogEntry]
+
+
+def _get_changelog(database: Database, nr_changes: int, **uuids: str) -> ChangelogJSON:
     """Return the recent most nr_changes changes from the changelog."""
-    limit = int(nr_changes)
     changes = [
-        {
-            "delta": item["delta"]["description"],
-            "email": item["delta"].get("email", ""),
-            "timestamp": item.get("timestamp") or item["start"],
-        }
-        for item in list(measurements.changelog(database, limit, **uuids))
-        + list(reports.changelog(database, limit, **uuids))
+        ChangelogEntry(
+            delta=str(item["delta"]["description"]),
+            email=str(item["delta"].get("email", "")),
+            timestamp=str(item.get("timestamp") or item["start"]),
+        )
+        for item in list(measurements.changelog(database, nr_changes, **uuids))
+        + list(reports.changelog(database, nr_changes, **uuids))
     ]
-    return {"changelog": sorted(changes, reverse=True, key=lambda change: cast(str, change["timestamp"]))[:limit]}
+    return {"changelog": sorted(changes, reverse=True, key=lambda change: change["timestamp"])[:nr_changes]}
 
 
-@bottle.get("/api/internal/changelog/source/<source_uuid>/<nr_changes>", authentication_required=False)
-def get_source_changelog(source_uuid: SourceId, nr_changes: str, database: Database):
+@bottle.get("/api/internal/changelog/source/<source_uuid>/<nr_changes:int>", authentication_required=False)
+def get_source_changelog(source_uuid: SourceId, nr_changes: int, database: Database) -> ChangelogJSON:
     """Return the recent most nr_changes changes from the source changelog."""
     return _get_changelog(database, nr_changes, source_uuid=source_uuid)
 
 
-@bottle.get("/api/internal/changelog/metric/<metric_uuid>/<nr_changes>", authentication_required=False)
-def get_metric_changelog(metric_uuid: MetricId, nr_changes: str, database: Database):
+@bottle.get("/api/internal/changelog/metric/<metric_uuid>/<nr_changes:int>", authentication_required=False)
+def get_metric_changelog(metric_uuid: MetricId, nr_changes: int, database: Database) -> ChangelogJSON:
     """Return the recent most nr_changes changes from the metric changelog."""
     return _get_changelog(database, nr_changes, metric_uuid=metric_uuid)
 
 
-@bottle.get("/api/internal/changelog/subject/<subject_uuid>/<nr_changes>", authentication_required=False)
-def get_subject_changelog(subject_uuid: SubjectId, nr_changes: str, database: Database):
+@bottle.get("/api/internal/changelog/subject/<subject_uuid>/<nr_changes:int>", authentication_required=False)
+def get_subject_changelog(subject_uuid: SubjectId, nr_changes: int, database: Database) -> ChangelogJSON:
     """Return the recent most nr_changes changes from the subject changelog."""
     return _get_changelog(database, nr_changes, subject_uuid=subject_uuid)
 
 
-@bottle.get("/api/internal/changelog/report/<report_uuid>/<nr_changes>", authentication_required=False)
-def get_report_changelog(report_uuid: ReportId, nr_changes: str, database: Database):
+@bottle.get("/api/internal/changelog/report/<report_uuid>/<nr_changes:int>", authentication_required=False)
+def get_report_changelog(report_uuid: ReportId, nr_changes: int, database: Database) -> ChangelogJSON:
     """Return the recent most nr_changes changes from the report changelog."""
     return _get_changelog(database, nr_changes, report_uuid=report_uuid)
 
 
-@bottle.get("/api/internal/changelog/<nr_changes>", authentication_required=False)
-def get_changelog(nr_changes: str, database: Database):
+@bottle.get("/api/internal/changelog/<nr_changes:int>", authentication_required=False)
+def get_changelog(nr_changes: int, database: Database) -> ChangelogJSON:
     """Return the recent most nr_changes changes from the changelog."""
     return _get_changelog(database, nr_changes)
