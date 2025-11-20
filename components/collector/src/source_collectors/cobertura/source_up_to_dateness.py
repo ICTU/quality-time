5b1830b8adb6a2930faf2a1654084f25107c868e
Frank Niessink <frank@niessink.com>
2025-11-20 10:00:29 +0100

    Fix Cobertura XML timestamp (#12288)
    When using Cobertura XML files as source, be prepared for timestamps without milliseconds. Also, report an error if the timestamp is missing.
    
    Fixes #12247.

--- components/collector/src/source_collectors/cobertura/source_up_to_dateness.py
+++ components/collector/src/source_collectors/cobertura/source_up_to_dateness.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 from base_collectors import TimePassedCollector, XMLFileSourceCollector
 from collector_utilities.date_time import datetime_from_timestamp
+from collector_utilities.exceptions import NotFoundError
 from collector_utilities.functions import parse_source_response_xml
 
 if TYPE_CHECKING:
@@ -15,7 +16,12 @@ if TYPE_CHECKING:
 class CoberturaSourceUpToDateness(XMLFileSourceCollector, TimePassedCollector):
     """Collector to collect the Cobertura report age."""
 
+    CUTOFF_YEAR_FOR_TIMESTAMP = 1980
+
     async def _parse_source_response_date_time(self, response: Response) -> datetime:
         """Override to parse the timestamp from the response."""
         tree = await parse_source_response_xml(response)
-        return datetime_from_timestamp(int(tree.get("timestamp", 0)))
+        if timestamp := tree.get("timestamp"):
+            dt = datetime_from_timestamp(float(timestamp))
+            return dt if dt.year > self.CUTOFF_YEAR_FOR_TIMESTAMP else datetime_from_timestamp(float(timestamp) * 1000)
+        raise NotFoundError(type_of_thing="Cobertura XML tag", name_of_thing="timestamp")
--- docs/src/changelog.md
+++ docs/src/changelog.md
@@ -17,6 +17,7 @@ If your currently installed *Quality-time* version is not the penultimate versio
 ### Fixed
 
 - Clarify in the product vision that checking the correct configuration of sources is out of scope for Quality-time. Fixes [#3821](https://github.com/ICTU/quality-time/issues/3821).
+- When using Cobertura XML files as source, be prepared for timestamps without milliseconds. Also, report an error if the timestamp is missing. Fixes [#12247](https://github.com/ICTU/quality-time/issues/12247).
 - When measuring suppressed violations with SonarQube as source, take the impacted software qualities into account if configured by the user. Fixes [#12263](https://github.com/ICTU/quality-time/issues/12263).
 - Source configurations in the report source overview did not display parameter values. Fixes [#12281](https://github.com/ICTU/quality-time/issues/12281).
 
