769e0fb4f255921d4f74398a5248f308249985ca
Frank Niessink <frank@niessink.com>
2025-12-22 16:56:46 +0100

    Fix parsing OWASP Dependency Check JSON from GitLab
    When measuring source up-to-dateness or source version with OWASP Dependency Check JSON reports located in GitLab, parsing the JSON would fail because GitLab returns the JSON files with mimetype application/octet-stream.
    
    Fixes #12323.

--- components/collector/src/source_collectors/owasp_dependency_check_json/base.py
+++ components/collector/src/source_collectors/owasp_dependency_check_json/base.py
@@ -1,19 +1,27 @@
 """Base classes for OWASP Dependency-Check JSON collectors."""
 
 from abc import ABC
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING, ClassVar, cast
 
 from base_collectors import JSONFileSourceCollector
 from collector_utilities.exceptions import JSONAttributeError
 
+from .json_types import OWASPDependencyCheckJSON
+
 if TYPE_CHECKING:
-    from .json_types import OWASPDependencyCheckJSON
+    from collector_utilities.type import Response
 
 
 class OWASPDependencyCheckJSONBase(JSONFileSourceCollector, ABC):
     """Base class for OWASP Dependency-Check JSON collectors."""
 
     allowed_report_schemas: ClassVar[list[str]] = ["1.1"]
+
+    async def _json(self, response: Response) -> OWASPDependencyCheckJSON:
+        """Extract the JSON from the response."""
+        json = cast(OWASPDependencyCheckJSON, await response.json(content_type=None))
+        self._check_report_schema(json)
+        return json
 
     def _check_report_schema(self, json: OWASPDependencyCheckJSON) -> None:
         """Check that the report schema is allowed."""
--- components/collector/src/source_collectors/owasp_dependency_check_json/source_up_to_dateness.py
+++ components/collector/src/source_collectors/owasp_dependency_check_json/source_up_to_dateness.py
@@ -1,12 +1,11 @@
 """OWASP Dependency-Check JSON source up-to-dateness collector."""
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 from base_collectors import TimePassedCollector
 from collector_utilities.date_time import parse_datetime
 
 from .base import OWASPDependencyCheckJSONBase
-from .json_types import OWASPDependencyCheckJSON
 
 if TYPE_CHECKING:
     from datetime import datetime
@@ -19,6 +18,5 @@ class OWASPDependencyCheckJSONSourceUpToDateness(OWASPDependencyCheckJSONBase, T
 
     async def _parse_source_response_date_time(self, response: Response) -> datetime:
         """Override to parse the report date from the JSON."""
-        json = cast(OWASPDependencyCheckJSON, await response.json())
-        self._check_report_schema(json)
+        json = await self._json(response)
         return parse_datetime(json.get("projectInfo", {}).get("reportDate", ""))
