e594fdfa6ea5fd44bd317dc150f25377f519b06e
Frank Niessink <frank@niessink.com>
2026-02-10 22:07:31 +0100

    Traceback on missing active sprint
    When measuring time remaining with Jira as source, don't show a traceback when there is no active sprint, but rather a user friendly error message.
    
    Fixes #12617.

--- components/collector/tests/source_collectors/jira/test_time_remaining.py
+++ components/collector/tests/source_collectors/jira/test_time_remaining.py
@@ -19,22 +19,34 @@ class JiraTimeRemainingTest(JiraTestCase):
             "isLast": True,
             "values": [{"id": 2, "name": "Board 2"}],
         }
-        self.sprints_json = {
-            "values": [
+
+    def sprints_json(self, *, active_sprint: bool = True) -> dict[str, list[dict[str, str]]]:
+        """Create a Jira sprints JSON fixture with or without active sprint."""
+        sprints = []
+        if active_sprint:
+            sprints.append(
                 {
                     "endDate": (datetime.now().astimezone() + timedelta(days=5)).isoformat(),
                     "id": "sprint_id",
                     "originBoardId": "2",
                     "self": "https://jira/rest/agile/1.0/sprint/sprint_id",
-                },
-            ],
-        }
+                }
+            )
+        return {"values": sprints}
 
     async def test_end_of_active_sprint(self):
         """Test that the time remaining to the end of the active sprint is returned."""
-        get_request_json_side_effect = [self.boards_json, self.sprints_json, self.sprints_json]
+        sprints_json = self.sprints_json(active_sprint=True)
+        get_request_json_side_effect = [self.boards_json, sprints_json, sprints_json]
         response = await self.collect(get_request_json_side_effect=get_request_json_side_effect)
         expected_landing_url = (
             "https://jira/secure/RapidBoard.jspa?rapidView=2&view=reporting&chart=sprintRetrospective&sprint=sprint_id#"
         )
         self.assert_measurement(response, value="5", landing_url=expected_landing_url, entities=[])
+
+    async def test_no_active_sprint(self):
+        """Test that an error message is show if there is no active sprint."""
+        sprints_json = self.sprints_json(active_sprint=False)
+        get_request_json_side_effect = [self.boards_json, sprints_json, sprints_json]
+        response = await self.collect(get_request_json_side_effect=get_request_json_side_effect)
+        self.assert_measurement(response, parse_error="No active sprint found", landing_url="https://jira", entities=[])
