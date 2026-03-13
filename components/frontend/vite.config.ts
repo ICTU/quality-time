914b85c4fd7dd914fda33e035c09cd2ba466c74c
Frank Niessink <frank@niessink.com>
2026-03-13 20:50:31 +0100

    Remove Babel plugin

--- components/frontend/vite.config.ts
+++ components/frontend/vite.config.ts
@@ -9,9 +9,6 @@ export default defineConfig({
     plugins: [
         react({
             jsxImportSource: "@emotion/react",
-            babel: {
-                plugins: ["@emotion/babel-plugin"],
-            },
         }),
     ],
     resolve: {
