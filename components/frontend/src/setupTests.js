2733b840d53a84ebb089ddd1a23cf9d3cf80b960
Frank Niessink <frank@niessink.com>
2026-03-15 18:11:38 +0100

    Remove work-around. Closes #12719. (#12762)

--- components/frontend/src/setupTests.js
+++ components/frontend/src/setupTests.js
@@ -3,8 +3,3 @@ import "@testing-library/jest-dom"
 import { toHaveNoViolations } from "jest-axe"
 
 expect.extend(toHaveNoViolations)
-
-// Suppress MUI anchorEl "not in document layout" warnings in jsdom (zero-sized rects).
-// Our issue: https://github.com/ICTU/quality-time/issues/12719
-// Upstream issue: https://github.com/mui/material-ui/issues/47792
-globalThis.MUI_TEST_ENV = true
