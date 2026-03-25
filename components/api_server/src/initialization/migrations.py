51064a72a7b9416c5e162b98efb84ddb30c91baf
Frank Niessink <frank@niessink.com>
2026-03-25 18:38:36 +0100

    Add migration to remove Checkmarx sources (#12821)

--- components/api_server/src/initialization/migrations.py
+++ components/api_server/src/initialization/migrations.py
@@ -15,8 +15,9 @@ def perform_migrations(database: Database) -> None:
     for report in database.reports.find(filter={"last": True, "deleted": {"$exists": False}}):
         report_uuid = report["report_uuid"]
         logger.info("Checking report %s for necessary updates", report_uuid)
-        if change_description := remove_metric_addition(report):
-            logger.info("Updating report %s to %s", report_uuid, change_description)
+        changes = [change for change in (remove_metric_addition(report), remove_checkmarx(report)) if change]
+        if any(changes):
+            logger.info("Updating report %s to %s", report_uuid, " and ".join(change for change in changes if change))
             replace_document(database.reports, report)
 
 
@@ -24,12 +25,30 @@ def remove_metric_addition(report) -> str:
     """Remove the addition field from all metrics."""
     # Added after Quality-time v5.47.1, see https://github.com/ICTU/quality-time/issues/12142
     change_description = ""
-    for subject in report["subjects"].values():
-        for metric in subject["metrics"].values():
-            if "addition" in metric:
-                change_description = "remove the addition field from metrics"
-                del metric["addition"]
+    for metric in metrics(report):
+        if "addition" in metric:
+            change_description = "remove the addition field from metrics"
+            del metric["addition"]
     return change_description
+
+
+def remove_checkmarx(report) -> str:
+    """Remove the Checkmarx source from all metrics."""
+    # Added after Quality-time v5.49.0, see https://github.com/ICTU/quality-time/issues/12798
+    change_description = ""
+    for metric in metrics(report):
+        sources = metric.get("sources", {})
+        if source_ids_to_remove := [source_id for source_id, source in sources.items() if source["type"] == "cxsast"]:
+            change_description = "remove the Checkmarx source from metrics"
+            for source_id in source_ids_to_remove:
+                del metric["sources"][source_id]
+    return change_description
+
+
+def metrics(report):
+    """Return the metrics in the report."""
+    for subject in report.get("subjects", {}).values():
+        yield from subject.get("metrics", {}).values()
 
 
 def replace_document(collection: Collection, document) -> None:
